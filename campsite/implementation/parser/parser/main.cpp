425da34c9b0b77fdb34861022cfd6264d7b11193
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2002-01-13 13:29:27 +0000

    fix start watchdog bug

--- campsite/implementation/parser/parser/main.cpp
+++ campsite/implementation/parser/parser/main.cpp
@@ -249,21 +249,19 @@ void StartWatchDog(bool p_bRunAsDaemon)
 	while (1)
 	{
 		nMainThreadPid = fork();
+		if (nMainThreadPid == -1)
+		{
+			sleep(10);
+			continue;
+		}
 		if (nMainThreadPid != 0)
 		{
 			signal(SIGTERM, SigHandler);
-			int nStatus;
-			waitpid(nMainThreadPid, &nStatus, 0);
-			cout << "Child exited with status " << nStatus << endl;
-			sleep(5);
+			waitpid(nMainThreadPid, NULL, 0);
+			sleep(10);
 			continue;
 		}
-		if (nMainThreadPid == 0)
-			return ;
-		if (nMainThreadPid < 0)
-		{
-			sleep(10);
-		}
+		return ; // nMainThreadPid == 0
 	}
 }
 
