dc9c17866fb250dc2bd2ae20931b50496e50a62e
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2003-02-18 02:15:09 +0000

    *** empty log message ***

---
+++ implementation/management/pub/issues/sections/locals.es.php
@@ -0,0 +1,26 @@
+<?
+
+regGS("Name<BR><SMALL>(click to see articles)</SMALL>","Nombre");
+regGS("Delete section $1","Borrar sección $1");
+regGS("No sections.","Sin sección");
+regGS("Adding new section","Agregando nuevas secciones");
+regGS("You do not have the right to add sections.","No tiene derecho a agregar nuevas secciones");
+regGS("The section $1 has been successfuly added.","La sección $1 ha sido agregada con éxito");
+regGS("Please check if another section with the same number does not already exist.","Favor revise si otra sección con el mismo número no existe ya");
+regGS("Section $1 added to issue $2. $3 ($4) of $5","Sección $1 agregada a edición $2. $3 (4) de $5");
+regGS("Delete section","Borrar sección");
+regGS("You do not have the right to delete sections.","No tiene derecho a borrar secciones");
+regGS("Are you sure you want to delete the section $1?","¿Esta seguro de querer borrar sección $1?");
+regGS("Deleting section","Borrando sección");
+regGS("The section $1 has been deleted.","La sección $1 ha sido borrada");
+regGS("Section $1 deleted from issue $2. $3 $4 of $5","Sección $1 borrada e edición $2. $3 $4 de $5");
+regGS("Change section name","Cambiar nombre de sección");
+regGS("You do not have the right to change section details","No tiene derecho a cambiar detalles de sección");
+regGS("Updating section name","Actualizando nombre de secciones");
+regGS("The section $1 has been successfuly modified.","La sección $1 fue modificada con éxito");
+regGS("The section could not be changed.","La sección no pudo ser cambiada");
+regGS("Section $1 updated to issue $2. $3 ($4) of $5","Sección $1 actualizada a edición $2. 3 ($4) de $5");
+regGS("Name<BR><SMALL>(click to add article)</SMALL>","Nombre");
+
+
+?>
