4c92469813389f862b8f2175ceff0beea9679538
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2003-02-18 02:34:04 +0000

    moved "access denied" message from locals to globals

--- implementation/management/locals.es.php
+++ implementation/management/locals.es.php
@@ -1,7 +1,6 @@
 <?
 
 regGS("Please enter your user name and password","Por favor entre su nombre de usuario y clave");
-regGS("Access denied","Acceso denegado");
 regGS("You do not have the right to access this page.","No tiene derecho para accesar esta pagina.");
 regGS("Login failed","Acceso fallido");
 regGS("Pease make sure that you typed the correct user name and password.","Por favor asegurese de que escribio correctamente su nombre de usuario y clave.");
