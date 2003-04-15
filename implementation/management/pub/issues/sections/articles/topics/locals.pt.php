fc41ea87e0078066ece4dffa59be76749614a94a
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2003-04-15 21:52:21 +0000

    portuguese translation

---
+++ implementation/management/pub/issues/sections/articles/topics/locals.pt.php
@@ -0,0 +1,22 @@
+<?
+regGS("Add","Adicionar");
+regGS("Delete topic $1 from article","Eliminar tema $1 do artigo");
+regGS("Article topics","Temas do Artigo");
+regGS("Available topics","Temas disponiveis");
+regGS("No article topics.","Não foram definidos temas");
+regGS("No topics.","Não existem temas");
+regGS("Topic name","Tema");
+regGS("Add topic $1 to article","Adicionar tema $1 ao artigo");
+regGS("Delete article topic","Apagar tema do Artigo");
+regGS("You do not have the right to delete article topics","Não tem permissões para eliminar temas do artigo");
+regGS("Are you sure you want to delete the topic $1?","Eliminar tema $1?");
+regGS("Deleting article topic","Eliminando tema de artigo");
+regGS("The topic $1 has been successfully deleted.","O tema $1 foi eliminado");
+regGS("Article topic $1 deleted","O tema $1 do artigo foi eliminado");
+regGS("Adding new topic to article","Adicionando tema ao artigo");
+regGS("The topic $1 has been successfully added.","O Tema $1 foi adicionado");
+regGS("You do not have the right to add topics to article","Não tem permissões para adicionar temas a artigos");
+regGS("Adding new topic...","Adicionando novo Tema");
+regGS("Topic $1 added to article","Tema $1 foi adicionado ao artigo");
+
+?>
