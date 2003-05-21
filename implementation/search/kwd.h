81f81abfef5280a61f1595fc0742a8326d9aeb69
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2003-05-21 20:27:31 +0000

    increased the max keyword length

--- implementation/search/kwd.h
+++ implementation/search/kwd.h
@@ -29,7 +29,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #ifndef TOL_KWD_H__
 #define TOL_KWD_H__
 
-#define MAX_KWD	16
+#define MAX_KWD	69
 
 struct kwd_t {
   char *k;
