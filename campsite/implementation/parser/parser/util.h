af32a5e09cb6415b957bf87f28fcaaf0e525b4ee
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2003-05-23 21:07:23 +0000

    corrected function documentation for EscapeHTML

--- campsite/implementation/parser/parser/util.h
+++ campsite/implementation/parser/parser/util.h
@@ -76,7 +76,7 @@ int IsValidType(const char*, MYSQL*);
 //		operator; after use this must be deallocated using delete operator
 const char* const EscapeURL(const char* src);
 
-// EscapeURL: return the given character string escaped for HTML
+// EscapeHTML: return the given character string escaped for HTML
 // Parameters: const char* src - pointer to const char; string to escape; must not be
 //		NULL
 // Returns: pointer to escaped string; this is dynamically allocated using new
