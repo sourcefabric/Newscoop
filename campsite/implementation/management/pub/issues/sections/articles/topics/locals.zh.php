2295da7aed86949f678970a22abc0f86b29a18ea
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2003-10-28 04:30:18 +0000

    chinese translation

---
+++ campsite/implementation/management/pub/issues/sections/articles/topics/locals.zh.php
@@ -0,0 +1,22 @@
+<?
+regGS("Add","增加");
+regGS("Delete topic $1 from article","从文章中删除主题$1");
+regGS("Article topics","文章主题");
+regGS("Available topics","可用主题");
+regGS("No article topics.","无文章主题");
+regGS("No topics.","无主题");
+regGS("Topic name","主题名称");
+regGS("Add topic $1 to article","增加主题$1到文章中");
+regGS("Delete article topic","删除文章主题");
+regGS("You do not have the right to delete article topics","您无权删除文章主题");
+regGS("Are you sure you want to delete the topic $1?","您确认要删除主题$1？");
+regGS("Deleting article topic","正在删除文章主题");
+regGS("The topic $1 has been successfully deleted.","主题$1已被成功删除");
+regGS("Article topic $1 deleted","文章主题已删除");
+regGS("Adding new topic to article","文章增加新主题");
+regGS("The topic $1 has been successfully added.","增加主题$1成功");
+regGS("You do not have the right to add topics to article","您无权添加文章主题");
+regGS("Adding new topic...","正在增加新主题");
+regGS("Topic $1 added to article","主题$1已添加到文章中");
+
+?>
---
+++ campsite/implementation/management/users/subscriptions/sections/locals.zh.php
@@ -0,0 +1,30 @@
+<?
+regGS("Subscribed sections","&#24050;&#35746;&#38405;&#21306;");
+regGS("Add new section to subscription","&#20026;&#35746;&#38405;&#22686;&#21152;&#26032;&#21306;");
+regGS("Start Date<BR><SMALL>(yyyy-mm-dd)</SMALL>","&#36215;&#22987;&#26085;&#26399;(yyyy-mm-dd)");
+regGS("Delete subscription to section $1?","&#21024;&#38500;&#21306;$1&#30340;&#35746;&#38405;");
+regGS("No sections in the current subscription.","&#24403;&#21069;&#35746;&#38405;&#20013;&#26080;&#21306;");
+regGS("Change all sections","&#25913;&#21464;&#25152;&#26377;&#21306;");
+regGS("WARNING: If you subscribe to all sections, the periods for previously added sections will be overriden!","&#35686;&#21578;&#65306;&#22914;&#26524;&#20320;&#35746;&#38405;&#25152;&#26377;&#21306;&#65292;&#20197;&#21069;&#22686;&#21152;&#30340;&#21306;&#30340;&#26102;&#38388;&#23558;&#20316;&#24223;&#65281;");
+regGS("Adding sections","&#27491;&#22312;&#22686;&#21152;&#21306;");
+regGS("Adding sections to subscription","&#27491;&#22312;&#21521;&#35746;&#38405;&#22686;&#21152;&#21306;");
+regGS("The section was added successfully.","&#21306;&#34987;&#25104;&#21151;&#28155;&#21152;");
+regGS("Please check if there isn't another subscription with the same section.","&#35831;&#30830;&#35748;&#26159;&#21542;&#23384;&#22312;&#21516;&#26679;&#21306;&#30340;&#20854;&#23427;&#35746;&#38405;");
+regGS("Are you sure you want to delete the subscription to the section $1?","&#20320;&#30830;&#35748;&#35201;&#21024;&#38500;&#21306;$1&#30340;&#35746;&#38405;&#65311;");
+regGS("The subscription to the section $1 has been deleted.","&#21521;&#21306;$1&#30340;&#35746;&#38405;&#24050;&#21024;&#38500;");
+regGS("The subscription to the section $1 could not be deleted.","&#21521;&#21306;$1&#30340;&#35746;&#38405;&#19981;&#33021;&#21024;&#38500;");
+regGS("Change subscription","&#25913;&#21464;&#35746;&#38405;");
+regGS("You do not have the right to change subscriptions.","&#20320;&#26080;&#26435;&#20462;&#25913;&#35746;&#38405;");
+regGS("Paid Days","&#24050;&#20184;&#26102;&#38388;");
+regGS("-- ALL SECTIONS --","&#65293;&#65293;&#25152;&#26377;&#21306;&#65293;&#65293;");
+regGS("Updating subscription","&#27491;&#22312;&#26356;&#26032;&#35746;&#38405;");
+regGS("The subscription has been updated.","&#35746;&#38405;&#24050;&#34987;&#26356;&#26032;");
+regGS("The subscription could not be updated.","&#35746;&#38405;&#19981;&#33021;&#34987;&#26356;&#26032;");
+regGS("Changing subscription","&#27491;&#22312;&#25913;&#21464;&#35746;&#38405;");
+regGS("Change paid status","&#25913;&#21464;&#24050;&#20184;&#29366;&#24577;");
+regGS("Are you sure you want to change the subscription to the section $1 status to $2?","&#20320;&#30830;&#35748;&#20462;&#25913;&#21521;&#21306;$1&#30340;&#35746;&#38405;&#21040;$2&#65311;");
+regGS("Change subscription paid status","&#25913;&#21464;&#35746;&#38405;&#24050;&#20184;&#29366;&#24577;");
+regGS("Not paid","&#26410;&#20184;");
+regGS("Paid","&#24050;&#20184;");
+
+?>
