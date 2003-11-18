4efb476f91ed80287aae327c4010ceec45088a37
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2003-11-18 03:23:10 +0000

    moved some messages to globals

--- implementation/management/popup/locals.hr.php
+++ implementation/management/popup/locals.hr.php
@@ -1,14 +1,4 @@
 <?
-regGS("No languages","Nema jezika");
-regGS("---Select language---","---Odaberi jezik---");
-regGS("No publications","Nema publikacija");
-regGS("---Select publication---","---Odaberi publikaciju---");
-regGS("No issues","Nema izdanja");
-regGS("---Select issue---","---Odaberi izdanje---");
-regGS("No sections","Nema rubrika");
-regGS("---Select section---","---Odaberi rubriku");
-regGS("No articles","Nema članaka");
-regGS("---Select article---","---Odaberi članak---");
 regGS("Add image","Dodaj sliku");
 regGS("No images","Nema slika");
 
