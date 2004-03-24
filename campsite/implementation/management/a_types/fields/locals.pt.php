eaed084a4bf8c90c93f2f8ef6bc09cb3b447d845
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2004-03-24 21:55:35 +0000

    Portuguese update

--- campsite/implementation/management/a_types/fields/locals.pt.php
+++ campsite/implementation/management/a_types/fields/locals.pt.php
@@ -1,6 +1,7 @@
 <?
 regGS("Article type fields","Campo para este Tipo de Artigo");
 regGS("Article type","Tipo de Artigo");
+regGS("Add new field","Adicionar novo Campo");
 regGS("No fields.","Não existem Campos definidos");
 regGS("Delete field $1","Eliminar campo $1");
 regGS("Article body","Corpo do Artigo");
@@ -8,7 +9,7 @@ regGS("Text","Texto");
 regGS("Adding new field","Adicionando campo");
 regGS("You do not have the right to add article type fields.","Não tem permissões para adicionar campos");
 regGS("The field $1 already exists.","Campo $1 já existente");
-regGS("The $1  must not be void and may only contain letters and underscore (_) character.","O nome do Campo $1 não pode ser nulo e tem de conter apenas letras");
+regGS("The $1  must not be void and may only contain letters.","O nome do Campo $1 não pode ser nulo e tem de conter apenas letras");
 regGS("Invalid field type.","Tipo de campo inválido");
 regGS("The field $1 has been created.","Campo $1 foi criado");
 regGS("Article type field $1 created","Campo $1 foi criado");
