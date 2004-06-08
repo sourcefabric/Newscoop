72d9b8d190d6010a4b8826c0fad14c0df03ced9c
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2004-06-08 23:58:53 +0000

    moved readQueryString() method from CURLShortNames to CURL

--- implementation/parser/parser/curl.cpp
+++ implementation/parser/parser/curl.cpp
@@ -23,4 +23,37 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 ******************************************************************************/
 
+
 #include "curl.h"
+
+
+// readQueryString(): internal method; reads the parameters from the query string
+void CURL::readQueryString(const string& p_rcoQueryString)
+{
+	if (p_rcoQueryString == "")
+		return;
+
+	string::size_type nStart = 0;
+	while (true)
+	{
+		// read the parameter name
+		string::size_type nIndex = p_rcoQueryString.find('=', nStart);
+		if (nIndex == string::npos)
+			break;
+		string coParam = p_rcoQueryString.substr(nStart, nIndex - nStart);
+
+		// read the parameter value
+		nStart = nIndex + 1;
+		nIndex = p_rcoQueryString.find("&", nStart);
+		nIndex = nIndex == string::npos ? p_rcoQueryString.size() : nIndex;
+		string coValue = p_rcoQueryString.substr(nStart, nIndex - nStart);
+
+		// set the parameter value in the parameter map
+		setValue(coParam, coValue);
+
+		// prepare for the next iteration
+		nStart = nIndex + 1;
+		if (nStart >= p_rcoQueryString.size())
+			break;
+	}
+}
