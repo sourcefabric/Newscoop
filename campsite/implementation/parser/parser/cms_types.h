ba0af9439b4296dacaf7681b67108e0b03de679d
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2004-09-06 03:03:33 +0000

    - in TAction enumeratio: added CMS_ACT_URL identifier - added new enumeration type: TPubLevel - for description of the publication level

--- campsite/implementation/parser/parser/cms_types.h
+++ campsite/implementation/parser/parser/cms_types.h
@@ -54,34 +54,34 @@ using std::binary_function;
 
 // TDataType: data types recognised by template parser
 typedef enum {
-    CMS_DT_NONE = 0,
-    CMS_DT_INTEGER = 1,
-    CMS_DT_STRING = 2,
-    CMS_DT_SWITCH = 3,
-    CMS_DT_DATE = 4,
-    CMS_DT_TIME = 5,
-    CMS_DT_DATETIME = 6,
-    CMS_DT_ENUM = 7,
-    CMS_DT_TOPIC = 8
+	CMS_DT_NONE = 0,
+	CMS_DT_INTEGER = 1,
+	CMS_DT_STRING = 2,
+	CMS_DT_SWITCH = 3,
+	CMS_DT_DATE = 4,
+	CMS_DT_TIME = 5,
+	CMS_DT_DATETIME = 6,
+	CMS_DT_ENUM = 7,
+	CMS_DT_TOPIC = 8
 } TDataType;
 
 // result codes returned by lex (in lexem class) - see tol_lex.h
 typedef enum {
-    CMS_ERR_EOF = -3,
-    CMS_ERR_LESS_IN_TOKEN = -2,
-    CMS_ERR_END_QUOTE_MISSING = -1,
-    CMS_LEX_NONE = 0,
-    CMS_LEX_IDENTIFIER = 1,
-    CMS_LEX_STATEMENT = 2,
-    CMS_LEX_END_STATEMENT = 4,
-    CMS_LEX_START_STATEMENT = 8
+	CMS_ERR_EOF = -3,
+	CMS_ERR_LESS_IN_TOKEN = -2,
+	CMS_ERR_END_QUOTE_MISSING = -1,
+	CMS_LEX_NONE = 0,
+	CMS_LEX_IDENTIFIER = 1,
+	CMS_LEX_STATEMENT = 2,
+	CMS_LEX_END_STATEMENT = 4,
+	CMS_LEX_START_STATEMENT = 8
 } TLexResult;
 
 // class of attribute: normal and type (a special attribute used for dynamic attributes)
 // see tol_atoms.h - CTypeAttributes
 typedef enum {
-    CMS_NORMAL_ATTR = 0,
-    CMS_TYPE_ATTR = 1
+	CMS_NORMAL_ATTR = 0,
+	CMS_TYPE_ATTR = 1
 } TAttrClass;
 
 extern const int CMS_CT_DEFAULT;
@@ -95,38 +95,50 @@ extern const int CMS_CT_WITH;
 
 // TAction: actions identifiers
 typedef enum _TAction {
-    CMS_ACT_NONE = 0x0000,
-    CMS_ACT_LANGUAGE = 0x0001,
-    CMS_ACT_INCLUDE = 0x0002,
-    CMS_ACT_PUBLICATION = 0x0003,
-    CMS_ACT_ISSUE = 0x0004,
-    CMS_ACT_SECTION = 0x0005,
-    CMS_ACT_ARTICLE = 0x0006,
-    CMS_ACT_LIST = 0x0007,
-    CMS_ACT_URLPARAMETERS = 0x0008,
-    CMS_ACT_FORMPARAMETERS = 0x0009,
-    CMS_ACT_PRINT = 0x000a,
-    CMS_ACT_IF = 0x000b,
-    CMS_ACT_DATE = 0x000c,
-    CMS_ACT_TEXT = 0x000d,
-    CMS_ACT_LOCAL = 0x000e,
-    CMS_ACT_SUBSCRIPTION = 0x000f,
-    CMS_ACT_EDIT = 0x0010,
-    CMS_ACT_SELECT = 0x0011,
-    CMS_ACT_USER = 0x0012,
-    CMS_ACT_LOGIN = 0x0013,
-    CMS_ACT_SEARCH = 0x0014,
-    CMS_ACT_WITH = 0x0015,
-    CMS_ACT_TOPIC = 0x0016,
-    CMS_ACT_URIPATH = 0x0017
+	CMS_ACT_NONE = 0x0000,
+	CMS_ACT_LANGUAGE = 0x0001,
+	CMS_ACT_INCLUDE = 0x0002,
+	CMS_ACT_PUBLICATION = 0x0003,
+	CMS_ACT_ISSUE = 0x0004,
+	CMS_ACT_SECTION = 0x0005,
+	CMS_ACT_ARTICLE = 0x0006,
+	CMS_ACT_LIST = 0x0007,
+	CMS_ACT_URLPARAMETERS = 0x0008,
+	CMS_ACT_FORMPARAMETERS = 0x0009,
+	CMS_ACT_PRINT = 0x000a,
+	CMS_ACT_IF = 0x000b,
+	CMS_ACT_DATE = 0x000c,
+	CMS_ACT_TEXT = 0x000d,
+	CMS_ACT_LOCAL = 0x000e,
+	CMS_ACT_SUBSCRIPTION = 0x000f,
+	CMS_ACT_EDIT = 0x0010,
+	CMS_ACT_SELECT = 0x0011,
+	CMS_ACT_USER = 0x0012,
+	CMS_ACT_LOGIN = 0x0013,
+	CMS_ACT_SEARCH = 0x0014,
+	CMS_ACT_WITH = 0x0015,
+	CMS_ACT_TOPIC = 0x0016,
+	CMS_ACT_URIPATH = 0x0017,
+	CMS_ACT_URL = 0x0018
 } TAction;
 
+// TPubLevel: identifies the levels in the publication structure
+typedef enum _TPubLevel {
+	CMS_PL_ROOT = 0x0000,
+	CMS_PL_LANGUAGE = 0x0001,
+	CMS_PL_PUBLICATION = 0x0002,
+	CMS_PL_ISSUE = 0x0003,
+	CMS_PL_SECTION = 0x0004,
+	CMS_PL_ARTICLE = 0x0005,
+	CMS_PL_SUBTITLE = 0x0006
+} TPubLevel;
+
 typedef enum _TSubsUnit {
-    CMS_SU_NONE = 0,
-    CMS_SU_DAY = 1,
-    CMS_SU_WEEK = 2,
-    CMS_SU_MONTH = 3,
-    CMS_SU_YEAR = 4
+	CMS_SU_NONE = 0,
+	CMS_SU_DAY = 1,
+	CMS_SU_WEEK = 2,
+	CMS_SU_MONTH = 3,
+	CMS_SU_YEAR = 4
 } TSubsUnit;
 
 class CAtom;
