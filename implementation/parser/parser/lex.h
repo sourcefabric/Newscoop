abada674b2480f0c3f1e47565e67a1ab8f43b5fa
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2004-09-09 03:14:28 +0000

    renamed CMS_ST_URL to CMS_ST_URI and ST_URL to ST_URI

--- implementation/parser/parser/lex.h
+++ implementation/parser/parser/lex.h
@@ -87,7 +87,7 @@ using std::map;
 #define CMS_ST_CURRENTSUBTITLE 40
 #define CMS_ST_TOPIC 41
 #define CMS_ST_URIPATH 42
-#define CMS_ST_URL 43
+#define CMS_ST_URI 43
 
 // statement names
 #define ST_LANGUAGE "Language"
@@ -132,7 +132,7 @@ using std::map;
 #define ST_CURRENTSUBTITLE "CurrentSubtitle"
 #define ST_TOPIC "Topic"
 #define ST_URIPATH "URIPath"
-#define ST_URL "URL"
+#define ST_URI "URI"
 
 // The lexem returned by lex class
 class CLexem
