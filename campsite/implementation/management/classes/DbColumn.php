b2c4df590301814d4aa75b9e3038834ef7e14581
paul <paul@952d13e4-84fd-0310-bd7c-ee2827c68691>
2004-09-19 22:48:28 +0000

    Initial checkin - the beginning of the campsite API.

---
+++ campsite/implementation/management/classes/DbColumn.php
@@ -0,0 +1,45 @@
+<?
+
+class DbColumn {
+	var $m_dbTableName;
+	var $m_dbColumnName;
+	var $Field;
+	var $Type;
+	var $Null;
+	var $Key;
+	var $Default;
+	var $Extra;
+	
+	function DbColumn($p_tableName, $p_columnName = null) {
+		$this->m_dbTableName = $p_tableName;
+		$this->m_dbColumnName = $p_columnName;
+	} // constructor
+	
+	function getDbTableName() {
+		return $this->m_dbTableName;
+	} // fn getDbTableName
+	
+	function fetch($p_recordSet = null) {
+		if (!is_null($p_recordSet)) {
+			foreach ($p_recordSet as $key => $value) {
+				$this->$key = $value;
+			}
+		}
+	} // fn fetch
+	
+	function getName() {
+		return $this->Field;
+	}
+
+	function getPrintName() {
+		return substr($this->Field, 1);
+	}
+	
+	function getType() {
+		return strtolower($this->Type);
+	}
+	
+} // class DbColumn
+
+
+?>
---
+++ campsite/implementation/management/classes/common.php
@@ -0,0 +1,59 @@
+<?
+/**
+ * Load include files.
+ */
+function load_common_include_files() {
+	require_once($_SERVER['DOCUMENT_ROOT']."/db_connect.php");
+	require_once($_SERVER['DOCUMENT_ROOT']."/priv/lib_campsite.php");
+	$globalfile=selectLanguageFile($_SERVER['DOCUMENT_ROOT']."/priv",'globals');
+	$localfile=selectLanguageFile('.','locals');
+	@include ($globalfile);
+	@include ($localfile);
+	include ($_SERVER['DOCUMENT_ROOT']."/priv/languages.php");
+} // fn load_common_include_files
+
+/**
+ * Check if user has access to this screen.
+ */
+function check_basic_access($request) {
+	$access = false;
+	$XPerm = array();
+	$user = array();
+	
+	// Check for required info.
+    if (!isset($request['TOL_UserId']) || !isset($request["TOL_UserKey"])) {
+    	return array($access, $user, $XPerm);
+    }
+    
+    // Check if user exists in the table.
+    $queryStr = "SELECT * FROM Users "
+    			." WHERE Id=".$request["TOL_UserId"]
+    			." AND KeyId=".$request["TOL_UserKey"];
+    $query = mysql_query($queryStr);
+    if ($query && (mysql_num_rows($query) > 0)) {
+    	// User exists.
+		$access = true;
+		$user = mysql_fetch_array($query,MYSQL_ASSOC);
+
+		// Fetch the user's permissions.
+		$queryStr = "SELECT * FROM UserPerm "
+					." WHERE IdUser=".$user['Id'];
+		$query2 = mysql_query($queryStr);
+		if ($query2 && (mysql_num_rows($query2) > 0)) {
+			$tmpXPerm = mysql_fetch_array($query2, MYSQL_ASSOC);
+			// Make XPerm a boolean array.
+			$XPerm = array();
+			foreach ($tmpXPerm as $key => $value) {
+				$XPerm[$key] = ($value == 'Y');
+			}
+		}
+		else {
+			// A non-admin can enter the administration area;
+			// they exist but do not have ANY rights.
+			$access = 0;
+		}
+	}
+	return array($access, $user, $XPerm);
+} // fn check_basic_access
+
+?>
