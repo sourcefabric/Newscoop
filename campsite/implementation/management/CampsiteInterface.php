666d094b69d04db7878cb01e963a332cb4981711
paul <paul@952d13e4-84fd-0310-bd7c-ee2827c68691>
2004-10-12 01:58:20 +0000

    Added CreateSelect(), added comments.

--- campsite/implementation/management/CampsiteInterface.php
+++ campsite/implementation/management/CampsiteInterface.php
@@ -5,6 +5,9 @@
  */
 class CampsiteInterface {
 
+	/**
+	 * Display the copyright notice and close the HTML page.
+	 */
 	function CopyrightNotice() {
 		?>
 		<HR NOSHADE SIZE="1" COLOR="BLACK">
@@ -20,18 +23,64 @@ class CampsiteInterface {
 	} // fn CopyrightNotice
 	
 	
-	function ArticleLink($p_articleObj, $p_adminLanguageId, $p_targetFileName = "", $backlink = "") {
+	/**
+	 * Create a HTML SELECT drop down box.
+	 *
+	 */
+	function CreateSelect($p_name, $p_options, $p_selected = null, $p_extras ="", $p_valuesIncluded = false) {
 		?>
-		<A HREF="/priv/pub/issues/sections/articles/<?php echo $p_targetFileName ?>?Pub=<?php  echo $p_articleObj->getPublicationId(); ?>&Issue=<?php echo $p_articleObj->getIssueId(); ?>&Section=<?php echo $p_articleObj->getSectionId(); ?>&Article=<?php echo $p_articleObj->getArticleId(); ?>&Language=<?php echo $p_adminLanguageId; ?>&sLanguage=<?php echo $p_articleObj->getLanguageId(); ?><?php if ($backlink != "") { ?>&Back=<?php echo urlencode($backlink); } ?>">
+    	<select name="<?php echo $p_name ?>" <?php echo $p_extras ?>>
+    	<?php
+    	foreach ($p_options as $key => $value) {
+    		if ($p_valuesIncluded) {
+    			?>
+    			<option value="<?php echo $key; ?>" <?php if (!is_null($p_selected) && ($p_selected == $key)) { echo "selected"; } ?>><?php echo $value; ?></option>
+    			<?php
+    		}
+    		else {
+    			?>
+    			<option <?php if (!is_null($p_selected) && ($p_selected == $value)) { echo "selected"; } ?>><?php echo $value; ?></option>
+    			<?php    			
+    		}
+    	}
+    	?>
+    	</select>
+		<?php
+	} // fn CreateSelect
+	
+	/**
+	 * Create a link to an article.
+	 *
+	 * @param Article p_articleObj
+	 *		The article we want to display.
+	 *
+	 * @param int p_adminLanguageId
+	 *		The administration interface language ID. 
+	 *		(TODO: we should store this in the session!)
+	 *
+	 * @param string p_targetFileName
+	 *		Which file in the "articles" directory to call.
+	 *
+	 * @param string p_backLink
+	 *		I'm not entirely sure what this is for.  I put it in for backward compatibility.
+	 */
+	function ArticleLink($p_articleObj, $p_adminLanguageId, $p_targetFileName = "", $p_backLink = "") {
+		?>
+		<A HREF="/priv/pub/issues/sections/articles/<?php echo $p_targetFileName ?>?Pub=<?php  echo $p_articleObj->getPublicationId(); ?>&Issue=<?php echo $p_articleObj->getIssueId(); ?>&Section=<?php echo $p_articleObj->getSectionId(); ?>&Article=<?php echo $p_articleObj->getArticleId(); ?>&Language=<?php echo $p_adminLanguageId; ?>&sLanguage=<?php echo $p_articleObj->getLanguageId(); ?><?php if ($p_backLink != "") { ?>&Back=<?php echo urlencode($p_backLink); } ?>">
 		<?php
 	} // fn ArticleLink
 	
 	
+	/**
+	 * Display an error and close the page with the copyright notice.
+	 */
 	function DisplayError($errorStr) {
 		?>
+		<br>
 		<BLOCKQUOTE>
-		<LI><?php putGS($errorStr); ?></LI>
+		<UL><LI><?php putGS($errorStr); ?></LI></UL>
 		</BLOCKQUOTE>
+		<br>
 		<?php
 		CampsiteInterface::CopyrightNotice();
 	} // fn DisplayError
