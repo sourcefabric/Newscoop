cf7e5ae0dcfea7a918befdcd6a25913d00278c2d
paul <paul@952d13e4-84fd-0310-bd7c-ee2827c68691>
2004-10-12 02:04:41 +0000

    Added support for campsite-specific tags: subhead and internal links.

--- implementation/management/pub/issues/sections/articles/HtmlArea_Campsite.php
+++ implementation/management/pub/issues/sections/articles/HtmlArea_Campsite.php
@@ -13,42 +13,45 @@ function HtmlArea_Campsite($dbColumns) {
 <script type="text/javascript" src="/javascript/htmlarea/htmlarea.js"></script>
 
 <script type="text/javascript">
-function campsiteExternalLink(editor, objectName, object, link) {
+/** 
+ * This is a copy of the _createlink function, except that it calls 
+ * a different popup window.
+ *
+ */
+function CampsiteInternalLink(editor, objectName, object, link) {
 	var outparam = null;
 	if (typeof link == "undefined") {
 		link = editor.getParentElement();
-		if (link && !/^a$/i.test(link.tagName)) {
+		if (link && !/^a$/i.test(link.tagName))
 			link = null;
-		}
 	}
+	popupWindowTarget = "campsite_internal_link.php";
 	if (link) {
 		outparam = {
-			f_href   : HTMLArea.is_ie ? editor.stripBaseURL(link.href) : link.getAttribute("href"),
-			f_title  : link.title,
-			f_target : link.target
+		f_href   : HTMLArea.is_ie ? editor.stripBaseURL(link.href) : link.getAttribute("href"),
+		f_title  : link.title,
+		f_target : link.target
 		};
+		popupWindowTarget += "?" + outparam["f_href"].replace("campsite_internal_link?", "");
 	}
-	editor._popupDialog("link.html", function(param) {
-		if (!param) {
+	editor._popupDialog(popupWindowTarget, function(param) {
+		if (!param)
 			return false;
-		}
 		var a = link;
-		if (!a) {
-			try {
-				editor._doc.execCommand("createlink", false, param.f_href);
-				a = editor.getParentElement();
-				var sel = editor._getSelection();
-				var range = editor._createRange(sel);
-				if (!HTMLArea.is_ie) {
-					a = range.startContainer;
-					if (!/^a$/i.test(a.tagName)) {
-						a = a.nextSibling;
-						if (a == null)
-							a = range.startContainer.parentNode;
-					}
+		if (!a) try {
+			editor._doc.execCommand("createlink", false, param.f_href);
+			a = editor.getParentElement();
+			var sel = editor._getSelection();
+			var range = editor._createRange(sel);
+			if (!HTMLArea.is_ie) {
+				a = range.startContainer;
+				if (!/^a$/i.test(a.tagName)) {
+					a = a.nextSibling;
+					if (a == null)
+						a = range.startContainer.parentNode;
 				}
-			} catch(e) {}
-		}
+			}
+		} catch(e) {}
 		else {
 			var href = param.f_href.trim();
 			editor.selectNodeContents(a);
@@ -61,9 +64,8 @@ function campsiteExternalLink(editor, objectName, object, link) {
 				a.href = href;
 			}
 		}
-		if (!(a && /^a$/i.test(a.tagName))) {
+		if (!(a && /^a$/i.test(a.tagName)))
 			return false;
-		}
 		a.target = param.f_target.trim();
 		a.title = param.f_title.trim();
 		editor.selectNodeContents(a);
@@ -83,21 +85,36 @@ initdocument = function () {
 			?>
 			var editor = new HTMLArea("<?php print $dbColumn->getName(); ?>");
  			var config = editor.config;
+			config.pageStyle = "@import url(article_stylesheet.css);";
 	 		config.registerButton({
 	 			// The ID of the button.
-				id        : "campsite-external-link", 
+				id        : "campsite-subhead", 
 				// The tooltip.
-				tooltip   : "External Link",
+				tooltip   : "Subhead",
 				// Image to be displayed in the toolbar.
-				image     : "/javascript/htmlarea/images/ed_link.gif",
+				image     : "/javascript/htmlarea/images/campsite_subhead.gif",
 				// TRUE = enabled in text mode
 				// FALSE = disabled in text mode
 				textMode  : false,
 				// Called when the button is clicked.
-				action    : campsiteExternalLink,
-	//									function(editor) {
-	//										editor.surroundHTML("<!** Link external >", "<!** EndLink>");
-	//						                },
+				action    : function(editor) {editor.surroundHTML('<span class="campsite_subhead">', '</span>');},
+				// The button will be disabled if outside 
+				// the specified element.
+				context   : ''
+				});
+				
+	 		config.registerButton({
+	 			// The ID of the button.
+				id        : "campsite-internal-link", 
+				// The tooltip.
+				tooltip   : "Internal Link",
+				// Image to be displayed in the toolbar.
+				image     : "/javascript/htmlarea/images/campsite_internal_link.gif",
+				// TRUE = enabled in text mode
+				// FALSE = disabled in text mode
+				textMode  : false,
+				// Called when the button is clicked.
+				action    : CampsiteInternalLink,
 				// The button will be disabled if outside 
 				// the specified element.
 				context   : ''
@@ -114,7 +131,7 @@ initdocument = function () {
 		
 				[ "justifyleft", "justifycenter", "justifyright", "justifyfull", "separator",
 				  "outdent", "indent", "separator",
-				  "unorderedlist", "orderedlist", "separator", "inserthorizontalrule", "campsite-external-link", "insertimage" ],
+				  "unorderedlist", "orderedlist", "separator", "inserthorizontalrule", "campsite-subhead", "campsite-internal-link", "createlink", "insertimage" ],
 				  ["copy", "cut", "paste", "space", "separator", "undo", "redo", "separator", "lefttoright", "righttoleft", "separator", "htmlmode", "popupeditor"]
 			];
 			editor.registerPlugin(ListType);
