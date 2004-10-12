72b5bb012c343ea3ca000fdbf1b61fb765d3de1a
paul <paul@952d13e4-84fd-0310-bd7c-ee2827c68691>
2004-10-12 02:07:29 +0000

    Got rid of old M4 yuckiness.  Implemented permission-based dual-pane view.

--- implementation/management/pub/issues/sections/articles/preview.php
+++ implementation/management/pub/issues/sections/articles/preview.php
@@ -12,113 +12,34 @@ $articleObj =& new Article($Pub, $Issue, $Section, $sLanguage, $Article);
 $issueObj =& new Issue($Pub, $Language, $Issue);
 $templateObj =& new Template($issueObj->getArticleTemplateId());
 
-?>
-<HTML>
-<HEAD>
-    <META http-equiv="Content-Type" content="text/html; charset=UTF-8">
-	<META HTTP-EQUIV="Expires" CONTENT="now">
-    <META HTTP-EQUIV="Set-Cookie" CONTENT="TOL_Access=all; path=/">
-	<META HTTP-EQUIV="Set-Cookie" CONTENT="TOL_Preview=on; path=/">
-	<TITLE><?php putGS("Preview article"); ?></TITLE>
-	<?php if ($access == 0) { ?>
-	<META HTTP-EQUIV="Refresh" CONTENT="0; URL=/priv/logout.php">
-	<?php  } ?>
-</HEAD>
+setcookie("TOL_Access", "all", null, "/");
+setcookie("TOL_Preview", "on", null, "/");
 
+?>
 <?php  
 if ($access) {
-
-        todefnum('Pub');
-        todefnum('Issue');
-        todefnum('Section');
-        todefnum('Language');
-        todefnum('sLanguage');
-        todefnum('Article');
-
-
-    query ("SELECT * FROM Articles WHERE IdPublication=$Pub AND NrIssue=$Issue AND NrSection=$Section AND Number=$Article AND IdLanguage=$sLanguage", 'q_art');
-    if ($NUM_ROWS) {
-	query ("SELECT * FROM Sections WHERE IdPublication=$Pub AND NrIssue=$Issue AND IdLanguage=$Language AND Number=$Section", 'q_sect');
-	if ($NUM_ROWS) {
-	    query ("SELECT * FROM Publications WHERE Id=$Pub", 'q_pub');
-	    if ($NUM_ROWS) {
-		fetchRow($q_art);
-    		fetchRow($q_sect);
-		fetchRow($q_pub);
-		query ("SELECT * FROM Issues WHERE IdPublication=$Pub AND Number=$Issue AND IdLanguage=$Language", 'q_iss');
-		fetchRow($q_iss);
-if (($NUM_ROWS !=0)&&($templateObj->getName() != "")) {
-	?>
-	<!--<FRAMESET ROWS="60%,*" BORDER="2">-->
-	<FRAMESET ROWS="100%">
-		<FRAME SRC="<?php print "/look/".$templateObj->getName(); ?>?IdPublication=<?php p($Pub); ?>&NrIssue=<?php p($Issue); ?>&NrSection=<?php p($Section); ?>&NrArticle=<?php p($Article); ?>&IdLanguage=<?php p($sLanguage); ?>" NAME="body" FRAMEBORDER="1">
-		<!--<FRAME NAME="e" SRC="empty.php" FRAMEBORDER="1">-->
-	</FRAMESET>
-	<?php  
+	if (($articleObj->exists()) && ($templateObj->getName() != "")) {
+		if ($XPerm["ManageTempl"] || $XPerm["DeleteTempl"]) {
+			// Show dual-pane view for those with template management priviledges
+			?>
+			<FRAMESET ROWS="60%,*" BORDER="2">
+				<FRAME SRC="<?php print "/look/".$templateObj->getName(); ?>?IdPublication=<?php p($articleObj->getPublicationId()); ?>&NrIssue=<?php p($articleObj->getIssueId()); ?>&NrSection=<?php p($articleObj->getSectionId()); ?>&NrArticle=<?php p($articleObj->getArticleId()); ?>&IdLanguage=<?php p($articleObj->getLanguageId()); ?>" NAME="body" FRAMEBORDER="1">
+				<FRAME NAME="e" SRC="empty.php" FRAMEBORDER="1">
+			</FRAMESET>
+			<?php  
+		}
+		else {
+			// Show single pane for everyone else.
+			?>
+			<FRAMESET ROWS="100%">
+				<FRAME SRC="<?php print "/look/".$templateObj->getName(); ?>?IdPublication=<?php p($articleObj->getPublicationId()); ?>&NrIssue=<?php p($articleObj->getIssueId()); ?>&NrSection=<?php p($articleObj->getSectionId()); ?>&NrArticle=<?php p($articleObj->getArticleId()); ?>&IdLanguage=<?php p($articleObj->getLanguageId()); ?>" NAME="body" FRAMEBORDER="1">
+			</FRAMESET>
+			<?php
+		}
+	} 
+	else { 
+		ArticleTop($articleObj, $issueObj->getLanguageId(), "Preview article", $access, false);
+		CampsiteInterface::DisplayError('This article cannot be previewed. Please make sure it has a <B><I>single article</I></B> template selected.');
+	}
 } 
-else { 
-	?>
-	<STYLE>
-	BODY { font-family: Tahoma, Arial, Helvetica, sans-serif; font-size: 10pt; }
-	SMALL { font-family: Tahoma, Arial, Helvetica, sans-serif; font-size: 8pt; }
-	FORM { font-family: Tahoma, Arial, Helvetica, sans-serif; font-size: 10pt; }
-	TH { font-family: Tahoma, Arial, Helvetica, sans-serif; font-size: 10pt; }
-	TD { font-family: Tahoma, Arial, Helvetica, sans-serif; font-size: 10pt; }
-	BLOCKQUOTE { font-family: Tahoma, Arial, Helvetica, sans-serif; font-size: 10pt; }
-	UL { font-family: Tahoma, Arial, Helvetica, sans-serif; font-size: 10pt; }
-	LI { font-family: Tahoma, Arial, Helvetica, sans-serif; font-size: 10pt; }
-	A  { font-family: Tahoma, Arial, Helvetica, sans-serif; font-size: 10pt; text-decoration: none; color: darkblue; }
-	ADDRESS { font-family: Tahoma, Arial, Helvetica, sans-serif; font-size: 8pt; }
-</STYLE>
-
-<BODY  BGCOLOR="WHITE" TEXT="BLACK" LINK="DARKBLUE" ALINK="RED" VLINK="DARKBLUE">
-
-<TABLE BORDER="0" CELLSPACING="0" CELLPADDING="1" WIDTH="100%">
-	<TR>
-		<TD ROWSPAN="2" WIDTH="1%"><IMG SRC="/priv/img/sign_big.gif" BORDER="0"></TD>
-		<TD>
-		    <DIV STYLE="font-size: 12pt"><B><?php  putGS("Preview article"); ?></B></DIV>
-		    <HR NOSHADE SIZE="1" COLOR="BLACK">
-		</TD>
-	</TR>
-	<TR><TD>&nbsp;</TD></TR>
-</TABLE>
-
-<?php 
-    query ("SELECT Name FROM Languages WHERE Id=$Language", 'q_lang');
-    query ("SELECT Name FROM Languages WHERE Id=$sLanguage", 'q_slang');
-    fetchRow($q_lang);
-    fetchRow($q_slang);
-?><TABLE BORDER="0" CELLSPACING="1" CELLPADDING="1" WIDTH="100%"><TR>
-<TD ALIGN="RIGHT" WIDTH="1%" NOWRAP VALIGN="TOP">&nbsp;<?php  putGS("Publication"); ?>:</TD><TD BGCOLOR="#D0D0B0" VALIGN="TOP"><B><?php  pgetHVar($q_pub,'Name'); ?></B></TD>
-
-<TD ALIGN="RIGHT" WIDTH="1%" NOWRAP VALIGN="TOP">&nbsp;<?php  putGS("Issue"); ?>:</TD><TD BGCOLOR="#D0D0B0" VALIGN="TOP"><B><?php  pgetHVar($q_iss,'Number'); ?>. <?php  pgetHVar($q_iss,'Name'); ?> (<?php  pgetHVar($q_lang,'Name'); ?>)</B></TD>
-
-<TD ALIGN="RIGHT" WIDTH="1%" NOWRAP VALIGN="TOP">&nbsp;<?php  putGS("Section"); ?>:</TD><TD BGCOLOR="#D0D0B0" VALIGN="TOP"><B><?php  pgetHVar($q_sect,'Number'); ?>. <?php  pgetHVar($q_sect,'Name'); ?></B></TD>
-
-<TD ALIGN="RIGHT" WIDTH="1%" NOWRAP VALIGN="TOP">&nbsp;<?php  putGS("Article"); ?>:</TD><TD BGCOLOR="#D0D0B0" VALIGN="TOP"><B><?php  pgetHVar($q_art,'Name'); ?> (<?php  pgetHVar($q_slang,'Name'); ?>)</B></TD>
-
-</TR></TABLE>
-
-<BLOCKQUOTE>
-	<LI><?php  putGS('This article cannot be previewed. Please make sure it has a <B><I>single article</I></B> template selected.'); ?></LI>
-</BLOCKQUOTE>
-
-<HR NOSHADE SIZE="1" COLOR="BLACK">
-<a STYLE='font-size:8pt;color:#000000' href='http://www.campware.org' target='campware'>CAMPSITE  2.1.5 &copy 1999-2004 MDLF, maintained and distributed under GNU GPL by CAMPWARE</a>
-</BODY>
-<?php  } ?><?php  } else { ?><BLOCKQUOTE>
-	<LI><?php  putGS('No such publication.'); ?></LI>
-</BLOCKQUOTE>
-<?php  } ?>
-<?php  } else { ?><BLOCKQUOTE>
-	<LI><?php  putGS('No such section.'); ?></LI>
-</BLOCKQUOTE>
-<?php  } ?>
-<?php  } else { ?><BLOCKQUOTE>
-	<LI><?php  putGS('No such article.'); ?></LI>
-</BLOCKQUOTE>
-<?php  } ?>
-<?php  } ?>
-
-</HTML>
+?>
