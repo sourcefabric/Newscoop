6306c6ca5eb6610866db23299a49ed121c73beb4
paul <paul@952d13e4-84fd-0310-bd7c-ee2827c68691>
2004-11-14 01:22:31 +0000

    Massively changed this class.  Now the db column values are stored in an array instead of as internal member variables of the class.  Added getKey(), setKey(), update(), getProperty(), setColumnNames().  setProperty() has a new argument to allow you to specify whether to write the change to the database.  Added ability to use auto-increment keys with create() function.

--- campsite/implementation/management/classes/DatabaseObject.php
+++ campsite/implementation/management/classes/DatabaseObject.php
@@ -1,32 +1,44 @@
 <?php
 class DatabaseObject {
 	/**
+	 * The name of the database table.
 	 * Redefine this in the subclass.
 	 * @var string
 	 */
 	var $m_dbTableName = "";
 
 	/**
+	 * The column names used for the key.
 	 * Redefine this in the subclass.
 	 * @var array
 	 */
-	var $m_primaryKeyColumnNames = array();
+	var $m_keyColumnNames = array();
 
 	/**
-	 * Redefine this in the subclass.
+	 * Whether or not the primary key is an auto-increment field.
+	 * @var boolean
+	 */
+	var $m_keyIsAutoIncrement = false;
+	
+	/**
+	 * An array of (ColumnName => Value).
 	 * @var array
 	 */
-	var $m_columnNames = array();
+	var $m_data = array();
+
 	
-	
-	function DatabaseObject() {
-		if (count($this->m_columnNames) == 0) {
-			// Automatically initialize column names
-			foreach ($this as $varName => $value) {
-				if (substr($varName, 0, 2) != "m_") {
-					$this->m_columnNames[] = $varName;
-				}
-			}
+	/**
+	 * DatabaseObject represents a row in a database table.
+	 * This class is meant to be subclassed in order to implement a
+	 * specific table in the database.
+	 *
+	 * @param array p_columnNames
+	 *		The column names of this table.  These are optional.
+	 *
+	 */
+	function DatabaseObject($p_columnNames = null) {
+		if (!is_null($p_columnNames)) {
+			$this->setColumnNames($p_columnNames);
 		}
 	} // constructor
 	
@@ -35,14 +47,37 @@ class DatabaseObject {
 	 * Return the column names used for the primary key.
 	 * @return array
 	 */
-	function getPrimaryKeyColumnNames() { return $this->m_primaryKeyColumnNames; }
+	function getKeyColumnNames() { return $this->m_keyColumnNames; }
 
 	
 	/**
-	 * Return a mapping of database column name => internal var name.
+	 * Return the column names of this table.
 	 * @return array
 	 */	
-	function getColumnNames() { return $this->m_columnNames; }
+	function getColumnNames() { return array_keys($this->m_data); }
+
+	
+	/**
+	 * Initialize the column names for this object.
+	 * All column values will be initialized to null.
+	 *
+	 * @param array p_columnNames
+	 * 		The column names in the database.
+	 *
+	 * @return void
+	 */
+	function setColumnNames($p_columnNames) {
+		foreach ($p_columnNames as $columnName) {
+			$this->m_data[$columnName] = null;
+		}
+	} // fn setColumnNames
+	
+	
+	/**
+	 * Return the row as an array indexed by the column names.
+	 * @return array
+	 */
+	function getData() { return $this->m_data; }
 	
 	
 	/**
@@ -53,11 +88,46 @@ class DatabaseObject {
 	
 	
 	/**
+	 * Return the key as an array indexed by column names.
+	 * @return array
+	 */
+	function getKey() {
+		$key = array();
+		foreach ($this->m_keyColumnNames as $columnName) {
+			$key[$columnName] = $this->m_data[$columnName];
+		}
+		return $key;
+	} // fn getKey
+	
+	
+	/**
+	 * Set the key column names and optionally the values as well.
+	 * @param array p_columnNames
+	 *		Can be either:
+	 *		[0] => "column name 1", [1] => "column name 2", ...
+	 *		or:
+	 *		["column name 1"] => "value", ["column name 2"] => "value",...
+	 *
+	 * @return void
+	 */
+	function setKey($p_columnNames) {
+		if (isset($p_columnNames[0])) {
+			$this->m_keyColumnNames = array_values($p_columnNames);
+		}
+		else {
+			$this->m_keyColumnNames = array_keys($p_columnNames);
+			foreach ($this->m_keyColumnNames as $columnName) {
+				$this->m_data[$columnName] = $p_columnNames[$columnName];
+			}
+		}
+	} // fn setKey
+	
+	/**
 	 * Fetch a single record from the database for the given key.
 	 *
 	 * @param array p_recordSet
 	 *		If the record has already been fetched and we just need to 
-	 * 		assign the values to the object's internal member variables.
+	 * 		assign the data to the object's internal member variable.
 	 *
 	 * @return boolean
 	 *		TRUE on success, FALSE on failure
@@ -68,7 +138,7 @@ class DatabaseObject {
 		if (is_null($p_recordSet)) {
 			$queryStr = "SELECT ";
 			$tmpColumnNames = array();
-			foreach ($this->m_columnNames as $columnName) {
+			foreach ($this->getColumnNames() as $columnName) {
 				$tmpColumnNames[] = "`".$columnName."`";
 			}
 			$queryStr .= implode(", ", $tmpColumnNames);
@@ -77,8 +147,8 @@ class DatabaseObject {
 			$queryStr .= " LIMIT 1";
 			$resultSet =& $Campsite["db"]->GetRow($queryStr);
 			if ($resultSet) {
-				foreach ($this->m_columnNames as $dbColumnName) {
-					$this->$dbColumnName = $resultSet[$dbColumnName];
+				foreach ($this->getColumnNames() as $dbColumnName) {
+					$this->m_data[$dbColumnName] = $resultSet[$dbColumnName];
 				}
 			}
 			else {
@@ -86,9 +156,7 @@ class DatabaseObject {
 			}
 		}
 		else {
-			foreach ($this->m_columnNames as $dbColumnName) {
-				$this->$dbColumnName = $p_recordSet[$dbColumnName];
-			}			
+			$this->m_data =& $p_recordSet;
 		}
 		return true;
 	} // fn fetch
@@ -101,7 +169,7 @@ class DatabaseObject {
 	 */
 	function exists() {
 		global $Campsite;
-		$queryStr = "SELECT `".$this->m_primaryKeyColumnNames[0]."`";
+		$queryStr = "SELECT `".$this->m_keyColumnNames[0]."`";
 		$queryStr .= " FROM " . $this->m_dbTableName;
 		$queryStr .= " WHERE " . $this->getKeyWhereClause();
 		$queryStr .= " LIMIT 1";
@@ -117,34 +185,22 @@ class DatabaseObject {
 	 */
 	function getKeyWhereClause() {
 		$whereParts = array();
-		foreach ($this->m_primaryKeyColumnNames as $columnName) {
-			$whereParts[] = "`" . $columnName . "`='". $this->$columnName ."'";
+		foreach ($this->m_keyColumnNames as $columnName) {
+			$whereParts[] = "`" . $columnName . "`='". $this->m_data[$columnName] ."'";
 		}
 		return implode(" AND ", $whereParts);		
 	} // fn getKeyWhereClause
 	
 	
 	/**
-	 * Return a string used to create or set columns in the database.
+	 * Return true if the object has all the values required
+	 * to fetch a unique record from the table.
 	 *
-	 * @return string
-	 */ 
-	function getKeyCreateClause() {
-		$parts = array();
-		foreach ($this->m_primaryKeyColumnNames as $columnName) {
-			$parts[] = "`".$columnName."`='".$this->$columnName."'";
-		}
-		return implode(", ", $parts);
-	} // fn getKeyCreateClause
-	
-	
-	/**
-	 *
-	 *
+	 * @return boolean
 	 */
 	function keyValuesExist() {
-		foreach ($this->m_primaryKeyColumnNames as $columnName) {
-			if (!$this->$columnName) {
+		foreach ($this->m_keyColumnNames as $columnName) {
+			if (!$this->m_data[$columnName]) {
 				return false;
 			}
 		}
@@ -164,18 +220,50 @@ class DatabaseObject {
 	 */
 	function create($p_values = null) {
 		global $Campsite;
-		$queryStr = "INSERT IGNORE INTO " . $this->m_dbTableName
-					. " SET " . $this->getKeyCreateClause();
+		$queryStr = "INSERT IGNORE INTO " . $this->m_dbTableName;
+		
+		// Make sure we have the key required to create the row.
+		// If auto-increment is set, the database will create the key for us.
+		$columnNames = array();
+		$columnValues = array();
+		if ($this->keyValuesExist()) {
+			// If the key values exist, use those.
+			$columnNames = array_keys($this->getKey());
+			$columnValues = array_values($this->getKey());
+		}
+		elseif (!$this->m_keyIsAutoIncrement) {
+			// We dont have the key values and
+			// the key is not an auto-increment value,
+			// so we cant create the row.
+			return false;			
+		}
+		
+		// Optionally set some values when we create the row.
 		if (!is_null($p_values)) {
 			$parts = array();
 			foreach ($p_values as $columnName => $value) {
 				// Construct value string for the SET clause.
-				$parts[] = "`".$columnName."`='".$value."'";
+				$columnNames[] = $columnName;
+				$columnValues[] = "'".$value."'";
+				$this->m_data[$columnName] = $value;
 			}
-			$queryStr .= ",".implode(", ", $parts);
 		}
+
+		$queryStr .= "(" . implode(",", $columnNames) . ")";
+		$queryStr .= " VALUES (".implode(",", $columnValues) .")";
+
+		// Create the row.
 		$Campsite["db"]->Execute($queryStr);
-		return ($Campsite["db"]->AffectedRows() > 0);
+		$success = ($Campsite["db"]->Affected_Rows() > 0);
+		
+		// Fetch the row ID if it is auto-increment
+		if ($this->m_keyIsAutoIncrement) {
+			// There should only be one key column because
+			// its an auto-increment key.
+			$this->m_data[$this->m_keyColumnNames[0]] = 
+				$Campsite["db"]->Insert_ID();
+		}
+		return $success;
 	} // fn create
 	
 	
@@ -188,11 +276,43 @@ class DatabaseObject {
 	function delete() {
 		global $Campsite;
 		$queryStr = "DELETE FROM " . $this->m_dbTableName
-					. " WHERE " . $this->getKeyWhereClause();
+					." WHERE " . $this->getKeyWhereClause()
+					." LIMIT 1";
 		$Campsite["db"]->Execute($queryStr);
 		return ($Campsite["db"]->Affected_Rows() > 0);
 	} // fn delete
 
+
+	/**
+	 * Return the data stored in the given column.
+	 *
+	 * @param string p_dbColumnName
+	 *
+	 * @param boolean p_forceFetchFromDatabase
+	 *		Get the data from the database instead of cached value 
+	 *		that is stored in the object.
+	 * 
+	 * @return mixed
+	 *		Return a string if the property exists,
+	 *		return a PEAR_Error otherwise.
+	 */
+	function getProperty($p_dbColumnName, $p_forceFetchFromDatabase = false) {
+		global $Campsite;
+		if (array_key_exists($p_dbColumnName, $this->m_data)) {
+			if ($p_forceFetchFromDatabase) {
+				$queryStr = "SELECT ".$p_dbColumnName
+							." FROM ".$this->m_dbTableName
+							." WHERE ".$this->getKeyWhereClause()
+							." LIMIT 1";
+				$this->m_data[$p_dbColumnName] = $Campsite["db"]->GetOne($queryStr);
+			}
+			return $this->m_data[$p_dbColumnName];
+		}
+		else {
+			return new PEAR_Error("Property " . $p_dbColumnName . " does not exist.");
+		}
+	} // fn getProperty
+	
 	
 	/**
 	 * Set the given column name to the given value.
@@ -204,7 +324,12 @@ class DatabaseObject {
 	 * @param string p_value
 	 *		The value to set.
 	 *
-	 * @param boolean p_refetch
+	 * @param boolean p_commit
+	 *		If set to true, the value will be written to the database immediately.
+	 *		If set to false, the value will not be written to the database.
+	 *		Default is true.
+	 *
+	 * @param boolean p_isSql
 	 *		Set this to TRUE if p_value consists of SQL commands.
 	 *		There is no way to know what the result of the command is,
 	 *		so we will need to refetch the value from the database in
@@ -213,33 +338,88 @@ class DatabaseObject {
 	 * @return boolean
 	 *		TRUE if the database was changed, FALSE if it wasnt.
 	 */
-	function setProperty($p_dbColumnName, $p_value, $p_refetch = false) {
+	function setProperty($p_dbColumnName, $p_value, $p_commit = true, $p_isSql = false) {
 		global $Campsite;
-		if ($p_value == $this->$p_dbColumnName) {
+		// If we are not committing, the value cannot be SQL.
+		if (!$p_commit && $p_isSql) {
 			return false;
 		}
-		if (!$this->keyValuesExist()) {
+		// Check that the value is a valid column name.
+		if (!array_key_exists($p_dbColumnName, $this->m_data)) {
 			return false;
 		}
-		$queryStr = "UPDATE ".$this->m_dbTableName
-					. " SET `". $p_dbColumnName."`='".$p_value."'"
-					. " WHERE ".$this->getKeyWhereClause();
-		$Campsite["db"]->Execute($queryStr);
-		$databaseChanged = ($Campsite["db"]->Affected_Rows() > 0);
-		if (!$p_refetch) {
-			$this->$p_dbColumnName = $p_value;
+		// If the value hasnt changed, dont update it.
+		if ($p_value == $this->m_data[$p_dbColumnName]) {
+			return false;
+		}
+		// If we dont have the key to this row, we cant update it.
+		if ($p_commit && !$this->keyValuesExist()) {
+			return false;
+		}
+		// Commit value to the database if requested.
+		$databaseChanged = false;
+		if ($p_commit) {
+			$value = $p_value;
+			if (!$p_isSql) {
+				$value = "'".$p_value."'";
+			}
+			$queryStr = "UPDATE ".$this->m_dbTableName
+						." SET `". $p_dbColumnName."`=".$value
+						." WHERE ".$this->getKeyWhereClause()
+						." LIMIT 1";
+			$Campsite["db"]->Execute($queryStr);
+			$databaseChanged = ($Campsite["db"]->Affected_Rows() > 0);
+		}
+		// Store the value locally.
+		if (!$p_isSql) {
+			$this->m_data[$p_dbColumnName] = $p_value;
 		}
 		else {
-			if ($databaseChanged) {
-				$queryStr = "SELECT ".$this->$p_dbColumnName 
-							." FROM ".$this->m_dbTableName
-							." WHERE ".$this->getKeyWhereClause();
-				$this->$p_dbColumnName = $Campsite["db"]->GetOne($queryStr);
-			}
+			// Fetch the data from the database.  This is for the
+			// case when the database execute some operation (e.g. "DATE")
+			// to create the new value.
+			$queryStr = "SELECT ".$p_dbColumnName
+						." FROM ".$this->m_dbTableName
+						." WHERE ".$this->getKeyWhereClause()
+						." LIMIT 1";
+			$this->m_data[$p_dbColumnName] = $Campsite["db"]->GetOne($queryStr);
 		}
 		return $databaseChanged;
 	} // fn setProperty
 	
+	
+	/**
+	 * Update the database row with the given values.
+	 *
+	 * @param array $p_columns
+	 *		Array of (Column_Name => Value)
+	 *
+	 * @return boolean
+	 *		Return true if the database was updated, false otherwise.
+	 */
+	function update($p_columns = null) {
+		global $Campsite;
+		if (is_null($p_columns)) {
+			return false;
+		}
+        $queryStr = "UPDATE " . $this->m_dbTableName
+        			." SET ";
+        $setColumns = array();
+        foreach ($p_columns as $columnName => $columnValue) {
+        	if (!array_key_exists($columnName, $this->m_data)) {
+        		//return new PEAR_Error("Column name ".$columnName." does not exist.");
+        		return false;
+        	}
+        	$setColumns[] = $columnName . "='". $columnValue ."'";
+        	$this->m_data[$columnName] = $columnValue;
+        }
+        $queryStr .= implode(",", $setColumns);
+        $queryStr .= " WHERE " . $this->getKeyWhereClause();
+        $queryStr .= " LIMIT 1";
+        $Campsite["db"]->Execute($queryStr);
+		$databaseChanged = ($Campsite["db"]->Affected_Rows() > 0);
+		return $databaseChanged;
+	} // fn update
 	
 } // class DatabaseObject
 
