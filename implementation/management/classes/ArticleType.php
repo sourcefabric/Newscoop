3148a325a219c6277009db1bf4f0aa84c96b013f
paul <paul@952d13e4-84fd-0310-bd7c-ee2827c68691>
2004-11-14 01:16:18 +0000

    Internal changes required because DatabaseObject now holds data in an array instead of as member variables in the class.  Much of the functionality in ArticleType has been implemented in DatabaseObject.

--- implementation/management/classes/ArticleType.php
+++ implementation/management/classes/ArticleType.php
@@ -4,23 +4,20 @@ require_once($_SERVER['DOCUMENT_ROOT']."/classes/DbColumn.php");
 
 class ArticleType extends DatabaseObject {
 	var $m_columnNames = array("NrArticle", "IdLanguage");
-	var $m_primaryKeyColumnNames = array("NrArticle", "IdLanguage");
+	var $m_keyColumnNames = array("NrArticle", "IdLanguage");
 	var $m_dbTableName;
-	var $NrArticle;
-	var $IdLanguage;
+	var $m_columnNames = array("NrArticle", "IdLanguage");
 	
 	function ArticleType($p_articleType, $p_articleId, $p_languageId) {
-		parent::DatabaseObject();
 		$this->m_dbTableName = "X".$p_articleType;
-		$this->NrArticle = $p_articleId;
-		$this->IdLanguage = $p_languageId;
 		// Get user-defined values.
 		$dbColumns = $this->getUserDefinedColumns();
-		foreach ($dbColumns as $dbColumn) {
-			$columnName = $dbColumn->getName();
-			$this->$columnName = null;
-			array_push($this->m_columnNames, $columnName);
+		foreach ($dbColumns as $columnMetaData) {
+			$this->m_columnNames[] = $columnMetaData->getName();
 		}
+		parent::DatabaseObject($this->m_columnNames);
+		$this->setProperty("NrArticle", $p_articleId, false);
+		$this->setProperty("IdLanguage", $p_languageId, false);
 		if ($this->exists()) {
 			$this->fetch();
 		}
@@ -45,79 +42,7 @@ class ArticleType extends DatabaseObject {
 		}
 		return $metadata;
 	} // fn getUserDefinedColumns
-	
-
-	function getColumnValue($p_columnName) {
-		return $this->$p_columnName;
-	} // fn getColumnValue
-	
-	
-	/**
-	 *
-	 * @param boolean p_refetch
-	 *		Set this to true if you are including SQL commands in the column value.
-	 *		This will cause a refetch of the row since there is no way to determine
-	 *		what value the column was set to. 
-	 */
-	function setColumnValue($p_columnName, $p_columnValue, $p_refetch = false) {
-		global $Campsite;
-		if (!isset($this->$p_columnName) || ($p_columnValue == $this->$p_columnName)) {
-			return false;
-		}
-		$queryStr = "UPDATE ".$this->m_dbTableName;
-		if (!$p_refetch) {
-			$queryStr .= " SET ".$p_columnName."='".$p_columnValue."'";
-		}
-		else {
-			$queryStr .= " SET ".$p_columnName."=".$p_columnValue;
-		}
-		$queryStr .= " WHERE ".$this->getKeyWhereClause();
-		$Campsite["db"]->Execute($queryStr);
-		$databaseChanged = ($Campsite["db"]->Affected_Rows() > 0);
-		if ($p_refetch) {
-			$this->fetch();
-		}
-		else {
-			$this->$p_columnName = $p_columnValue;
-		}
-		return $databaseChanged;
-	} // fn setColumnValue
-	
-	
-	/**
-	 * Set the body of the article.
-	 *
-	 * @param string body
-	 * 
-	 * @return void
-	 */
-	function setBody($body) {
-		$queryStr = "UPDATE ".$this->getArticleTypeTableName()
-					." SET Fbody='".addslashes(trim($body))."'"
-					." WHERE NrArticle='".$this->m_number."'"
-					." AND IdLanguage='".$this->m_language."'";
-		mysql_query($queryStr);
-		$this->m_body = trim($body);
-	} // fn setBody
-
-	
-	/**
-	 * Set the intro of the article.
-	 *
-	 * @param string into
-	 *
-	 * @return void
-	 */
-	function setIntro($intro) {
-		parent::setProperty("Fintro", "m_intro", $p_intro);
-		$queryStr = "UPDATE ".$this->getArticleTypeTableName()
-					." SET Fintro='".addslashes(trim($intro))."'"
-					." WHERE NrArticle='".$this->m_number."'"
-					." AND IdLanguage='".$this->m_language."'";
-		mysql_query($queryStr);
-		$this->m_intro = trim($intro);
-	} // fn setIntro	
-	
+		
 } // class ArticleType
 
 ?>
