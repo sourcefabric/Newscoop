ce10653caf0530f913d7bd8943955ab575e32309
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2004-11-28 20:07:23 +0000

    remove libxml directory from xmlreader.h included file path

--- campsite/implementation/parser/parser/cxmlreader.h
+++ campsite/implementation/parser/parser/cxmlreader.h
@@ -27,7 +27,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #define CXMLREADER_H
 
 
-#include <libxml/xmlreader.h>
+#include <xmlreader.h>
 #include <string>
 #include <stdexcept>
 
