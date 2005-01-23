d96a41a75197b41444935ef5eee684a3ebceda75
paul <paul@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-01-23 19:13:24 +0000

    PHP output of previous M4 files

---
+++ campsite/implementation/management/pub/issues/sections/articles/images/do_add.php
@@ -0,0 +1,96 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN"
+	"http://www.w3.org/TR/REC-html40/loose.dtd">
+<HTML>
+<?php  include ("../../../../../lib_campsite.php");
+    $globalfile=selectLanguageFile('../../../../..','globals');
+    $localfile=selectLanguageFile('.','locals');
+    @include ($globalfile);
+    @include ($localfile);
+    include ("../../../../../languages.php");   ?>
+<?php  require_once("$DOCUMENT_ROOT/db_connect.php"); ?>
+
+
+<?php 
+    todefnum('TOL_UserId');
+    todefnum('TOL_UserKey');
+    query ("SELECT * FROM Users WHERE Id=$TOL_UserId AND KeyId=$TOL_UserKey", 'Usr');
+    $access=($NUM_ROWS != 0);
+    if ($NUM_ROWS) {
+	fetchRow($Usr);
+	query ("SELECT * FROM UserPerm WHERE IdUser=".getVar($Usr,'Id'), 'XPerm');
+	 if ($NUM_ROWS){
+	 	fetchRow($XPerm);
+	 }
+	 else $access = 0;						//added lately; a non-admin can enter the administration area; he exists but doesn't have ANY rights
+	 $xpermrows= $NUM_ROWS;
+    }
+    else {
+	query ("SELECT * FROM UserPerm WHERE 1=0", 'XPerm');
+    }
+?>
+    
+
+
+    <?php  if ($access) {
+	query ("SELECT AddImage FROM UserPerm WHERE IdUser=".getVar($Usr,'Id'), 'Perm');
+	 if ($NUM_ROWS) {
+		fetchRow($Perm);
+		$access = (getVar($Perm,'AddImage') == "Y");
+	}
+	else $access = 0;
+    } ?>
+    
+ 
+
+<HEAD>
+    <META http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+	<META HTTP-EQUIV="Expires" CONTENT="now">
+	<TITLE><?php  putGS("Adding new image"); ?></TITLE>
+<?php  if ($access == 0) { ?>	<META HTTP-EQUIV="Refresh" CONTENT="0; URL=/priv/ad.php?ADReason=<?php  print encURL(getGS("You do not have the right to add images" )); ?>">
+<?php  } else {
+    ## added by sebastian ##
+    todefnum('Id');
+    ########################
+    dSystem( "$scriptBase/process_i '$Id'");
+}
+?>
+</HEAD>
+
+<?php  if ($access) { ?><STYLE>
+	BODY { font-family: Tahoma, Arial, Helvetica, sans-serif; font-size: 10pt; }
+	SMALL { font-family: Tahoma, Arial, Helvetica, sans-serif; font-size: 8pt; }
+	FORM { font-family: Tahoma, Arial, Helvetica, sans-serif; font-size: 10pt; }
+	TH { font-family: Tahoma, Arial, Helvetica, sans-serif; font-size: 10pt; }
+	TD { font-family: Tahoma, Arial, Helvetica, sans-serif; font-size: 10pt; }
+	BLOCKQUOTE { font-family: Tahoma, Arial, Helvetica, sans-serif; font-size: 10pt; }
+	UL { font-family: Tahoma, Arial, Helvetica, sans-serif; font-size: 10pt; }
+	LI { font-family: Tahoma, Arial, Helvetica, sans-serif; font-size: 10pt; }
+	A  { font-family: Tahoma, Arial, Helvetica, sans-serif; font-size: 10pt; text-decoration: none; color: darkblue; }
+	ADDRESS { font-family: Tahoma, Arial, Helvetica, sans-serif; font-size: 8pt; }
+</STYLE>
+
+<BODY  BGCOLOR="WHITE" TEXT="BLACK" LINK="DARKBLUE" ALINK="RED" VLINK="DARKBLUE">
+<TABLE BORDER="0" CELLSPACING="0" CELLPADDING="1" WIDTH="100%">
+	<TR>
+		<TD ROWSPAN="2" WIDTH="1%"><IMG SRC="/priv/img/sign_big.gif" BORDER="0"></TD>
+		<TD>
+		    <DIV STYLE="font-size: 12pt"><B><?php  putGS("Adding new image"); ?></B></DIV>
+		    <HR NOSHADE SIZE="1" COLOR="BLACK">
+		</TD>
+	</TR>
+	<TR><TD ALIGN=RIGHT><TABLE BORDER="0" CELLSPACING="1" CELLPADDING="0"><TR>	<TR><TD>&nbsp;</TD></TR>
+</TR></TABLE></TD></TR>
+</TABLE>
+
+<BLOCKQUOTE>
+	<LI><?php  putGS('Adding new image'); ?></LI>
+<?php  $logtext = getGS('Image uploaded'); query ("INSERT INTO Log SET TStamp=NOW(), IdEvent=41, User='".getVar($Usr,'UName')."', Text='$logtext'"); ?>
+</BLOCKQUOTE>
+
+<HR NOSHADE SIZE="1" COLOR="BLACK">
+<a STYLE='font-size:8pt;color:#000000' href='http://www.campware.org' target='campware'>CAMPSITE  2.1.5 &copy 1999-2004 MDLF, maintained and distributed under GNU GPL by CAMPWARE</a>
+</BODY>
+<?php  } ?>
+
+</HTML>
---
+++ campsite/implementation/management/pub/issues/sections/articles/images/do_edit.php
@@ -0,0 +1,208 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN"
+	"http://www.w3.org/TR/REC-html40/loose.dtd">
+<HTML>
+<?php  include ("../../../../../lib_campsite.php");
+    $globalfile=selectLanguageFile('../../../../..','globals');
+    $localfile=selectLanguageFile('.','locals');
+    @include ($globalfile);
+    @include ($localfile);
+    include ("../../../../../languages.php");   ?>
+<?php  require_once("$DOCUMENT_ROOT/db_connect.php"); ?>
+
+
+<?php 
+    todefnum('TOL_UserId');
+    todefnum('TOL_UserKey');
+    query ("SELECT * FROM Users WHERE Id=$TOL_UserId AND KeyId=$TOL_UserKey", 'Usr');
+    $access=($NUM_ROWS != 0);
+    if ($NUM_ROWS) {
+	fetchRow($Usr);
+	query ("SELECT * FROM UserPerm WHERE IdUser=".getVar($Usr,'Id'), 'XPerm');
+	 if ($NUM_ROWS){
+	 	fetchRow($XPerm);
+	 }
+	 else $access = 0;						//added lately; a non-admin can enter the administration area; he exists but doesn't have ANY rights
+	 $xpermrows= $NUM_ROWS;
+    }
+    else {
+	query ("SELECT * FROM UserPerm WHERE 1=0", 'XPerm');
+    }
+?>
+    
+
+
+<HEAD>
+    <META http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+	<META HTTP-EQUIV="Expires" CONTENT="now">
+	<TITLE><?php  putGS("Updating image information"); ?></TITLE>
+<?php  if ($access == 0) { ?>	<META HTTP-EQUIV="Refresh" CONTENT="0; URL=/priv/logout.php">
+<?php  } ?></HEAD>
+
+<?php  if ($access) { ?><STYLE>
+	BODY { font-family: Tahoma, Arial, Helvetica, sans-serif; font-size: 10pt; }
+	SMALL { font-family: Tahoma, Arial, Helvetica, sans-serif; font-size: 8pt; }
+	FORM { font-family: Tahoma, Arial, Helvetica, sans-serif; font-size: 10pt; }
+	TH { font-family: Tahoma, Arial, Helvetica, sans-serif; font-size: 10pt; }
+	TD { font-family: Tahoma, Arial, Helvetica, sans-serif; font-size: 10pt; }
+	BLOCKQUOTE { font-family: Tahoma, Arial, Helvetica, sans-serif; font-size: 10pt; }
+	UL { font-family: Tahoma, Arial, Helvetica, sans-serif; font-size: 10pt; }
+	LI { font-family: Tahoma, Arial, Helvetica, sans-serif; font-size: 10pt; }
+	A  { font-family: Tahoma, Arial, Helvetica, sans-serif; font-size: 10pt; text-decoration: none; color: darkblue; }
+	ADDRESS { font-family: Tahoma, Arial, Helvetica, sans-serif; font-size: 8pt; }
+</STYLE>
+
+<BODY  BGCOLOR="WHITE" TEXT="BLACK" LINK="DARKBLUE" ALINK="RED" VLINK="DARKBLUE">
+<?php 
+    todefnum('Pub');
+    todefnum('Issue');
+    todefnum('Section');
+    todefnum('Article');
+    todefnum('Language');
+    todefnum('sLanguage');
+    todefnum('Image');
+    todef('cDescription');
+    todef('cPhotographer');
+    todef('cPlace');
+    todef('cDate');
+?>
+<TABLE BORDER="0" CELLSPACING="0" CELLPADDING="1" WIDTH="100%">
+	<TR>
+		<TD ROWSPAN="2" WIDTH="1%"><IMG SRC="/priv/img/sign_big.gif" BORDER="0"></TD>
+		<TD>
+		    <DIV STYLE="font-size: 12pt"><B><?php  putGS("Updating image information"); ?></B></DIV>
+		    <HR NOSHADE SIZE="1" COLOR="BLACK">
+		</TD>
+	</TR>
+	<TR><TD ALIGN=RIGHT><TABLE BORDER="0" CELLSPACING="1" CELLPADDING="0"><TR><TD><A HREF="/priv/pub/issues/sections/articles/images/?Pub=<?php  p($Pub); ?>&Issue=<?php  p($Issue); ?>&Article=<?php  p($Article); ?>&Language=<?php  p($Language); ?>&sLanguage=<?php  p($sLanguage); ?>&Section=<?php  p($Section); ?>" ><IMG SRC="/priv/img/tol.gif" BORDER="0" ALT="<?php  putGS("Images"); ?>"></A></TD><TD><A HREF="/priv/pub/issues/sections/articles/images/?Pub=<?php  p($Pub); ?>&Issue=<?php  p($Issue); ?>&Article=<?php  p($Article); ?>&Language=<?php  p($Language); ?>&sLanguage=<?php  p($sLanguage); ?>&Section=<?php  p($Section); ?>" ><B><?php  putGS("Images");  ?></B></A></TD>
+<TD><A HREF="/priv/pub/issues/sections/articles/?Pub=<?php  p($Pub); ?>&Issue=<?php  p($Issue); ?>&Language=<?php  p($Language); ?>&Section=<?php  p($Section); ?>" ><IMG SRC="/priv/img/tol.gif" BORDER="0" ALT="<?php  putGS("Articles"); ?>"></A></TD><TD><A HREF="/priv/pub/issues/sections/articles/?Pub=<?php  p($Pub); ?>&Issue=<?php  p($Issue); ?>&Language=<?php  p($Language); ?>&Section=<?php  p($Section); ?>" ><B><?php  putGS("Articles");  ?></B></A></TD>
+<TD><A HREF="/priv/pub/issues/sections/?Pub=<?php  p($Pub); ?>&Issue=<?php  p($Issue); ?>&Language=<?php  p($Language); ?>" ><IMG SRC="/priv/img/tol.gif" BORDER="0" ALT="<?php  putGS("Sections"); ?>"></A></TD><TD><A HREF="/priv/pub/issues/sections/?Pub=<?php  p($Pub); ?>&Issue=<?php  p($Issue); ?>&Language=<?php  p($Language); ?>" ><B><?php  putGS("Sections");  ?></B></A></TD>
+<TD><A HREF="/priv/pub/issues/?Pub=<?php  p($Pub); ?>" ><IMG SRC="/priv/img/tol.gif" BORDER="0" ALT="<?php  putGS("Issues"); ?>"></A></TD><TD><A HREF="/priv/pub/issues/?Pub=<?php  p($Pub); ?>" ><B><?php  putGS("Issues");  ?></B></A></TD>
+<TD><A HREF="/priv/pub/" ><IMG SRC="/priv/img/tol.gif" BORDER="0" ALT="<?php  putGS("Publications"); ?>"></A></TD><TD><A HREF="/priv/pub/" ><B><?php  putGS("Publications");  ?></B></A></TD>
+<TD><A HREF="/priv/home.php" ><IMG SRC="/priv/img/tol.gif" BORDER="0" ALT="<?php  putGS("Home"); ?>"></A></TD><TD><A HREF="/priv/home.php" ><B><?php  putGS("Home");  ?></B></A></TD>
+<TD><A HREF="/priv/logout.php" ><IMG SRC="/priv/img/tol.gif" BORDER="0" ALT="<?php  putGS("Logout"); ?>"></A></TD><TD><A HREF="/priv/logout.php" ><B><?php  putGS("Logout");  ?></B></A></TD>
+</TR></TABLE></TD></TR>
+</TABLE>
+
+<?php 
+query ("SELECT Description, Photographer, Place, Date, ContentType FROM Images WHERE IdPublication=$Pub AND NrIssue=$Issue AND NrSection=$Section AND NrArticle=$Article AND Number=$Image", 'q_img');
+if ($NUM_ROWS) {
+    query ("SELECT * FROM Articles WHERE IdPublication=$Pub AND NrIssue=$Issue AND NrSection=$Section AND Number=$Article", 'q_art');
+    if ($NUM_ROWS) {
+	query ("SELECT * FROM Sections WHERE IdPublication=$Pub AND NrIssue=$Issue AND IdLanguage=$Language AND Number=$Section", 'q_sect');
+	if ($NUM_ROWS) {
+	    query ("SELECT * FROM Issues WHERE IdPublication=$Pub AND Number=$Issue AND IdLanguage=$Language", 'q_iss');
+	    if ($NUM_ROWS) {
+		query ("SELECT * FROM Publications WHERE Id=$Pub", 'q_pub');
+		if ($NUM_ROWS) {
+		    query ("SELECT Name FROM Languages WHERE Id=$Language", 'q_lang');
+
+		    fetchRow($q_art);
+		    fetchRow($q_sect);
+		    fetchRow($q_iss);
+		    fetchRow($q_pub);
+		    fetchRow($q_lang);
+		    fetchRow($q_img);
+?><TABLE BORDER="0" CELLSPACING="1" CELLPADDING="1" WIDTH="100%"><TR>
+<TD ALIGN="RIGHT" WIDTH="1%" NOWRAP VALIGN="TOP">&nbsp;<?php  putGS("Publication"); ?>:</TD><TD BGCOLOR="#D0D0B0" VALIGN="TOP"><B><?php  pgetHVar($q_pub,'Name'); ?></B></TD>
+
+<TD ALIGN="RIGHT" WIDTH="1%" NOWRAP VALIGN="TOP">&nbsp;<?php  putGS("Issue"); ?>:</TD><TD BGCOLOR="#D0D0B0" VALIGN="TOP"><B><?php  pgetHVar($q_iss,'Number'); ?>. <?php  pgetHVar($q_iss,'Name'); ?> (<?php  pgetHVar($q_lang,'Name'); ?>)</B></TD>
+
+<TD ALIGN="RIGHT" WIDTH="1%" NOWRAP VALIGN="TOP">&nbsp;<?php  putGS("Section"); ?>:</TD><TD BGCOLOR="#D0D0B0" VALIGN="TOP"><B><?php  pgetHVar($q_sect,'Number'); ?>. <?php  pgetHVar($q_sect,'Name'); ?></B></TD>
+
+<TD ALIGN="RIGHT" WIDTH="1%" NOWRAP VALIGN="TOP">&nbsp;<?php  putGS("Article"); ?>:</TD><TD BGCOLOR="#D0D0B0" VALIGN="TOP"><B><?php  pgetHVar($q_art,'Name'); ?></B></TD>
+
+</TR></TABLE>
+
+	<?php  if($xpermrows) {
+		$xaccess=(getvar($XPerm,'ChangeArticle') == "Y");
+		if($xaccess =='') $xaccess = 0;
+	}
+	else $xaccess = 0;
+	?>
+	
+
+<?php 
+    query ("SELECT ($xaccess != 0) or ((".getVar($q_art,'IdUser')." = ".getVar($Usr,'Id').") and ('".getVar($q_art,'Published')."' = 'N'))", 'q_xperm');
+    fetchRowNum($q_xperm);
+    if (getNumVar($q_xperm,0)) {
+	if ($cDescription == "")
+	    $cDescription= "None";
+	    ?><P>
+<CENTER><TABLE BORDER="0" CELLSPACING="0" CELLPADDING="8" BGCOLOR="POST" ALIGN="CENTER">
+	<TR>
+		<TD COLSPAN="2">
+			<B> <font color="do_edit.php"><?php  putGS("Edit image information"); ?> </font></B>
+			<HR NOSHADE SIZE="1" COLOR="BLACK">
+		</TD>
+	</TR>
+	<TR>
+		<TD COLSPAN="2"><BLOCKQUOTE>
+<?php 
+    query ("UPDATE Images SET Description='$cDescription', Photographer='$cPhotographer', Place='$cPlace', Date='$cDate' WHERE IdPublication=$Pub AND NrIssue=$Issue AND NrSection=$Section AND NrArticle=$Article AND Number=$Image");
+    if ($AFFECTED_ROWS > 0) { ?>	<LI><?php  putGS('Image information has been updated.'); ?></LI>
+<?php  $logtext = getGS('Changed image properties of $1',encHTML($cDescription)); query ("INSERT INTO Log SET TStamp=NOW(), IdEvent=43, User='".getVar($Usr,'UName')."', Text='$logtext'"); ?>
+<?php  } else { ?>	<LI><?php  putGS('Image information could not be updated.'); ?></LI>
+<?php  } ?>	</BLOCKQUOTE></TD>
+	</TR>
+	<TR>
+		<TD COLSPAN="2">
+		<DIV ALIGN="CENTER">
+<?php  if ($AFFECTED_ROWS > 0) { ?>		<INPUT TYPE="button" NAME="Done" VALUE="<?php  putGS('Done'); ?>" ONCLICK="location.href='/priv/pub/issues/sections/articles/images/?Pub=<?php  p($Pub); ?>&Issue=<?php  p($Issue); ?>&Article=<?php  p($Article); ?>&Language=<?php  p($Language); ?>&sLanguage=<?php  p($sLanguage); ?>&Section=<?php  p($Section); ?>'">
+<?php  } else { ?>		<INPUT TYPE="button" NAME="OK" VALUE="<?php  putGS('OK'); ?>" ONCLICK="location.href='/priv/pub/issues/sections/articles/images/edit.php?Pub=<?php  p($Pub); ?>&Issue=<?php  p($Issue); ?>&Article=<?php  p($Article); ?>&Language=<?php  p($Language); ?>&sLanguage=<?php  p($sLanguage); ?>&Section=<?php  p($Section); ?>&Image=<?php  p($Image); ?>'">
+<?php  } ?>		</DIV>
+		</TD>
+	</TR>
+</TABLE></CENTER>
+<P>
+<?php  } else { ?>    
+<P>
+<CENTER><TABLE BORDER="0" CELLSPACING="0" CELLPADDING="8" BGCOLOR="#C0D0FF" ALIGN="CENTER">
+	<TR>
+		<TD COLSPAN="2">
+			<B> <font color="red"><?php  putGS("Access denied"); ?> </font></B>
+			<HR NOSHADE SIZE="1" COLOR="BLACK">
+		</TD>
+	</TR>
+	<TR>
+		<TD COLSPAN="2"><BLOCKQUOTE><font color=red><li><?php  putGS("You do not have the right to change this image information.  You may only edit your own articles and once submitted an article can only changed by authorized users." ); ?></li></font></BLOCKQUOTE></TD>
+	</TR>
+	<TR>
+		<TD COLSPAN="2">
+		<DIV ALIGN="CENTER">
+		<A HREF="/priv/pub/issues/sections/articles/images/?Pub=<?php  p($Pub); ?>&Issue=<?php  p($Issue); ?>&Article=<?php  p($Article); ?>&Language=<?php  p($Language); ?>&sLanguage=<?php  p($sLanguage); ?>&Section=<?php  p($Section); ?>"><IMG SRC="/priv/img/button/ok.gif" BORDER="0" ALT="OK"></A>
+		</DIV>
+		</TD>
+	</TR>
+</TABLE></CENTER>
+</FORM>
+<P>
+
+<?php  } ?>
+<?php  } else { ?><BLOCKQUOTE>
+	<LI><?php  putGS('No such publication.'); ?></LI>
+</BLOCKQUOTE>
+<?php  } ?>
+<?php  } else { ?><BLOCKQUOTE>
+	<LI><?php  putGS('No such issue.'); ?></LI>
+</BLOCKQUOTE>
+<?php  } ?>
+<?php  } else { ?><BLOCKQUOTE>
+	<LI><?php  putGS('No such section.'); ?></LI>
+</BLOCKQUOTE>
+<?php  } ?>
+<?php  } else { ?><BLOCKQUOTE>
+	<LI><?php  putGS('No such article.'); ?></LI>
+</BLOCKQUOTE>
+<?php  } ?>
+<?php  } else { ?><BLOCKQUOTE>
+	<LI><?php  putGS('No such image.'); ?></LI>
+</BLOCKQUOTE>
+<?php  } ?>
+<HR NOSHADE SIZE="1" COLOR="BLACK">
+<a STYLE='font-size:8pt;color:#000000' href='http://www.campware.org' target='campware'>CAMPSITE  2.1.5 &copy 1999-2004 MDLF, maintained and distributed under GNU GPL by CAMPWARE</a>
+</BODY>
+<?php  } ?>
+
+</HTML>
+
