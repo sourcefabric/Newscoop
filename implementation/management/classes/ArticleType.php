41d7a0b589d9098ae8496ee3db239c13f5b6a751
paul <paul@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-01-24 03:56:18 +0000

    Replaced single quotes with double quotes

--- implementation/management/classes/ArticleType.php
+++ implementation/management/classes/ArticleType.php
@@ -1,23 +1,23 @@
 <?php
-require_once($_SERVER['DOCUMENT_ROOT']."/classes/DatabaseObject.php");
-require_once($_SERVER['DOCUMENT_ROOT']."/classes/DbColumn.php");
+require_once($_SERVER['DOCUMENT_ROOT'].'/classes/DatabaseObject.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/classes/DbColumn.php');
 
 class ArticleType extends DatabaseObject {
-	var $m_columnNames = array("NrArticle", "IdLanguage");
-	var $m_keyColumnNames = array("NrArticle", "IdLanguage");
+	var $m_columnNames = array('NrArticle', 'IdLanguage');
+	var $m_keyColumnNames = array('NrArticle', 'IdLanguage');
 	var $m_dbTableName;
-	var $m_columnNames = array("NrArticle", "IdLanguage");
+	var $m_columnNames = array('NrArticle', 'IdLanguage');
 	
 	function ArticleType($p_articleType, $p_articleId, $p_languageId) {
-		$this->m_dbTableName = "X".$p_articleType;
+		$this->m_dbTableName = 'X'.$p_articleType;
 		// Get user-defined values.
 		$dbColumns = $this->getUserDefinedColumns();
 		foreach ($dbColumns as $columnMetaData) {
 			$this->m_columnNames[] = $columnMetaData->getName();
 		}
 		parent::DatabaseObject($this->m_columnNames);
-		$this->setProperty("NrArticle", $p_articleId, false);
-		$this->setProperty("IdLanguage", $p_languageId, false);
+		$this->setProperty('NrArticle', $p_articleId, false);
+		$this->setProperty('IdLanguage', $p_languageId, false);
 		if ($this->exists()) {
 			$this->fetch();
 		}
@@ -31,9 +31,9 @@ class ArticleType extends DatabaseObject {
 	 */
 	function getUserDefinedColumns() {
 		global $Campsite;
-		$queryStr = "SHOW COLUMNS FROM ".$this->m_dbTableName
+		$queryStr = 'SHOW COLUMNS FROM '.$this->m_dbTableName
 					." LIKE 'F%'";
-		$queryArray = $Campsite["db"]->GetAll($queryStr);
+		$queryArray = $Campsite['db']->GetAll($queryStr);
 		$metadata = array();
 		foreach ($queryArray as $row) {
 			$columnMetadata =& new DbColumn($this->m_dbTableName);
