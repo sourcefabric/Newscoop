45bbd3f487b41637a85e0f76abc70e8fb06c1468
paul <paul@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-01-24 03:59:02 +0000

    Replaced single quotes with double quotes.

--- campsite/implementation/management/classes/Language.php
+++ campsite/implementation/management/classes/Language.php
@@ -1,16 +1,16 @@
 <?php
-require_once($_SERVER['DOCUMENT_ROOT']."/classes/config.php");
-require_once($_SERVER['DOCUMENT_ROOT']."/classes/DatabaseObject.php");
+require_once($_SERVER['DOCUMENT_ROOT'].'/classes/config.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/classes/DatabaseObject.php');
 
 class Language extends DatabaseObject {
-	var $m_dbTableName = "Languages";
-	var $m_keyColumnNames = array("Id");
+	var $m_dbTableName = 'Languages';
+	var $m_keyColumnNames = array('Id');
 	var $m_keyIsAutoIncrement = true;
-	var $m_columnNames = array("Id", "Name", "Code");
+	var $m_columnNames = array('Id', 'Name', 'Code');
 	
 	function Language($p_languageId = null) {
 		parent::DatabaseObject($this->m_columnNames);
-		$this->setProperty("Id", $p_languageId, false);
+		$this->setProperty('Id', $p_languageId, false);
 		if (!is_null($p_languageId)) {
 			$this->fetch();
 		}
@@ -18,8 +18,8 @@ class Language extends DatabaseObject {
 	
 	function getAllLanguages() {
 		global $Campsite;
-		$queryStr = "SELECT * FROM Languages";
-		$query = $Campsite["db"]->Execute($queryStr);
+		$queryStr = 'SELECT * FROM Languages';
+		$query = $Campsite['db']->Execute($queryStr);
 		$languages = array();
 		while ($row = $query->FetchRow()) {
 			$tmpLanguage =& new Language();
@@ -30,16 +30,16 @@ class Language extends DatabaseObject {
 	} // fn getAllLanguages
 
 	function getLanguageId() {
-		return $this->getProperty("Id");
+		return $this->getProperty('Id');
 	} // fn getLanguageId
 	
 	
 	function getName() {
-		return $this->getProperty("Name");
+		return $this->getProperty('Name');
 	} // fn getName
 	
 	function getCode() {
-		return $this->getProperty("Code");
+		return $this->getProperty('Code');
 	} // fn getCode
 	
 } // class Language
