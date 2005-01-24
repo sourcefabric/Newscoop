052d96e9d257be01d20ce0cb35855daea6b0e50c
paul <paul@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-01-24 03:54:28 +0000

    API for the Log table

---
+++ implementation/management/classes/Log.php
@@ -0,0 +1,31 @@
+<?php
+
+class Log extends DatabaseObject {
+	var $m_keyColumnNames = array('IdEvent');
+	var $m_keyIsAutoIncrement = false;
+	var $m_dbTableName = 'Log';
+	var $m_columnNames = array(
+		'TStamp', 
+		'IdEvent', 
+		'User', 
+		'Text');
+	
+	/**
+	 * This is a static function.
+	 * Write a message to the log table.
+	 *
+	 * @param string p_text
+	 * @param string p_userName
+	 * @param int p_eventId
+	 *
+	 * @return void
+	 */
+	function Message($p_text, $p_userName = '', $p_eventId = 0) {
+		global $Campsite;
+		$queryStr = "INSERT INTO Log SET TStamp=NOW(), IdEvent=$p_eventId, User='$p_userName', Text='$p_text'";
+		$Campsite['db']->Execute($queryStr);
+	} // fn Message
+	
+} // class Log
+
+?>
