21ab82084f3a4c35dba9a39e128042d648f56c31
paul <paul@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-02-20 22:03:04 +0000

    Changed title.  Show the template ID.  Added ability to delete image - but only if no other articles use the image.  Added ability to unlink the image.

--- campsite/implementation/management/pub/issues/sections/articles/images/index.php
+++ campsite/implementation/management/pub/issues/sections/articles/images/index.php
@@ -36,7 +36,7 @@ $languageObj =& new Language($Language);
 <HEAD>
     <META http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	<META HTTP-EQUIV="Expires" CONTENT="now">
-	<TITLE><?php  putGS('Images'); ?></TITLE>
+	<TITLE><?php  putGS('Article Image List'); ?></TITLE>
 	<LINK rel="stylesheet" type="text/css" href="<?php echo $Campsite['website_url'] ?>/css/admin_stylesheet.css">
 </HEAD>
 
@@ -45,7 +45,7 @@ $languageObj =& new Language($Language);
 	<TR>
 		<TD ROWSPAN="2" WIDTH="1%"><IMG SRC="/priv/img/sign_big.gif" BORDER="0"></TD>
 		<TD>
-		    <DIV STYLE="font-size: 12pt"><B><?php putGS('Images'); ?></B></DIV>
+		    <DIV STYLE="font-size: 12pt"><B><?php putGS('Article Image List'); ?></B></DIV>
 		    <HR NOSHADE SIZE="1" COLOR="BLACK">
 		</TD>
 	</TR>
@@ -66,36 +66,75 @@ $languageObj =& new Language($Language);
 <TD ALIGN="RIGHT" WIDTH="1%" NOWRAP VALIGN="TOP">&nbsp;<?php putGS('Section'); ?>:</TD><TD BGCOLOR="#D0D0B0" VALIGN="TOP"><B><?php echo htmlspecialchars($sectionObj->getSectionId()); ?>. <?php echo htmlspecialchars($sectionObj->getName()); ?></B></TD>
 
 <TD ALIGN="RIGHT" WIDTH="1%" NOWRAP VALIGN="TOP">&nbsp;<?php putGS('Article'); ?>:</TD><TD BGCOLOR="#D0D0B0" VALIGN="TOP"><B><?php echo htmlspecialchars($articleObj->getTitle()); ?></B></TD>
-
 </TR></TABLE>
 
 <table>
+<tr>
+	<td>
+		<TABLE BORDER="0" CELLSPACING="0" CELLPADDING="1">
+		<TR>
+			<TD><A HREF="../edit.php?Pub=<?php  p($Pub); ?>&Issue=<?php  p($Issue); ?>&Section=<?php  p($Section); ?>&Article=<?php  p($Article); ?>&Language=<?php  p($Language); ?>&sLanguage=<?php  p($sLanguage); ?>" ><IMG SRC="/priv/img/tol.gif" BORDER="0"></A></TD>
+			<TD><A HREF="../edit.php?Pub=<?php  p($Pub); ?>&Issue=<?php  p($Issue); ?>&Section=<?php  p($Section); ?>&Article=<?php  p($Article); ?>&Language=<?php  p($Language); ?>&sLanguage=<?php  p($sLanguage); ?>" ><B><?php  putGS('Back to article details'); ?></B></A></TD>
+		</TR>
+		</TABLE>
+	</td>
+</tr>
 <?php  if ($User->hasPermission('AddImage')) { ?>
-<tr><td><TABLE BORDER="0" CELLSPACING="0" CELLPADDING="1"><TR><TD><A HREF="add.php?Pub=<?php  p($Pub); ?>&Issue=<?php  p($Issue); ?>&Section=<?php  p($Section); ?>&Article=<?php  p($Article); ?>&Language=<?php  p($Language); ?>&sLanguage=<?php  p($sLanguage); ?>" ><IMG SRC="/priv/img/tol.gif" BORDER="0"></A></TD><TD><A HREF="add.php?Pub=<?php  p($Pub); ?>&Issue=<?php  p($Issue); ?>&Section=<?php  p($Section); ?>&Article=<?php  p($Article); ?>&Language=<?php  p($Language); ?>&sLanguage=<?php  p($sLanguage); ?>" ><B><?php  putGS('Add new image'); ?></B></A></TD></TR></TABLE></td>
-<td><TABLE BORDER="0" CELLSPACING="0" CELLPADDING="1"><TR><TD><A HREF="select.php?Pub=<?php  p($Pub); ?>&Issue=<?php  p($Issue); ?>&Section=<?php  p($Section); ?>&Article=<?php  p($Article); ?>&Language=<?php  p($Language); ?>&sLanguage=<?php  p($sLanguage); ?>" ><IMG SRC="/priv/img/tol.gif" BORDER="0"></A></TD><TD><A HREF="/priv/imagearchive" ><B><?php  putGS('Select an old image (new)'); ?></B></A></TD></TR></TABLE></td>
-<td><TABLE BORDER="0" CELLSPACING="0" CELLPADDING="1"><TR><TD><A HREF="select.php?Pub=<?php  p($Pub); ?>&Issue=<?php  p($Issue); ?>&Section=<?php  p($Section); ?>&Article=<?php  p($Article); ?>&Language=<?php  p($Language); ?>&sLanguage=<?php  p($sLanguage); ?>" ><IMG SRC="/priv/img/tol.gif" BORDER="0"></A></TD><TD><A HREF="select.php?Pub=<?php  p($Pub); ?>&Issue=<?php  p($Issue); ?>&Section=<?php  p($Section); ?>&Article=<?php  p($Article); ?>&Language=<?php  p($Language); ?>&sLanguage=<?php  p($sLanguage); ?>" ><B><?php  putGS('Select an old image'); ?></B></A></TD></TR></TABLE></td></tr>
+<tr>
+	<td>
+		<TABLE BORDER="0" CELLSPACING="0" CELLPADDING="1">
+		<TR>
+			<TD><A HREF="add.php?Pub=<?php  p($Pub); ?>&Issue=<?php  p($Issue); ?>&Section=<?php  p($Section); ?>&Article=<?php  p($Article); ?>&Language=<?php  p($Language); ?>&sLanguage=<?php  p($sLanguage); ?>" ><IMG SRC="/priv/img/tol.gif" BORDER="0"></A></TD>
+			<TD><A HREF="add.php?Pub=<?php  p($Pub); ?>&Issue=<?php  p($Issue); ?>&Section=<?php  p($Section); ?>&Article=<?php  p($Article); ?>&Language=<?php  p($Language); ?>&sLanguage=<?php  p($sLanguage); ?>" ><B><?php  putGS('Add new image'); ?></B></A></TD>
+		</TR>
+		</TABLE>
+	</td>
+	<td>
+		<TABLE BORDER="0" CELLSPACING="0" CELLPADDING="1">
+		<TR>
+			<TD><IMG SRC="/priv/img/tol.gif" BORDER="0"></TD>
+			<TD><?php echo CampsiteInterface::ArticleLink($articleObj, $Language, "images/search.php"); ?><B><?php  putGS('Add an existing image'); ?></B></A></TD>
+		</TR>
+		</TABLE>
+	</td>
+</tr>
 <?php  } ?>
-<tr><td><TABLE BORDER="0" CELLSPACING="0" CELLPADDING="1"><TR><TD><A HREF="../edit.php?Pub=<?php  p($Pub); ?>&Issue=<?php  p($Issue); ?>&Section=<?php  p($Section); ?>&Article=<?php  p($Article); ?>&Language=<?php  p($Language); ?>&sLanguage=<?php  p($sLanguage); ?>" ><IMG SRC="/priv/img/tol.gif" BORDER="0"></A></TD><TD><A HREF="../edit.php?Pub=<?php  p($Pub); ?>&Issue=<?php  p($Issue); ?>&Section=<?php  p($Section); ?>&Article=<?php  p($Article); ?>&Language=<?php  p($Language); ?>&sLanguage=<?php  p($sLanguage); ?>" ><B><?php  putGS('Back to article details'); ?></B></A></TD></TR></TABLE></td></tr>
 </table>
 
 <P>
 <?php 
-$articleImages = ArticleImage::FetchImagesByArticleId($articleObj->getArticleId());
+$articleImages = ArticleImage::GetImagesByArticleId($articleObj->getArticleId());
+if (count($articleImages) <= 0) {
+	?>
+	
+	<table>
+	<tr><td style="padding-top: 10px; padding-bottom: 20px; padding-left: 30px;">
+	There are currently no images associated with this article.<br>
+	Click one of the "Add Image" links above to add one.
+	</td></tr>
+	</table>
+	<?php
+}
+
 if (count($articleImages) > 0) {
-	?><TABLE BORDER="0" CELLSPACING="1" CELLPADDING="0" WIDTH="100%">
+	?><TABLE BORDER="0" CELLSPACING="1" CELLPADDING="3" WIDTH="100%">
 	<TR BGCOLOR="#C0D0FF">
-		<TD ALIGN="LEFT" VALIGN="TOP" WIDTH="1%" ></TD>
-		<TD ALIGN="LEFT" VALIGN="TOP"  ><B><?php  putGS('Click to view image'); ?></B></TD>
-		<TD ALIGN="LEFT" VALIGN="TOP"  ><B><?php  putGS('Photographer'); ?></B></TD>
-		<TD ALIGN="LEFT" VALIGN="TOP"  ><B><?php  putGS('Place'); ?></B></TD>
-		<TD ALIGN="LEFT" VALIGN="TOP"  ><B><?php  putGS('Date<BR><SMALL>(yyyy-mm-dd)</SMALL>'); ?></B></TD>
+		<TD ALIGN="LEFT" VALIGN="TOP" width="1%" nobr><B><?php putGS('Number'); ?></B></TD>
+		<TD ALIGN="LEFT" VALIGN="TOP" width="1%"><B><?php putGS('Thumbnail'); ?></B></TD>
+		<TD ALIGN="LEFT" VALIGN="TOP"><B><?php putGS('Description'); ?></B></TD>
+		<TD ALIGN="LEFT" VALIGN="TOP"><B><?php putGS('Photographer'); ?></B></TD>
+		<TD ALIGN="LEFT" VALIGN="TOP"><B><?php putGS('Place'); ?></B></TD>
+		<TD ALIGN="LEFT" VALIGN="TOP"><B><?php putGS('Date<BR><SMALL>(yyyy-mm-dd)</SMALL>'); ?></B></TD>
 	<?php  if ($User->hasPermission('ChangeImage')) { ?>
-		<TD ALIGN="LEFT" VALIGN="TOP" WIDTH="1%" ><B><?php  putGS('Info'); ?></B></TD>
+		<TD ALIGN="LEFT" VALIGN="TOP" WIDTH="1%" ><B><?php  putGS('Edit'); ?></B></TD>
 	<?php  }
 	    
-	if ($User->hasPermission('DeleteImage')) { ?>
-		<TD ALIGN="LEFT" VALIGN="TOP" WIDTH="1%" ><B><?php  putGS('Unlink'); ?></B></TD>
-	<?php  } ?>
+	if ($User->hasPermission('ChangeArticle')) { ?>
+		<TD ALIGN="LEFT" VALIGN="TOP" WIDTH="1%" ><B><?php putGS('Remove Image From Article'); ?></B></TD>
+		<TD ALIGN="LEFT" VALIGN="TOP" WIDTH="1%" ><B><?php putGS('Delete'); ?></B></TD>
+		<?php  
+	} 
+	?>
 	</TR>
 <?php 
 	$imageCount = 0;
@@ -103,15 +142,18 @@ if (count($articleImages) > 0) {
 		$image =& $articleImage->getImage();
 	?>	
 	<TR <?php  if (($imageCount%2)==0) { ?>BGCOLOR="#D0D0B0"<?php  } else { ?>BGCOLOR="#D0D0D0"<?php  } ?>>
-		<TD ALIGN="RIGHT">
+		<TD ALIGN="center"><?php echo $articleImage->getTemplateId(); ?></td>
+		<TD ALIGN="center">
+			<a href="<?php echo CampsiteInterface::ArticleUrl($articleObj, $Language, "images/view.php") . "&ImageId=".$image->getImageId(); ?>">
 			<?php if (file_exists($image->getThumbnailStorageLocation())) { ?>
-				<img src="<?php echo $image->getThumbnailUrl(); ?>">
+				<img src="<?php echo $image->getThumbnailUrl(); ?>" border="0">
 			<?php } else { ?>
-				<img src="<?php echo $image->getImageUrl(); ?>" width="64" height="64">
+				<img src="<?php echo $image->getImageUrl(); ?>" width="64" height="64" border="0">
 			<?php } ?>				
+			</a>
 		</TD>
 		<TD >
-			<A HREF="/priv/pub/issues/sections/articles/images/view.php?Pub=<?php  p($Pub); ?>&Issue=<?php  p($Issue); ?>&Section=<?php  p($Section); ?>&Article=<?php  p($Article); ?>&Image=<?php echo $image->getImageId(); ?>&Language=<?php  p($Language); ?>&sLanguage=<?php  p($sLanguage); ?>"><?php echo htmlspecialchars($image->getDescription()); ?></A>
+			<a href="<?php echo CampsiteInterface::ArticleUrl($articleObj, $Language, "images/view.php") .'&ImageId='.$image->getImageId(); ?>"><?php echo htmlspecialchars($image->getDescription()); ?></A>
 		</TD>
 		<TD >
 			<?php echo htmlspecialchars($image->getPhotographer()); ?>&nbsp;
@@ -124,14 +166,28 @@ if (count($articleImages) > 0) {
 		</TD>
 	<?php  if ($User->hasPermission('ChangeImage')) { ?>
 		<TD ALIGN="CENTER">
-			<A HREF="/priv/pub/issues/sections/articles/images/edit.php?Pub=<?php  p($Pub); ?>&Issue=<?php  p($Issue); ?>&Section=<?php  p($Section); ?>&Article=<?php  p($Article); ?>&Image=<?php echo $image->getImageId(); ?>&Language=<?php  p($Language); ?>&sLanguage=<?php  p($sLanguage); ?>"><?php  putGS('Change');?></A>
+			<A HREF="/priv/pub/issues/sections/articles/images/edit.php?PublicationId=<?php  p($Pub); ?>&IssueId=<?php  p($Issue); ?>&SectionId=<?php  p($Section); ?>&ArticleId=<?php  p($Article); ?>&ImageId=<?php echo $image->getImageId(); ?>&InterfaceLanguageId=<?php  p($Language); ?>&ArticleLanguageId=<?php  p($sLanguage); ?>&ImageTemplateId=<?php p($articleImage->getTemplateId()); ?>"><?php  putGS('Change');?></A>
 		</TD>
 	<?php  }
 	    if ($User->hasPermission('ChangeArticle')) { ?>
-		<TD ALIGN="CENTER">
-			<A HREF="/priv/pub/issues/sections/articles/images/do_unlink.php?PublicationId=<?php  p($Pub); ?>&IssueId=<?php  p($Issue); ?>&SectionId=<?php  p($Section); ?>&ArticleId=<?php  p($Article); ?>&ImageId=<?php echo $image->getImageId(); ?>&ImageTemplateId=<?php echo $articleImage->getTemplateId(); ?>&LanguageId=<?php  p($Language); ?>&sLanguageId=<?php  p($sLanguage); ?>"><IMG SRC="/priv/img/icon/x.gif" BORDER="0" ALT="<?php  putGS('Unlink image $1', $image->getDescription()); ?>" onclick="return confirm('<?php putGS('Are you sure you want to unlink image \'$1\' from the article?', htmlspecialchars($image->getDescription())); ?>');"></A>
-		</TD>
-	<?php  
+			<TD ALIGN="CENTER">
+				<A HREF="/priv/pub/issues/sections/articles/images/do_unlink.php?PublicationId=<?php  p($Pub); ?>&IssueId=<?php  p($Issue); ?>&SectionId=<?php  p($Section); ?>&ArticleId=<?php  p($Article); ?>&ImageId=<?php echo $image->getImageId(); ?>&ImageTemplateId=<?php echo $articleImage->getTemplateId(); ?>&ArticleLanguageId=<?php  p($Language); ?>&InterfaceLanguageId=<?php  p($sLanguage); ?>" onclick="return confirm('<?php putGS('Are you sure you want to remove the image \\\'$1\\\' from the article?', htmlspecialchars($image->getDescription())); ?>');"><IMG SRC="/priv/img/icon/unlink.gif" BORDER="0" ALT="<?php  putGS('Unlink image $1', $image->getDescription()); ?>"></A>
+			</TD>
+			<td align="center">
+			<?php 
+			if (count(ArticleImage::GetArticlesThatUseImage($image->getImageId())) == 1) {
+				?>
+				<A HREF="/priv/pub/issues/sections/articles/images/do_del.php?PublicationId=<?php  p($Pub); ?>&IssueId=<?php  p($Issue); ?>&SectionId=<?php  p($Section); ?>&ArticleId=<?php  p($Article); ?>&ImageId=<?php echo $image->getImageId(); ?>&ArticleLanguageId=<?php  p($sLanguage); ?>&InterfaceLanguageId=<?php p($Language); ?>" onclick="return confirm('<?php putGS('Are you sure you want to delete the image \\\'$1\\\'?', htmlspecialchars($image->getDescription())); ?>');"><IMG SRC="/priv/img/icon/delete.gif" BORDER="0" ALT="<?php  putGS('Delete image $1', $image->getDescription()); ?>"></A>
+				<?php
+			}
+			else {
+				?>
+				&nbsp;
+				<?php	
+			}
+			?>
+			</td>
+			<?
 	    } ?>
 	</TR>
 <?php 
