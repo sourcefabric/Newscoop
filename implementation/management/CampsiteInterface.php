f813ef465efac08d119aa8bd67ebe835d9227213
paul <paul@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-02-20 21:24:05 +0000

    Added ArticleUrl().  Updated copyright notice.

--- implementation/management/CampsiteInterface.php
+++ implementation/management/CampsiteInterface.php
@@ -11,11 +11,11 @@ class CampsiteInterface {
 	function CopyrightNotice() {
 		global $Campsite;
 		?>
-		<HR NOSHADE SIZE="1" COLOR="BLACK">
-		<a style='font-size:8pt;color:#000000' 
+		<HR NOSHADE SIZE="1">
+		<a style='font-size:8pt;' 
 		   href='http://www.campware.org' 
 		   target='campware'>
-		CAMPSITE  <?php echo $Campsite["version"] ?> &copy 1999-2004 MDLF, 
+		CAMPSITE  <?php echo $Campsite["version"] ?> &copy 1999-2005 MDLF, 
 		maintained and distributed under GNU GPL by CAMPWARE
 		</a>
 		</BODY>
@@ -50,7 +50,7 @@ class CampsiteInterface {
 	} // fn CreateSelect
 	
 	/**
-	 * Create a link to an article.
+	 * Create a HTML HREF link to an article.
 	 *
 	 * @param Article p_articleObj
 	 *		The article we want to display.
@@ -64,11 +64,40 @@ class CampsiteInterface {
 	 * @param string p_backLink
 	 *		I'm not entirely sure what this is for.  I put it in for backward compatibility.
 	 */
-	function ArticleLink($p_articleObj, $p_sectionLanguageId, $p_targetFileName = "", $p_backLink = "") {
-		?>
-		<A HREF="/priv/pub/issues/sections/articles/<?php echo $p_targetFileName ?>?Pub=<?php  echo $p_articleObj->getPublicationId(); ?>&Issue=<?php echo $p_articleObj->getIssueId(); ?>&Section=<?php echo $p_articleObj->getSectionId(); ?>&Article=<?php echo $p_articleObj->getArticleId(); ?>&Language=<?php echo $p_sectionLanguageId; ?>&sLanguage=<?php echo $p_articleObj->getLanguageId(); ?><?php if ($p_backLink != "") { ?>&Back=<?php echo urlencode($p_backLink); } ?>">
-		<?php
+	function ArticleLink($p_articleObj, $p_interfaceLanguageId, $p_targetFileName = "", $p_backLink = "") {
+		$str = '<A HREF="'.CampsiteInterface::ArticleUrl($p_articleObj, $p_interfaceLanguageId, $p_targetFileName, $p_backLink).'">';
+		return $str;
 	} // fn ArticleLink
+	
+	
+	/**
+	 * Create a link to an article.
+	 *
+	 * @param Article p_articleObj
+	 *		The article we want to display.
+	 *
+	 * @param int p_interfaceLanguageId
+	 *		The language ID for the interface language. 
+	 *
+	 * @param string p_targetFileName
+	 *		Which file in the "articles" directory to call.
+	 *
+	 * @param string p_backLink
+	 *		I'm not entirely sure what this is for.  I put it in for backward compatibility.
+	 */
+	function ArticleUrl($p_articleObj, $p_interfaceLanguageId, $p_targetFileName = "", $p_backLink = "") {
+		$str = '/priv/pub/issues/sections/articles/'.$p_targetFileName
+			.'?Pub='.$p_articleObj->getPublicationId()
+			.'&Issue='.$p_articleObj->getIssueId()
+			.'&Section='.$p_articleObj->getSectionId()
+			.'&Article='.$p_articleObj->getArticleId()
+			.'&Language='.$p_interfaceLanguageId
+			.'&sLanguage='.$p_articleObj->getLanguageId(); 
+		if ($p_backLink != "") { 
+			$str .='&Back='.urlencode($p_backLink); 
+		} 
+		return $str;
+	} // fn ArticleUrl
 	
 	
 	/**
