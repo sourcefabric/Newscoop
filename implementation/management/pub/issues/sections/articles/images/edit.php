967be845f166b20074b215948cd1020f6e645a42
paul <paul@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-02-20 21:55:53 +0000

    Added back in the display of the image template ID.  Fixed some formatting issues.  Changed some variable names to make things more readable.

--- implementation/management/pub/issues/sections/articles/images/edit.php
+++ implementation/management/pub/issues/sections/articles/images/edit.php
@@ -14,20 +14,21 @@ if (!$access) {
 	header('Location: /priv/logout.php');
 	exit;
 }
-$Pub = isset($_REQUEST['Pub'])?$_REQUEST['Pub']:0;
-$Issue = isset($_REQUEST['Issue'])?$_REQUEST['Issue']:0;
-$Section = isset($_REQUEST['Section'])?$_REQUEST['Section']:0;
-$Language = isset($_REQUEST['Language'])?$_REQUEST['Language']:0;
-$sLanguage = isset($_REQUEST['sLanguage'])?$_REQUEST['sLanguage']:0;
-$Article = isset($_REQUEST['Article'])?$_REQUEST['Article']:0;
-$Image = isset($_REQUEST['Image'])?$_REQUEST['Image']:0;
+$PublicationId = isset($_REQUEST['PublicationId'])?$_REQUEST['PublicationId']:0;
+$IssueId = isset($_REQUEST['IssueId'])?$_REQUEST['IssueId']:0;
+$SectionId = isset($_REQUEST['SectionId'])?$_REQUEST['SectionId']:0;
+$InterfaceLanguageId = isset($_REQUEST['InterfaceLanguageId'])?$_REQUEST['InterfaceLanguageId']:0;
+$ArticleLanguageId = isset($_REQUEST['ArticleLanguageId'])?$_REQUEST['ArticleLanguageId']:0;
+$ArticleId = isset($_REQUEST['ArticleId'])?$_REQUEST['ArticleId']:0;
+$ImageId = isset($_REQUEST['ImageId'])?$_REQUEST['ImageId']:0;
+$ImageTemplateId = isset($_REQUEST['ImageTemplateId'])?$_REQUEST['ImageTemplateId']:0;
 
-$publicationObj =& new Publication($Pub);
-$issueObj =& new Issue($Pub, $Language, $Issue);
-$sectionObj =& new Section($Pub, $Issue, $Language, $Section);
-$articleObj =& new Article($Pub, $Issue, $Section, $sLanguage, $Article);
-$languageObj =& new Language($Language);
-$imageObj =& new Image($Image);
+$publicationObj =& new Publication($PublicationId);
+$issueObj =& new Issue($PublicationId, $InterfaceLanguageId, $IssueId);
+$sectionObj =& new Section($PublicationId, $IssueId, $InterfaceLanguageId, $SectionId);
+$articleObj =& new Article($PublicationId, $IssueId, $SectionId, $ArticleLanguageId, $ArticleId);
+$languageObj =& new Language($InterfaceLanguageId);
+$imageObj =& new Image($ImageId);
 
 // This file can only be accessed if the user has the right to change articles
 // or the user created this article and it hasnt been published yet.
@@ -57,10 +58,10 @@ if (!$User->hasPermission('ChangeArticle')
 		    <HR NOSHADE SIZE="1" COLOR="BLACK">
 		</TD>
 	</TR>
-	<TR><TD ALIGN=RIGHT><TABLE BORDER="0" CELLSPACING="1" CELLPADDING="0"><TR><TD><A HREF="/priv/pub/issues/sections/articles/images/?Pub=<?php  p($Pub); ?>&Issue=<?php  p($Issue); ?>&Article=<?php  p($Article); ?>&Language=<?php  p($Language); ?>&sLanguage=<?php  p($sLanguage); ?>&Section=<?php  p($Section); ?>" ><IMG SRC="/priv/img/tol.gif" BORDER="0" ALT="<?php  putGS('Images'); ?>"></A></TD><TD><A HREF="/priv/pub/issues/sections/articles/images/?Pub=<?php  p($Pub); ?>&Issue=<?php  p($Issue); ?>&Article=<?php  p($Article); ?>&Language=<?php  p($Language); ?>&sLanguage=<?php  p($sLanguage); ?>&Section=<?php  p($Section); ?>" ><B><?php  putGS('Images');  ?></B></A></TD>
-<TD><A HREF="/priv/pub/issues/sections/articles/?Pub=<?php  p($Pub); ?>&Issue=<?php  p($Issue); ?>&Language=<?php  p($Language); ?>&Section=<?php  p($Section); ?>" ><IMG SRC="/priv/img/tol.gif" BORDER="0" ALT="<?php  putGS("Articles"); ?>"></A></TD><TD><A HREF="/priv/pub/issues/sections/articles/?Pub=<?php  p($Pub); ?>&Issue=<?php  p($Issue); ?>&Language=<?php  p($Language); ?>&Section=<?php  p($Section); ?>" ><B><?php  putGS('Articles');  ?></B></A></TD>
-<TD><A HREF="/priv/pub/issues/sections/?Pub=<?php  p($Pub); ?>&Issue=<?php  p($Issue); ?>&Language=<?php  p($Language); ?>" ><IMG SRC="/priv/img/tol.gif" BORDER="0" ALT="<?php  putGS('Sections'); ?>"></A></TD><TD><A HREF="/priv/pub/issues/sections/?Pub=<?php  p($Pub); ?>&Issue=<?php  p($Issue); ?>&Language=<?php  p($Language); ?>" ><B><?php  putGS('Sections');  ?></B></A></TD>
-<TD><A HREF="/priv/pub/issues/?Pub=<?php  p($Pub); ?>" ><IMG SRC="/priv/img/tol.gif" BORDER="0" ALT="<?php  putGS('Issues'); ?>"></A></TD><TD><A HREF="/priv/pub/issues/?Pub=<?php  p($Pub); ?>" ><B><?php  putGS('Issues');  ?></B></A></TD>
+	<TR><TD ALIGN=RIGHT><TABLE BORDER="0" CELLSPACING="1" CELLPADDING="0"><TR><TD><A HREF="/priv/pub/issues/sections/articles/images/?Pub=<?php  p($PublicationId); ?>&Issue=<?php  p($IssueId); ?>&Article=<?php  p($ArticleId); ?>&Language=<?php  p($InterfaceLanguageId); ?>&sLanguage=<?php  p($ArticleLanguageId); ?>&Section=<?php  p($SectionId); ?>" ><IMG SRC="/priv/img/tol.gif" BORDER="0" ALT="<?php  putGS('Images'); ?>"></A></TD><TD><A HREF="/priv/pub/issues/sections/articles/images/?Pub=<?php  p($PublicationId); ?>&Issue=<?php  p($IssueId); ?>&Article=<?php  p($ArticleId); ?>&Language=<?php  p($InterfaceLanguageId); ?>&sLanguage=<?php  p($ArticleLanguageId); ?>&Section=<?php  p($SectionId); ?>" ><B><?php  putGS('Images');  ?></B></A></TD>
+<TD><A HREF="/priv/pub/issues/sections/articles/?Pub=<?php  p($PublicationId); ?>&Issue=<?php  p($IssueId); ?>&Language=<?php  p($InterfaceLanguageId); ?>&Section=<?php  p($SectionId); ?>" ><IMG SRC="/priv/img/tol.gif" BORDER="0" ALT="<?php  putGS("Articles"); ?>"></A></TD><TD><A HREF="/priv/pub/issues/sections/articles/?Pub=<?php  p($PublicationId); ?>&Issue=<?php  p($IssueId); ?>&Language=<?php  p($InterfaceLanguageId); ?>&Section=<?php  p($SectionId); ?>" ><B><?php  putGS('Articles');  ?></B></A></TD>
+<TD><A HREF="/priv/pub/issues/sections/?Pub=<?php  p($PublicationId); ?>&Issue=<?php  p($IssueId); ?>&Language=<?php  p($InterfaceLanguageId); ?>" ><IMG SRC="/priv/img/tol.gif" BORDER="0" ALT="<?php  putGS('Sections'); ?>"></A></TD><TD><A HREF="/priv/pub/issues/sections/?Pub=<?php  p($PublicationId); ?>&Issue=<?php  p($IssueId); ?>&Language=<?php  p($InterfaceLanguageId); ?>" ><B><?php  putGS('Sections');  ?></B></A></TD>
+<TD><A HREF="/priv/pub/issues/?Pub=<?php  p($PublicationId); ?>" ><IMG SRC="/priv/img/tol.gif" BORDER="0" ALT="<?php  putGS('Issues'); ?>"></A></TD><TD><A HREF="/priv/pub/issues/?Pub=<?php  p($PublicationId); ?>" ><B><?php  putGS('Issues');  ?></B></A></TD>
 <TD><A HREF="/priv/pub/" ><IMG SRC="/priv/img/tol.gif" BORDER="0" ALT="<?php  putGS('Publications'); ?>"></A></TD><TD><A HREF="/priv/pub/" ><B><?php  putGS('Publications');  ?></B></A></TD>
 <TD><A HREF="/priv/home.php" ><IMG SRC="/priv/img/tol.gif" BORDER="0" ALT="<?php  putGS('Home'); ?>"></A></TD><TD><A HREF="/priv/home.php" ><B><?php  putGS('Home');  ?></B></A></TD>
 <TD><A HREF="/priv/logout.php" ><IMG SRC="/priv/img/tol.gif" BORDER="0" ALT="<?php  putGS('Logout'); ?>"></A></TD><TD><A HREF="/priv/logout.php" ><B><?php  putGS('Logout');  ?></B></A></TD>
@@ -88,6 +89,12 @@ if (!$User->hasPermission('ChangeArticle')
 		</TD>
 	</TR>
 	<TR>
+		<TD ALIGN="RIGHT" ><?php  putGS('Number'); ?>:</TD>
+		<TD>
+		<INPUT TYPE="TEXT" NAME="cNumber" VALUE="<?php echo $ImageTemplateId; ?>" class="input_text" SIZE="32" MAXLENGTH="10">
+		</TD>
+	</TR>
+	<TR>
 		<TD ALIGN="RIGHT" ><?php  putGS('Description'); ?>:</TD>
 		<TD>
 		<INPUT TYPE="TEXT" NAME="cDescription" VALUE="<?php echo htmlspecialchars($imageObj->getDescription()); ?>" class="input_text" SIZE="32" MAXLENGTH="128">
@@ -108,21 +115,21 @@ if (!$User->hasPermission('ChangeArticle')
 	<TR>
 		<TD ALIGN="RIGHT" ><?php  putGS('Date'); ?>:</TD>
 		<TD>
-		<INPUT TYPE="TEXT" NAME="cDate" VALUE="<?php echo htmlspecialchars($imageObj->getDate()); ?>" class="input_text" SIZE="10" MAXLENGTH="10"><?php putGS('YYYY-MM-DD'); ?>
+		<INPUT TYPE="TEXT" NAME="cDate" VALUE="<?php echo htmlspecialchars($imageObj->getDate()); ?>" class="input_text" SIZE="11" MAXLENGTH="10"> <?php putGS('YYYY-MM-DD'); ?>
 		</TD>
 	</TR>
 	<TR>
 		<TD COLSPAN="2">
 		<DIV ALIGN="CENTER">
-	    <INPUT TYPE="HIDDEN" NAME="Pub" VALUE="<?php  p($Pub); ?>">
-	    <INPUT TYPE="HIDDEN" NAME="Issue" VALUE="<?php  p($Issue); ?>">
-	    <INPUT TYPE="HIDDEN" NAME="Section" VALUE="<?php  p($Section); ?>">
-	    <INPUT TYPE="HIDDEN" NAME="Article" VALUE="<?php  p($Article); ?>">
-	    <INPUT TYPE="HIDDEN" NAME="Language" VALUE="<?php  p($Language); ?>">
-	    <INPUT TYPE="HIDDEN" NAME="sLanguage" VALUE="<?php  p($sLanguage); ?>">
-	    <INPUT TYPE="HIDDEN" NAME="Image" VALUE="<?php  p($Image); ?>">
+	    <INPUT TYPE="HIDDEN" NAME="Pub" VALUE="<?php  p($PublicationId); ?>">
+	    <INPUT TYPE="HIDDEN" NAME="Issue" VALUE="<?php  p($IssueId); ?>">
+	    <INPUT TYPE="HIDDEN" NAME="Section" VALUE="<?php  p($SectionId); ?>">
+	    <INPUT TYPE="HIDDEN" NAME="Article" VALUE="<?php  p($ArticleId); ?>">
+	    <INPUT TYPE="HIDDEN" NAME="Language" VALUE="<?php  p($InterfaceLanguageId); ?>">
+	    <INPUT TYPE="HIDDEN" NAME="sLanguage" VALUE="<?php  p($ArticleLanguageId); ?>">
+	    <INPUT TYPE="HIDDEN" NAME="Image" VALUE="<?php  p($ImageId); ?>">
 		<INPUT TYPE="submit" NAME="Save" VALUE="<?php  putGS('Save changes'); ?>" class="button">
-		<INPUT TYPE="button" NAME="Cancel" VALUE="<?php  putGS('Cancel'); ?>" class="button" ONCLICK="location.href='/priv/pub/issues/sections/articles/images/?Pub=<?php  p($Pub); ?>&Issue=<?php  p($Issue); ?>&Article=<?php  p($Article); ?>&Language=<?php  p($Language); ?>&sLanguage=<?php  p($sLanguage); ?>&Section=<?php  p($Section); ?>'">
+		<INPUT TYPE="button" NAME="Cancel" VALUE="<?php  putGS('Cancel'); ?>" class="button" ONCLICK="location.href='/priv/pub/issues/sections/articles/images/?Pub=<?php  p($PublicationId); ?>&Issue=<?php  p($IssueId); ?>&Article=<?php  p($ArticleId); ?>&Language=<?php  p($InterfaceLanguageId); ?>&sLanguage=<?php  p($ArticleLanguageId); ?>&Section=<?php  p($SectionId); ?>'">
 		</DIV>
 		</TD>
 	</TR>
