bde7ec1ced5a1b30a7c8fccf0532aef8883ee26a
paul <paul@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-02-21 18:16:36 +0000

    Use ImageNav class for navigation.  Replaced icons.  Removed use of CAMPSITE_IMAGEARCHIVE_DIR.  Added ability to search by UploadedBy, Last Modified, TimeCreated.

--- campsite/implementation/management/imagearchive/index.php
+++ campsite/implementation/management/imagearchive/index.php
@@ -2,6 +2,7 @@
 require_once($_SERVER['DOCUMENT_ROOT'].'/classes/common.php');
 load_common_include_files();
 require_once($_SERVER['DOCUMENT_ROOT'].'/classes/Image.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/classes/ImageSearch.php');
 require_once($_SERVER['DOCUMENT_ROOT'].'/priv/CampsiteInterface.php');
 require_once($_SERVER['DOCUMENT_ROOT'].'/priv/imagearchive/include.inc.php');
 
@@ -21,44 +22,33 @@ $SearchPhotographer = array_get_value($_REQUEST, 'search_photographer', '');
 $SearchPlace = array_get_value($_REQUEST, 'search_place', '');
 $SearchDate = array_get_value($_REQUEST, 'search_date', '');
 $SearchInUse = array_get_value($_REQUEST, 'search_inuse', '');
+$SearchUploadedBy = array_get_value($_REQUEST, 'search_uploadedby', '');
 
-$searchKeywords = array('search_description' => $SearchDescription,
-						'search_photographer' => $SearchPhotographer,
-						'search_place' => $SearchPlace,
-						'search_date' => $SearchDate,
-						'search_inuse' => $SearchInUse);
-						
-$Link = CreateImageLinks($searchKeywords, $OrderBy, $OrderDirection, $ImageOffset, $ImagesPerPage, $view);
+$imageNav =& new ImageNav($_REQUEST, CAMPSITE_IMAGEARCHIVE_IMAGES_PER_PAGE, $view);
 
 ///////////////////////////////////////////////////////////////////////
 
 // build the links for ordering (search results) //////////////////////
 if ($OrderDirection == 'DESC') {
 	$ReverseOrderDirection = "ASC";
-	$OrderSign = '<img src="/priv/img/down.png" border="0">';
+	$OrderSign = '<img src="/priv/img/icon/search_order_direction_down.gif" border="0">';
 } else {
 	$ReverseOrderDirection = "DESC";
-	$OrderSign = '<img src="/priv/img/up.png" border="0">';
+	$OrderSign = '<img src="/priv/img/icon/search_order_direction_up.gif" border="0">';
 }
 
-$IdHref  = CAMPSITE_IMAGEARCHIVE_DIR
-	.'?order_by=id'
-	.$Link['search'];
-$DescriptionHref  = CAMPSITE_IMAGEARCHIVE_DIR
-	.'?order_by=description'
-	.$Link['search'];
-$PhotographerHref  = CAMPSITE_IMAGEARCHIVE_DIR
-	.'?order_by=photographer'
-	.$Link['search'];
-$PlaceHref  = CAMPSITE_IMAGEARCHIVE_DIR
-	.'?order_by=place'
-	.$Link['search'];
-$DateHref  = CAMPSITE_IMAGEARCHIVE_DIR
-	.'?order_by=date'
-	.$Link['search'];
-$InUseHref = CAMPSITE_IMAGEARCHIVE_DIR
-	.'?order_by=inuse'
-	.$Link['search'];
+$IdHref  = 'index.php?order_by=id'
+	.$imageNav->getKeywordSearchLink();
+$DescriptionHref  = 'index.php?order_by=description'
+	.$imageNav->getKeywordSearchLink();
+$PhotographerHref  = 'index.php?order_by=photographer'
+	.$imageNav->getKeywordSearchLink();
+$PlaceHref  = 'index.php?order_by=place'
+	.$imageNav->getKeywordSearchLink();
+$DateHref  = 'index.php?order_by=date'
+	.$imageNav->getKeywordSearchLink();
+$InUseHref = 'index.php?order_by=inuse'
+	.$imageNav->getKeywordSearchLink();
 ///////////////////////////////////////////////////////////////////////
 $DescriptionOrderIcon = '';
 $PhotographerOrderIcon = '';
@@ -69,55 +59,37 @@ $IdOrderIcon = '';
 switch ($OrderBy) {
 case 'description':
 	$DescriptionOrderIcon = $OrderSign;
-	$DescriptionHref = CAMPSITE_IMAGEARCHIVE_DIR
-		.'?order_by=description'
-		.'&order_direction='.$ReverseOrderDirection
-		.$Link['search'];
+	$DescriptionHref .= '&order_direction='.$ReverseOrderDirection;
 	break;
 case 'photographer':
 	$PhotographerOrderIcon = $OrderSign;
-	$PhotographerHref = CAMPSITE_IMAGEARCHIVE_DIR
-		.'?order_by=photographer'
-		.'&order_direction='.$ReverseOrderDirection
-		.$Link['search'];
+	$PhotographerHref .= '&order_direction='.$ReverseOrderDirection;
 	break;
 case 'place':
 	$PlaceOrderIcon = $OrderSign;
-	$PlaceHref = CAMPSITE_IMAGEARCHIVE_DIR
-		.'?order_by=place'
-		.'&order_direction='.$ReverseOrderDirection
-		.$Link['search'];
+	$PlaceHref .= '&order_direction='.$ReverseOrderDirection;
 	break;
 case 'date':
 	$DateOrderIcon = $OrderSign;
-	$DateHref = CAMPSITE_IMAGEARCHIVE_DIR
-		.'?order_by=date'
-		.'&order_direction='.$ReverseOrderDirection
-		.$Link['search'];
+	$DateHref .= '&order_direction='.$ReverseOrderDirection;
 	break;
 case 'inuse':
 	$InUseOrderIcon = $OrderSign;
-	$InUseHref = CAMPSITE_IMAGEARCHIVE_DIR
-		.'?order_by=inuse'
-		.'&order_direction='.$ReverseOrderDirection
-		.$Link['search'];
+	$InUseHref .= '&order_direction='.$ReverseOrderDirection;
 	break;
 case 'id':
-default:
 	$IdOrderIcon = $OrderSign;
-	$IdHref = CAMPSITE_IMAGEARCHIVE_DIR
-		.'?order_by=id'
-		.'&order_direction='.$ReverseOrderDirection
-		.$Link['search'];
+	$IdHref .= '&order_direction='.$ReverseOrderDirection;
 	break;
 }
 ///////////////////////////////////////////////////////////////////////
 
 $TotalImages = Image::GetTotalImages();
-$imageSearch =& new ImageSearch($_REQUEST);
+$imageSearch =& new ImageSearch($_REQUEST, CAMPSITE_IMAGEARCHIVE_IMAGES_PER_PAGE);
 $imageSearch->run();
 $imageData =& $imageSearch->getImages();
 $NumImagesFound = $imageSearch->getNumImagesFound();
+$uploadedByUsers =& Image::GetUploadUsers();
 ?>
 
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN"
@@ -136,7 +108,7 @@ $NumImagesFound = $imageSearch->getNumImagesFound();
 	<TR>
 		<TD ROWSPAN="2" WIDTH="1%"><IMG SRC="/priv/img/sign_big.gif" BORDER="0"></TD>
 		<TD>
-			<DIV STYLE="font-size: 12pt"><B><?php putGS('Image Archive'); ?></B></DIV>
+			<DIV STYLE="font-size: 12pt"><B><?php putGS('Image archive'); ?></B></DIV>
 			<HR NOSHADE SIZE="1" COLOR="BLACK">
 		</TD>
 	</TR>
@@ -154,21 +126,21 @@ $NumImagesFound = $imageSearch->getNumImagesFound();
   <tr>
 <?php
 if ($User->hasPermission('AddImage')) { ?>
-    <td><A HREF="<?php echo CAMPSITE_IMAGEARCHIVE_DIR; ?>add.php?<?php echo Image_GetSearchUrl($_REQUEST); ?>"><IMG SRC="/priv/img/tol.gif" BORDER="0" alt="<?php  putGS('Add new image'); ?>"></A></TD><TD><A HREF="<?php echo CAMPSITE_IMAGEARCHIVE_DIR; ?>add.php?<?php echo Image_GetSearchUrl($_REQUEST); ?>"><B><?php  putGS('Add new image'); ?></B></A></TD>
+    <td><A HREF="add.php?<?php echo $imageNav->getSearchLink(); ?>"><IMG SRC="/priv/img/tol.gif" BORDER="0" alt="<?php  putGS('Add new image'); ?>"></A></TD><TD><A HREF="add.php?<?php echo $imageNav->getSearchLink(); ?>"><B><?php  putGS('Add new image'); ?></B></A></TD>
 <?php } ?>
     
-    <td><a href="<?php echo CAMPSITE_IMAGEARCHIVE_DIR; ?>index.php?view=<?php echo $view; ?>"><IMG SRC="/priv/img/tol.gif" BORDER="0" ALT="<?php putGS("Reset search conditions"); ?>"></a></td><td><a href="<?php echo CAMPSITE_IMAGEARCHIVE_DIR; ?>index.php?view=<?php echo $view; ?>"><b><?php echo putGS('Reset search conditions'); ?></b></a></td>
+    <td><a href="index.php?view=<?php echo $view; ?>"><IMG SRC="/priv/img/tol.gif" BORDER="0" ALT="<?php putGS("Reset search conditions"); ?>"></a></td><td><a href="index.php?view=<?php echo $view; ?>"><b><?php echo putGS('Reset search conditions'); ?></b></a></td>
 
     <td>&nbsp;&nbsp;&nbsp;&nbsp;</td>
     <td><B><?php putGS('View', 'View'); ?>:</b></td>
-    <td><A HREF="<?php echo CAMPSITE_IMAGEARCHIVE_DIR; ?>index.php?<?php echo $Link['search'].$Link['order_by']; ?>&view=thumbnail"><IMG SRC="/priv/img/tol.gif" BORDER="0" alt="<?php  putGS('Thumbnail'); ?>"></A></TD><TD><A HREF="index.php?<?php echo $Link['search'].$Link['order_by']; ?>&view=thumbnail"><B><?php putGS('Thumbnail'); ?></B></A></TD>
-    <td><A HREF="<?php echo CAMPSITE_IMAGEARCHIVE_DIR; ?>index.php?<?php echo $Link['search'].$Link['order_by']; ?>&view=gallery"><IMG SRC="/priv/img/tol.gif" BORDER="0" alt="<?php  putGS('Gallery'); ?>"></A></TD><TD><A HREF="index.php?<?php echo $Link['search'].$Link['order_by']; ?>&view=gallery"><B><?php putGS('Gallery'); ?></B></A></TD>
-    <td><A HREF="<?php echo CAMPSITE_IMAGEARCHIVE_DIR; ?>index.php?<?php echo $Link['search'].$Link['order_by']; ?>&view=flat"><IMG SRC="/priv/img/tol.gif" BORDER="0" alt="<?php  putGS('Text only'); ?>"></A></TD><TD><A HREF="index.php?<?php echo $Link['search'].$Link['order_by']; ?>&view=flat"><B><?php  putGS('Text only'); ?></B></A></TD>
+    <td><A HREF="index.php?<?php echo $imageNav->getSearchLink(); ?>&view=thumbnail"><IMG SRC="/priv/img/tol.gif" BORDER="0" alt="<?php  putGS('Thumbnail'); ?>"></A></TD><TD><A HREF="index.php?<?php echo $imageNav->getSearchLink(); ?>&view=thumbnail"><B><?php putGS('Thumbnail'); ?></B></A></TD>
+    <td><A HREF="index.php?<?php echo $imageNav->getSearchLink(); ?>&view=gallery"><IMG SRC="/priv/img/tol.gif" BORDER="0" alt="<?php  putGS('Gallery'); ?>"></A></TD><TD><A HREF="index.php?<?php echo $imageNav->getSearchLink(); ?>&view=gallery"><B><?php putGS('Gallery'); ?></B></A></TD>
+    <td><A HREF="index.php?<?php echo $imageNav->getSearchLink(); ?>&view=flat"><IMG SRC="/priv/img/tol.gif" BORDER="0" alt="<?php  putGS('Text only'); ?>"></A></TD><TD><A HREF="index.php?<?php echo $imageNav->getSearchLink(); ?>&view=flat"><B><?php  putGS('Text only'); ?></B></A></TD>
   </tr>
 </table>
 
 <TABLE BORDER="0" CELLSPACING="0" CELLPADDING="3" style="border: 1px solid #00008b; margin-bottom: 10px; margin-top: 5px;" align="center">
-<form method="GET" action="index.php">
+<form method="POST" action="index.php">
 <input type="hidden" name="order_by" value="<?php echo $OrderBy; ?>">
 <input type="hidden" name="order_direction" value="<?php echo $OrderDirection; ?>">
 <input type="hidden" name="view" value="<?php echo $view; ?>">
@@ -176,14 +148,37 @@ if ($User->hasPermission('AddImage')) { ?>
 <tr BGCOLOR="#C0D0FF">
 	<td style="padding-left: 10px; color: #00008b;"><?php putGS('Description')?>:</td>
 	<td><input type="text" name="search_description" value="<?php echo $SearchDescription; ?>" style="border: 1px solid #00008b; background-color: #f0f0ff; text-indent: 3px; width: 150px;"></td>
-	<td style=" color: #00008b;"><?php putGS('Photographer'); ?>:</td>
+	<td style="color: #00008b;"><?php putGS('Photographer'); ?>:</td>
 	<td><input type="text" name="search_photographer" value="<?php echo $SearchPhotographer; ?>"  style="border: 1px solid #00008b; background-color: #f0f0ff; text-indent: 3px; width: 100px;"></td>
-	<td style=" color: #00008b;"><?php putGS('Place'); ?>:</td>
+	<td style="color: #00008b;"><?php putGS('Place'); ?>:</td>
 	<td><input type="text" name="search_place" value="<?php echo $SearchPlace; ?>" style="border: 1px solid #00008b; background-color: #f0f0ff; text-indent: 3px; width: 100px;"></td>
-	<td style=" color: #00008b;"><?php putGS('Date'); ?>:</td>
+	<td style="color: #00008b;"><?php putGS('Date'); ?>:</td>
 	<td><input type="text" name="search_date" value="<?php echo $SearchDate; ?>" style="border: 1px solid #00008b; background-color: #f0f0ff; text-indent: 3px; width: 80px;"></td>
+	<td style="color: #00008b;" nowrap>Uploaded by:</td>
+	<td>
+		<select name="search_uploadedby" class="input_select" style="width: 100px;">
+		<option value="0"></option>
+		<?php 
+		foreach ($uploadedByUsers as $tmpUser) {
+			?>
+			<option value="<?php echo $tmpUser->getId(); ?>" <?php if ($tmpUser->getId() == $SearchUploadedBy)  { echo "selected"; } ?>><?php echo htmlspecialchars($tmpUser->getName()); ?></option>
+			<?php
+		}
+		?>
+		</select>
+	</td>
 	<td><input type="submit" name="submit_button" value="Search" class="button"></td>
 </tr>
+<tr BGCOLOR="#C0D0FF">
+	<td colspan="11" align="center" >
+		Additional searches: &nbsp;
+		<a href="index.php?<?php $imageNav->getSearchLink() ?>&order_by=time_created" style="font-size: 9pt; font-weight: bold; text-decoration: underline;">Most Recently Added</a><?php if ($OrderBy == "time_created") { echo "*"; } ?>
+		&nbsp;
+		<a href="index.php?<?php $imageNav->getSearchLink() ?>&order_by=last_modified" style="font-size: 9pt; font-weight: bold; text-decoration: underline;">Most Recently Modified</a><?php if ($OrderBy == "last_modified") { echo "*"; } ?>
+	</td>
+	
+</tr>
+</form>
 </table>
 
 <?php
