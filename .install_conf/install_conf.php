366ec0471d0f94e5b521ea85ab91d7a85e9cf3ea
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-02-25 18:26:16 +0000

    new files needed for feature 247: Improve management of multiple instances of CAMPSITE on a single server

---
+++ .install_conf/install_conf.php
@@ -0,0 +1,15 @@
+<?php
+
+$Campsite['APACHE_USER'] = 'apache';
+$Campsite['APACHE_GROUP'] = 'apache';
+$Campsite['CAMPSITE_DIR'] = '/usr/local/campsite';
+$Campsite['BIN_DIR'] = '/usr/local/campsite/bin';
+$Campsite['ETC_DIR'] = '/usr/local/campsite/etc';
+$Campsite['WWW_DIR'] = '/usr/local/campsite/www';
+$Campsite['WWW_COMMON_DIR'] = '/usr/local/campsite/www-common';
+
+$campsite_local_vars['install'] = array("APACHE_USER", "APACHE_GROUP", "CAMPSITE_DIR",
+	"BIN_DIR", "ETC_DIR", "WWW_DIR", "WWW_COMMON_DIR");
+
+?>
+
