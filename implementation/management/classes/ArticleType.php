f526ff147e3f46d924216fc165d451f710905ff3
paul <paul@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-02-28 06:37:30 +0000

    Added copy(), copyToExistingRecord(), GetAllTypes()

--- implementation/management/classes/ArticleType.php
+++ implementation/management/classes/ArticleType.php
@@ -6,8 +6,13 @@ class ArticleType extends DatabaseObject {
 	var $m_columnNames = array('NrArticle', 'IdLanguage');
 	var $m_keyColumnNames = array('NrArticle', 'IdLanguage');
 	var $m_dbTableName;
-	var $m_columnNames = array('NrArticle', 'IdLanguage');
 	
+	/**
+	 * An 
+	 * @param string p_articleType
+	 * @param int p_articleId
+	 * @param int p_languageId
+	 */
 	function ArticleType($p_articleType, $p_articleId, $p_languageId) {
 		$this->m_dbTableName = 'X'.$p_articleType;
 		// Get user-defined values.
@@ -22,6 +27,46 @@ class ArticleType extends DatabaseObject {
 			$this->fetch();
 		}
 	} // constructor
+	
+	
+	/**
+	 * Copy the row in the database.
+	 * @param int p_destArticleId
+	 * @return void
+	 */
+	function copy($p_destArticleId) {
+		global $Campsite;
+		$tmpData = $this->m_data;
+		unset($tmpData['NrArticle']);
+		foreach ($tmpData as $key => $data) {
+			$tmpData[$key] = "'".$data."'";
+		}
+		
+		$queryStr = 'INSERT IGNORE INTO '.$this->m_dbTableName
+			.'(NrArticle,'.implode(',', array_keys($this->m_columnNames)).')'
+			.' VALUES ('.$p_destArticleId.','.implode(',', $tmpData).')';
+		$Campsite['db']->Execute($queryStr);
+	} // fn copy
+	
+	
+	/**
+	 * Copy the row in the database.
+	 * @param int p_destArticleId
+	 * @return void
+	 */
+	function copyToExistingRecord($p_destArticleId) {
+		global $Campsite;
+		$tmpData = $this->m_data;
+		unset($tmpData['NrArticle']);
+		unset($tmpData['IdLanguage']);
+		$setQuery = array();
+		foreach ($tmpData as $key => $data) {
+			$setQuery[] = $key."='".addslashes($data)."'";
+		}		
+		$queryStr = 'UPDATE '.$this->m_dbTableName.' SET '.implode(',', $setQuery)
+				." WHERE NrArticle=$p_destArticleId AND IdLanguage=".$this->m_data['IdLanguage'];
+		$Campsite['db']->Execute($queryStr);
+	} // fn copyToExistingRecord
 	
 	
 	/**
@@ -44,7 +89,22 @@ class ArticleType extends DatabaseObject {
 		}
 		return $metadata;
 	} // fn getUserDefinedColumns
-		
+
+	
+	function GetAllTypes() {
+		global $Campsite;
+		$queryStr = "SHOW TABLES LIKE 'X%'";
+		$tableNames = $Campsite['db']->GetCol($queryStr);
+		if (!is_array($tableNames)) {
+			$tableNames = array();
+		}
+		$finalNames = array();
+		foreach ($tableNames as $tmpName) {
+			$finalNames[] = substr($tmpName, 1);
+		}
+		return $finalNames;
+	} // fn GetAllTypes
+	
 } // class ArticleType
 
 ?>
