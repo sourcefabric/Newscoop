367ada0e77552dce693706eab8c3c8050edb3889
paul <paul@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-02-28 06:03:00 +0000

    Change the order of the articles.

---
+++ implementation/management/pub/issues/sections/articles/do_move.php
@@ -0,0 +1,79 @@
+<?php
+require_once($_SERVER['DOCUMENT_ROOT']. "/priv/pub/issues/sections/articles/article_common.php");
+require_once($_SERVER['DOCUMENT_ROOT']."/classes/Log.php");
+
+// Check permissions
+list($access, $User) = check_basic_access($_REQUEST);
+if (!$access) {
+	header("Location: /priv/logout.php");
+	exit;
+}
+if (!$User->hasPermission('Publish')) {
+	header("Location: /priv/ad.php?ADReason=".urlencode(getGS("You do not have the right to change this article.  You may only edit your own articles and once submitted an article can only changed by authorized users." )));
+	exit;
+}
+
+// Get input
+$Pub = Input::get('Pub', 'int', 0);
+$Issue = Input::get('Issue', 'int', 0);
+$Section = Input::get('Section', 'int', 0);
+$Language = Input::get('Language', 'int', 0);
+$sLanguage = Input::get('sLanguage', 'int', 0);
+$Article = Input::get('Article', 'int', 0);
+$ArticleOffset = Input::get('ArtOffs', 'int', 0, true);
+$MoveType = Input::get('move', 'string', 'up_rel');
+$MoveToPosition = Input::get('pos', 'int', 1, true);
+
+if (!Input::isValid()) {
+	header("Location: /priv/logout.php");
+	exit;	
+}
+
+
+$publicationObj =& new Publication($Pub);
+if (!$publicationObj->exists()) {
+	header("Location: /priv/ad.php?ADReason=".urlencode(getGS('Publication does not exist.')));
+	exit;	
+}
+
+$issueObj =& new Issue($Pub, $Language, $Issue);
+if (!$issueObj->exists()) {
+	header("Location: /priv/ad.php?ADReason=".urlencode(getGS('Issue does not exist.')));
+	exit;	
+}
+
+$sectionObj =& new Section($Pub, $Issue, $Language, $Section);
+if (!$sectionObj->exists()) {
+	header("Location: /priv/ad.php?ADReason=".urlencode(getGS('Section does not exist.')));
+	exit;	
+}
+
+$languageObj =& new Language($Language);
+
+$articleObj =& new Article($Pub, $Issue, $Section, $sLanguage, $Article);
+if (!$articleObj->exists()) {
+	header("Location: /priv/ad.php?ADReason=".urlencode(getGS('Article does not exist.')));
+	exit;	
+}
+
+switch ($MoveType) {
+case 'up_rel':
+	$articleObj->moveRelative('up');
+	break;
+case 'down_rel':
+	$articleObj->moveRelative('down');
+	break;
+case 'abs':
+	$articleObj->moveAbsolute($MoveToPosition);
+	break;
+default: ;
+}
+
+## added by sebastian
+if (function_exists ("incModFile")) {
+	incModFile();
+}
+
+header("Location: ".CampsiteInterface::ArticleUrl($articleObj, $Language, "index.php")."&ArtOffs=".$ArticleOffset);
+exit;
+?>
