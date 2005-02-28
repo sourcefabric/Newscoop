03066f4131515c2f83e275bca851343aac5bb9de
paul <paul@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-02-28 06:29:30 +0000

    Initial checkin

---
+++ campsite/implementation/management/classes/ArticleIndex.php
@@ -0,0 +1,53 @@
+<?
+require_once($_SERVER['DOCUMENT_ROOT'].'/classes/DatabaseObject.php');
+
+class ArticleIndex extends DatabaseObject {
+	var $m_keyColumnNames = array(
+		'IdPublication',
+		'IdLanguage',
+		'IdKeyword',
+		'NrIssue',
+		'NrSection',
+		'NrArticle');
+	var $m_dbTableName = 'ArticleIndex';
+	var $m_columnNames = array(
+		'IdPublication',
+		'IdLanguage',
+		'IdKeyword',
+		'NrIssue',
+		'NrSection',
+		'NrArticle');
+	
+	function ArticleIndex() { }
+	
+	/**
+	 * @return int
+	 */
+	function getArticleId() {
+		return $this->getProperty('NrArticle');
+	} // fn getArticleId
+
+	
+	/**
+	 * Remove index pointers for the given article.
+	 * @param int p_publicationId
+	 * @param int p_issueId
+	 * @param int p_sectionId
+	 * @param int p_languageId
+	 * @param int p_articleId
+	 * @return void
+	 */
+	function OnArticleDelete($p_publicationId, $p_issueId, $p_sectionId, $p_languageId, $p_articleId) {
+		global $Campsite;
+		$queryStr = 'DELETE FROM ArticleIndex'
+					." WHERE IdPublication=$p_publicationId "
+					." AND NrIssue=$p_issueId "
+					." AND NrSection=$p_sectionId "
+					." AND NrArticle=$p_articleId "
+					." AND IdLanguage=$p_languageId";
+		$Campsite['db']->Execute($queryStr);		
+	} // fn OnArticleDelete
+	
+} // class ArticleIndex
+
+?>
