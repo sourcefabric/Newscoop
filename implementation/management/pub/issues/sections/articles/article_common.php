9bf47e4862f75cfb0c5b17cb64210d81f501e0bd
paul <paul@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-02-28 06:04:14 +0000

    Added Input.php to the list of includes

--- implementation/management/pub/issues/sections/articles/article_common.php
+++ implementation/management/pub/issues/sections/articles/article_common.php
@@ -8,6 +8,7 @@ require_once($_SERVER['DOCUMENT_ROOT']."/classes/Publication.php");
 require_once($_SERVER['DOCUMENT_ROOT']."/classes/Language.php");
 require_once($_SERVER['DOCUMENT_ROOT']."/classes/User.php");
 require_once($_SERVER['DOCUMENT_ROOT']."/classes/Template.php");
+require_once($_SERVER['DOCUMENT_ROOT']."/classes/Input.php");
 require_once($_SERVER['DOCUMENT_ROOT']."/priv/CampsiteInterface.php");
 require_once($_SERVER['DOCUMENT_ROOT']. "/priv/pub/issues/sections/articles/article_top.php");
 require_once($_SERVER['DOCUMENT_ROOT']. "/priv/pub/issues/sections/articles/HtmlArea_Campsite.php");
