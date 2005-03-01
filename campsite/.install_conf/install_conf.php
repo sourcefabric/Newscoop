c48d3fbeb99f731ecacecbc91b60d0df37c76c5e
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-03-01 17:22:06 +0000

    - changed the configuration file format - added new variables

--- campsite/.install_conf/install_conf.php
+++ campsite/.install_conf/install_conf.php
@@ -7,8 +7,9 @@ $Campsite['BIN_DIR'] = '/usr/local/campsite/bin';
 $Campsite['ETC_DIR'] = '/usr/local/campsite/etc';
 $Campsite['WWW_DIR'] = '/usr/local/campsite/www';
 $Campsite['WWW_COMMON_DIR'] = '/usr/local/campsite/www-common';
+$Campsite['DEFAULT_SMTP_SERVER_ADDRESS'] = 'localhost';
+$Campsite['DEFAULT_SMTP_SERVER_PORT'] = '25';
 
-$CampsiteVars['install'] = array("APACHE_USER", "APACHE_GROUP", "CAMPSITE_DIR", "BIN_DIR", "ETC_DIR", "WWW_DIR", "WWW_COMMON_DIR");
+$CampsiteVars['install'] = array('APACHE_USER', 'APACHE_GROUP', 'CAMPSITE_DIR', 'BIN_DIR', 'ETC_DIR', 'WWW_DIR', 'WWW_COMMON_DIR', 'DEFAULT_SMTP_SERVER_ADDRESS', 'DEFAULT_SMTP_SERVER_PORT');
 
 ?>
-
