0e8498e962a60d8bebf268cec92af897b5df9642
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-03-07 14:52:07 +0000

    modified to use new configuration files

--- campsite/implementation/management/classes/config.php
+++ campsite/implementation/management/classes/config.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once($_SERVER['DOCUMENT_ROOT'].'/include/adodb/adodb.inc.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/configuration.php');
 
 global $Campsite;
 $Campsite = array();
@@ -9,7 +10,8 @@ $Campsite['db'] = ADONewConnection('mysql'); # eg 'mysql' or 'postgres'
 //$Campsite['db']->debug = true;
 // Set fetch mode to return associative arrays
 $Campsite['db']->SetFetchMode(ADODB_FETCH_ASSOC);
-$Campsite['db']->Connect('localhost', 'root', '', 'campsite');
+$Campsite['db']->Connect($Campsite['DATABASE_SERVER_ADDRESS'], $Campsite['DATABASE_USER'],
+	$Campsite['DATABASE_PASSWORD'], $Campsite['DATABASE_NAME']);
 
 $scheme = (substr($_SERVER['REQUEST_URI'], 0, 5) == 'https')?'https://':'http://';
 
@@ -17,4 +19,5 @@ $Campsite['hostname'] = $_SERVER['SERVER_NAME'];
 $Campsite['website_url'] = $scheme.$Campsite['hostname'];
 
 $Campsite['version'] = '2.2';
+
 ?>
