e1061ffeab0d1e62eeb739ec57097fff08aaa433
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-03-08 19:46:47 +0000

    moved database connect code to db_connect.php

--- implementation/management/classes/config.php
+++ implementation/management/classes/config.php
@@ -1,17 +1,6 @@
 <?php
 
-require_once($_SERVER['DOCUMENT_ROOT'].'/include/adodb/adodb.inc.php');
-require_once($_SERVER['DOCUMENT_ROOT'].'/configuration.php');
-
-global $Campsite;
-$Campsite = array();
-
-$Campsite['db'] = ADONewConnection('mysql'); # eg 'mysql' or 'postgres'
-//$Campsite['db']->debug = true;
-// Set fetch mode to return associative arrays
-$Campsite['db']->SetFetchMode(ADODB_FETCH_ASSOC);
-$Campsite['db']->Connect($Campsite['DATABASE_SERVER_ADDRESS'], $Campsite['DATABASE_USER'],
-	$Campsite['DATABASE_PASSWORD'], $Campsite['DATABASE_NAME']);
+require($_SERVER['DOCUMENT_ROOT'].'/db_connect.php');
 
 $scheme = (substr($_SERVER['REQUEST_URI'], 0, 5) == 'https')?'https://':'http://';
 
