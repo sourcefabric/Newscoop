427d7d3eac2c54f7143217aa1ea08d87a0fae9e5
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-03-08 19:43:21 +0000

    replaced old connection source lines with ADO database object

--- implementation/management/root/db_connect.php
+++ implementation/management/root/db_connect.php
@@ -1,9 +1,13 @@
 <?php
 
-require_once($_SERVER['DOCUMENT_ROOT'].'/configuration.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/include/adodb/adodb.inc.php');
+require($_SERVER['DOCUMENT_ROOT'].'/configuration.php');
 
-mysql_connect($Campsite['DATABASE_SERVER_ADDRESS'], $Campsite['DATABASE_USER'],
-	$Campsite['DATABASE_PASSWORD']);
-mysql_select_db($Campsite['DATABASE_NAME']);
+$Campsite['db'] = ADONewConnection('mysql'); # eg 'mysql' or 'postgres'
+//$Campsite['db']->debug = true;
+// Set fetch mode to return associative arrays
+$Campsite['db']->SetFetchMode(ADODB_FETCH_ASSOC);
+$Campsite['db']->Connect($Campsite['DATABASE_SERVER_ADDRESS'], $Campsite['DATABASE_USER'],
+	$Campsite['DATABASE_PASSWORD'], $Campsite['DATABASE_NAME']);
 
 ?>
