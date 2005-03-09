2124b3ebf227780a1b0f5080eae767e6c3dd41d4
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-03-09 23:55:03 +0000

    - removed unnecessary code - reformatted code

--- implementation/management/localizer/display.php
+++ implementation/management/localizer/display.php
@@ -3,31 +3,19 @@ require_once($_SERVER['DOCUMENT_ROOT']."/configuration.php");
 require_once($Campsite['HTML_DIR'] . "/$ADMIN_DIR/languages.php");
 
 foreach ($_REQUEST as $key=>$val)
-  $GLOBALS[$key] = $val;
+	$GLOBALS[$key] = $val;
 
-// function regGS($key,$value){
-//     global $lang,$base,$trans;
-//     if ($lang=='base'){
-//         $base[$key]=$value;
-//     }
-//     else{
-//  $trans[$key]=$value;
-//     }
-// }
+$langshort=substr($file,strlen($file)-6,2);
 
+//if the language is not registered, set the charset to english
+if (!isset($languages[$langshort])) {
+	$langshort='en';
+}
 
-    $langshort=substr($file,strlen($file)-6,2);
-
-    //if the language is not registered, set the charset to english
-    if (!isset($languages[$langshort])){
- $langshort='en';
-    }
-
-
-$fn=$Campsite['HTML_DIR'] . "/$ADMIN_DIR/$dir/$file";
-$enfn=substr($fn,0,strlen($fn)-6).'en.php';
-print "$dir / $file<HR><FORM method=post action=save.php>";
-
+$fn = $Campsite['HTML_DIR'] . "/$ADMIN_DIR/$dir/$file";
+$enfn = substr($fn,0,strlen($fn)-6).'en.php';
+echo "<b>" . substr($dir, 1) . "/" . $file . "</b>\n<HR>\n";
+echo "<FORM method=post action=index.php>\n";
 
 $gs = array();
 $lang='base';
@@ -38,28 +26,24 @@ $lang='trans';
 include($fn);
 $trans = $gs;
 
-echo "<p>enfn: $enfn</p>\n";
-echo "<p>fn: $fn</p>\n";
-
 $nr=0;
-while (list ($key,$value)=each($base)){
-    if (isset($trans[$key]) && (trim($trans[$key])!='')){
- $insval=$trans[$key];
- $pre='';
- $post='';
-    }
-    else{
- $insval='';
- $pre="<FONT COLOR=red>";
- $post="</FONT>";
-    }
+while (list ($key,$value)=each($base)) {
+	if (isset($trans[$key]) && (trim($trans[$key])!='')) {
+		$insval=$trans[$key];
+		$pre='';
+		$post='';
+	} else {
+		$insval='';
+		$pre="<FONT COLOR=red>";
+		$post="</FONT>";
+	}
 
-    if ($key!=''){
-        print "$pre$value$post<BR>\n";
-        print "<input name=base$nr type=hidden value=\"$key\">\n";
- print "<input name=translation$nr type=text size=50 value=\"$insval\"><BR><HR>\n";
-        $nr++;
-    }
+	if ($key!='') {
+		print "$pre$value$post<BR>\n";
+		print "<input name=base$nr type=hidden value=\"$key\">\n";
+		print "<input name=translation$nr type=text size=50 value=\"$insval\"><BR><HR>\n";
+		$nr++;
+	}
 }
 
 print "<INPUT TYPE=hidden name=pieces value=$nr>";
@@ -67,5 +51,5 @@ print "<INPUT TYPE=hidden name=destfile value='$fn'>";
 
 ?>
 
-<input type=submit Value='save'>
+<input type="submit" name="save" value="save">
 </form>
