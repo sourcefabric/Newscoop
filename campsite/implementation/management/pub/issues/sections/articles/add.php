7e8a42e6ead461c424d14acaa45930a888b71011
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-03-09 23:58:41 +0000

    removed unnecessary code

--- campsite/implementation/management/pub/issues/sections/articles/add.php
+++ campsite/implementation/management/pub/issues/sections/articles/add.php
@@ -39,12 +39,7 @@ if (function_exists ("incModFile")) {
 	incModFile ();
 }
 ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN"
-	"http://www.w3.org/TR/REC-html40/loose.dtd">
-<HTML>
 <HEAD>
-    <META http-equiv="Content-Type" content="text/html; charset=UTF-8">
-	<META HTTP-EQUIV="Expires" CONTENT="now">
 	<TITLE><?php  putGS("Add new article"); ?></TITLE>
 	<LINK rel="stylesheet" type="text/css" href="<?php echo $Campsite["website_url"] ?>/css/admin_stylesheet.css">
 </HEAD>
