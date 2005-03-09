c2a2ded2b64002a41f9093b11bbd28e3d4113548
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-03-09 23:56:50 +0000

    - reformatted code, fixed includes - do not display empty directories

--- campsite/implementation/management/localizer/menu.php
+++ campsite/implementation/management/localizer/menu.php
@@ -42,20 +42,18 @@ function createArrays($fen,$dirname,$newlang){
     $foreignfile="$dirname/$fen$newlang.php";
     include("$foreignfile");
     writeFile($foreignfile,$fen);
-
 }
 
 function verifyFile($fen,$dirname,$newlang){
     $check="$dirname/$fen$newlang.php";
     $fh=@fopen($check,'r');
     if ($fh!=null){
- fclose($fh);
-    }
-    else{
- print "Creating empty $fen\n";
- $fh=fopen($check,'w');
- fwrite($fh,"<?php \n?>");
- fclose($fh);
+        fclose($fh);
+    } else {
+        print "Creating empty $fen\n";
+        $fh=fopen($check,'w');
+        fwrite($fh,"<?php \n?>");
+        fclose($fh);
     }
     createArrays($fen,$dirname,$newlang);
 }
@@ -63,73 +61,63 @@ function verifyFile($fen,$dirname,$newlang){
 
 
 function parseFolder($dirname, $depth){
-	global $createnew,$newlang,$langarray, $Campsite, $ADMIN;
+	global $createnew, $newlang, $langarray, $Campsite, $ADMIN;
 	$full_dir = $Campsite['HTML_COMMON_DIR'] . "/priv/$dirname";
-	$handle=opendir($full_dir);
-	$space=3;
+	$handle = opendir($full_dir);
+	$space = 3;
+	$files = array();
+	$dirs = array();
 	while (($file = readdir($handle))!==false) {
-		$fullname=$full_dir."/".$file;
-		$filetype=filetype($fullname);
-		$isdir=false;
-		$isfile=false;
-		// avoiding the links
-		if ($filetype=="dir") $isdir=true;
-		else if ($filetype!="link") $isfile=true;
-		// if it's a file
-		if ($isfile){
-			// filling the array
-			$files[]=$file;
+		$fullname = "$full_dir/$file";
+		if (is_dir($fullname) && $file != "." && $file != "..") {
+			$dirs[] = $file; // filling the directories array
 		}
-		// if it's a directory but not the .. or .
-		else if ($isdir&&$file!="."&&$file!=".."){
-			// filling the array
-			$dirs[]=$file;
+		if (is_file($fullname)
+			&& (strpos($file, 'locals') === 0 || strpos($file, 'globals') === 0)
+			&& substr($file, strlen($file) - 4) === '.php') {
+			$files[] = $file; // filling the files array
 		}
 	}
-	//$isfileforlang=false;
-	if (isset($files)){
-		for($i=0;$i<count($files);$i++){
-			$filen=$files[$i];
+	sort($files);
+	sort($dirs);
+	if (sizeof($files) > 0) {
+		if ($depth > 0)
+			echo substr(strtoupper($dirname), 1)."\n";
+//			print str_repeat(' ',($depth-1)*$space).strtoupper($dirname)."\n";
+		foreach ($files as $index=>$filen) {
 
+			if ((strpos($filen, 'locals') === 0 || strpos($filen, 'globals') === 0)
+				&& substr($filen, strlen($filen) - 6) == 'en.php') {
+				$filesinen[] = $filen;
+			}
 
-     if ( ( (strpos($filen,'locals')===0) ||
-            (strpos($filen,'globals')===0) ) &&
-     (substr($filen,strlen($filen)-6)=='en.php')){
-  $filesinen[]=$filen;
-     }
+			if ((strpos($filen, 'locals') === 0 || strpos($filen, 'globals') === 0)
+				&& substr($filen, strlen($filen) - 4) === '.php'
+				&& (substr($filen, strlen($filen) - 6) != 'en.php'
+					|| substr($dirname, 1, 7) == 'modules')
+				//enable editing english files for modules
+				) {
+				if (strpos($filen, 'globals') === 0) {
+					$langarray[]=substr($filen,8,2);
+				}
 
+				print str_repeat(' ', $depth*$space)."<a href='/$ADMIN/localizer/?display=on&file=$filen&dir=$dirname'>$filen</a>\n";
+				usleep (0); // usleep(0) seems to be senseless, but needed because
+					        // of a strange behavior of PHP in combination with IE
+			}
+		} //for
 
-
-     if ( ( (strpos($filen,'locals')===0) ||
-            (strpos($filen,'globals')===0) )
-        &&
-        (substr($filen,strlen($filen)-4)==='.php')
-        &&
-        (substr($filen,strlen($filen)-6)!='en.php' || substr($dirname,1,7)=='modules')  //enable editing english files for modules
-        ){
-
-        if ( (strpos($filen,'globals')===0) ){
-      $langarray[]=substr($filen,8,2);
-  }
-
-        print str_repeat(' ',$depth*$space)."<a href='/$ADMIN/localizer/?display=on&file=$filen&dir=$dirname'>$filen</a>\n";
-        usleep (0); //usleep(0) seems to be senseless, but needet for a strange behavior of PHP in combination with IE
-     }
-        }//for
-
- if (isset($filesinen)&&($createnew)){
-     foreach($filesinen as $fen){
-  verifyFile(substr($fen,0,strlen($fen)-6),$full_dir,$newlang);
-  //print substr($fen,0,strlen($fen)-6);
-     }
- }
-    }
-    if (isset($dirs)){
- for($i=0;$i<count($dirs);$i++){
-     print str_repeat(' ',$depth*$space).strtoupper($dirs[$i])."\n";
-     parseFolder("$dirname/".$dirs[$i],$depth+1);
-        }
-    }
+		if (isset($filesinen)&&($createnew)) {
+			foreach($filesinen as $fen) {
+				verifyFile(substr($fen,0,strlen($fen)-6),$full_dir,$newlang);
+			}
+		}
+	}
+	if (isset($dirs)) {
+		for($i=0;$i<count($dirs);$i++) {
+			parseFolder("$dirname/".$dirs[$i],$depth+1);
+		}
+	}
 }
 
 
@@ -141,8 +129,7 @@ if ( (isset($newlang)) && ($newlang!='') && (strlen($newlang)>1) &&($newlang!='e
     $createnew=true;
     print "creating files for: $newlang";
     $langarray[]=$newlang;
-}
-else{
+}else{
     $createnew=false;
 }
 $langarray[]='en';
@@ -160,7 +147,7 @@ parseFolder("", 0);
 print '</PRE>';
 
 ?>
-<form action=# method=post>
+<form action="index.php" method="post">
 
 <SELECT NAME=newlang>
 <?php
@@ -184,25 +171,19 @@ $langfile.="\n".'?>';
 
 ?>
 </select>
- <input type=submit value='create language files'>
+<p><input type=submit value='create language files'></p>
 </form>
 
 <?php
 if ($createnew){
-    $langf=fopen('../languages.php','w');
+    $langf=fopen($_SERVER['DOCUMENT_ROOT']."/$ADMIN_DIR/languages.php",'w');
     fwrite($langf,$langfile);
     fclose($langf);
 }
-?>
-
-
-<?php
 if ($createnew){ ?>
     <SCRIPT>
- document.location.href='menu.php';
+ document.location.href='index.php';
     </SCRIPT>
 <?php
-//exit();
-
 }
 ?>
