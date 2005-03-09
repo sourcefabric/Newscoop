3660b59d91e605e1de58b87829c167fab8a407cf
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-03-09 15:50:30 +0000

    removed unneeded code

--- campsite/implementation/management/home.php
+++ campsite/implementation/management/home.php
@@ -36,28 +36,8 @@ list($YourArticles, $NumYourArticles) = Article::GetArticlesByUser($User->getId(
 
 list($SubmittedArticles, $NumSubmittedArticles) = Article::GetSubmittedArticles($NArtOffs, $NumDisplayArticles);
 ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN"
-	"http://www.w3.org/TR/REC-html40/loose.dtd">
-<HTML>
 <HEAD>
-    <META http-equiv="Content-Type" content="text/html; charset=UTF-8">
-	<META HTTP-EQUIV="Expires" CONTENT="now">
 	<LINK rel="stylesheet" type="text/css" href="<?php echo $Campsite["website_url"] ?>/css/admin_stylesheet.css">
-	<script>
-	<!--
-	/*
-	A slightly modified version of "Break-out-of-frames script"
-	By JavaScript Kit (http://javascriptkit.com)
-	*/
-	
-	if (window != top.fmain && window != top) {
-		if (top.fmenu)
-			top.fmain.location.href=location.href
-		else
-			top.location.href=location.href
-	}
-	// -->
-	</script>
 	<TITLE><?php  putGS("Home"); ?></TITLE>
 </HEAD>
 <BODY  BGCOLOR="WHITE" TEXT="BLACK" LINK="DARKBLUE" ALINK="RED" VLINK="DARKBLUE">
