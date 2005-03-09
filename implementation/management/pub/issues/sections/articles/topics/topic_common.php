aa05327d4dbf0e218d5421569ec1581d344a7551
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-03-09 23:59:44 +0000

    include common files

---
+++ implementation/management/pub/issues/sections/articles/topics/topic_common.php
@@ -0,0 +1,14 @@
+<?php
+require_once($_SERVER['DOCUMENT_ROOT']."/classes/common.php");
+load_common_include_files("$ADMIN_DIR/pub/issues/sections/articles/topics");
+require_once($_SERVER['DOCUMENT_ROOT']."/classes/Article.php");
+require_once($_SERVER['DOCUMENT_ROOT']."/classes/Section.php");
+require_once($_SERVER['DOCUMENT_ROOT']."/classes/Issue.php");
+require_once($_SERVER['DOCUMENT_ROOT']."/classes/Publication.php");
+require_once($_SERVER['DOCUMENT_ROOT']."/classes/Language.php");
+require_once($_SERVER['DOCUMENT_ROOT']."/classes/User.php");
+require_once($_SERVER['DOCUMENT_ROOT']."/classes/Template.php");
+require_once($_SERVER['DOCUMENT_ROOT']."/classes/Input.php");
+require_once($_SERVER['DOCUMENT_ROOT']."/$ADMIN_DIR/CampsiteInterface.php");
+require_once($_SERVER['DOCUMENT_ROOT']. "/$ADMIN_DIR/pub/issues/sections/articles/article_top.php");
+?>
