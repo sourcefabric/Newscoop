c531103244fe3b4573cc96d07238e2f4ee8b1bce
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-03-09 15:51:06 +0000

    - removed unneeded code - include home.php

--- implementation/management/index.php
+++ implementation/management/index.php
@@ -1,21 +1,10 @@
 <?php
-require($_SERVER['DOCUMENT_ROOT']."/classes/common.php");
+require_once($_SERVER['DOCUMENT_ROOT']."/classes/common.php");
 load_common_include_files($ADMIN_DIR);
 list($access, $User) = check_basic_access($_REQUEST);
 if (!$access) {
 	header("Location: /$ADMIN/login.php");
 	return;
 }
+require_once($Campsite['HTML_DIR'] . "/$ADMIN_DIR/home.php");
 ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN"
-	"http://www.w3.org/TR/REC-html40/loose.dtd">
-<HTML>
-<HEAD>
-    <META http-equiv="Content-Type" content="text/html; charset=UTF-8">
-	<TITLE><?php putGS("CAMPSITE"); ?></TITLE>
-</HEAD>
-<FRAMESET ROWS="75,*" BORDER="0">
-    <FRAME SRC="menu.php" NAME="fmenu" FRAMEBORDER="0" MARGINWIDTH="0" SCROLLING="AUTO">
-    <FRAME SRC="home.php" NAME="fmain" FRAMEBORDER="0" MARGINWIDTH="0" SCROLLING="AUTO">
-</FRAMESET>
-</HTML>
