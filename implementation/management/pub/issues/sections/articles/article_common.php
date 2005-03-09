2b8a68410308053a0f85180ba33240522303650c
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-03-09 20:10:30 +0000

    replaced include with require_once

--- implementation/management/pub/issues/sections/articles/article_common.php
+++ implementation/management/pub/issues/sections/articles/article_common.php
@@ -1,5 +1,5 @@
 <?php
-include($_SERVER['DOCUMENT_ROOT']."/classes/common.php");
+require_once($_SERVER['DOCUMENT_ROOT']."/classes/common.php");
 load_common_include_files("$ADMIN_DIR/pub/issues/sections/articles");
 require_once($_SERVER['DOCUMENT_ROOT']."/classes/Article.php");
 require_once($_SERVER['DOCUMENT_ROOT']."/classes/Section.php");
