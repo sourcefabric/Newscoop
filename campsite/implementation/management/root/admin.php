d83dd54622ae3b86bfaf70e80c7c63dc927bfb26
paul <paul@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-03-11 07:23:43 +0000

    Fixed bug so that images will be cached by the browser.  Fixed bug so that other files besides php and images files can be loaded.

--- campsite/implementation/management/root/admin.php
+++ campsite/implementation/management/root/admin.php
@@ -2,34 +2,41 @@
 
 require_once($_SERVER['DOCUMENT_ROOT'].'/configuration.php');
 
-$no_menu_scripts = array("/login.php", "/do_login.php", "/popup/index.php",
-	"/popup/art.php", "/popup/empty.php", "/popup/i1.php", "/popup/i2.php",
-	"/popup/i3.php", "/popup/i4.php", "/popup/img.php", "/popup/index.php",
-	"/popup/iss.php", "/popup/lang.php", "/popup/pub.php", "/popup/sect.php"
+$no_menu_scripts = array('/login.php', '/do_login.php', '/popup/index.php',
+	'/popup/art.php', '/popup/empty.php', '/popup/i1.php', '/popup/i2.php',
+	'/popup/i3.php', '/popup/i4.php', '/popup/img.php', '/popup/index.php',
+	'/popup/iss.php', '/popup/lang.php', '/popup/pub.php', '/popup/sect.php'
 	);
 
 $request_uri = $_SERVER['REQUEST_URI'];
 $call_script = substr($request_uri, strlen("/$ADMIN"));
 
 // Remove any GET parameters
-if (($question_mark = strpos($call_script, "?")) !== false) {
+if (($question_mark = strpos($call_script, '?')) !== false) {
 	$call_script = substr($call_script, 0, $question_mark);
 }
 
+$is_image = (strstr($call_script, '/img/') !== false);
+//$extension = strtolower(substr($call_script, strlen($call_script) - 4));
+$extension = '';
+if (($extension_start = strrpos($call_script, '.')) !== false) {
+	$extension = strtolower(substr($call_script, $extension_start));
+}
+	
 // Is it an image?
-if (strncmp($call_script, "/img/", 5) == 0) {
-	$extension = substr(strrchr($call_script, "."), 1);
+if ($is_image) {
+	$extension = substr(strrchr($call_script, '.'), 1);
 	header("Content-type: image/$extension");
-	header("Expires: now");
+	//header('Expires: now');
 	readfile($Campsite['HTML_DIR'] . "/$ADMIN_DIR/$call_script");
 } 
-else {
+elseif (($extension == '.php') || ($extension == '')) {
 	// Requested file is not an image
 
 	// If they arent trying to login in...
-	if (($call_script != "/login.php") && ($call_script != "/do_login.php")) {
+	if (($call_script != '/login.php') && ($call_script != '/do_login.php')) {
 		// Check if the user is logged in already
-		require_once($_SERVER['DOCUMENT_ROOT']."/classes/common.php");
+		require_once($_SERVER['DOCUMENT_ROOT'].'/classes/common.php');
 		load_common_include_files($ADMIN_DIR);
 		list($access, $User) = check_basic_access($_REQUEST);
 		if (!$access) {
@@ -39,19 +46,17 @@ else {
 		}
 	}
 		
-	$extension = strtolower(substr($call_script, strlen($call_script) - 4));
-	
 	// If its not a PHP file, assume its a directory.
-	if ($extension != ".php") {
+	if ($extension != '.php') {
 		// If its a directory
-		if (($call_script != "") && ($call_script[strlen($call_script)-1] != "/") ) {
-			$call_script .= "/";
+		if (($call_script != '') && ($call_script[strlen($call_script)-1] != '/') ) {
+			$call_script .= '/';
 		}
-		$call_script .= "index.php";
+		$call_script .= 'index.php';
 	}
 	$needs_menu = ! in_array($call_script, $no_menu_scripts);
 
-	$menu = "";
+	$menu = '';
 	if ($needs_menu) {
 		ob_start();
 		echo "<html><table width=\"100%\">\n<tr><td>\n";
@@ -68,5 +73,7 @@ else {
 		echo "</td></tr>\n</table>\n</html>\n";
 	}
 }
-
+else {
+	readfile($Campsite['HTML_DIR'] . "/$ADMIN_DIR/$call_script");	
+}
 ?>
