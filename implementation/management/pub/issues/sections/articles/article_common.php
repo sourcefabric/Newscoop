745e690935f5d53e8adc3803f9db8aefefa5f505
paul <paul@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-03-11 06:36:51 +0000

    Replaced double quotes with single quotes

--- implementation/management/pub/issues/sections/articles/article_common.php
+++ implementation/management/pub/issues/sections/articles/article_common.php
@@ -1,14 +1,14 @@
 <?php
-require_once($_SERVER['DOCUMENT_ROOT']."/classes/common.php");
+require_once($_SERVER['DOCUMENT_ROOT'].'/classes/common.php');
 load_common_include_files("$ADMIN_DIR/pub/issues/sections/articles");
-require_once($_SERVER['DOCUMENT_ROOT']."/classes/Article.php");
-require_once($_SERVER['DOCUMENT_ROOT']."/classes/Section.php");
-require_once($_SERVER['DOCUMENT_ROOT']."/classes/Issue.php");
-require_once($_SERVER['DOCUMENT_ROOT']."/classes/Publication.php");
-require_once($_SERVER['DOCUMENT_ROOT']."/classes/Language.php");
-require_once($_SERVER['DOCUMENT_ROOT']."/classes/User.php");
-require_once($_SERVER['DOCUMENT_ROOT']."/classes/Template.php");
-require_once($_SERVER['DOCUMENT_ROOT']."/classes/Input.php");
+require_once($_SERVER['DOCUMENT_ROOT'].'/classes/Article.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/classes/Section.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/classes/Issue.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/classes/Publication.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/classes/Language.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/classes/User.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/classes/Template.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/classes/Input.php');
 require_once($_SERVER['DOCUMENT_ROOT']."/$ADMIN_DIR/CampsiteInterface.php");
 require_once($_SERVER['DOCUMENT_ROOT']. "/$ADMIN_DIR/pub/issues/sections/articles/article_top.php");
 require_once($_SERVER['DOCUMENT_ROOT']. "/$ADMIN_DIR/pub/issues/sections/articles/HtmlArea_Campsite.php");
