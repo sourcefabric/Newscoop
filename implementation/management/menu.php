969029eb85076572e18f6f224bc08912d8b9bafb
paul <paul@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-03-11 06:19:04 +0000

    Switched menu to use the JSCookMenu, added icons for everything

--- implementation/management/menu.php
+++ implementation/management/menu.php
@@ -1,8 +1,7 @@
 <?php
+require_once($_SERVER['DOCUMENT_ROOT']."/classes/config.php");
 require_once($_SERVER['DOCUMENT_ROOT']."/classes/common.php");
-load_common_include_files($ADMIN_DIR);
-todefnum('TOL_UserId');
-todefnum('TOL_UserKey');
+load_common_include_files("$ADMIN_DIR");
 list($access, $User) = check_basic_access($_REQUEST);
 if (!$access) {
 	exit;
@@ -11,214 +10,99 @@ $menu_index = 1;
 $max_menu_items = 5;
 $counter_reset = false;
 $counter_resets = 0;
-$preConfigMenu = array();
-$preConfigMenu[] = array('title'=>'Article Types', 
-					  'link' => "/$ADMIN/a_types/",
-					  'permission' => 'ManageArticleTypes'); 
-$preConfigMenu[] = array('title' => 'Topics', 
-					  'link' => "/$ADMIN/topics/",
-					  'permission' => 'ManageTopics'); 
-$preConfigMenu[] = array('title' => 'Languages', 
-					  'link' => "/$ADMIN/languages/",
-					  'permission' => 'ManageLanguages' ); 
-$preConfigMenu[] = array('title' => 'Templates', 
-					  'link' => "/$ADMIN/templates/",
-					  'permission' => 'ManageLanguages'); 
-$preConfigMenu[] = array('title' => 'Countries', 
-					  'link' => "/$ADMIN/country/",
-					  'permission' => 'ManageCountries'); 
-$preConfigMenu[] = array('title' => 'Logs', 
-					  'link' => "/$ADMIN/logs/",
-					  'permission' => 'ViewLogs'); 
-$preConfigMenu[] = array('title' => 'Localizer', 
-					  'link' => "/$ADMIN/localizer/",
-					  'permission' => 'ManageLocalizer'); 
-$configMenu = array();
-foreach ($preConfigMenu as $item) {
-	if ($User->hasPermission($item['permission'])) {
-		$configMenu[] = $item;
-	}
-}
-$numConfigMenuItems = count($configMenu);
-$numConfigMenuColumns = ($numConfigMenuItems/2) + ($numConfigMenuItems%2);
-
-$preUserMenu = array();
-$preUserMenu[] = array('title'=>'Users', 
-					  'link' => "/$ADMIN/users/",
-					  'permission' => 'ManageUsers'); 
-$preUserMenu[] = array('title' => 'User Types', 
-					  'link' => "/$ADMIN/u_types/",
-					  'permission' => 'ManageUserTypes'); 
-$userMenu = array();
-foreach ($preUserMenu as $item) {
-	if ($User->hasPermission($item['permission'])) {
-		$userMenu[] = $item;
-	}
-}
-
-$preObsoleteMenu = array();
-$preObsoleteMenu[] = array('title'=>'Glossary', 
-					  'link' => "/$ADMIN/glossary/",
-					  'permission' => 'ManageDictionary'); 
-$preObsoleteMenu[] = array('title' => 'Infotype', 
-					  'link' => "/$ADMIN/infotype/",
-					  'permission' => 'ManageClasses'); 
-$obsoleteMenu = array();
-foreach ($preObsoleteMenu as $item) {
-	if ($User->hasPermission($item['permission'])) {
-		$obsoleteMenu[] = $item;
-	}
-}
-
 ?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN"
+	"http://www.w3.org/TR/REC-html40/loose.dtd">
+<HTML>
 <HEAD>
-	<META http-equiv="Content-Type" content="text/html; charset=UTF-8">
-	<LINK rel="stylesheet" type="text/css" href="<?php echo $Campsite["website_url"] ?>/css/admin_stylesheet.css">
+    <META http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<META HTTP-EQUIV="Expires" CONTENT="now">
+	<script language="JavaScript" src="<?php echo $Campsite["website_url"]; ?>/javascript/JSCookMenu/JSCookMenu.js" type="text/javascript"></script>
+	<LINK REL="stylesheet" HREF="<?php echo $Campsite["website_url"]; ?>/javascript/JSCookMenu/ThemeOffice/theme.css" TYPE="text/css">
+<script language="JavaScript" src="<?php echo $Campsite["website_url"]; ?>/javascript/JSCookMenu/ThemeOffice/theme.js" type="text/javascript"></script>
+	<SCRIPT LANGUAGE="JavaScript"><!--
+	var myMenu =
+	[
+	    ['<img src="/<?php p($ADMIN); ?>/img/icon/content.png" align="middle" style="padding-bottom: 3px;" />', ' <?php putGS('Content'); ?>', '', '', '',
+	    	['<img src="/<?php p($ADMIN); ?>/img/icon/publication.png" />', '<?php putGS('Publications'); ?>', '/<?php p($ADMIN); ?>/pub/index.php' ],
+	    	['<img src="/<?php p($ADMIN); ?>/img/icon/image_archive.png" />', '<?php putGS('Image archive'); ?>', '/<?php p($ADMIN); ?>/imagearchive/index.php' ]
+	    ],
+	    _cmSplit,
+	    ['<img src="/<?php p($ADMIN); ?>/img/icon/actions.png" align="middle" />', ' <?php putGS('Actions'); ?>', '', '', '',
+	    	['<img src="/<?php p($ADMIN); ?>/img/icon/add_article.png" />', '<?php putGS('Add new article'); ?>', '/<?php p($ADMIN); ?>/pub/add_article.php'],
+	    	['<img src="/<?php p($ADMIN); ?>/img/icon/change_password.png" />', '<?php putGS('Change your password'); ?>', '/<?php p($ADMIN); ?>/users/chpwd.php']
+	    ],
+	    _cmSplit,
+	    ['<img src="/<?php p($ADMIN); ?>/img/icon/configure.png" align="middle" />', ' <?php putGS('Configure'); ?>', '', '', '',
+	    	['<img src="/<?php p($ADMIN); ?>/img/icon/publishing_environment.png" />', '<?php putGS('Publishing environment'); ?>', '', '', '',
+	    		['<img src="/<?php p($ADMIN); ?>/img/icon/templates.png" />', '<?php putGS('Templates');?>', '/<?php p($ADMIN); ?>/templates/' ],
+	    		['<img src="/<?php p($ADMIN); ?>/img/icon/article_types.png" />', '<?php putGS('Article Types'); ?>', '/<?php p($ADMIN); ?>/a_types/' ],
+	    		['<img src="/<?php p($ADMIN); ?>/img/icon/topics.png" />', '<?php putGS('Topics'); ?>', '/<?php p($ADMIN); ?>/topics/' ],
+	    		['<img src="/<?php p($ADMIN); ?>/img/icon/languages.png" />', '<?php putGS('Languages'); ?>', '/<?php p($ADMIN); ?>/languages/' ],
+	    		['<img src="/<?php p($ADMIN); ?>/img/icon/countries.png" />', '<?php putGS('Countries'); ?>', '/<?php p($ADMIN); ?>/country/' ],
+	    	],
+	    	['<img src="/<?php p($ADMIN); ?>/img/icon/localizer.png" />', '<?php putGS('Localizer'); ?>', '/<?php p($ADMIN); ?>/localizer/',  ],
+	    	['<img src="/<?php p($ADMIN); ?>/img/icon/logs.png" />', '<?php putGS('Logs'); ?>', 'logs/', ]
+	    ],
+	    _cmSplit,
+	    ['<img src="/<?php p($ADMIN); ?>/img/icon/users.png" align="middle" />', ' <?php putGS('Users'); ?>', '', '', '',
+	    	['<img src="/<?php p($ADMIN); ?>/img/icon/users.png" />', '<?php putGS('Users'); ?>', '/<?php p($ADMIN); ?>/users/' ], 
+	    	['<img src="/<?php p($ADMIN); ?>/img/icon/user_types.png" />', '<?php putGS('User Types'); ?>', '/<?php p($ADMIN); ?>/u_types/' ], 
+	    ],
+	    _cmSplit,
+	    ['<img src="/<?php p($ADMIN); ?>/img/icon/obsolete.png" align="middle" />', ' <?php putGS('Obsolete'); ?>', '', '', '',
+	    	['<img src="/<?php p($ADMIN); ?>/img/icon/glossary.png" />', '<?php putGS('Glossary'); ?>', '/<?php p($ADMIN); ?>/glossary/' ], 
+	    	['<img src="/<?php p($ADMIN); ?>/img/icon/infotypes.png" />', '<?php putGS('Infotype'); ?>', '/<?php p($ADMIN); ?>/infotype/' ], 
+	    ]	    	
+	];
+	--></SCRIPT>
+
+	<TITLE>Campsite <?php p($Campsite['version']); ?></TITLE>
 </HEAD>
 
-<BODY>
-<div style="position: absolute; top: 74px; width: 100%; border-bottom: 1px solid black;"></div>
-
-<table width="100%" cellpadding="0" cellspacing="0" border="0" bgcolor="#D0D0B0">
+<BODY BGCOLOR="white" TEXT="BLACK" LINK="DARKBLUE" ALINK="RED" VLINK="DARKBLUE" style="margin: 0px;" margintop="0" marginright="0" marginleft="0">
+<table width="100%" cellpadding="0" cellspacing="0" border="0" style="border: 1px solid black;">
 <tr>
-	<td width="100px" style="padding: 15px; font-size: 14px; font-weight: bold; border-right: 1px solid black;" align="center">
-		Campsite<br>
-		v<?php p($Campsite['version']); ?>
-		<!--<img src="/<?php echo $ADMIN; ?>/img/CampsiteLogo2.png">-->
-	</td>
-	<td width="100%">
-
-		<TABLE CELLSPACING="0" CELLPADDING="0" bgcolor="#D0D0B0">
-		<TR>
-		<TD VALIGN="TOP">
-			<TABLE border="0" CELLSPACING="0" CELLPADDING="0" BGCOLOR="#D0D0B0" width="100%">
-			<TR>
-				<!-- BEGIN Content Menu -->
-				<td valign="top" style="padding-left: 10px;">
-					<table cellpadding="1" cellspacing="0" border="0">
-					<tr>
-						<td colspan="2" valign="top">
-							Content:
-						</td>
-					</tr>
-					<tr>
-						<TD ALIGN="left"><A HREF="/<?php echo $ADMIN; ?>/pub/"><IMG SRC="/<?php echo $ADMIN; ?>/img/tol.gif" BORDER="0" ALT="<?php  putGS("Publications"); ?>"></A></TD>
-						<TD align="left" NOWRAP><A HREF="/<?php echo $ADMIN; ?>/pub/" ONCLICK=""><?php  putGS("Publications"); ?></A></TD>
-					</tr>
-					<tr>
-						<TD ALIGN="left"><A HREF="/<?php echo $ADMIN; ?>/imagearchive/"><IMG SRC="/<?php echo $ADMIN; ?>/img/tol.gif" BORDER="0" ALT="<?php  putGS("Image archive"); ?>"></A></TD>
-						<TD align="left" NOWRAP><A HREF="/<?php echo $ADMIN; ?>/imagearchive/" ONCLICK=""><?php  putGS("Image archive"); ?></A></TD>
-					</tr>
-					</table>
-				</td>
-				<!-- END Content Menu -->
-				
-				<?php if ($numConfigMenuItems > 0) { ?>
-				<!-- BEGIN Configure Menu -->
-				<td valign="top" style="padding-left: 30px;">
-					<table cellpadding="0" cellspacing="0">
-					<tr>
-						<td colspan="2">
-							Configure:
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<table border="0" cellpadding="1" cellspacing="0">
-							<tr>
-								<?php
-								$count = 0;
-								foreach ($configMenu as $menuItem) {
-									if (($count % ($numConfigMenuColumns)) == 0) {
-										echo "</tr><tr>";
-									}
-									?>
-									<TD ALIGN="RIGHT"><A HREF="<?php p($menuItem['link']) ?>"><IMG SRC="/<?php echo $ADMIN; ?>/img/tol.gif" BORDER="0" ALT="<?php putGS($menuItem['title']); ?>"></A></TD>
-									<TD NOWRAP><A HREF="<?php p($menuItem['link']); ?>"><?php  putGS($menuItem['title']); ?></A></TD>
-								<?php
-									$count++;
-								} 
-								?>
-							</tr>
-							</table>
-						</td>
-					</tr>
-					</table>
-				</td>	
-				<!-- END Configure Menu -->
-				<?php } ?>
-				
-				<?php if (count($userMenu) > 0) { ?>
-				<!-- BEGIN User menu -->
-				<td valign="top" style="padding-left: 30px;">
-					<table cellpadding="1" cellspacing="0">
-					<tr>
-						<td colspan="2">
-							Users:
-						</td>
-					</tr>
-						<?php
-						foreach ($userMenu as $menuItem) { ?>
-							<tr>
-								<TD ALIGN="left"><A HREF="<?php p($menuItem['link']) ?>"><IMG SRC="/<?php echo $ADMIN; ?>/img/tol.gif" BORDER="0" ALT="<?php putGS($menuItem['title']); ?>"></A></TD>
-								<TD align="left" NOWRAP><A HREF="<?php p($menuItem['link']); ?>"><?php  putGS($menuItem['title']); ?></A></TD>
-							</tr>
-							<?php 
-						}
-						?>
-					</table>
-				</td>	
-				<!-- END User menu -->
-				<?php } ?>
-				
-				<?php if (count($obsoleteMenu) > 0) { ?>
-				<!-- BEGIN Obsolete menu -->
-				<td valign="top" style="padding-left: 30px;">
-					<table cellpadding="1" cellspacing="0">
-					<tr>
-						<td colspan="2">
-							Obsolete:
-						</td>
-					</tr>
-						<?php
-						foreach ($obsoleteMenu as $menuItem) {
-							if ($User->hasPermission($menuItem['permission'])) { ?>
-							<tr>
-								<TD ALIGN="left"><A HREF="<?php p($menuItem['link']) ?>"><IMG SRC="/<?php echo $ADMIN; ?>/img/tol.gif" BORDER="0" ALT="<?php putGS($menuItem['title']); ?>"></A></TD>
-								<TD align="left" NOWRAP><A HREF="<?php p($menuItem['link']); ?>"><?php  putGS($menuItem['title']); ?></A></TD>
-							</tr>
-							<?php 
-							} 
-						}
-						?>
-					</table>
-				</td>	
-				<!-- END Obsolete menu -->
-				<?php } ?>
-			</tr>
-			</TABLE>
-		</TD>	
-		</TR>
-		</TABLE>
-	</td>
-	
-	<TD WIDTH="1%" VALIGN="TOP" style="height: 75px; border-left: 1px solid black; padding-left: 5px; padding-right: 5px;">
-		<TABLE CELLSPACING="1" CELLPADDING="1" BGCOLOR="#D0D0B0" width="100%" height="100%">
-		<TR>
-			<TD ALIGN="left"><A HREF="/<?php echo $ADMIN; ?>/" ONCLICK=""><IMG SRC="/<?php echo $ADMIN; ?>/img/tol.gif" BORDER="0" ALT="<?php  putGS("Home"); ?>"></A></TD>
-			<TD NOWRAP><A HREF="/<?php echo $ADMIN; ?>/" ONCLICK=""><?php  putGS("Home"); ?></A></TD>
+	<td valign="top" align="left" width="100%" style="padding-top: 0px; ">
+    	<table border="0" cellpadding="0" cellspacing="0">
+    	<tr>
+	    	<td style="padding-left: 10px; padding-right: 20px; font-size: 14pt; font-weight: bold; color: black; font-style: Verdana;" align="left" valign="middle">
+	    		Campsite v<?php p($Campsite['version']); ?>
+	    	</td>
+	    	<td style="padding-left: 5px;">
+	    	<DIV ID="myMenuID"></DIV>
+			<SCRIPT LANGUAGE="JavaScript"><!--
+				cmDraw ('myMenuID', myMenu, 'hbr', cmThemeOffice, 'ThemeOffice');
+			--></SCRIPT>
+			</td>
 		</tr>
-		
+		</table>
+	</td>
+	<td align="right">
+		<table cellpadding="0" cellspacing="0">
 		<tr>
-			<TD ALIGN="left"><A HREF="" ONCLICK="window.open('/<?php echo $ADMIN; ?>/popup/', 'fpopup', 'menu=no,width=500,height=410'); return false;"><IMG SRC="/<?php echo $ADMIN; ?>/img/tol.gif" BORDER="0" ALT="<?php  putGS("Quick Menu"); ?>"></A></TD>
-			<TD NOWRAP><A HREF="" ONCLICK="window.open('/<?php echo $ADMIN; ?>/popup/', 'fpopup', 'menu=no,width=500,height=410'); return false;"><?php  putGS("Quick Menu"); ?></A></TD>
-		</TR>
-		
-		<TR>
-			<TD ALIGN="RIGHT"><A HREF="/<?php echo $ADMIN; ?>/logout.php" ONCLICK=""><IMG SRC="/<?php echo $ADMIN; ?>/img/tol.gif" BORDER="0" ALT="<?php  putGS("Logout"); ?>"></A></TD>
-			<TD NOWRAP COLSPAN="3" ALIGN="LEFT"><A HREF="/<?php echo $ADMIN; ?>/logout.php" ONCLICK=""><?php  putGS("Logout"); ?></A></TD>
-		</TR>
-		</TABLE>
-	</TD>
+			<td>
+				<A HREF="/<?php p($ADMIN); ?>/home.php"><img src="/<?php p($ADMIN); ?>/img/icon/home.png" border="0" alt="<?php putGS('Home'); ?>"></a>
+			</td>
+			<td style="font-weight: bold; padding-left: 2px;">
+				<A HREF="/<?php p($ADMIN); ?>/home.php" style="color: black; text-decoration: none;"><?php putGS('Home'); ?></A>
+			</td>
+			
+			<td style="padding-left: 10px;">
+				<A HREF="" ONCLICK="window.open('/<?php p($ADMIN); ?>/popup/', 'fpopup', 'menu=no,width=500,height=410'); return false;"><img src="/<?php p($ADMIN); ?>/img/icon/quick_menu.png" border="0" alt="<?php putGS('Quick Menu'); ?>"></a>
+			</td>
+			<td style="font-weight: bold; padding-left: 2px;" nowrap>
+				<A HREF="" ONCLICK="window.open('/<?php p($ADMIN); ?>/popup/', 'fpopup', 'menu=no,width=500,height=410'); return false;" style="color: black; text-decoration: none;"><?php putGS('Quick Menu'); ?></a>
+			</td>
+			
+			<td style="padding-left: 10px;">
+				<A HREF="/<?php p($ADMIN); ?>/logout.php"><img src="/<?php p($ADMIN); ?>/img/icon/logout.png" border="0" alt="<?php putGS('Logout'); ?>"></a>
+			</td>
+			<td style="font-weight: bold; padding-left: 2px; padding-right: 10px;">
+				<A HREF="/<?php p($ADMIN); ?>/logout.php" style="color: black; text-decoration: none;"><?php putGS('Logout'); ?></a>
+			</td>
+		</tr>
+		</table>
+	</td>
 </tr>
 </table>
