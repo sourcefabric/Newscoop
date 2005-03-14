caf70fd7e4bb24839a7627eae9650aa502ff12b9
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-03-14 21:35:05 +0000

    deleted obsolete messages, added new ones

--- campsite/implementation/management/templates/locals.en.php
+++ campsite/implementation/management/templates/locals.en.php
@@ -2,14 +2,8 @@
 // -- index.m4
 
 // -- idx.m4
-regGS("Select template","Select template:en");
-regGS("Templates management","Templates management:en");
-regGS("You do not have the right to change default templates.","You do not have the right to change default templates.:en");
 regGS("Create new folder","Create new folder:en");
 regGS("Upload template","Upload template:en");
-regGS("Select the template for displaying the front page.","Select the template for displaying the front page.:en");
-regGS("Select the template for displaying a single article.","Select the template for displaying a single article.:en");
-regGS("You do no have access to the $1 directory!", "You do no have access to the $1 directory!:en");
 regGS("Refresh templates directory", "Refresh templates directory:en");
 
 // -- do_new_dir.m4
@@ -17,6 +11,7 @@ regGS("Creating new folder","Creating new folder:en");
 regGS("You do not have the right to create new folders.","You do not have the right to create new folders.:en");
 regGS("The folder $1 has been created","The folder $1 has been created:en");
 regGS("The folder $1 could not be created","The folder $1 could not be created:en");
+regGS("A file or folder having the name $1 already exists","A file or folder having the name $1 already exists:en");
 
 // -- del.m4
 regGS("Delete templates","Delete templates:en");
@@ -33,7 +28,7 @@ regGS("The folder has been deleted.","The folder has been deleted.:en");
 regGS("The folder could not be deleted.","The folder could not be deleted.:en");
 regGS("The template has been deleted.","The template has been deleted.:en");
 regGS("The template could not be deleted.","The template could not be deleted.:en");
-regGS("Templates deleted from $1","Templates deleted from $1:en");
+regGS("Template $1 was deleted","Template $1 was deleted:en");
 regGS("Delete folder","Delete folder:en");
 regGS("Folders","Folders:en");
 regGS("No folders.","No folders.:en");
@@ -77,5 +72,7 @@ regGS("The templates directory was refreshed succesfully.", "The templates direc
 regGS("There were $1 new template(s) found.", "There were $1 new template(s) found.:en");
 regGS("The following templates had been registered but are missing from the templates directory and were deleted:", "The following templates had been registered but are missing from the templates directory and were deleted::en");
 regGS("The following templates were registered and used but are missing from the templates directory:", "The following templates were registered and used but are missing from the templates directory::en");
+
+regGS("You do not have the right to modify templates.","You do not have the right to modify templates.:en");
 
 ?>
