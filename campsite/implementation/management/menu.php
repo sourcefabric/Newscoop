c6205197705d2b26a102befbebe1bad95c5371dc
paul <paul@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-03-14 20:46:34 +0000

    Added permission controls for menus - only display menu items that the user has permission to view

--- campsite/implementation/management/menu.php
+++ campsite/implementation/management/menu.php
@@ -6,10 +6,30 @@ list($access, $User) = check_basic_access($_REQUEST);
 if (!$access) {
 	exit;
 }
-$menu_index = 1;
-$max_menu_items = 5;
-$counter_reset = false;
-$counter_resets = 0;
+
+$showPublishingEnvironmentMenu = ($User->hasPermission("ManageTempl") 
+	|| $User->hasPermission("DeleteTempl") 
+	|| $User->hasPermission("ManageArticleTypes") 
+	|| $User->hasPermission("DeleteArticleTypes") 
+	|| $User->hasPermission("ManageTopics") 
+	|| $User->hasPermission("ManageLanguages") 
+	|| $User->hasPermission("DeleteLanguages") 
+	|| $User->hasPermission("ManageCountries") 
+	|| $User->hasPermission("DeleteCountries"));
+
+$showConfigureMenu = ($showPublishingEnvironmentMenu
+	|| $User->hasPermission("ManageLocalizer") 
+	|| $User->hasPermission("ViewLogs"));
+
+$showUserMenu = ($User->hasPermission("ManageUsers") 
+	|| $User->hasPermission("DeleteUsers") 
+	|| $User->hasPermission("ManageSubscriptions") 
+	|| $User->hasPermission("ManageUserTypes"));
+	
+$showObsoleteMenu = ($User->hasPermission("ManageDictionary") 
+	|| $User->hasPermission("DeleteDictionary") 
+	|| $User->hasPermission("ManageClasses"));
+	
 ?>
 <HEAD>
     <META http-equiv="Content-Type" content="text/html; charset=UTF-8">
@@ -21,37 +41,69 @@ $counter_resets = 0;
 	<SCRIPT LANGUAGE="JavaScript"><!--
 	var myMenu =
 	[
-	    ['<img src="/<?php p($ADMIN); ?>/img/icon/content.png" align="middle" style="padding-bottom: 3px;" />', ' <?php putGS('Content'); ?>', '', '', '',
-	    	['<img src="/<?php p($ADMIN); ?>/img/icon/publication.png" />', '<?php putGS('Publications'); ?>', '/<?php p($ADMIN); ?>/pub/index.php' ],
-	    	['<img src="/<?php p($ADMIN); ?>/img/icon/image_archive.png" />', '<?php putGS('Image archive'); ?>', '/<?php p($ADMIN); ?>/imagearchive/index.php' ]
+	    ['<img src="/<?php p($ADMIN); ?>/img/icon/content.gif" align="middle" style="padding-bottom: 3px;" />', ' <?php putGS('Content'); ?>', '', '', '',
+	    	['<img src="/<?php p($ADMIN); ?>/img/icon/publication.gif" />', '<?php putGS('Publications'); ?>', '/<?php p($ADMIN); ?>/pub/index.php' ],
+	    	['<img src="/<?php p($ADMIN); ?>/img/icon/image_archive.gif" />', '<?php putGS('Image archive'); ?>', '/<?php p($ADMIN); ?>/imagearchive/index.php' ]
 	    ],
 	    _cmSplit,
-	    ['<img src="/<?php p($ADMIN); ?>/img/icon/actions.png" align="middle" />', ' <?php putGS('Actions'); ?>', '', '', '',
-	    	['<img src="/<?php p($ADMIN); ?>/img/icon/add_article.png" />', '<?php putGS('Add new article'); ?>', '/<?php p($ADMIN); ?>/pub/add_article.php'],
-	    	['<img src="/<?php p($ADMIN); ?>/img/icon/change_password.png" />', '<?php putGS('Change your password'); ?>', '/<?php p($ADMIN); ?>/users/chpwd.php']
+	    ['<img src="/<?php p($ADMIN); ?>/img/icon/actions.gif" align="middle" />', ' <?php putGS('Actions'); ?>', '', '', '',
+	    	<?php if ($User->hasPermission("AddArticle")) { ?>
+	    	['<img src="/<?php p($ADMIN); ?>/img/icon/add_article.gif" />', '<?php putGS('Add new article'); ?>', '/<?php p($ADMIN); ?>/pub/add_article.php'],
+	    	<?php } ?>
+	    	['<img src="/<?php p($ADMIN); ?>/img/icon/change_password.gif" />', '<?php putGS('Change your password'); ?>', '/<?php p($ADMIN); ?>/users/chpwd.php']
 	    ],
+	    <?php if ($showConfigureMenu) { ?>
 	    _cmSplit,
-	    ['<img src="/<?php p($ADMIN); ?>/img/icon/configure.png" align="middle" />', ' <?php putGS('Configure'); ?>', '', '', '',
-	    	['<img src="/<?php p($ADMIN); ?>/img/icon/publishing_environment.png" />', '<?php putGS('Publishing environment'); ?>', '', '', '',
-	    		['<img src="/<?php p($ADMIN); ?>/img/icon/templates.png" />', '<?php putGS('Templates');?>', '/<?php p($ADMIN); ?>/templates/' ],
-	    		['<img src="/<?php p($ADMIN); ?>/img/icon/article_types.png" />', '<?php putGS('Article Types'); ?>', '/<?php p($ADMIN); ?>/a_types/' ],
-	    		['<img src="/<?php p($ADMIN); ?>/img/icon/topics.png" />', '<?php putGS('Topics'); ?>', '/<?php p($ADMIN); ?>/topics/' ],
-	    		['<img src="/<?php p($ADMIN); ?>/img/icon/languages.png" />', '<?php putGS('Languages'); ?>', '/<?php p($ADMIN); ?>/languages/' ],
-	    		['<img src="/<?php p($ADMIN); ?>/img/icon/countries.png" />', '<?php putGS('Countries'); ?>', '/<?php p($ADMIN); ?>/country/' ],
+	    ['<img src="/<?php p($ADMIN); ?>/img/icon/configure.gif" align="middle" />', ' <?php putGS('Configure'); ?>', '', '', '',
+	    	<?php if ($showPublishingEnvironmentMenu) { ?>
+	    	['<img src="/<?php p($ADMIN); ?>/img/icon/publishing_environment.gif" />', '<?php putGS('Publishing environment'); ?>', '', '', '',
+	    		<?php if ($User->hasPermission("ManageTempl") || $User->hasPermission("DeleteTempl")) { ?>
+	    		['<img src="/<?php p($ADMIN); ?>/img/icon/templates.gif" />', '<?php putGS('Templates');?>', '/<?php p($ADMIN); ?>/templates/' ],
+	    		<?php } ?>
+	    		<?php if ($User->hasPermission("ManageArticleTypes") || $User->hasPermission("DeleteArticleTypes")) { ?>
+	    		['<img src="/<?php p($ADMIN); ?>/img/icon/article_types.gif" />', '<?php putGS('Article Types'); ?>', '/<?php p($ADMIN); ?>/a_types/' ],
+	    		<?php } ?>
+	    		<?php if ($User->hasPermission("ManageTopics")) { ?>
+	    		['<img src="/<?php p($ADMIN); ?>/img/icon/topics.gif" />', '<?php putGS('Topics'); ?>', '/<?php p($ADMIN); ?>/topics/' ],
+	    		<?php } ?>
+	    		<?php if ($User->hasPermission("ManageLanguages") || $User->hasPermission("DeleteLanguages")) { ?>
+	    		['<img src="/<?php p($ADMIN); ?>/img/icon/languages.gif" />', '<?php putGS('Languages'); ?>', '/<?php p($ADMIN); ?>/languages/' ],
+	    		<?php } ?>
+	    		<?php if ($User->hasPermission("ManageCountries") || $User->hasPermission("DeleteCountries")) { ?>
+	    		['<img src="/<?php p($ADMIN); ?>/img/icon/countries.gif" />', '<?php putGS('Countries'); ?>', '/<?php p($ADMIN); ?>/country/' ],
+	    		<?php } ?>
 	    	],
-	    	['<img src="/<?php p($ADMIN); ?>/img/icon/localizer.png" />', '<?php putGS('Localizer'); ?>', '/<?php p($ADMIN); ?>/localizer/',  ],
-	    	['<img src="/<?php p($ADMIN); ?>/img/icon/logs.png" />', '<?php putGS('Logs'); ?>', 'logs/', ]
+	    	<?php } // if ($showPublishingEnvironmentMenu) ?>
+	    	<?php if ($User->hasPermission("ManageLocalizer")) { ?>
+	    	['<img src="/<?php p($ADMIN); ?>/img/icon/localizer.gif" />', '<?php putGS('Localizer'); ?>', '/<?php p($ADMIN); ?>/localizer/',  ],
+	    	<?php } ?>
+	    	<?php if ($User->hasPermission("ViewLogs")) { ?>
+	    	['<img src="/<?php p($ADMIN); ?>/img/icon/logs.gif" />', '<?php putGS('Logs'); ?>', 'logs/', ]
+	    	<?php } ?>
 	    ],
+	    <?php } // if ($showConfigureMenu) ?>
+	    <?php if ($showUserMenu) { ?>
 	    _cmSplit,
-	    ['<img src="/<?php p($ADMIN); ?>/img/icon/users.png" align="middle" />', ' <?php putGS('Users'); ?>', '', '', '',
-	    	['<img src="/<?php p($ADMIN); ?>/img/icon/users.png" />', '<?php putGS('Users'); ?>', '/<?php p($ADMIN); ?>/users/' ], 
-	    	['<img src="/<?php p($ADMIN); ?>/img/icon/user_types.png" />', '<?php putGS('User Types'); ?>', '/<?php p($ADMIN); ?>/u_types/' ], 
+	    ['<img src="/<?php p($ADMIN); ?>/img/icon/users.gif" align="middle" />', ' <?php putGS('Users'); ?>', '', '', '',
+	    	<?php if ($User->hasPermission("ManageUsers") || $User->hasPermission("DeleteUsers") || $User->hasPermission("ManageSubscriptions")) { ?>
+	    	['<img src="/<?php p($ADMIN); ?>/img/icon/users.gif" />', '<?php putGS('Users'); ?>', '/<?php p($ADMIN); ?>/users/' ], 
+	    	<?php } ?>
+	    	<?php if ($User->hasPermission("ManageUserTypes")) { ?>
+	    	['<img src="/<?php p($ADMIN); ?>/img/icon/user_types.gif" />', '<?php putGS('User Types'); ?>', '/<?php p($ADMIN); ?>/u_types/' ], 
+	    	<?php } ?>
 	    ],
+	    <?php } // if ($showUserMenu) ?>
+	    <?php if ($showObsoleteMenu)  { ?>
 	    _cmSplit,
-	    ['<img src="/<?php p($ADMIN); ?>/img/icon/obsolete.png" align="middle" />', ' <?php putGS('Obsolete'); ?>', '', '', '',
-	    	['<img src="/<?php p($ADMIN); ?>/img/icon/glossary.png" />', '<?php putGS('Glossary'); ?>', '/<?php p($ADMIN); ?>/glossary/' ], 
-	    	['<img src="/<?php p($ADMIN); ?>/img/icon/infotypes.png" />', '<?php putGS('Infotype'); ?>', '/<?php p($ADMIN); ?>/infotype/' ], 
-	    ]	    	
+	    ['<img src="/<?php p($ADMIN); ?>/img/icon/obsolete.gif" align="middle" />', ' <?php putGS('Obsolete'); ?>', '', '', '',
+	    	<?php if ($User->hasPermission("ManageDictionary") || $User->hasPermission("DeleteDictionary")) { ?>
+	    	['<img src="/<?php p($ADMIN); ?>/img/icon/glossary.gif" />', '<?php putGS('Glossary'); ?>', '/<?php p($ADMIN); ?>/glossary/' ], 
+	    	<?php } ?>
+	    	<?php if ($User->hasPermission("ManageClasses")) { ?>
+	    	['<img src="/<?php p($ADMIN); ?>/img/icon/infotypes.gif" />', '<?php putGS('Infotype'); ?>', '/<?php p($ADMIN); ?>/infotype/' ], 
+	    	<?php } ?>
+	    ]
+	    <?php } // if ($showObsoleteMenu) ?>
 	];
 	--></SCRIPT>
 
@@ -64,11 +116,11 @@ $counter_resets = 0;
 	<td valign="top" align="left" width="100%" style="padding-top: 0px; ">
 		<table border="0" cellpadding="0" cellspacing="0">
 		<tr>
-			<td style="padding-left: 5px; padding-right: 10px; padding-top: 2px; padding-bottom: 2px; font-size: 14pt; font-weight: bold; color: black; font-style: Verdana;" align="left" valign="middle">
+			<td style="padding-left: 10px; padding-right: 20px; padding-top: 2px; padding-bottom: 2px; font-size: 14pt; font-weight: bold; color: black; font-style: Verdana;" align="left" valign="middle">
 				<IMG SRC="/admin/img/sign_big.gif" BORDER="0" align="middle">
 				Campsite v<?php p($Campsite['version']); ?>
 			</td>
-			<td style="padding-left: 2px;">
+			<td style="padding-left: 5px;">
 			<DIV ID="myMenuID"></DIV>
 			<SCRIPT LANGUAGE="JavaScript"><!--
 				cmDraw ('myMenuID', myMenu, 'hbr', cmThemeOffice, 'ThemeOffice');
@@ -81,21 +133,21 @@ $counter_resets = 0;
 		<table cellpadding="0" cellspacing="0">
 		<tr>
 			<td>
-				<A HREF="/<?php p($ADMIN); ?>/home.php"><img src="/<?php p($ADMIN); ?>/img/icon/home.png" border="0" alt="<?php putGS('Home'); ?>"></a>
+				<A HREF="/<?php p($ADMIN); ?>/home.php"><img src="/<?php p($ADMIN); ?>/img/icon/home.gif" border="0" alt="<?php putGS('Home'); ?>"></a>
 			</td>
 			<td style="font-weight: bold; padding-left: 2px;">
 				<A HREF="/<?php p($ADMIN); ?>/home.php" style="color: black; text-decoration: none;"><?php putGS('Home'); ?></A>
 			</td>
 			
 			<td style="padding-left: 10px;">
-				<A HREF="" ONCLICK="window.open('/<?php p($ADMIN); ?>/popup/', 'fpopup', 'menu=no,width=500,height=410'); return false;"><img src="/<?php p($ADMIN); ?>/img/icon/quick_menu.png" border="0" alt="<?php putGS('Quick Menu'); ?>"></a>
+				<A HREF="" ONCLICK="window.open('/<?php p($ADMIN); ?>/popup/', 'fpopup', 'menu=no,width=500,height=410'); return false;"><img src="/<?php p($ADMIN); ?>/img/icon/quick_menu.gif" border="0" alt="<?php putGS('Quick Menu'); ?>"></a>
 			</td>
 			<td style="font-weight: bold; padding-left: 2px;" nowrap>
 				<A HREF="" ONCLICK="window.open('/<?php p($ADMIN); ?>/popup/', 'fpopup', 'menu=no,width=500,height=410'); return false;" style="color: black; text-decoration: none;"><?php putGS('Quick Menu'); ?></a>
 			</td>
 			
 			<td style="padding-left: 10px;">
-				<A HREF="/<?php p($ADMIN); ?>/logout.php"><img src="/<?php p($ADMIN); ?>/img/icon/logout.png" border="0" alt="<?php putGS('Logout'); ?>"></a>
+				<A HREF="/<?php p($ADMIN); ?>/logout.php"><img src="/<?php p($ADMIN); ?>/img/icon/logout.gif" border="0" alt="<?php putGS('Logout'); ?>"></a>
 			</td>
 			<td style="font-weight: bold; padding-left: 2px; padding-right: 10px;">
 				<A HREF="/<?php p($ADMIN); ?>/logout.php" style="color: black; text-decoration: none;"><?php putGS('Logout'); ?></a>
