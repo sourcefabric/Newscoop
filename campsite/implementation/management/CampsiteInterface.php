400f8414c61f5d13fbaa50d953f8e8020c094ca8
paul <paul@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-03-14 23:19:54 +0000

    Fixed copyright notice

--- campsite/implementation/management/CampsiteInterface.php
+++ campsite/implementation/management/CampsiteInterface.php
@@ -11,13 +11,12 @@ class CampsiteInterface {
 	function CopyrightNotice() {
 		global $Campsite;
 		?>
-		<HR NOSHADE SIZE="1">
-		<a style='font-size:8pt; color: black;' 
-		   href='http://www.campware.org' 
-		   target='campware'>
-		CAMPSITE  <?php echo $Campsite["version"] ?> &copy 1999-2005 MDLF, 
+		<TABLE width="100%" style="border-top: 1px solid black;"><tr><td style="padding-left: 5px;">
+		<a style="font-size:8pt; color: black;" href="http://www.campware.org" target="campware">
+		Campsite <?php echo $Campsite["version"] ?> &copy 1999-2005 MDLF, 
 		maintained and distributed under GNU GPL by CAMPWARE
 		</a>
+		</td></tr></table>
 		</BODY>
 		</HTML>		
 		<?php
