3431328d4057724e990d8ecb5cffd398685ec2b7
paul <paul@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-03-15 05:26:58 +0000

    Removed delay when logging out - previously it would wait to load the page before logging out.

--- campsite/implementation/management/logout.php
+++ campsite/implementation/management/logout.php
@@ -7,30 +7,6 @@ setcookie("TOL_UserKey", "", time() - 3600);
 if (function_exists ("incModFile")) {
 	incModFile ();
 }
+header("Location: /$ADMIN/login.php");
+exit;
 ?>
-
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN"
-	"http://www.w3.org/TR/REC-html40/loose.dtd">
-<HTML>
-<HEAD>
-    <META http-equiv="Content-Type" content="text/html; charset=UTF-8">
-	<script>
-	<!--
-	/*
-	A slightly modified version of "Break-out-of-frames script"
-	By JavaScript Kit (http://javascriptkit.com)
-	*/
-	
-	if (window != top.fmain && window != top) {
-		if (top.fmenu)
-			top.fmain.location.href=location.href
-		else
-			top.location.href=location.href
-	}
-	// -->
-	</script>
-	<META HTTP-EQUIV="Expires" CONTENT="now">
-</HEAD>
-<BODY OnLoad="parent.location='/<?php echo $ADMIN; ?>/login.php'">
-</BODY>
-</HTML>
