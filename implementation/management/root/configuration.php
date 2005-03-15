e089032939fcdba4e1ded8a720da232a3f0aaf29
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-03-15 16:59:38 +0000

    removed script common and instance dir - not needed anymore

--- implementation/management/root/configuration.php
+++ implementation/management/root/configuration.php
@@ -9,13 +9,11 @@ foreach ($configuration_files as $index=>$conf_file)
 
 $Campsite['HTML_COMMON_DIR'] = $Campsite['WWW_COMMON_DIR'] . "/html";
 $Campsite['CGI_COMMON_DIR'] = $Campsite['WWW_COMMON_DIR'] . "/cgi-bin";
-$Campsite['SCRIPT_COMMON_DIR'] = $Campsite['WWW_COMMON_DIR'] . "/script";
 $Campsite['INCLUDE_COMMON_DIR'] = $Campsite['WWW_COMMON_DIR'] . "/include";
 
 $db_name = $Campsite['DATABASE_NAME'];
 $Campsite['HTML_DIR'] = $Campsite['WWW_DIR'] . "/$db_name/html";
 $Campsite['CGI_DIR'] = $Campsite['WWW_DIR'] . "/$db_name/cgi-bin";
-$Campsite['SCRIPT_DIR'] = $Campsite['WWW_DIR'] . "/$db_name/script";
 $Campsite['INCLUDE_DIR'] = $Campsite['WWW_DIR'] . "/$db_name/include";
 
 $ADMIN_DIR = "admin-files";
