35b800c71e556542d7a84156d3f3b52de61958b0
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-03-16 18:00:15 +0000

    add new events to Events table

---
+++ implementation/database/upgrade/2.1.x/data-required.sql
@@ -0,0 +1,6 @@
+BEGIN;
+
+INSERT INTO Events (Id, Name, Notify, IdLanguage) VALUES (154, 'Duplicate section', 'N', 1);
+INSERT INTO Events (Id, Name, Notify, IdLanguage) VALUES (155, 'Duplicate article', 'N', 1);
+
+COMMIT;
