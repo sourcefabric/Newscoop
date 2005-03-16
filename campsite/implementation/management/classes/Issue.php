682a039cf4379c4b05903ee86968bc448ca18706
paul <paul@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-03-16 21:28:22 +0000

    Used DbObjectArray class to simplify code.  Added comments.  Added sqlOptions to GetIssuesInPublication().

--- campsite/implementation/management/classes/Issue.php
+++ campsite/implementation/management/classes/Issue.php
@@ -1,6 +1,7 @@
 <?php
 require_once($_SERVER['DOCUMENT_ROOT'].'/classes/config.php');
 require_once($_SERVER['DOCUMENT_ROOT'].'/classes/DatabaseObject.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/classes/DbObjectArray.php');
 
 class Issue extends DatabaseObject {
 	var $m_dbTableName = 'Issues';
@@ -17,15 +18,21 @@ class Issue extends DatabaseObject {
 		'ArticleTplId',
 		'ShortName');
 	
-	function Issue($p_publicationId, $p_languageId, $p_issueId = null) {
+	/**
+	 * 
+	 * @param int p_publicationId
+	 * @param int p_languageId
+	 * @param int p_issueId
+	 */
+	function Issue($p_publicationId = null, $p_languageId = null, $p_issueId = null) {
 		parent::DatabaseObject($this->m_columnNames);
 		$this->setProperty('IdPublication', $p_publicationId, false);
 		$this->setProperty('IdLanguage', $p_languageId, false);
 		$this->setProperty('Number', $p_issueId, false);
-		if (!is_null($p_issueId)) {
+		if (!is_null($p_publicationId) && !is_null($p_languageId) && !is_null($p_issueId)) {
 			$this->fetch();
 		}
-	} // ctor
+	} // constructor
 
 	
 	/**
@@ -33,75 +40,104 @@ class Issue extends DatabaseObject {
 	 * @return array
 	 */
 	function GetAllIssues() {
-		global $Campsite;
+		//global $Campsite;
 		$queryStr = 'SELECT * FROM Issues ';
-		$query = $Campsite['db']->Execute($queryStr);
-		$issues = array();
-		while ($row = $query->FetchRow($queryStr)) {
-			$tmpIssue =& new Issue();
-			$tmpIssue->fetch($row);
-			$issues[] = $tmpIssue;
-		}
+		$issues =& DbObjectArray::Create('Issue', $queryStr);
+//		$query = $Campsite['db']->Execute($queryStr);
+//		$issues = array();
+//		while ($row = $query->FetchRow($queryStr)) {
+//			$tmpIssue =& new Issue();
+//			$tmpIssue->fetch($row);
+//			$issues[] = $tmpIssue;
+//		}
 		return $issues;		
 	} // fn GetAllIssues
 	
 	
-	function GetIssuesInPublication($p_publicationId, $p_languageId = null) {
-		global $Campsite;
+	/**
+	 * Get all the issues in the given publication as return them as an array 
+	 * of Issue objects.
+	 *
+	 * @param int p_publicationId
+	 * @param int p_languageId
+	 * @param array p_sqlOptions
+	 *
+	 * @return array
+	 */
+	function GetIssuesInPublication($p_publicationId, $p_languageId = null, $p_sqlOptions = null) {
 		$queryStr = 'SELECT * FROM Issues '
 					." WHERE IdPublication='".$p_publicationId."'";
 		if (!is_null($p_languageId)) {
 			$queryStr .= " AND IdLanguage='".$p_languageId."'";
 		}
-		$query = $Campsite['db']->Execute($queryStr);
-		$issues = array();
-		while ($row = $query->FetchRow()) {
-			$tmpIssue =& new Issue($row['IdPublication'], $row['IdLanguage']);
-			$tmpIssue->fetch($row);
-			$issues[] = $tmpIssue;
-		}
+		$queryStr = DatabaseObject::ProcessOptions($queryStr, $p_sqlOptions);
+		$issues =& DbObjectArray::Create('Issue', $queryStr);
 		return $issues;
 	} // fn GetAllIssuesInPublication
 	
 	
+	/**
+	 * @return int
+	 */
 	function getPublicationId() {
 		return $this->getProperty('IdPublication');
 	} // fn getPublicationId
 	
 	
+	/**
+	 * @return int
+	 */
 	function getLanguageId() {
 		return $this->getProperty('IdLanguage');
 	} // fn getLanguageId
 	
 	
+	/**
+	 * @return int
+	 */
 	function getIssueId() {
 		return $this->getProperty('Number');
 	} // fn getIssueId
 	
 	
+	/**
+	 * @return string
+	 */
 	function getName() {
 		return $this->getProperty('Name');
 	} // fn getName
 	
 	
+	/**
+	 * @return string
+	 */
 	function getShortName() {
 		return $this->getProperty('ShortName');
-	}
+	} // fn getShortName
 	
 	
+	/**
+	 * @return int
+	 */
 	function getArticleTemplateId() {
 		return $this->getProperty('ArticleTplId');
-	}
+	} // fn getArticleTemplateId
 	
 	
+	/**
+	 * @return int
+	 */
 	function getSectionTemplateId() {
 		return $this->getProperty('SectionTplId');
-	}
+	} // fn getSectionTemplateId
 	
 	
+	/**
+	 * @return int
+	 */
 	function getIssueTemplateId() {
 		return $this->getProperty('IssueTplId');
-	}
+	} // fn getIssueTemplateId
 } // class Issue
 
 ?>
