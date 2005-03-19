b52d0f9ac6f229eed5c4a63067a779a6f99a838c
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-03-19 15:51:35 +0000

    implemented more functions

--- campsite/implementation/parser/parser/ccampsiteinstance.cpp
+++ campsite/implementation/parser/parser/ccampsiteinstance.cpp
@@ -25,22 +25,77 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include <pwd.h>
 #include <grp.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <sys/stat.h> 
+#include <unistd.h>
 
 #include "ccampsiteinstance.h"
 
 using std::cout;
 using std::endl;
 
-pid_t CCampsiteInstance::run(InstanceFunction p_pInstanceFunction) throw (RunException)
+
+/**
+ * class CCampsiteInstance implementation
+ *
+ */
+
+bool CCampsiteInstance::isRunning() const
 {
+	return m_bRunning;
+}
+
+pid_t CCampsiteInstance::run() throw (RunException)
+{
+	return -1;
 }
 
 void CCampsiteInstance::stop()
 {
+	if (!m_bRunning)
+		return;
+}
+
+const CCampsiteInstanceMap& CCampsiteInstance::readFromDirectory(const string& p_rcoDir,
+		InstanceFunction p_pInstFunc) throw (ConfException)
+{
+	DIR* pDir = opendir(p_rcoDir.c_str());
+	if (pDir == NULL)
+		throw ConfException(string("Invalid configuration directory ") + p_rcoDir);
+
+	for (struct dirent* pFile = readdir(pDir); pFile != NULL; pFile = readdir(pDir))
+	{
+		if (strcmp(pFile->d_name, ".") == 0 || strcmp(pFile->d_name, "..") == 0)
+			continue;
+
+		string coFileName = p_rcoDir + "/" + pFile->d_name;
+		struct stat FileStat;
+		if (stat(coFileName.c_str(), &FileStat) != 0)
+			continue;
+		if (!S_ISDIR(FileStat.st_mode))
+			continue;
+
+		new CCampsiteInstance(coFileName, p_pInstFunc);
+	}
+
+	closedir(pDir);
+
+	return CCampsiteInstanceRegister::get().getCampsiteInstances();
 }
 
 const ConfAttrValue& CCampsiteInstance::ReadConf() throw (ConfException)
 {
+	VerifyDir(m_coConfDir);
+
+	string::size_type nSlashPos = m_coConfDir.rfind('/');
+	while ((nSlashPos + 1) == m_coConfDir.length() && nSlashPos != string::npos)
+	{
+		m_coConfDir.erase(nSlashPos, 1);
+		nSlashPos = m_coConfDir.rfind('/');
+	}
+	m_coName = m_coConfDir.substr(nSlashPos != string::npos ? nSlashPos + 1 : 0);
+
 	// read parser configuration
 	string coParserConfFile = m_coConfDir + "/parser_conf.php";
 	m_coAttributes.open(coParserConfFile);
@@ -60,4 +115,84 @@ const ConfAttrValue& CCampsiteInstance::ReadConf() throw (ConfException)
 	m_coAttributes.open(coDatabaseConfFile);
 
 	return m_coAttributes;
+}
+
+void CCampsiteInstance::RegisterInstance()
+{
+	CCampsiteInstanceRegister::get().insert(*this);
+}
+
+void CCampsiteInstance::VerifyDir(const string& p_rcoDir) throw (ConfException)
+{
+	DIR* pDir = opendir(p_rcoDir.c_str());
+	if (pDir == NULL)
+		throw ConfException(string("Invalid configuration directory ") + p_rcoDir);
+	closedir(pDir);
+}
+
+
+/**
+ * class CCampsiteInstanceRegister implementation
+ *
+ */
+
+CCampsiteInstanceRegister g_coCampsiteInstanceRegister;
+
+
+CCampsiteInstanceRegister& CCampsiteInstanceRegister::get()
+{
+	return g_coCampsiteInstanceRegister;
+}
+
+void CCampsiteInstanceRegister::erase(pid_t p_nInstancePID)
+{
+#ifdef _REENTRANT
+	CMutexHandler coLockHandler(&m_coMutex);
+#endif
+	map < pid_t, string, less<pid_t> >::iterator coIt;
+	coIt = m_coInstancePIDs.find(p_nInstancePID);
+	if (coIt != m_coInstancePIDs.end())
+		erase((*coIt).second);
+}
+
+void CCampsiteInstanceRegister::erase(const string& p_rcoInstanceName)
+{
+#ifdef _REENTRANT
+	CMutexHandler coLockHandler(&m_coMutex);
+#endif
+	CCampsiteInstanceMap::const_iterator coIt;
+	coIt = m_coCCampsiteInstances.find(p_rcoInstanceName);
+	if (coIt == m_coCCampsiteInstances.end())
+		return;
+	CCampsiteInstance* pcoInstance = (*coIt).second;
+	if (pcoInstance->isRunning())
+		m_coInstancePIDs.erase(pcoInstance->getPID());
+	m_coCCampsiteInstances.erase(p_rcoInstanceName);
+	delete pcoInstance;
+}
+
+const CCampsiteInstance* CCampsiteInstanceRegister::getCampsiteInstance(pid_t p_nInstancePID) const
+		throw (InvalidValue)
+{
+#ifdef _REENTRANT
+	CMutexHandler coLockHandler(&m_coMutex);
+#endif
+	map < pid_t, string, less<pid_t> >::const_iterator coIt;
+	coIt = m_coInstancePIDs.find(p_nInstancePID);
+	if (coIt == m_coInstancePIDs.end())
+		throw InvalidValue("Campsite instance name");
+	return getCampsiteInstance((*coIt).second);
+}
+
+const CCampsiteInstance* CCampsiteInstanceRegister::getCampsiteInstance(const string& p_rcoInstanceName) const
+		throw (InvalidValue)
+{
+#ifdef _REENTRANT
+	CMutexHandler coLockHandler(&m_coMutex);
+#endif
+	CCampsiteInstanceMap::const_iterator coIt;
+	coIt = m_coCCampsiteInstances.find(p_rcoInstanceName);
+	if (coIt == m_coCCampsiteInstances.end())
+		throw InvalidValue("Campsite instance PID");
+	return (*coIt).second;
 }
