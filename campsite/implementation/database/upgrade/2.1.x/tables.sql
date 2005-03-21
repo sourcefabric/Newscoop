a6a54fc31b7b862f582d9756cbc9b7eccbacdfd3
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-03-21 17:08:35 +0000

    Added new permissions for HTMLArea WYSIWYG editor in UserTypes table

--- campsite/implementation/database/upgrade/2.1.x/tables.sql
+++ campsite/implementation/database/upgrade/2.1.x/tables.sql
@@ -266,4 +266,28 @@ ADD `EditorUnderline` ENUM( 'N', 'Y' ) DEFAULT 'N' NOT NULL ,
 ADD `EditorUndoRedo` ENUM( 'N', 'Y' ) DEFAULT 'N' NOT NULL ,
 ADD `EditorCopyCutPaste` ENUM( 'N', 'Y' ) DEFAULT 'N' NOT NULL ;
 
+ALTER TABLE `UserTypes` ADD `EditorImage` ENUM( 'N', 'Y' ) DEFAULT 'N' NOT NULL ,
+ADD `EditorTextAlignment` ENUM( 'N', 'Y' ) DEFAULT 'N' NOT NULL ,
+ADD `EditorFontColor` ENUM( 'N', 'Y' ) DEFAULT 'N' NOT NULL ,
+ADD `EditorFontSize` ENUM( 'N', 'Y' ) DEFAULT 'N' NOT NULL ,
+ADD `EditorFontFace` ENUM( 'N', 'Y' ) DEFAULT 'N' NOT NULL ,
+ADD `EditorTable` ENUM( 'N', 'Y' ) DEFAULT 'N' NOT NULL ,
+ADD `EditorSuperscript` ENUM( 'N', 'Y' ) DEFAULT 'N' NOT NULL ,
+ADD `EditorSubscript` ENUM( 'N', 'Y' ) DEFAULT 'N' NOT NULL ,
+ADD `EditorStrikethrough` ENUM( 'N', 'Y' ) DEFAULT 'N' NOT NULL ,
+ADD `EditorIndent` ENUM( 'N', 'Y' ) DEFAULT 'N' NOT NULL ,
+ADD `EditorListBullet` ENUM( 'N', 'Y' ) DEFAULT 'N' NOT NULL ,
+ADD `EditorListNumber` ENUM( 'N', 'Y' ) DEFAULT 'N' NOT NULL ,
+ADD `EditorHorizontalRule` ENUM( 'N', 'Y' ) DEFAULT 'N' NOT NULL ,
+ADD `EditorSourceView` ENUM( 'N', 'Y' ) DEFAULT 'N' NOT NULL ,
+ADD `EditorEnlarge` ENUM( 'N', 'Y' ) DEFAULT 'N' NOT NULL ,
+ADD `EditorTextDirection` ENUM( 'N', 'Y' ) DEFAULT 'N' NOT NULL ,
+ADD `EditorLink` ENUM( 'N', 'Y' ) DEFAULT 'N' NOT NULL ,
+ADD `EditorSubhead` ENUM( 'N', 'Y' ) DEFAULT 'N' NOT NULL,
+ADD `EditorBold` ENUM( 'N', 'Y' ) DEFAULT 'N' NOT NULL ,
+ADD `EditorItalic` ENUM( 'N', 'Y' ) DEFAULT 'N' NOT NULL ,
+ADD `EditorUnderline` ENUM( 'N', 'Y' ) DEFAULT 'N' NOT NULL ,
+ADD `EditorUndoRedo` ENUM( 'N', 'Y' ) DEFAULT 'N' NOT NULL ,
+ADD `EditorCopyCutPaste` ENUM( 'N', 'Y' ) DEFAULT 'N' NOT NULL ;
+
 COMMIT;
