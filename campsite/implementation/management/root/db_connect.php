27b9525adc9eebdb2c9670470fd5ef42a20eb32d
paul <paul@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-03-22 22:38:09 +0000

    Fixed bug where it could try to connect to the database multiple times in one session.  Now it will only connect if it isnt already connected.

--- campsite/implementation/management/root/db_connect.php
+++ campsite/implementation/management/root/db_connect.php
@@ -1,13 +1,16 @@
 <?php
 
 require_once($_SERVER['DOCUMENT_ROOT'].'/include/adodb/adodb.inc.php');
-require($_SERVER['DOCUMENT_ROOT'].'/configuration.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/configuration.php');
 
-$Campsite['db'] = ADONewConnection('mysql'); # eg 'mysql' or 'postgres'
-//$Campsite['db']->debug = true;
-// Set fetch mode to return associative arrays
-$Campsite['db']->SetFetchMode(ADODB_FETCH_ASSOC);
-$Campsite['db']->Connect($Campsite['DATABASE_SERVER_ADDRESS'], $Campsite['DATABASE_USER'],
-	$Campsite['DATABASE_PASSWORD'], $Campsite['DATABASE_NAME']);
+global $Campsite;
+if (!isset($Campsite['db'])) {
+	$Campsite['db'] = ADONewConnection('mysql'); # eg 'mysql' or 'postgres'
+	//$Campsite['db']->debug = true;
+	// Set fetch mode to return associative arrays
+	$Campsite['db']->SetFetchMode(ADODB_FETCH_ASSOC);
+	$Campsite['db']->Connect($Campsite['DATABASE_SERVER_ADDRESS'], $Campsite['DATABASE_USER'],
+		$Campsite['DATABASE_PASSWORD'], $Campsite['DATABASE_NAME']);
+}
 
 ?>
