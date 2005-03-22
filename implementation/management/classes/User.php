f33af77796cc72d7f9b984a65029e541fa442f7e
paul <paul@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-03-22 22:45:37 +0000

    Replaced the config.php with db_connect.php, which includes configuration.php - the new main config file.

--- implementation/management/classes/User.php
+++ implementation/management/classes/User.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once($_SERVER['DOCUMENT_ROOT'].'/classes/config.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/db_connect.php');
 require_once($_SERVER['DOCUMENT_ROOT'].'/classes/DatabaseObject.php');
 
 class User extends DatabaseObject {
