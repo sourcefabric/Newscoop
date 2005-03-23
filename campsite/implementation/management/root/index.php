2d4d0c70d2e9d441c75817214f14b36929025bce
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-03-23 00:44:57 +0000

    rewrote to use common parser connect functions

--- campsite/implementation/management/root/index.php
+++ campsite/implementation/management/root/index.php
@@ -1,79 +1,43 @@
 <?php
 
-$method = $_SERVER['REQUEST_METHOD'];
-if ($method == 'POST')
-	$params = copyParamArray($_POST);
-else
-	$params = readParameters($_SERVER['QUERY_STRING']);
+global $_SERVER;
+global $Campsite;
 
-$msg = "<CampsiteMessage MessageType=\"URLRequest\" MessageId=\"14214\">\n";
-$msg .= "\t<HTTPHost>" . htmlspecialchars($_SERVER['HTTP_HOST']) . "</HTTPHost>\n";
-$msg .= "\t<DocumentRoot>" . htmlspecialchars($_SERVER['DOCUMENT_ROOT']) . "</DocumentRoot>\n";
-$msg .= "\t<RemoteAddress>" . htmlspecialchars($_SERVER['REMOTE_ADDR']) . "</RemoteAddress>\n";
-$msg .= "\t<PathTranslated>" . htmlspecialchars($_SERVER['PATH_TRANSLATED']) . "</PathTranslated>\n";
-$msg .= "\t<RequestMethod>" . htmlspecialchars($_SERVER['REQUEST_METHOD']) . "</RequestMethod>\n";
-$msg .= "\t<RequestURI>" . htmlspecialchars($_SERVER['REQUEST_URI']) . "</RequestURI>\n";
-$msg .= "\t<Parameters>\n";
-for($i = 0; $i < count($params[0]); $i++)
-	$msg .= "\t\t<Parameter Name=\"" . htmlspecialchars($params[1][$i]) . "\" Type=\"string\">"
-	     . htmlspecialchars($params[2][$i]) . "</Parameter>\n";
-$msg .= "\t</Parameters>\n";
-$msg .= "\t<Cookies>\n";
-foreach ($_COOKIE as $cookie=>$value)
-	if ($value != "")
-		$msg .= "\t\t<Cookie Name=\"" . htmlspecialchars($cookie) . "\">" . htmlspecialchars($value)
-		     . "</Cookie>\n";
-$msg .= "\t</Cookies>\n";
-$msg .= "</CampsiteMessage>\n";
+// initialize needed global variables
+$_SERVER['DOCUMENT_ROOT'] = getenv("DOCUMENT_ROOT");
 
-//echo "<pre>\n" . htmlspecialchars($msg) . "\n";
-$size = sprintf("%04x", strlen($msg));
-//echo "size: " . $size . "\n";
+$DO_NOT_CONNECT_TO_DB = true;
+require_once($_SERVER['DOCUMENT_ROOT'].'/configuration.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/parser_utils.php');
 
-require_once("configuration.php");
-@$socket = fsockopen('127.0.0.1', $Campsite['PARSER_PORT'], $errno, $errstr, 30);
-if (!$socket) {
-	echo "$errstr ($errno)\n";
-	die();
-} else {
-//	echo "OK.\n</pre>\n";
-}
+// read server parameters
+$env_vars["HTTP_HOST"] = getenv("HTTP_HOST");
+$env_vars["DOCUMENT_ROOT"] = getenv("DOCUMENT_ROOT");
+$env_vars["REMOTE_ADDR"] = getenv("REMOTE_ADDR");
+$env_vars["PATH_TRANSLATED"] = getenv("PATH_TRANSLATED");
+$env_vars["REQUEST_METHOD"] = getenv("REQUEST_METHOD");
+$env_vars["REQUEST_URI"] = getenv("REQUEST_URI");
 
-fwrite($socket, "0001 ");
-fwrite($socket, $size . " ");
-fwrite($socket, $msg);
-// fclose($socket);
-// exit(0);
+// read parameters
+$parameters = read_parameters($query_string);
+$cookies = read_cookies($cookies_string);
 
-//echo "<p>reading:</p>\n";
-stream_set_timeout($socket, 10);
-do {
-	$str = fread($socket, 1000);
-	echo $str;
-} while ($str != "");
-fclose($socket);
+// echo "<p>request method: " . getenv("REQUEST_METHOD") . "</p>\n";
+// echo "<p>query string: $query_string</p>\n";
+// echo "<p>cookies string: $cookies_string</p>\n";
+// echo "<p>parameters:</p>\n";
+// foreach ($parameters as $name=>$value)
+// 	echo "<p>&nbsp;&nbsp;$name = $value</p>\n";
+// echo "<p>cookies:</p>\n";
+// foreach ($cookies as $name=>$value)
+// 	echo "<p>&nbsp;&nbsp;$name = $value</p>\n";
 
+$msg = create_url_request_message($env_vars, $parameters, $cookies);
+// echo "<p>URL request message:</p>\n";
+// echo "<pre>\n" . htmlspecialchars($msg) . "\n";
 
-function readParameters($queryString)
-{
-	$out = array();
-	$queryString .= "&";
-	preg_match_all("|([\w]*[%\d{2}]*)=([\w]*[%\d{2}]*)&|U", $queryString, $out, PREG_PATTERN_ORDER);
-	return $out;
-}
-
-function copyParamArray($params)
-{
-	$dest = array();
-	$i = 0;
-	foreach($params as $param=>$val)
-	{
-		$dest[0][$i] = $param . "=" . $val;
-		$dest[1][$i] = $param;
-		$dest[2][$i] = $val;
-		$i++;
-	}
-	return $dest;
-}
+$socket = send_message_to_parser($msg);
+$size_read = read_parser_output($socket);
+// echo "<p>size read: $size_read</p>\n";
 
 ?>
