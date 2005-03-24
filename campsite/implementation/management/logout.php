92b4a3a05446f4b162a2e9a06ce004c64e9aaafc
paul <paul@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-03-24 15:57:05 +0000

    Automatically unlock all the user's articles on logout.

--- campsite/implementation/management/logout.php
+++ campsite/implementation/management/logout.php
@@ -1,9 +1,18 @@
 <?php
+require_once($_SERVER['DOCUMENT_ROOT'].'/classes/Article.php');
+
+list($access, $User) = check_basic_access($_REQUEST);
+
 // Delete the cookies
 setcookie("TOL_UserId", "", time() - 3600);
 setcookie("TOL_UserKey", "", time() - 3600);
 
-## added by sebastian
+// Unlock all articles that are locked by this user
+if ($access) {
+	Article::UnlockByUser($User->getId());
+}
+
+// added by sebastian
 if (function_exists ("incModFile")) {
 	incModFile ();
 }
