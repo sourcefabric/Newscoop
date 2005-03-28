50c01d4e97b63276180d70aff6d9bc24c6e975d1
paul <paul@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-03-28 05:13:36 +0000

    Transformed this into a general error page.

--- implementation/management/ad.php
+++ implementation/management/ad.php
@@ -1,37 +1,39 @@
 <?php
-    require_once($_SERVER['DOCUMENT_ROOT'].'/db_connect.php');
-    require_once($Campsite['HTML_DIR']."/$ADMIN_DIR/lib_campsite.php");
-    $globalfile=selectLanguageFile($Campsite['HTML_DIR'] . "/$ADMIN_DIR",'globals');
-    $localfile=selectLanguageFile("$ADMIN_DIR","locals");
-    @include_once($globalfile);
-    @include_once($localfile);
-    require_once($Campsite['HTML_DIR'] . "/$ADMIN_DIR/languages.php");
+require_once('db_connect.php');
+require_once('CampsiteInterface.php');
+require_once('classes/common.php');
+load_common_include_files($ADMIN_DIR);
+require_once('classes/Input.php');
+$ADReason = Input::get('ADReason', 'string', getGS('You do not have the right to access this page.'), true);
+$BackLink = Input::get('Back', 'string', "/$ADMIN/home.php", true);
 ?>
-
-<?php  todef('ADReason',getGS('You do not have the right to access this page.'));?><CENTER><TABLE BORDER="0" CELLSPACING="0" CELLPADDING="8" class="message_box" ALIGN="CENTER">
-	<TR>
-		<TD COLSPAN="2">
-			<B> <font color="red"><?php  putGS("Access denied"); ?> </font></B>
-			<HR NOSHADE SIZE="1" COLOR="BLACK">
-		</TD>
-	</TR>
-	<TR>
-		<TD COLSPAN="2"><BLOCKQUOTE><font color=red><li><?php  print encHTML($ADReason); ?></li></font></BLOCKQUOTE></TD>
-	</TR>
-	<TR>
-		<TD COLSPAN="2">
+<p>
+<CENTER>
+<TABLE BORDER="0" CELLSPACING="0" CELLPADDING="8" class="message_box" ALIGN="CENTER" style="margin-top: 50px; margin-bottom: 50px;">
+<TR>
+	<TD COLSPAN="2">
+		<B><font color="red"><?php  putGS("Error"); ?> </font></B>
+		<HR NOSHADE SIZE="1" COLOR="BLACK">
+	</TD>
+</TR>
+<TR>
+	<TD COLSPAN="2">
+		<BLOCKQUOTE>
+		<font color="red">
+		<li><?php  print encHTML($ADReason); ?></li>
+		</font>
+		</BLOCKQUOTE>
+	</TD>
+</TR>
+<TR>
+	<TD COLSPAN="2" align="center">
 		<DIV ALIGN="CENTER">
-		<INPUT TYPE="button" class="button" NAME="OK" VALUE="<?php  putGS('OK'); ?>" ONCLICK="location.href='/admin/home.php'">
+		<INPUT TYPE="button" class="button" NAME="OK" VALUE="<?php  putGS('OK'); ?>" ONCLICK="location.href='<?php p($BackLink); ?>'">
 		</DIV>
-		</TD>
-	</TR>
-</TABLE></CENTER>
+	</TD>
+</TR>
+</TABLE>
+</CENTER>
 </FORM>
 
-<HR NOSHADE SIZE="1" COLOR="BLACK">
-<table cellpadding="0" cellspacing="0" width="100%"><tr><td style="padding-left: 5px;"><a STYLE="font-size:8pt;color:#000000" href="http://www.campware.org" target="campware">Campsite 2.2.0 &copy 1999-2005 MDLF, maintained and distributed under GNU GPL by CAMPWARE</a></td></tr></table>
-</BODY>
-
-</HTML>
-
-
+<?php CampsiteInterface::CopyrightNotice(); ?>
