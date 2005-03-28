76da7729d6100d1b09e6cdf608ffb23471f50e2c
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-03-28 16:30:45 +0000

    - replaced old input reading code - fixed logic when displaying the error messages

--- campsite/implementation/management/pub/issues/sections/articles/preview.php
+++ campsite/implementation/management/pub/issues/sections/articles/preview.php
@@ -1,49 +1,69 @@
 <?php  
 require_once($_SERVER['DOCUMENT_ROOT']. "/$ADMIN_DIR/pub/issues/sections/articles/article_common.php");
+
 list($access, $User) = check_basic_access($_REQUEST);
 if (!$access) {
 	header("Location: /$ADMIN/logout.php");
 	exit;
 }
-$Pub = isset($_REQUEST["Pub"])?$_REQUEST["Pub"]:0;
-$Issue = isset($_REQUEST["Issue"])?$_REQUEST["Issue"]:0;
-$Section = isset($_REQUEST["Section"])?$_REQUEST["Section"]:0;
-$Language = isset($_REQUEST["Language"])?$_REQUEST["Language"]:0;
-$sLanguage = isset($_REQUEST["sLanguage"])?$_REQUEST["sLanguage"]:0;
-$Article = isset($_REQUEST["Article"])?$_REQUEST["Article"]:0;
 
-$articleObj =& new Article($Pub, $Issue, $Section, $sLanguage, $Article);
+$Language = Input::get('Language', 'int', 0);
+$sLanguage = Input::get('sLanguage', 'int', 0);
+$Pub = Input::get('Pub', 'int', 0);
+$Issue = Input::get('Issue', 'int', 0);
+$Section = Input::get('Section', 'int', 0);
+$Article = Input::get('Article', 'int', 0);
+
+$languageObj = & new Language($Language);
+$publicationObj = & new Publication($Pub);
 $issueObj =& new Issue($Pub, $Language, $Issue);
-$templateObj =& new Template($issueObj->getArticleTemplateId());
+$sectionObj =& new Section($Pub, $Issue, $Language, $Section);
+$articleObj =& new Article($Pub, $Issue, $Section, $sLanguage, $Article);
 
-setcookie("TOL_Access", "all", null, "/");
-setcookie("TOL_Preview", "on", null, "/");
+$errorStr = "";
+if (!$articleObj->exists()) {
+	$errorStr = 'There was an error reading request parameters.';
+} else {
+	$templateId = $sectionObj->getArticleTemplateId();
+	if ($templateId == 0)
+		$templateId = $issueObj->getArticleTemplateId();
+	if ($templateId == 0)
+		$errorStr = 'This article cannot be previewed. Please make sure it has the article template selected.';
+}
 
+if ($errorStr != "")
+	CampsiteInterface::DisplayError($errorStr, null, true);
+
+setcookie("TOL_Access", "all", null, "/$ADMIN/");
+if ($User->hasPermission("ManageTempl") || $User->hasPermission("DeleteTempl"))
+	setcookie("TOL_Preview", "on", null, "/$ADMIN/");
+
+$templateObj =& new Template($templateId);
+
+$urlType = $publicationObj->getProperty('IdURLType');
+if ($urlType == 1) {
+	$templateObj = & new Template($templateId);
+	$url = "/look/" . $templateObj->getName() . "?IdLanguage=$Language"
+		. "&IdPublication=$Publication&NrIssue=$Issue&NrSection=$Section"
+		. "&NrArticle=$Article";
+} else {
+	$url = "/" . $languageObj->getCode() . "/" . $issueObj->getShortName();
+}
+
+if ($User->hasPermission("ManageTempl") || $User->hasPermission("DeleteTempl")) {
+	// Show dual-pane view for those with template management priviledges
 ?>
-<?php  
-if ($access) {
-	if (($articleObj->exists()) && ($templateObj->getName() != "")) {
-		if ($User->hasPermission("ManageTempl") || $User->hasPermission("DeleteTempl")) {
-			// Show dual-pane view for those with template management priviledges
-			?>
-			<FRAMESET ROWS="60%,*" BORDER="2">
-				<FRAME SRC="<?php print "/look/".$templateObj->getName(); ?>?IdPublication=<?php p($articleObj->getPublicationId()); ?>&NrIssue=<?php p($articleObj->getIssueId()); ?>&NrSection=<?php p($articleObj->getSectionId()); ?>&NrArticle=<?php p($articleObj->getArticleId()); ?>&IdLanguage=<?php p($articleObj->getLanguageId()); ?>" NAME="body" FRAMEBORDER="1">
-				<FRAME NAME="e" SRC="empty.php" FRAMEBORDER="1">
-			</FRAMESET>
-			<?php  
-		}
-		else {
-			// Show single pane for everyone else.
-			?>
-			<FRAMESET ROWS="100%">
-				<FRAME SRC="<?php print "/look/".$templateObj->getName(); ?>?IdPublication=<?php p($articleObj->getPublicationId()); ?>&NrIssue=<?php p($articleObj->getIssueId()); ?>&NrSection=<?php p($articleObj->getSectionId()); ?>&NrArticle=<?php p($articleObj->getArticleId()); ?>&IdLanguage=<?php p($articleObj->getLanguageId()); ?>" NAME="body" FRAMEBORDER="1">
-			</FRAMESET>
-			<?php
-		}
-	} 
-	else { 
-		ArticleTop($articleObj, $issueObj->getLanguageId(), "Preview article", false);
-		CampsiteInterface::DisplayError('This article cannot be previewed. Please make sure it has a <B><I>single article</I></B> template selected.');
-	}
-} 
+	<FRAMESET ROWS="60%,*" BORDER="2">
+		<FRAME SRC="<?php print $url; ?>" NAME="body" FRAMEBORDER="1">
+		<FRAME NAME="e" SRC="empty.php" FRAMEBORDER="1">
+	</FRAMESET>
+<?php
+} else {
+	// Show single pane for everyone else.
+?>
+	<FRAMESET ROWS="100%">
+		<FRAME SRC="<?php print $url; ?>" NAME="body" FRAMEBORDER="1">
+	</FRAMESET>
+<?php
+}
 ?>
