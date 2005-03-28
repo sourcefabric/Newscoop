28b6baf15d27a90045c608c17652710fab29306f
paul <paul@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-03-28 05:20:31 +0000

    Changed parameter names to the standard format.

--- implementation/management/classes/common.php
+++ implementation/management/classes/common.php
@@ -1,39 +1,41 @@
 <?php
 /**
  * Load include files.
+ * @param string p_currentDir
+ * @return void
  */
-function load_common_include_files($current_dir) {
+function load_common_include_files($p_currentDir) {
 	global $ADMIN_DIR;
 	require_once($_SERVER['DOCUMENT_ROOT'].'/configuration.php');
 	require_once($_SERVER['DOCUMENT_ROOT']."/$ADMIN_DIR/lib_campsite.php");
 	$globalfile = selectLanguageFile($_SERVER['DOCUMENT_ROOT']."/$ADMIN_DIR",'globals');
-	$localfile = selectLanguageFile($_SERVER['DOCUMENT_ROOT']."/$current_dir",'locals');
+	$localfile = selectLanguageFile($_SERVER['DOCUMENT_ROOT']."/$p_currentDir",'locals');
 	require_once($globalfile);
 	require_once($localfile);
 	require_once($_SERVER['DOCUMENT_ROOT']."/$ADMIN_DIR/languages.php");
 } // fn load_common_include_files
 
+
 /**
  * Check if user has access to this screen.
- *
+ * @param array p_request
  * @return array
- *
  */
-function check_basic_access($request) {
+function check_basic_access($p_request) {
 	require_once($_SERVER['DOCUMENT_ROOT'].'/classes/User.php');
 	$access = false;
 	$XPerm = array();
 	$user = array();
 	
 	// Check for required info.
-	if (!isset($request['TOL_UserId']) || !isset($request['TOL_UserKey'])) {
+	if (!isset($p_request['TOL_UserId']) || !isset($p_request['TOL_UserKey'])) {
 		return array($access, $user, $XPerm);
 	}
     
 	// Check if user exists in the table.
 	$queryStr = 'SELECT * FROM Users '
-				.' WHERE Id='.$request['TOL_UserId']
-				.' AND KeyId='.$request['TOL_UserKey'];
+				.' WHERE Id='.$p_request['TOL_UserId']
+				.' AND KeyId='.$p_request['TOL_UserKey'];
 	$query = mysql_query($queryStr);
 	if ($query && (mysql_num_rows($query) > 0)) {
 		// User exists.
