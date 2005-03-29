d31f571829384e97812e53d1dab2673a0f5e0170
paul <paul@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-03-29 20:31:46 +0000

    Replaced use of array_get_value() with Input::Get().  Fixed formatting of action links.

--- implementation/management/imagearchive/index.php
+++ implementation/management/imagearchive/index.php
@@ -1,6 +1,7 @@
 <?php
 require_once($_SERVER['DOCUMENT_ROOT'].'/classes/common.php');
 load_common_include_files("$ADMIN_DIR/imagearchive");
+require_once($_SERVER['DOCUMENT_ROOT'].'/classes/Input.php');
 require_once($_SERVER['DOCUMENT_ROOT'].'/classes/Image.php');
 require_once($_SERVER['DOCUMENT_ROOT'].'/classes/ImageSearch.php');
 require_once($_SERVER['DOCUMENT_ROOT']."/$ADMIN_DIR/CampsiteInterface.php");
@@ -13,16 +14,16 @@ if (!$access) {
 }
 
 // Initialize input variables ///////////////////////////////////////////////////
-$OrderBy = array_get_value($_REQUEST, 'order_by', 'id');
-$OrderDirection = array_get_value($_REQUEST, 'order_direction', 'ASC');
-$view = array_get_value($_REQUEST, 'view', 'thumbnail');
-$ImageOffset = array_get_value($_REQUEST, 'image_offset', 0);
-$SearchDescription = array_get_value($_REQUEST, 'search_description', '');
-$SearchPhotographer = array_get_value($_REQUEST, 'search_photographer', '');
-$SearchPlace = array_get_value($_REQUEST, 'search_place', '');
-$SearchDate = array_get_value($_REQUEST, 'search_date', '');
-$SearchInUse = array_get_value($_REQUEST, 'search_inuse', '');
-$SearchUploadedBy = array_get_value($_REQUEST, 'search_uploadedby', '');
+$OrderBy = Input::Get('order_by', 'string', 'id', true);
+$OrderDirection = Input::Get('order_direction', 'string', 'ASC', true);
+$view = Input::Get('view', 'thumbnail', true);
+$ImageOffset = Input::Get('image_offset', 'int', 0, true);
+$SearchDescription = Input::Get('search_description', 'string', '', true);
+$SearchPhotographer = Input::Get('search_photographer', 'string', '', true);
+$SearchPlace = Input::Get('search_place', 'string', '', true);
+$SearchDate = Input::Get('search_date', 'string', '', true);
+$SearchInUse = Input::Get('search_inuse', 'string', '', true);
+$SearchUploadedBy = Input::Get('search_uploadedby', 'string', '', true);
 
 $imageNav =& new ImageNav($_REQUEST, CAMPSITE_IMAGEARCHIVE_IMAGES_PER_PAGE, $view);
 
@@ -111,21 +112,46 @@ $uploadedByUsers =& Image::GetUploadUsers();
 </TR>
 </TABLE>
 
-<table>
-  <tr>
+<table cellpadding="0" cellspacing="0" style="padding-top: 3px; padding-bottom: 3px;">
+<tr>
 <?php
 if ($User->hasPermission('AddImage')) { ?>
-    <td><A HREF="add.php?<?php echo $imageNav->getSearchLink(); ?>"><IMG SRC="/<?php echo $ADMIN; ?>/img/icon/add.png" BORDER="0" alt="<?php  putGS('Add new image'); ?>"></A></TD><TD><A HREF="add.php?<?php echo $imageNav->getSearchLink(); ?>"><B><?php  putGS('Add new image'); ?></B></A></TD>
+    <td>
+    	<A HREF="add.php?<?php echo $imageNav->getSearchLink(); ?>"><IMG SRC="/<?php echo $ADMIN; ?>/img/icon/add.png" BORDER="0" alt="<?php  putGS('Add new image'); ?>"></A>
+    </TD>
+    <TD style="padding-left: 3px;">
+    	<A HREF="add.php?<?php echo $imageNav->getSearchLink(); ?>"><B><?php  putGS('Add new image'); ?></B></A>
+    </TD>
 <?php } ?>
     
-    <td><a href="index.php?view=<?php echo $view; ?>"><IMG SRC="/<?php echo $ADMIN; ?>/img/icon/reset.png" BORDER="0" ALT="<?php putGS("Reset search conditions"); ?>"></a></td><td><a href="index.php?view=<?php echo $view; ?>"><b><?php echo putGS('Reset search conditions'); ?></b></a></td>
-
-    <td>&nbsp;&nbsp;&nbsp;&nbsp;</td>
-    <td><B><?php putGS('View', 'View'); ?>:</b></td>
-    <td><A HREF="index.php?<?php echo $imageNav->getSearchLink(); ?>&view=thumbnail"><IMG SRC="/<?php echo $ADMIN; ?>/img/tol.gif" BORDER="0" alt="<?php  putGS('Thumbnail'); ?>"></A></TD><TD><A HREF="index.php?<?php echo $imageNav->getSearchLink(); ?>&view=thumbnail"><B><?php putGS('Thumbnail'); ?></B></A></TD>
-    <td><A HREF="index.php?<?php echo $imageNav->getSearchLink(); ?>&view=gallery"><IMG SRC="/<?php echo $ADMIN; ?>/img/tol.gif" BORDER="0" alt="<?php  putGS('Gallery'); ?>"></A></TD><TD><A HREF="index.php?<?php echo $imageNav->getSearchLink(); ?>&view=gallery"><B><?php putGS('Gallery'); ?></B></A></TD>
-    <td><A HREF="index.php?<?php echo $imageNav->getSearchLink(); ?>&view=flat"><IMG SRC="/<?php echo $ADMIN; ?>/img/tol.gif" BORDER="0" alt="<?php  putGS('Text only'); ?>"></A></TD><TD><A HREF="index.php?<?php echo $imageNav->getSearchLink(); ?>&view=flat"><B><?php  putGS('Text only'); ?></B></A></TD>
-  </tr>
+	<td class="action_link_container">
+		<a href="index.php?view=<?php echo $view; ?>"><IMG SRC="/<?php echo $ADMIN; ?>/img/icon/reset.png" BORDER="0" ALT="<?php putGS("Reset search conditions"); ?>"></a>
+	</td>
+	<td style="padding-left: 3px;">
+		<a href="index.php?view=<?php echo $view; ?>"><b><?php echo putGS('Reset search conditions'); ?></b></a>
+	</td>
+	
+	<td style="padding-left: 40px;">&nbsp;</td>
+	<td><B><?php putGS('View', 'View'); ?>:</b></td>
+	<td class="action_link_container">
+		<A HREF="index.php?<?php echo $imageNav->getSearchLink(); ?>&view=thumbnail"><IMG SRC="/<?php echo $ADMIN; ?>/img/tol.gif" BORDER="0" alt="<?php  putGS('Thumbnail'); ?>"></A>
+	</TD>
+	<TD>
+		<A HREF="index.php?<?php echo $imageNav->getSearchLink(); ?>&view=thumbnail"><B><?php putGS('Thumbnail'); ?></B></A>
+	</TD>	
+	<td class="action_link_container">
+		<A HREF="index.php?<?php echo $imageNav->getSearchLink(); ?>&view=gallery"><IMG SRC="/<?php echo $ADMIN; ?>/img/tol.gif" BORDER="0" alt="<?php  putGS('Gallery'); ?>"></A>
+	</TD>
+	<TD>
+		<A HREF="index.php?<?php echo $imageNav->getSearchLink(); ?>&view=gallery"><B><?php putGS('Gallery'); ?></B></A>
+	</TD>
+	<td class="action_link_container">
+		<A HREF="index.php?<?php echo $imageNav->getSearchLink(); ?>&view=flat"><IMG SRC="/<?php echo $ADMIN; ?>/img/tol.gif" BORDER="0" alt="<?php  putGS('Text only'); ?>"></A>
+	</TD>
+	<TD>
+		<A HREF="index.php?<?php echo $imageNav->getSearchLink(); ?>&view=flat"><B><?php  putGS('Text only'); ?></B></A>
+	</TD>
+</tr>
 </table>
 
 <TABLE BORDER="0" CELLSPACING="0" CELLPADDING="3" class="table_input" style="margin-bottom: 10px; margin-top: 5px;" align="center">
@@ -161,9 +187,9 @@ if ($User->hasPermission('AddImage')) { ?>
 <tr>
 	<td colspan="11" align="center" >
 		Additional searches: &nbsp;
-		<a href="index.php?<?php $imageNav->getSearchLink() ?>&order_by=time_created" style="font-size: 9pt; font-weight: bold; text-decoration: underline;">Most Recently Added</a><?php if ($OrderBy == "time_created") { echo "*"; } ?>
+		<a href="index.php?<?php p($imageNav->getSearchLink()); ?>&order_by=time_created" style="font-size: 9pt; font-weight: bold; text-decoration: underline;">Most Recently Added</a><?php if ($OrderBy == "time_created") { echo "*"; } ?>
 		&nbsp;
-		<a href="index.php?<?php $imageNav->getSearchLink() ?>&order_by=last_modified" style="font-size: 9pt; font-weight: bold; text-decoration: underline;">Most Recently Modified</a><?php if ($OrderBy == "last_modified") { echo "*"; } ?>
+		<a href="index.php?<?php p($imageNav->getSearchLink()); ?>&order_by=last_modified" style="font-size: 9pt; font-weight: bold; text-decoration: underline;">Most Recently Modified</a><?php if ($OrderBy == "last_modified") { echo "*"; } ?>
 	</td>
 	
 </tr>
