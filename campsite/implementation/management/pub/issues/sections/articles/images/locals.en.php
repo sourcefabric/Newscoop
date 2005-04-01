c250679f9ec0441538bfb214c9e4c9319292516b
paul <paul@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-04-01 03:47:42 +0000

    Commented out strings that are no longer used.  Added a couple that were missing.

--- campsite/implementation/management/pub/issues/sections/articles/images/locals.en.php
+++ campsite/implementation/management/pub/issues/sections/articles/images/locals.en.php
@@ -5,8 +5,8 @@ regGS("Article Image List","Article Image List:en");
 regGS("Back to Article Image List","Back to Article Image List:en");
 regGS("Add new image","Add New Image:en");
 regGS("Add an existing image","Add an Existing Image:en");
-regGS("Select an old image","Select an old image:en");
-regGS("Click to view image","Click to view image:en");
+//regGS("Select an old image","Select an old image:en");
+//regGS("Click to view image","Click to view image:en");
 regGS("Date<BR><SMALL>(yyyy-mm-dd)</SMALL>","Date<BR><SMALL>(yyyy-mm-dd)</SMALL>:en");
 regGS("Delete image $1","Delete image $1:en");
 regGS("No images.","No images.:en");
@@ -17,46 +17,47 @@ regGS("Please enter a description for the image.", "Please enter a description f
 regGS("You must select an image file to upload.", "You must select an image file to upload.:en");
 regGS("There are currently no images associated with this article.", "There are currently no images associated with this article.:en");
 regGS('Click one of the "Add Image" links above to add one.', 'Click one of the "Add Image" links above to add one.:en');
-
+regGS('Are you sure you want to remove the image \\\'$1\\\' from the article?',
+	  'Are you sure you want to remove the image \\\'$1\\\' from the article?:en');
 // -- view.m4
-regGS("View image","View image:en");
+//regGS("View image","View image:en");
 
 // -- edit.m4
 regGS("Change image information","Change image information:en");
 
 // -- do_edit.m4
-regGS("Updating image information","Updating image information:en");
-regGS("Edit image information","Edit image information:en");
-regGS("Image information has been updated.","Image information has been updated.:en");
-regGS("Image information could not be updated.","Image information could not be updated.:en");
+//regGS("Updating image information","Updating image information:en");
+//regGS("Edit image information","Edit image information:en");
+//regGS("Image information has been updated.","Image information has been updated.:en");
+//regGS("Image information could not be updated.","Image information could not be updated.:en");
 // log
 regGS("Changed image properties of $1","Changed image properties of $1:en");
 
 // -- del.m4
 regGS("Delete image","Delete image:en");
 regGS("You do not have the right to delete images","You do not have the right to delete images:en");
-regGS("Are you sure you want to delete the image $1?","Are you sure you want to delete the image $1?:en");
+regGS('Are you sure you want to delete the image \\\'$1\\\'?','Are you sure you want to delete the image \\\'$1\\\'?:en');
 
 // -- do_del.m4
-regGS("Deleting image","Deleting image:en");
-regGS("The image $1 has been successfully deleted.","The image $1 has been successfully deleted.:en");
-regGS("The image $1 could not be deleted.","The image $1 could not be deleted.:en");
+//regGS("Deleting image","Deleting image:en");
+//regGS("The image $1 has been successfully deleted.","The image $1 has been successfully deleted.:en");
+//regGS("The image $1 could not be deleted.","The image $1 could not be deleted.:en");
 // log
 regGS("Image $1 deleted","Image $1 deleted:en");
 
 // -- select.m4
-regGS("Back to current article","Back to current article:en");
-regGS("Add now","Add now:en");
-regGS("Add to current article","Add to current article:en");
+//regGS("Back to current article","Back to current article:en");
+//regGS("Add now","Add now:en");
+//regGS("Add to current article","Add to current article:en");
 
 // -- do_add.m4
-regGS("Adding new image","Adding new image:en");
+//regGS("Adding new image","Adding new image:en");
 regGS("You do not have the right to add images","You do not have the right to add images:en");
-regGS("Image uploaded","Image uploaded:en");
+//regGS("Image uploaded","Image uploaded:en");
 
 // -- do_sel.m4
-regGS("Selecting image","Selecting image:en");
+//regGS("Selecting image","Selecting image:en");
 regGS("The image $1 has been added.","The image $1 has been added.:en");
-regGS("The image $1 could not be added.","The image $1 could not be added.:en");
+//regGS("The image $1 could not be added.","The image $1 could not be added.:en");
 
 ?>
