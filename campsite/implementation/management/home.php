e3890eddac2828ff9091bb0fa3d4ae5b3ee58ca5
paul <paul@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-04-01 03:30:05 +0000

    Removed the publications, issues, and sections listing.  Displayed both Your Articles and Submitted Articles on the screen at the same time.

--- campsite/implementation/management/home.php
+++ campsite/implementation/management/home.php
@@ -16,15 +16,6 @@ if (!$access) {
 	exit;
 }
 
-// "What" means "what to display".
-// A value of "1" means "display Your Articles".
-// A value of "0" means "display Submitted Articles".
-if ($User->hasPermission("ChangeArticle")) {
-	todefnum('What',0);
-}
-else {
-	todefnum('What',1);
-}
 $NArtOffs = Input::Get('NArtOffs', 'int', 0, true);
 if ($NArtOffs<0) {
 	$NArtOffs=0;
@@ -33,32 +24,12 @@ $ArtOffs = Input::Get('ArtOffs', 'int', 0, true);
 if ($ArtOffs < 0) {
 	$ArtOffs=0; 
 }
-$showSections = Input::Get('show_sections', 'int', 1, true);
-
 $NumDisplayArticles=15;
 list($YourArticles, $NumYourArticles) = Article::GetArticlesByUser($User->getId(), $ArtOffs, 
 	$NumDisplayArticles);
 
 list($SubmittedArticles, $NumSubmittedArticles) = Article::GetSubmittedArticles($NArtOffs, $NumDisplayArticles);
 
-$publications =& Publication::GetAllPublications();
-$issues = array();
-foreach ($publications as $publication) {
-	$issues[$publication->getPublicationId()] =
-		Issue::GetIssuesInPublication($publication->getPublicationId(), null, 
-			array('LIMIT' => 5, 'ORDER BY' => array('Number' => 'DESC')));
-}
-$sections = array();
-//if ((count($publications) + count($issues)) <= 12) {
-if ($showSections) {
-	foreach ($publications as $publication) {
-		foreach ($issues[$publication->getPublicationId()] as $issue) {
-			$sections[$publication->getPublicationId().'-'.$issue->getIssueId()] = 
-				Section::GetSectionsInIssue($issue->getPublicationId(), $issue->getIssueId(),
-					$issue->getLanguageId());
-		}
-	}
-}
 ?>
 <HEAD>
 	<LINK rel="stylesheet" type="text/css" href="<?php echo $Campsite['WEBSITE_URL']; ?>/css/admin_stylesheet.css">
@@ -70,73 +41,12 @@ if ($showSections) {
 	<TD class="page_title" width="1%">
 	    <?php  putGS("Home"); ?>
 	</TD>
-	<TD style="font-size: 9pt; padding-right: 10px; padding-top: 0px; padding-bottom: 2px;" valign="bottom" align="right"><?php  putGS('Welcome $1!','<B>'.htmlspecialchars($User->getName()).'</B>'); ?></TD>
 </TR>
 </TABLE>
 
 <TABLE BORDER="0" CELLSPACING="4" CELLPADDING="2" WIDTH="100%">
 <TR>
-    <TD VALIGN="TOP" width="40%">
-		<TABLE BORDER="0" CELLSPACING="1" CELLPADDING="3" class="table_list" width="100%" style="padding: 0px;">
-		<tr class="table_list_header">
-			<td style="padding-left: 8px;">
-				<?php putGS('Publication'); ?> / <?php putGS('Issue'); ?> / <?php putGS('Section'); ?>
-				<?php 
-				if ($showSections) { ?>
-					<a href="home.php?ArtOffs=<?php p($ArtOffs); ?>&NArtOffs=<?php  p($NArtOffs);?>&What=<?php p($What); ?>&show_sections=0" style="font-size: smaller;">(<?php putGS('Hide'); ?>)</a>
-					<?php 
-				}
-				else { ?>
-					<a href="home.php?ArtOffs=<?php p($ArtOffs); ?>&NArtOffs=<?php  p($NArtOffs);?>&What=<?php p($What); ?>&show_sections=1" style="font-size: smaller;">(<?php putGS('Show'); ?>)</a>
-					<?php 							
-				} ?>
-			</td>
-			<?php if ($showSections) { ?>
-			<td align="center" nowrap width="1%">
-				<?php putGS('Add article'); ?>
-			</td>
-			<?php } ?>
-		</tr>
-		<?php 
-		$count = 1;
-		foreach ($publications as $publication) { 
-			$publicationId = $publication->getPublicationId();
-			?>
-			<tr <?php if (($count++%2)==1) {?> class="list_row_odd"<?php } else { ?>class="list_row_even"<?php } ?>>
-				<td style="padding-left: 8px;"><a href="/<?php echo $ADMIN; ?>/pub/issues/?Pub=<?php p($publicationId); ?>"><?php p(htmlspecialchars($publication->getName())); ?></a></td> <?php if ($showSections) { ?> <td>&nbsp;</td> <?php  } ?>
-			</tr>
-			<?PHP
-			if (isset($issues[$publicationId])) {
-				foreach ($issues[$publicationId] as $issue) { 
-					$issueId = $issue->getIssueId();
-					?>
-					<tr <?php if (($count++%2)==1) {?> class="list_row_odd"<?php } else { ?>class="list_row_even"<?php } ?>>
-						<td style="padding-left: 25px;"><a href="/<?php echo $ADMIN; ?>/pub/issues/sections/?Pub=<?php p($publicationId); ?>&Issue=<?php  p($issueId); ?>&Language=<?php p($issue->getLanguageId()); ?>"><?php p(htmlspecialchars($issue->getName())); ?></a></td><?php if ($showSections) { ?><td>&nbsp;</td><?php } ?>
-					</tr>
-					<?php 
-					if (isset($sections[$publicationId.'-'.$issueId])) {
-						foreach ($sections[$publicationId.'-'.$issueId] as $section) { ?>
-							<tr <?php if (($count++%2)==1) {?> class="list_row_odd"<?php } else { ?>class="list_row_even"<?php } ?>>
-								<td style="padding-left: 50px;">
-									<a href="/<?php echo $ADMIN; ?>/pub/issues/sections/articles/?Pub=<?php p($publicationId); ?>&Issue=<?php  p($issueId); ?>&Section=<?php p($section->getSectionId()); ?>&Language=<?php p($section->getLanguageId()); ?>"><?php p(htmlspecialchars($section->getName())); ?></a>
-								</td>
-								<td align="center">
-									<a href="/<?php p($ADMIN); ?>/pub/issues/sections/articles/add.php?Pub=<?php p($publicationId);?>&Issue=<?php p($issueId); ?>&Section=<?php p($section->getSectionId()); ?>&Language=<?php p($section->getLanguageId()); ?>&Wiz=1"><img src="/<?php p($ADMIN); ?>/img/icon/add_article.png" border="0" align="middle"></a>
-								</td>
-							</tr>
-							<?php
-						} // foreach ($sections
-					}
-				} // foreach ($issues
-			} // if (isset($issues[$publicationId]))
-		} // foreach ($publications
-		?>		
-		</table>
-	</TD>
-	
-	<TD VALIGN="TOP" align="right">
-		<?php  if ($What) { ?>
-
+	<TD VALIGN="TOP" align="right" width="50%">
 		<TABLE BORDER="0" CELLSPACING="1" CELLPADDING="3">
 		<TR class="table_list_header">
 			<TD ALIGN="LEFT" VALIGN="TOP" width="98%"><?php  putGS("Your articles"); ?></TD>
@@ -204,22 +114,21 @@ if ($showSections) {
     		<TD COLSPAN="2" NOWRAP>
 				<?php  
 				if ($ArtOffs > 0) { ?>
-					<B><A HREF="home.php?ArtOffs=<?php print ($ArtOffs - $NumDisplayArticles); ?>&What=<?php p($What); ?>&NArtOffs=<?php  p($NArtOffs);?>&show_sections=<?php p($showSections); ?>"><?php p(htmlspecialchars("<< ")); putGS('Previous'); ?></A></B>
+					<B><A HREF="home.php?ArtOffs=<?php print ($ArtOffs - $NumDisplayArticles); ?>&NArtOffs=<?php  p($NArtOffs);?>"><?php p(htmlspecialchars("<< ")); putGS('Previous'); ?></A></B>
 					<?php  
 				} 
 				if ( ($ArtOffs + $NumDisplayArticles) < $NumYourArticles ) { ?>
-					| <B><A HREF="home.php?ArtOffs=<?php print ($ArtOffs + $NumDisplayArticles); ?>&NArtOffs=<?php  p($NArtOffs);?>&What=<?php p($What); ?>&show_sections=<?php p($showSections); ?>"><?php putGS('Next'); p(htmlspecialchars(" >>")); ?></A></B>
+					| <B><A HREF="home.php?ArtOffs=<?php print ($ArtOffs + $NumDisplayArticles); ?>&NArtOffs=<?php  p($NArtOffs);?>"><?php putGS('Next'); p(htmlspecialchars(" >>")); ?></A></B>
 					<?php  
 				} 
 				?>	
 			</TD>
 		</TR>
 		</TABLE>
-		<?php  
-		} // if ($What)
-		else { 
-			// Submitted articles
-			?>
+	</td>
+	
+	<td VALIGN="TOP" align="right" width="50%">
+		<?php if ($User->hasPermission('ChangeArticle') || $User->hasPermission('Publish')) { ?>
 		<TABLE BORDER="0" CELLSPACING="1" CELLPADDING="3">
 		<TR class="table_list_header">
 			<TD ALIGN="left" VALIGN="TOP" width="99%"><?php  putGS("Submitted articles"); ?></TD>
@@ -250,50 +159,20 @@ if ($showSections) {
 			<TD COLSPAN="2" NOWRAP>
 			<?php 
 			if ($NArtOffs > 0) { ?>
-				<B><A HREF="home.php?NArtOffs=<?php p($NArtOffs - $NumDisplayArticles); ?>&What=<?php p($What); ?>&show_sections=<?php p($showSections); ?>"><?php p(htmlspecialchars("<< ")); putGS('Previous'); ?></A></B>
+				<B><A HREF="home.php?NArtOffs=<?php p($NArtOffs - $NumDisplayArticles); ?>"><?php p(htmlspecialchars("<< ")); putGS('Previous'); ?></A></B>
 				<?php  
     		}
     		if (($NArtOffs + $NumDisplayArticles) < $NumSubmittedArticles) { ?>
-    			| <B><A HREF="home.php?NArtOffs=<?php  p($NArtOffs + $NumDisplayArticles); ?>&What=<?php p($What); ?>&show_sections=<?php p($showSections); ?>"><?php putGS('Next'); p(htmlspecialchars(" >>")); ?></A></B>
+    			| <B><A HREF="home.php?NArtOffs=<?php  p($NArtOffs + $NumDisplayArticles); ?>"><?php putGS('Next'); p(htmlspecialchars(" >>")); ?></A></B>
 				<?php  
     		} 
     		?>	
 			</TD>
 		</TR>
 		</TABLE>
-		<?php  
-		} // if (!$What)
-		?>
-		<TABLE BORDER="0" CELLSPACING="0" CELLPADDING="1">
-		<TR>
 		<?php 
-    	if ($What) {
-			if ($User->hasPermission("ChangeArticle")) { ?>	
-			<TD>
-				<TABLE>
-				<TR>
-					<TD ALIGN="RIGHT"><A HREF="home.php?ArtOffs=<?php p($ArtOffs); ?>&NArtOffs=<?php  p($NArtOffs);?>&What=0&show_sections=<?php p($showSections); ?>"><IMG SRC="/<?php p($ADMIN); ?>/img/tol.gif" BORDER="0" ALT="<?php  putGS("Submitted articles"); ?>"></A></TD>
-					<TD NOWRAP><A HREF="home.php?ArtOffs=<?php p($ArtOffs); ?>&NArtOffs=<?php  p($NArtOffs);?>&What=0&show_sections=<?php p($showSections); ?>"><?php  putGS("Submitted articles"); ?></A></TD>
-				</TR>
-				</TABLE>
-			</TD>
-			<?php  
-			} 
-    	}    
- 		else { ?>	
- 			<TD>
- 				<TABLE>
-				<TR>
-					<TD ALIGN="RIGHT"><A HREF="home.php?ArtOffs=<?php p($ArtOffs); ?>&NArtOffs=<?php  p($NArtOffs);?>&What=1&show_sections=<?php p($showSections); ?>"><IMG SRC="/<?php p($ADMIN); ?>/img/tol.gif" BORDER="0" ALT="<?php  putGS("Your articles"); ?>"></A></TD>
-					<TD NOWRAP><A HREF="home.php?ArtOffs=<?php p($ArtOffs); ?>&NArtOffs=<?php  p($NArtOffs);?>&What=1&show_sections=<?php p($showSections); ?>"><?php  putGS("Your articles"); ?></A></TD>
-				</TR>
-				</TABLE>
-			</TD>
-			<?php  
- 		} 
- 		?>
-		</TR>
-		</TABLE>
+		} // if ($User->hasPermission('ChangeArticle') || $User->hasPermission('Publish'))
+		?>
     </TD>
 </TR>
 </TABLE>
