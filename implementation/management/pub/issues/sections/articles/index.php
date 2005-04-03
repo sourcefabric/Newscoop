622e0b76c35ec702b070768d21452d0b8a118bd2
paul <paul@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-04-03 04:46:57 +0000

    Fixed bug where certain characters in the image description could cause the delete confirmation dialog box not to show up.

--- implementation/management/pub/issues/sections/articles/index.php
+++ implementation/management/pub/issues/sections/articles/index.php
@@ -325,7 +325,7 @@ foreach ($allArticles as $articleObj) {
 
 		<?php  if ($User->hasPermission('DeleteArticle')) { ?>
 		<TD ALIGN="CENTER">
-			<A HREF="/<?php echo $ADMIN; ?>/pub/issues/sections/articles/do_del.php?Pub=<?php  p($Pub); ?>&Issue=<?php  p($Issue); ?>&Section=<?php  p($Section); ?>&Article=<?php p($articleObj->getArticleId()); ?>&Language=<?php  p($Language); ?>&sLanguage=<?php p($articleObj->getLanguageId()); ?>" onclick="return confirm('<?php htmlspecialchars(putGS('Are you sure you want to delete the article $1 ($2)?', '&quot;'.htmlspecialchars($articleObj->getTitle(), ENT_QUOTES).'&quot', htmlspecialchars($articleObj->getLanguageName(), ENT_QUOTES))); ?>');"><IMG SRC="/<?php echo $ADMIN; ?>/img/icon/delete.png" BORDER="0" ALT="<?php  putGS('Delete article $1', $articleObj->getTitle()); ?>" width="16" height="16"></A>
+			<A HREF="/<?php echo $ADMIN; ?>/pub/issues/sections/articles/do_del.php?Pub=<?php  p($Pub); ?>&Issue=<?php  p($Issue); ?>&Section=<?php  p($Section); ?>&Article=<?php p($articleObj->getArticleId()); ?>&Language=<?php  p($Language); ?>&sLanguage=<?php p($articleObj->getLanguageId()); ?>" onclick="return confirm('<?php putGS('Are you sure you want to delete the article $1 ($2)?', '&quot;'.javascriptspecialchars($articleObj->getTitle()).'&quot', javascriptspecialchars($articleObj->getLanguageName())); ?>');"><IMG SRC="/<?php echo $ADMIN; ?>/img/icon/delete.png" BORDER="0" ALT="<?php  putGS('Delete article $1', $articleObj->getTitle()); ?>" width="16" height="16"></A>
 		</TD>
 		<?php  }
 		if ($articleObj->getArticleId() != $previousArticleId)
