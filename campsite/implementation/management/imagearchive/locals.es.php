8f0745b435f5b9ff4b5fcb0a6447f876c7a76222
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-04-05 13:28:56 +0000

    removed obsolete messages

--- campsite/implementation/management/imagearchive/locals.es.php
+++ campsite/implementation/management/imagearchive/locals.es.php
@@ -1,34 +1,20 @@
 <?php 
 
 regGS("Add new image","Agregar nueva imagen");
-regGS("Select an old image","Seleccionar una imagen vieja");
-regGS("Click to view image","Click para ver imagen");
 regGS("Date <SMALL>(yyyy-mm-dd)</SMALL>","aaaa-mm-dd");
 regGS("Delete image $1","Borrar imagen $1");
 regGS("No images.","Sin imagen");
+regGS("Description <SMALL>(Click to view details)</SMALL>","");
+regGS("Gallery","");
+regGS("Text only","");
+regGS("Used in articles","");
 regGS("View image","Ver imagen");
+regGS("Back to image archive","");
 regGS("Change image information","Cambiar información de imagen");
-regGS("Updating image information","Actualizando información de imagen");
-regGS("Edit image information","Editar información de imagen");
-regGS("Image information has been updated.","Información de imagen ha sido actualizada");
-regGS("Image information could not be updated.","Información de imagen no pudo ser actualizada");
 regGS("Changed image properties of $1","Cambiar propiedades de imagen $1?");
-regGS("Delete image","Borrar imagen");
-regGS("You do not have the right to delete images","No tiene derecho a borrar imagenes");
 regGS("Are you sure you want to delete the image $1?","¿Esta seguro de querer borrar la imagen $1?");
-regGS("Deleting image","Borrando imagen");
-regGS("The image $1 has been successfully deleted.","La imagen $1 ha sido borrada con éxito");
-regGS("The image $1 could not be deleted.","La imagen $1 no pudo ser borrada");
 regGS("Image $1 deleted","Imagen $1 borrada");
-regGS("Back to current article","Regresar a artículo actual");
-regGS("Add now","Agregar ahora");
-regGS("Add to current article","Agregar a artículo actual");
-regGS("Adding new image","Agregando nueva imagen");
-regGS("You do not have the right to add images","No tiene derecho a agregar imágenes");
-regGS("Image uploaded","Imagen subida");
-regGS("Selecting image","Seleccionar imagen");
 regGS("The image $1 has been added.","La imagen $1 ha sido agregada");
-regGS("The image $1 could not be added.","La imagen $1 no pudo ser agregada");
 
 
 ?>
--- campsite/implementation/management/imagearchive/locals.pt.php
+++ campsite/implementation/management/imagearchive/locals.pt.php
@@ -1,32 +1,20 @@
 <?php 
+
 regGS("Add new image","Adicionar Imagem");
-regGS("Select an old image","Selecionar imagem já existente");
-regGS("Click to view image","Carregue para ver imagem");
 regGS("Date <SMALL>(yyyy-mm-dd)</SMALL>","Data (aaaa-mm-dd)");
 regGS("Delete image $1","Apagar imagem");
 regGS("No images.","Não existem imagens");
+regGS("Description <SMALL>(Click to view details)</SMALL>","");
+regGS("Gallery","");
+regGS("Text only","");
+regGS("Used in articles","");
 regGS("View image","Ver imagem");
+regGS("Back to image archive","");
 regGS("Change image information","Alterar informações da imagem");
-regGS("Updating image information","Actualizar informações da imagem");
-regGS("Edit image information","Editar informações da imagem");
-regGS("Image information has been updated.","As informações da imagem foram alteradas");
-regGS("Image information could not be updated.","As informações da imagem não podem ser alteradas");
 regGS("Changed image properties of $1","Alterando as informações da imagem $1");
-regGS("Delete image","Apagar imagem");
-regGS("You do not have the right to delete images","Não tem permissões para apagar imagens");
 regGS("Are you sure you want to delete the image $1?","Deseja apagar Imagem $1");
-regGS("Deleting image","Apagando Imagem");
-regGS("The image $1 has been successfully deleted.","Imagem $1 foi apagada");
-regGS("The image $1 could not be deleted.","Imagem $1 não pode ser apagada");
 regGS("Image $1 deleted","Imagem $1 apagada");
-regGS("Back to current article","Voltar ao Artigo");
-regGS("Add now","Adicionar ");
-regGS("Add to current article","Adicionar ao Artigo corrente");
-regGS("Adding new image","Adicionando Imagem");
-regGS("You do not have the right to add images","Não tem permissões para adicionar imagens");
-regGS("Image uploaded","Imagem transferida");
-regGS("Selecting image","Selecionando Imagem");
 regGS("The image $1 has been added.","Imagem $1 foi adicionada");
-regGS("The image $1 could not be added.","Imagem $1 não pode ser adicionada");
+
 
 ?>
