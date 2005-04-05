40711797606c07615c28540182229798b40101a3
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-04-05 13:29:56 +0000

    removed obsolete messages; modified a key message

--- campsite/implementation/management/imagearchive/locals.en.php
+++ campsite/implementation/management/imagearchive/locals.en.php
@@ -1,19 +1,11 @@
 <?php
-// added by sebastian
-//regGS("Search for images","Search for images");
-//regGS("Search conditions","Search conditions");
-//regGS("You cannot delete image $1","You cannot delete image $1");
-//regGS("Image is still attached to Article(s)","Image is still attached to Article(s)");
-
 
 // -- index.m4
 regGS("Add new image","Add new image:en");
-//regGS("Select an old image","Select an old image:en");
-regGS("Click to view image","Click to view image:en");
-regGS("Date<BR><SMALL>(yyyy-mm-dd)</SMALL>","Date <SMALL>(yyyy-mm-dd)</SMALL>:en");
+regGS("Date <SMALL>(yyyy-mm-dd)</SMALL>","Date <SMALL>(yyyy-mm-dd)</SMALL>:en");
 regGS("Delete image $1","Delete image $1:en");
 regGS("No images.","No images.:en");
-regGS("Description<BR><SMALL>(Click to view details)</SMALL>", "Description <SMALL>(Click to view details)</SMALL>:en");
+regGS("Description <SMALL>(Click to view details)</SMALL>", "Description <SMALL>(Click to view details)</SMALL>:en");
 regGS("Gallery", "Gallery:en");
 regGS("Text only", "Text only:en");
 regGS("Used in articles", "Used in articles:en");
@@ -25,39 +17,17 @@ regGS("Back to image archive", "Back to Image Archive:en");
 // -- edit.m4
 regGS("Change image information","Change image information:en");
 
-// -- do_edit.m4
-//regGS("Updating image information","Updating image information:en");
-regGS("Edit image information","Edit image information:en");
-//regGS("Image information has been updated.","Image information has been updated.:en");
-//regGS("Image information could not be updated.","Image information could not be updated.:en");
 // log
 regGS("Changed image properties of $1","Changed image properties of $1:en");
 
 // -- del.m4
-regGS("Delete image","Delete image:en");
-regGS("You do not have the right to delete images","You do not have the right to delete images:en");
 regGS("Are you sure you want to delete the image $1?","Are you sure you want to delete the image $1?:en");
 
 // -- do_del.m4
-//regGS("Deleting image","Deleting image:en");
-//regGS("The image $1 has been successfully deleted.","The image $1 has been successfully deleted.:en");
-//regGS("The image $1 could not be deleted.","The image $1 could not be deleted.:en");
 // log
 regGS("Image $1 deleted","Image $1 deleted:en");
 
-// -- select.m4
-//regGS("Back to current article","Back to current article:en");
-//regGS("Add now","Add now:en");
-//regGS("Add to current article","Add to current article:en");
-
-// -- do_add.m4
-regGS("Adding new image","Adding new image:en");
-regGS("You do not have the right to add images","You do not have the right to add images:en");
-regGS("Image uploaded","Image uploaded:en");
-
 // -- do_sel.m4
-//regGS("Selecting image","Selecting image:en");
 regGS("The image $1 has been added.","The image $1 has been added.:en");
-//regGS("The image $1 could not be added.","The image $1 could not be added.:en");
 
 ?>
