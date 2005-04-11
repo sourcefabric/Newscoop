948e4afa16fe12c3e9748ca1a523e38c374b7b7f
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-04-11 14:58:00 +0000

    updated translation

--- implementation/management/a_types/fields/locals.ar.php
+++ implementation/management/a_types/fields/locals.ar.php
@@ -1,26 +1,25 @@
 <?php 
 
-regGS("Article type fields","حقول أنواع المقال");
+regGS("Article type fields","حقول نوع المقال");
 regGS("Article type","نوع المقال");
-regGS("Add new field","أضف حقلا جديدا");
-regGS("No fields.","لا حقول");
-regGS("Delete field $1"," الحقل $1");
+regGS("No fields.","لا يوجد حقول");
+regGS("Delete field $1","حذف الحقل $1");
 regGS("Article body","صلب المقال");
-regGS("Text","النص");
+regGS("Text","نص");
 regGS("Adding new field","إضافة حقل جديد");
-regGS("You do not have the right to add article type fields.","لا يحق لك إضافة حقول نوع المقال");
-regGS("The field $1 already exists.","إن الحقل $1 موجود1");
-regGS("The $1  must not be void and may only contain letters.","لا يجب أن يكون الحقل $1 فارغا وقد يحتوي على أحرف فقط");
-regGS("Invalid field type.","نوع حقل غير صالح");
-regGS("The field $1 has been created."," وضع الحقل $1");
-regGS("Article type field $1 created","تم وضع الحقل $1 بنوع المقال");
-regGS("You do not have the right to delete article type fields.","لا يحق لك إلغاء الحقول الخاصة بنوع المقال");
-regGS("Delete field","إلغ الحقل");
-regGS("Are you sure you want to delete the field $1?","هل أنت متأكد من أنك تريد إلغاء الحقل  $1");
-regGS("You will also delete all fields with this name from all articles of this type from all publications.","عليك أن تلغي كل الحقول التي تحمل هذا الاسم من كل المقالات الموجودة في كل المنشورات");
-regGS("Deleting field","إلغاء الحقل");
-regGS("The field $1 has been deleted."," إلغاء الحقل $1");
-regGS("Article type field $1 deleted","تم إلغاء الحقل الخاص بنوع المقال $1");
+regGS("You do not have the right to add article type fields.","لا تملك صلاحية إضافة حقول أنواع المقالات");
+regGS("The field $1 already exists.","الحقل $1 موجود أصلا");
+regGS("The $1  must not be void and may only contain letters and underscore (_) character.","لا يمكن للحقل $1 أن يكون فارغاً ويمكن أن يحتوى على الأحروف و الـ _ فقط");
+regGS("Invalid field type.","نوع الحقل غير صحيح");
+regGS("The field $1 has been created.","تم خلق الحقل $1");
+regGS("Article type field $1 created","تم خلق حقل نوع المقال $1");
+regGS("You do not have the right to delete article type fields.","لا تملك صلاحية إضافة حقول أنواع المقالات");
+regGS("Delete field","حذف حقل");
+regGS("Are you sure you want to delete the field $1?","هل أنت متأكد من حذف الحقل $2 ؟");
+regGS("You will also delete all fields with this name from all articles of this type from all publications.","سوف تحذف كل الحقول بهذا الأسم من جميع المقالات من هذا النوع في جميع المنشورات");
+regGS("Deleting field","حذف الحقل");
+regGS("The field $1 has been deleted.","تم حذف الحقل $1");
+regGS("Article type field $1 deleted","تم حذف حقل نوع المقال $1");
 
 
 ?>
--- implementation/management/locals.ar.php
+++ implementation/management/locals.ar.php
@@ -1,10 +1,10 @@
 <?php 
 
-regGS("Please enter your user name and password","الرجاء ادخال الاسم وكلمة السر");
-regGS("You do not have the right to access this page.","غير مسموح قراءة هذه الصفحة");
-regGS("Login failed","فشل الدخول");
-regGS("Pease make sure that you typed the correct user name and password.","تأكد من إدخال اسم المستخدم الصحيح وكلمة السر");
-regGS("If your problem persists please contact the site administrator $1"," إذا استمرت هذه المشكلة، يرجى الاتصال بمدير الموقع");
+regGS("Please enter your user name and password","الرجاء إدخال أسم المستخدم وكلمة السر");
+regGS("You do not have the right to access this page.","لا تملك صلاحية الوصول لهذه الصفحة");
+regGS("Login failed","فشلت عملية الدخول");
+regGS("Pease make sure that you typed the correct user name and password.","الرجاء التأكد من إدخال أسم المستخدم وكلمة السر الصحيحين");
+regGS("If your problem persists please contact the site administrator $1","إذا استمريت بمواجه المشاكل, أتصل مع مدير الموقع ");
 regGS("Localizer","المترجم");
 
 
