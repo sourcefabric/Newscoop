6b1b46d43aac7b512c2164e4e78b86ac7d02c7a3
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-04-12 13:27:25 +0000

    relocate to admin directory and exit if the template path was not correct or the user did not have full rights

--- campsite/implementation/management/templates/index.php
+++ campsite/implementation/management/templates/index.php
@@ -8,13 +8,14 @@ if (!$access) {
 	exit;
 }
 
-$path = Input::Get('Path', 'string', '');
-foreach (split("/", $path) as $index=>$dir) {
-	if ($dir == "..") {
-		$path = "";
-		break;
-	}
+if (!$User->hasPermission('ManageTempl')) {
+	header("Location: /$ADMIN/");
+	exit;
 }
+
+$path = Input::Get('Path', 'string', '');
+if (!Template::IsValidPath($path))
+	$path = "";
 $print_path = $path != "" ? $path : "/";
 
 register_templates($Campsite['HTML_DIR'] . "/look", $errors);
@@ -48,22 +49,16 @@ if ($path != "") {
 		<TD><TABLE BORDER="0" CELLSPACING="0" CELLPADDING="1"><TR><TD><A HREF="?Path=<?php pencURL($new_path); ?>" ><IMG SRC="/<?php echo $ADMIN; ?>/img/tol.gif" BORDER="0"></A></TD><TD><A HREF="?Path=<?php pencURL($new_path); ?>" ><B><?php  putGS("Go up"); ?></B></A></TD></TR></TABLE></TD>
 <?php
 }
-
-if ($User->hasPermission('ManageTempl')) {
 ?>
 		<TD><TABLE BORDER="0" CELLSPACING="0" CELLPADDING="1"><TR><TD><A HREF="/<?php echo $ADMIN; ?>/templates/new_dir.php?Path=<?php  pencURL($path); ?>" ><IMG SRC="/<?php echo $ADMIN; ?>/img/icon/add.png" BORDER="0"></A></TD><TD><A HREF="/<?php echo $ADMIN; ?>/templates/new_dir.php?Path=<?php  pencURL($path); ?>" ><B><?php  putGS("Create new folder"); ?></B></A></TD></TR></TABLE></TD>
 		<TD><TABLE BORDER="0" CELLSPACING="0" CELLPADDING="1"><TR><TD><A HREF="/<?php echo $ADMIN; ?>/templates/upload_templ.php?Path=<?php  pencURL($path); ?>" ><IMG SRC="/<?php echo $ADMIN; ?>/img/tol.gif" BORDER="0"></A></TD><TD><A HREF="/<?php echo $ADMIN; ?>/templates/upload_templ.php?Path=<?php  pencURL($path); ?>" ><B><?php  putGS("Upload template"); ?></B></A></TD></TR></TABLE></TD>
 		<TD><TABLE BORDER="0" CELLSPACING="0" CELLPADDING="1"><TR><TD><A HREF="/<?php echo $ADMIN; ?>/templates/new_template.php?Path=<?php  pencURL($path); ?>" ><IMG SRC="/<?php echo $ADMIN; ?>/img/icon/add.png" BORDER="0"></A></TD><TD><A HREF="/<?php echo $ADMIN; ?>/templates/new_template.php?Path=<?php  pencURL($path); ?>" ><B><?php  putGS("Create new template"); ?></B></A></TD></TR></TABLE></TD>
-  <?php  }
-?>
 	</TR>
 </TABLE>
 <?php
-
 $listbasedir = $path;
 $dta = $User->hasPermission('DeleteTempl');
 require_once($Campsite['HTML_DIR'] . "/$ADMIN_DIR/templates/list_dir.php");
-
 ?>
 <P>
 <?php
