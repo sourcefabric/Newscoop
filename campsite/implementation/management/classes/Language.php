72182cdf261600d0fd29b6c355584f3f34bdffce
paul <paul@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-04-15 14:19:06 +0000

    Added getNativeName(), simplified code.

--- campsite/implementation/management/classes/Language.php
+++ campsite/implementation/management/classes/Language.php
@@ -1,12 +1,13 @@
 <?php
 require_once($_SERVER['DOCUMENT_ROOT'].'/db_connect.php');
 require_once($_SERVER['DOCUMENT_ROOT'].'/classes/DatabaseObject.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/classes/DbObjectArray.php');
 
 class Language extends DatabaseObject {
 	var $m_dbTableName = 'Languages';
 	var $m_keyColumnNames = array('Id');
 	var $m_keyIsAutoIncrement = true;
-	var $m_columnNames = array('Id', 'Name', 'Code');
+	var $m_columnNames = array('Id', 'Name', 'CodePage', 'OrigName', 'Code');
 	
 	/** 
 	 * Constructor.
@@ -25,20 +26,14 @@ class Language extends DatabaseObject {
 	 * @return array
 	 */
 	function GetAllLanguages() {
-		global $Campsite;
 		$queryStr = 'SELECT * FROM Languages ORDER BY Name';
-		$query = $Campsite['db']->Execute($queryStr);
-		$languages = array();
-		while ($row = $query->FetchRow()) {
-			$tmpLanguage =& new Language();
-			$tmpLanguage->fetch($row);
-			$languages[] = $tmpLanguage;
-		}
+		$languages = DbObjectArray::Create('Language', $queryStr);
 		return $languages;
 	} // fn GetAllLanguages
 
 	
 	/**
+	 * The unique ID of the language in the database.
 	 * @return int
 	 */
 	function getLanguageId() {
@@ -47,14 +42,25 @@ class Language extends DatabaseObject {
 	
 	
 	/**
+	 * Return the english name of this language.
 	 * @return string
 	 */
 	function getName() {
 		return $this->getProperty('Name');
 	} // fn getName
 	
+
+	/**
+	 * Return the name of the language as written in the language itself.
+	 * @return string
+	 */ 
+	function getNativeName() {
+		return $this->getProperty('OrigName');
+	} // fn get
+	
 	
 	/**
+	 * Get the two-letter code for this language.
 	 * @return string
 	 */
 	function getCode() {
