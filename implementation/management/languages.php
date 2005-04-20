e1603248c7babd4d688b0d349792eca20ad4d139
paul <paul@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-04-20 18:00:43 +0000

    Added native language names to each language

--- implementation/management/languages.php
+++ implementation/management/languages.php
@@ -1,21 +1,20 @@
 <?php 
 
-function registerLanguage($name,$code,$charset){
+function registerLanguage($p_name, $p_code, $p_charset, $p_origName = null){
 
 	global $languages;
-	$languages["$code"]=array("name"=>$name,"charset"=>$charset);
+	$languages["$p_code"]=array("name"=>$p_name,"charset"=>$p_charset,"orig_name"=>$p_origName);
 }
-registerLanguage('English','en','ISO_8859-1');
-registerLanguage('Serbo-Croatian','sh','ISO_8859-2');
-registerLanguage('Chinese','zh','UTF-8');
-registerLanguage('Croatian','hr','ISO_8859-2');
-registerLanguage('Czech','cz','ISO_8859-2');
-registerLanguage('German','de','ISO_8859-1');
-registerLanguage('Portuguese','pt','ISO_8859-1');
-registerLanguage('Romanian','ro','ISO_8859-2');
-registerLanguage('Russian','ru','ISO_8859-5');
-registerLanguage('Serbian (Cyrillic)','sr','ISO_8859-5');
-registerLanguage('Spanish','es','ISO_8859-1');
-registerLanguage('Arabic','ar','UTF-8');
+registerLanguage('Austrian', 'at', 'IS0_8859-1', 'Deutsch (Österreich)');
+registerLanguage('Croatian', 'hr', 'ISO_8859-2', 'Hrvatski');
+registerLanguage('Czech', 'cz', 'ISO_8859-2', 'Český');
+registerLanguage('English', 'en', 'ISO_8859-1', 'English');
+registerLanguage('German', 'de', 'ISO_8859-1', 'Deutsch');
+registerLanguage('Portuguese', 'pt', 'ISO_8859-1', 'Português');
+registerLanguage('Romanian', 'ro', 'ISO_8859-2', 'Română');
+registerLanguage('Russian', 'ru', 'ISO_8859-5', 'Русский');
+registerLanguage('Sebian (Cyrillic)', 'sr', 'ISO_8859-5', 'Српски (Ћирилица)');
+registerLanguage('Serbo-Croatian', 'sh', 'ISO_8859-2', 'Srpskohrvatski');
+registerLanguage('Spanish', 'es', 'ISO_8859-1', 'Español');
 
 ?>
