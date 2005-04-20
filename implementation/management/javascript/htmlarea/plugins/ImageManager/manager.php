449f28c02408457c8b4d830f07a45cbb9aa0705a
paul <paul@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-04-20 13:59:52 +0000

    Allowed the image manager interface to be translated

--- implementation/management/javascript/htmlarea/plugins/ImageManager/manager.php
+++ implementation/management/javascript/htmlarea/plugins/ImageManager/manager.php
@@ -2,7 +2,7 @@
 /**
  * The main GUI for the ImageManager.
  * @author $Author: paul $
- * @version $Id: manager.php,v 1.4 2005/03/24 16:12:54 paul Exp $
+ * @version $Id: manager.php,v 1.5 2005/04/20 13:58:39 paul Exp $
  * @package ImageManager
  */
 
@@ -10,14 +10,13 @@ require_once('config.inc.php');
 require_once('Classes/ImageManager.php');
 
 $manager = new ImageManager($IMConfig);
-$dirs = $manager->getDirs();
+//$dirs = $manager->getDirs();
 
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
 <html>
 <head>
-	<title>Insert Image</title>
   	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
  	<link href="assets/manager.css" rel="stylesheet" type="text/css" />	
 	<script type="text/javascript" src="assets/popup.js"></script>
@@ -26,19 +25,21 @@ $dirs = $manager->getDirs();
 	/*<![CDATA[*/
 		window.resizeTo(600, 460);
 	
-		if(window.opener)
+		if (window.opener) {
 			I18N = window.opener.ImageManager.I18N;
+		}
 	
-		var thumbdir = "<?php echo $IMConfig['thumbnail_dir']; ?>";
-		var base_url = "<?php echo $manager->getBaseURL(); ?>";
+		//var thumbdir = "<?php echo $IMConfig['thumbnail_dir']; ?>";
+		//var base_url = "<?php echo $manager->getBaseURL(); ?>";
 	/*]]>*/
 	</script>
 	<script type="text/javascript" src="assets/manager.js"></script>
+	<script>document.write("<title>"+i18n("Insert Image")+"</title>");</script>
 </head>
 <body>
-	<div class="title">Insert Image</div>
+	<div class="title"><script>document.write(i18n("Insert Image"));</script></div>
 	<form action="images.php" id="uploadForm" method="post" enctype="multipart/form-data">
-	<fieldset><legend>Image Manager</legend>
+	<fieldset><!--<legend><script>document.write(i18n("Image Manager"));</script></legend>-->
 	<div class="dirs">
 		<iframe src="images.php?article_id=<?php echo $_REQUEST['article_id']; ?>" name="imgManager" id="imgManager" class="imageFrame" scrolling="auto" title="Image Selection" frameborder="0"></iframe>
 	</div>
@@ -54,21 +55,21 @@ $dirs = $manager->getDirs();
 			<input type="hidden" id="orginal_width" />
 			<input type="hidden" id="orginal_height" />
 			<tr>
-				<td align="right"><label for="f_alt">Alt</label></td>
+				<td align="right"><label for="f_alt"><script>document.write(i18n("Alt"));</script></label></td>
 				<td><input type="text" id="f_alt" class="largelWidth" value="" /></td>
 			</tr>		
 			<tr>
-				<td align="right"><label for="f_caption">Caption</label></td>
+				<td align="right"><label for="f_caption"><script>document.write(i18n("Caption"));</script></label></td>
 				<td><input type="text" id="f_caption" class="largelWidth" value="" /></td>
 			</tr>		
 			<tr>
-				<td align="right"><label for="f_align">Align</label></td>
+				<td align="right"><label for="f_align"><script>document.write(i18n("Align"));</script></label></td>
 				<td>
 					<select size="1" id="f_align"  title="Positioning of this image">
-						<option value="none">Not Set</option>
-					  	<option value="left">Left</option>
-					  	<option value="right">Right</option>
-					  	<option value="middle">Middle</option>
+						<option value="none"><script>document.write(i18n("Not Set"));</script></option>
+					  	<option value="left"><script>document.write(i18n("Left"));</script></option>
+					  	<option value="right"><script>document.write(i18n("Right"));</script></option>
+					  	<option value="middle"><script>document.write(i18n("Middle"));</script></option>
 	<!--				<option value="texttop"                      >Texttop</option>
 					  	<option value="absmiddle"                    >Absmiddle</option>
 					  	<option value="baseline" selected="selected" >Baseline</option>
@@ -83,8 +84,8 @@ $dirs = $manager->getDirs();
 		<div style="text-align: right;"> 
 	          <hr />
 			  <!--<button type="button" class="buttons" onclick="return refresh();">Refresh</button>-->
-	          <button type="button" class="buttons" onclick="return onOK();">OK</button>
-	          <button type="button" class="buttons" onclick="return onCancel();">Cancel</button>
+	          <button type="button" class="buttons" onclick="return onOK();"><script>document.write(i18n("OK"));</script></button>
+	          <button type="button" class="buttons" onclick="return onCancel();"><script>document.write(i18n("Cancel"));</script></button>
 	    </div>
 	</form>
 </body>
