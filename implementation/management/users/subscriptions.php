840cceb98f7e663a9699bfacad59814d25ef2cf7
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-05-23 14:01:40 +0000

    initial version

---
+++ implementation/management/users/subscriptions.php
@@ -0,0 +1,66 @@
+<TABLE BORDER="0" CELLSPACING="1" CELLPADDING="3" WIDTH="100%" class="table_list">
+<tr class="table_list_header">
+	<td colspan="5">
+		<?php putGS("Subscriptions"); ?>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+		<?php $addURI = "/$ADMIN/users/subscriptions/add.php?User=".$editUser->getId(); ?>
+		<A HREF="<?php echo $addURI; ?>"><IMG SRC="/<?php echo $ADMIN; ?>/img/icon/add.png" BORDER="0"></A>&nbsp;
+		<A HREF="<?php echo $addURI; ?>"><B><?php putGS("Add new subscription"); ?></B></A>
+	</td>
+</tr>
+<?php
+
+check_basic_access($_REQUEST);
+if (!isset($editUser) || gettype($editUser) != 'object' || $editUser->getUserName() == '') {
+	CampsiteInterface::DisplayError('No such user account.',$_SERVER['REQUEST_URI']);
+	exit;
+}
+
+query ("SELECT * FROM Subscriptions WHERE IdUser=" . $editUser->getId() . " ORDER BY Id DESC", 'q_subs');
+if ($NUM_ROWS) {
+	$nr= $NUM_ROWS;
+	$color=0;
+	?>
+	<TR class="table_list_header">
+		<TD ALIGN="LEFT" VALIGN="TOP" nowrap><B><?php  putGS("Publication"); ?></B></TD>
+		<TD ALIGN="LEFT" VALIGN="TOP" nowrap><B><?php  putGS("Left to pay"); ?></B></TD>
+		<TD ALIGN="LEFT" VALIGN="TOP" nowrap><B><?php  putGS("Type"); ?></B></TD>
+		<TD ALIGN="LEFT" VALIGN="TOP" WIDTH="1%" ><B><?php  putGS("Active"); ?></B></TD>
+		<TD ALIGN="LEFT" VALIGN="TOP" WIDTH="1%" ><B><?php  putGS("Delete"); ?></B></TD>
+	</TR>
+<?php 
+	for($loop=0;$loop<$nr;$loop++) {
+		fetchRow($q_subs);
+?>	<TR <?php  if ($color) { $color=0; ?>class="list_row_even"<?php  } else { $color=1; ?>class="list_row_odd"<?php  } ?>>
+		<TD >
+<?php 
+		query ("SELECT Name FROM Publications WHERE Id=".getSVar($q_subs,'IdPublication'), 'q_pub');
+		fetchRow($q_pub);
+?>			<A HREF="/admin/users/subscriptions/sections/?Subs=<?php pgetUVar($q_subs,'Id'); ?>&Pub=<?php pgetUVar($q_subs,'IdPublication'); ?>&User=<?php  echo $editUser->getId(); ?>"><?php pgetHVar($q_pub,'Name'); ?></A>&nbsp;
+		</TD>
+		<TD >
+			<A HREF="/admin/users/subscriptions/topay.php?User=<?php echo $editUser->getId(); ?>&Subs=<?php pgetUVar($q_subs,'Id'); ?>">
+			<?php  pgetHVar($q_subs,'ToPay').' '.pgetHVar($q_subs,'Currency'); ?></A>
+		</TD>
+		<TD >
+			<?php  
+			$sType = getHVar($q_subs,'Type');
+			if ($sType == 'T')
+				putGS("Trial");
+			else
+				putGS("Paid");
+			?>
+		</TD>
+		<TD ALIGN="CENTER">
+			<A HREF="/admin/users/subscriptions/status.php?User=<?php echo $editUser->getId(); ?>&Subs=<?php pgetUVar($q_subs,'Id'); ?>"><?php if (getVar($q_subs,'Active') == "Y") { ?>Yes<?php  } else { ?>No<?php  } ?></A>
+		</TD>
+		<TD ALIGN="CENTER">
+			<A HREF="/admin/users/subscriptions/del.php?User=<?php echo $editUser->getId(); ?>&Subs=<?php pgetUVar($q_subs,'Id'); ?>"><IMG SRC="/admin/img/icon/delete.png" BORDER="0" ALT="<?php putGS('Delete subscriptions to $1',getHVar($q_pub,'Name') ); ?>" TITLE="<?php  putGS('Delete subscriptions to $1',getHVar($q_pub,'Name') ); ?>"></A>
+		</TD>
+	</TR>
+<?php 
+}
+?>
+<?php  } else { ?>
+<tr class="list_row_even"><td><?php  putGS('No subscriptions.'); ?></td></tr>
+<?php  } ?>
+</TABLE>
