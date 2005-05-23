8e56ae4a1f33575785595a1b3d966bd153686c82
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-05-23 19:06:42 +0000

    deleted obsolete code

--- campsite/implementation/management/users/ipaccesslist.php
+++ campsite/implementation/management/users/ipaccesslist.php
@@ -1,57 +1,60 @@
 <?php
-query ("SELECT Name FROM Users WHERE Id=$User", 'users');
-if ($NUM_ROWS) {
-	fetchRow($users);
-?>
-<?php  } ?>
-<TABLE BORDER="0" CELLSPACING="0" CELLPADDING="0" WIDTH="100%">
-<TR>
-	<TD><TABLE BORDER="0" CELLSPACING="0" CELLPADDING="1"><TR><TD><A HREF="ipadd.php?User=<?php  p($User); ?>" ><IMG SRC="/admin/img/icon/add.png" BORDER="0"></A></TD><TD><A HREF="ipadd.php?User=<?php  p($User); ?>" ><B><?php  putGS("Add new IP address group"); ?></B></A></TD></TR></TABLE></TD>
-	<TD ALIGN="RIGHT">
-	</TD>
-</TABLE>
-<P><?php
-todefnum('IPOffs');
-if ($IPOffs < 0)
-	$IPOffs= 0;
 
-query ("SELECT (StartIP & 0xff000000) >> 24 as ip0, (StartIP & 0x00ff0000) >> 16 as ip1, (StartIP & 0x0000ff00) >> 8 as ip2, StartIP & 0x000000ff as ip3, StartIP, Addresses FROM SubsByIP WHERE IdUser = $User LIMIT $IPOffs, 11", 'IPs');
+check_basic_access($_REQUEST);
+if (!isset($editUser) || gettype($editUser) != 'object' || $editUser->getUserName() == '') {
+	CampsiteInterface::DisplayError('No such user account.',$_SERVER['REQUEST_URI']);
+	exit;
+}
+
+?>
+<TABLE BORDER="0" CELLSPACING="1" CELLPADDING="3" WIDTH="100%" class="table_list">
+	<tr class="table_list_header">
+		<td colspan="3" align="left">
+			 <?php putGS('User IP access list management'); ?>
+		</td>
+	</tr>
+	<tr class="table_list_header">
+		<td align="right" colspan="3" align="right">
+			<a href="ipadd.php?User=<?php echo $editUser->getId(); ?>">
+			<img src="/<?php echo $ADMIN; ?>/img/icon/add.png" BORDER="0"></a>&nbsp;
+			<a href="ipadd.php?User=<?php echo $editUser->getId(); ?>" ><B><?php putGS("Add new IP address group"); ?></b></a>
+		</td>
+	</tr>
+<?php
+
+query ("SELECT Name FROM Users WHERE Id=" . $editUser->getId(), 'users');
+if ($NUM_ROWS)
+	fetchRow($users);
+
+$sql = "SELECT (StartIP & 0xff000000) >> 24 as ip0, (StartIP & 0x00ff0000) >> 16 as ip1, (StartIP & 0x0000ff00) >> 8 as ip2, StartIP & 0x000000ff as ip3, StartIP, Addresses FROM SubsByIP WHERE IdUser = ".$editUser->getId();
+query($sql, 'IPs');
 if ($NUM_ROWS) {
 	$nr= $NUM_ROWS;
-	$i= 10;
 	$color= 0;
-?><TABLE BORDER="0" CELLSPACING="1" CELLPADDING="3" WIDTH="100%" class="table_list">
-	<TR class="table_list_header">
-		<TD ALIGN="LEFT" VALIGN="TOP"  ><B><?php  putGS("Start IP"); ?></B></TD>
-		<TD ALIGN="LEFT" VALIGN="TOP"  ><B><?php  putGS("Number of addresses"); ?></B></TD>
-		<TD ALIGN="LEFT" VALIGN="TOP" WIDTH="1%" ><B><?php  putGS("Delete"); ?></B></TD>
-	</TR>
-<?php 
+?>
+	<tr class="table_list_header">
+		<td align="left" valign="top"><B><?php putGS("Start IP"); ?></b></td>
+		<td align="left" valign="top"><b><?php putGS("Number of addresses"); ?></b></td>
+		<td align="left" valign="top" width="1%"><b><?php putGS("Delete"); ?></b></td>
+	</tr>
+<?php
 	for($loop=0;$loop<$nr;$loop++) {
 		fetchRow($IPs);
-		if ($i) { ?>	<TR <?php  if ($color) { $color=0; ?>class="list_row_even"<?php  } else { $color=1; ?>class="list_row_odd"<?php  } ?>>
-		<TD >
+		?>	<tr <?php  if ($color) { $color=0; ?>class="list_row_even"<?php  } else { $color=1; ?>class="list_row_odd"<?php  } ?>>
+		<TD>
 			<?php  p(getHVar($IPs,'ip0').'.'.getHVar($IPs,'ip1').'.'.getHVar($IPs,'ip2').'.'.getHVar($IPs,'ip3') ); ?>
-		</TD>
-		<TD >
+		</td>
+		<td>
 			<?php  pgetHVar($IPs,'Addresses'); ?>
-		</TD>
-		<TD ALIGN="CENTER">
-			<A HREF="/admin/users/ipdel.php?User=<?php  p($User); ?>&StartIP=<?php  pgetVar($IPs,'StartIP'); ?>"><IMG SRC="/admin/img/icon/delete.png" BORDER="0" ALT="<?php  putGS('Delete'); ?>" TITLE="<?php  putGS('Delete'); ?>"></A>
-		</TD>
-	</TR>
+		</td>
+		<td align="center">
+			<a href="/<?php echo $ADMIN; ?>/users/ipdel.php?User=<?php echo $editUser->getId(); ?>&StartIP=<?php  pgetVar($IPs,'StartIP'); ?>"><img src="/<?php echo $ADMIN; ?>/img/icon/delete.png" border="0" ALT="<?php putGS('Delete'); ?>" title="<?php putGS('Delete'); ?>"></a>
+		</td>
+	</tr>
 <?php  
-			$i--;
-		}
 	}
-?>	<TR><TD COLSPAN="2" NOWRAP>
-<?php  if ($IPOffs <= 0) { ?>		&lt;&lt; <?php  putGS('Previous'); ?>
-<?php  } else { ?>		<B><A HREF="ipaccesslist.php?User=<?php  p($User); ?>&IPOffs=<?php  p($IPOffs - 10); ?>">&lt;&lt; <?php  putGS('Previous'); ?></A></B>
-<?php  } ?><?php  if ($nr < 11) { ?>		 | <?php  putGS('Next'); ?> &gt;&gt;
-<?php  } else { ?>		 | <B><A HREF="ipaccesslist.php?User=<?php  p($User); ?>&IPOffs=<?php  p($IPOffs + 10); ?>"><?php  putGS('Next'); ?> &gt;&gt</A></B>
-<?php  } ?>	</TD></TR>
-</TABLE>
-<?php  } else { ?><BLOCKQUOTE>
-	<LI><?php  putGS('No records.'); ?></LI>
-</BLOCKQUOTE>
-<?php  } ?>
+} else {
+?><tr class="list_row_odd"><td colspan="3"><?php  putGS('No records.'); ?></td></tr>
+<?php } ?>
+</table>
+<br>
