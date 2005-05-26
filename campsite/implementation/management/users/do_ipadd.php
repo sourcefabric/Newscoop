b8a37be56ff3ad6aee6fa9c0d55aeeca5188ac6d
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-05-26 15:58:33 +0000

    replace m4 script with PHP script

---
+++ campsite/implementation/management/users/do_ipadd.php
@@ -0,0 +1,64 @@
+<?php
+
+require_once($_SERVER['DOCUMENT_ROOT']. "/$ADMIN_DIR/users/users_common.php");
+require_once($_SERVER['DOCUMENT_ROOT']. "/classes/Log.php");
+
+list($access, $User) = check_basic_access($_REQUEST);
+
+read_user_common_parameters(); // $uType, $userOffs, $lpp, search parameters
+$uType = 'Readers';
+compute_user_rights($User, &$canManage, &$canDelete);
+if (!$canManage) {
+	CampsiteInterface::DisplayError('You do not have the right to change user account information.');
+	exit;
+}
+
+// read input
+$userId = Input::Get('User', 'int', 0);
+$editUser = new User($userId);
+if ($editUser->getUserName() == '') {
+	CampsiteInterface::DisplayError('No such user account.');
+	exit;
+}
+$backLink = "/$ADMIN/users/edit.php?uType=Readers&User=$userId";
+$cStartIP1 = Input::Get('cStartIP1', 'int', 0);
+$cStartIP2 = Input::Get('cStartIP2', 'int', 0);
+$cStartIP3 = Input::Get('cStartIP3', 'int', 0);
+$cStartIP4 = Input::Get('cStartIP4', 'int', 0);
+$cAddresses = Input::Get('cAddresses', 'int', 0);
+
+// check if input was correct
+if ($cStartIP1 == 0 || $cStartIP2 == 0 || $cStartIP3 == 0 || $cStartIP4 == 0) {
+	CampsiteInterface::DisplayError(array('You must complete the $1 field.', 'Start IP'),
+		$backLink);
+	exit;
+}
+if ($cAddresses == 0) {
+	CampsiteInterface::DisplayError(array('You must complete the $1 field.',
+			'Number of addresses'), $backLink);
+	exit;
+}
+
+// check if the IP address group exists already
+$StartIP = $cStartIP1*256*256*256+$cStartIP2*256*256+$cStartIP3*256+$cStartIP4;
+$ip = "$cStartIP1.$cStartIP2.$cStartIP3.$cStartIP4";
+query ("SELECT Addresses FROM SubsByIP WHERE IdUser=$userId and StartIP=$StartIP", 'ig');
+if ($NUM_ROWS) {
+	CampsiteInterface::DisplayError(array('An IP address group having the $1 start address already exists.', $ip), $backLink);
+	exit;
+}
+
+query ("INSERT IGNORE INTO SubsByIP SET IdUser=$userId, StartIP='$StartIP', Addresses=$cAddresses");
+$created = ($AFFECTED_ROWS > 0);
+if ($created) {
+	$logtext = getGS('IP Group $1 added for user $2', encHTML("$ip:$cAddresses"),
+		encHTML($editUser->getUserName()));
+	Log::Message($logtext, $editUser->getUserName(), 57);
+} else {
+	CampsiteInterface::DisplayError(array('There was an error creating the IP address group.', "$ip:$cAddresses"), $backLink);
+	exit;
+}
+
+header("Location: /$ADMIN/users/edit.php?uType=Readers&User=$userId");
+
+?>
---
+++ campsite/implementation/management/users/do_ipdel.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once($_SERVER['DOCUMENT_ROOT']. "/$ADMIN_DIR/users/users_common.php");
+require_once($_SERVER['DOCUMENT_ROOT']. "/classes/Log.php");
+
+list($access, $User) = check_basic_access($_REQUEST);
+
+read_user_common_parameters(); // $uType, $userOffs, $lpp, search parameters
+$uType = 'Readers';
+compute_user_rights($User, &$canManage, &$canDelete);
+if (!$canManage) {
+	CampsiteInterface::DisplayError('You do not have the right to change user account information.');
+	exit;
+}
+
+$userId = Input::Get('User', 'int', 0);
+$editUser = new User($userId);
+if ($editUser->getUserName() == '') {
+	CampsiteInterface::DisplayError('No such user account.');
+	exit;
+}
+$StartIP = Input::Get('StartIP', 'string', '');
+
+query ("SELECT Addresses FROM SubsByIP WHERE IdUser=$userId and StartIP=$StartIP", 'ig');
+if ($NUM_ROWS) {
+	fetchRow($ig);
+	query("DELETE FROM SubsByIP WHERE IdUser=$userId and StartIP=$StartIP");
+	$logtext = getGS('The IP address group $1 has been deleted.',
+		"$StartIP:" . getHVar($ig, 'Addresses'));
+	Log::Message($logtext, $editUser->getUserName(), 58);
+}
+
+header("Location: /$ADMIN/users/edit.php?uType=Readers&User=$userId");
+
+?>
