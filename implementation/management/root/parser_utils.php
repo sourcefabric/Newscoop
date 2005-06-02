12fc57eb8147ccc06902222a03943c4784370448
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-06-02 19:00:33 +0000

    removed reference from function parameters in read_parameters()

--- implementation/management/root/parser_utils.php
+++ implementation/management/root/parser_utils.php
@@ -87,10 +87,10 @@ function read_parameters(&$query_string)
 {
 	switch (getenv("REQUEST_METHOD")) {
 	case "GET":
-		return read_get_parameters(&$query_string);
+		return read_get_parameters($query_string);
 		break;
 	case "POST":
-		return read_post_parameters(&$query_string);
+		return read_post_parameters($query_string);
 		break;
 	default:
 		echo "<p>Unable to process " . getenv("REQUEST_METHOD") . " request method</p>";
