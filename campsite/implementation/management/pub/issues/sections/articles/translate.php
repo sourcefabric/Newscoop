daa013b8a2fd15b3904064525280ff956adee028
paul <paul@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-06-07 06:27:05 +0000

    Added Back link.

--- campsite/implementation/management/pub/issues/sections/articles/translate.php
+++ campsite/implementation/management/pub/issues/sections/articles/translate.php
@@ -64,6 +64,18 @@ $topArray = array('Pub' => $publicationObj, 'Issue' => $issueObj,
 				  'Section' => $sectionObj, 'Article'=>$articleObj);
 CampsiteInterface::ContentTop('Translate article', $topArray, true, true);
 ?>
+<table>
+<tr>
+	<td>
+		<TABLE BORDER="0" CELLSPACING="0" CELLPADDING="1">
+		<TR>
+			<TD><A HREF="edit.php?Pub=<?php  p($Pub); ?>&Issue=<?php  p($Issue); ?>&Section=<?php  p($Section); ?>&Article=<?php  p($Article); ?>&Language=<?php  p($Language); ?>&sLanguage=<?php  p($sLanguage); ?>" ><IMG SRC="/<?php echo $ADMIN; ?>/img/icon/back.png" BORDER="0"></A></TD>
+			<TD><A HREF="edit.php?Pub=<?php  p($Pub); ?>&Issue=<?php  p($Issue); ?>&Section=<?php  p($Section); ?>&Article=<?php  p($Article); ?>&Language=<?php  p($Language); ?>&sLanguage=<?php  p($sLanguage); ?>" ><B><?php  putGS('Back to article details'); ?></B></A></TD>
+		</TR>
+		</TABLE>
+	</td>
+</tr>
+</table>
 <P>
 <FORM NAME="dialog" METHOD="POST" ACTION="do_translate.php" >
 <INPUT TYPE="HIDDEN" NAME="Pub" VALUE="<?php  p($Pub); ?>">
