fd79d567dc46ef1d2e07544ad9b259f7214d2b5a
paul <paul@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-06-07 11:32:14 +0000

    Get rid of conversion code, rearrange some stuff.

--- campsite/implementation/management/localizer/index.php
+++ campsite/implementation/management/localizer/index.php
@@ -1,26 +1,18 @@
 <?php
-include_once($_SERVER['DOCUMENT_ROOT']."/configuration.php");
-include_once($_SERVER['DOCUMENT_ROOT']."/$ADMIN_DIR/CampsiteInterface.php");
-include_once($_SERVER['DOCUMENT_ROOT']."/classes/Input.php");
-include_once('Localizer.php');
-
-$localizer =& Localizer::getInstance();
-Localizer::LoadLanguageFiles('/localizer', 'locals');
-Localizer::LoadLanguageFiles('/', 'globals');
+require_once($_SERVER['DOCUMENT_ROOT']."/configuration.php");
+require_once($_SERVER['DOCUMENT_ROOT']."/$ADMIN_DIR/CampsiteInterface.php");
+require_once($_SERVER['DOCUMENT_ROOT']."/classes/Input.php");
+require_once('Localizer.php');
 
 global $g_translationStrings;
+global $g_localizerConfig;
 
 $action = Input::Get('action', 'string', 'translate', true);
-
-// Show the converstion screen if this is the first time the
-// user is using the new localizer.
-//echo Localizer::GetMode()."<br>";
-if ((Localizer::GetMode() == 'php') && ($action != 'convert_confirm')) {
-	$action = 'convert';
-}
-
-if (isset($_REQUEST['find_translation_strings'])) {
-	$action = 'find_translation_strings';
+$base = Input::Get('base', 'string', $g_localizerConfig['FILENAME_PREFIX'], true);
+$directory = Input::Get('dir', 'string', $g_localizerConfig['BASE_DIR'], true);
+if ($directory == '/globals') {
+	$directory = '/';
+	$base = 'globals';
 }
 
 //echo "<pre>";
@@ -39,127 +31,80 @@ if (isset($_REQUEST['find_translation_strings'])) {
 <?php
 //echo "Action: $action<br>";
 switch ($action) {
-case 'convert': 
-	include('convert.php');
-    break;
-    
-case 'convert_confirm':
-	include('do_convert.php');
-	break;
-
-case 'translate':
-    require_once("translate.php");    
-    translationForm($_REQUEST);
-	break;
-
-case 'save_translation':
-    $base = Input::Get('base');
-    $directory = Input::Get('dir');
-	if ($directory == '/globals') {
-		$directory = '/';
-		$base = 'globals';
-	}
-    $targetLanguageId = Input::Get('localizer_target_language');
-    $data = Input::Get('data', 'array');
-    Localizer::ModifyStrings($base, $directory, $targetLanguageId, $data);
-    // Localizer strings are changed -> reload files
-    Localizer::LoadLanguageFiles('/', 'globals');
-    Localizer::LoadLanguageFiles('/localizer', 'locals'); 
-    require_once("translate.php");    
-    translationForm($_REQUEST);
-	break;
-
+	case 'translate':
+	    require_once("translate.php");    
+	    translationForm($_REQUEST);
+		break;
 	
-//case 'add_string':
-//	$base = Input::Get('base');
-//	$directory = Input::Get('dir');
-//	if ($directory == '/globals') {
-//		$directory = '/';
-//		$base = 'globals';
-//	}
-//	$pos = Input::Get('pos');
-//	if ($pos == 'begin') {
-//		$pos = 0;
-//	}
-//	elseif ($pos == 'end') {
-//		$pos = null;
-//	}
-//
-//    $msg = Localizer::CompareKeys($directory, $_REQUEST['newKey']);
-//    if (count($msg) > 0) {
-//        foreach ($msg as $val => $err) {
-//            while ($key = array_search($val, $_REQUEST['newKey'])) {
-//                unset($_REQUEST['newKey'][$key]);
-//            }
-//        }
-//    }
-//	// skip if all was unset above
-//    if (count($_REQUEST['newKey'])) {  
-//        Localizer::AddStringAtPosition($base, $directory, $pos, $_REQUEST['newKey']);
-//    }
-//
-//    require_once("translate.php");
-//    translationForm($_REQUEST);
-//	break;
-
-case 'remove_string':
-	$base = Input::Get('base');
-	$directory = Input::Get('dir');
-	if ($directory == '/globals') {
-		$directory = '/';
-		$base = 'globals';
-	}
-    $deleteMe = Input::Get('string', 'string');
-    Localizer::RemoveString($base, $directory, $deleteMe);
-    require_once("translate.php");    
-    translationForm($_REQUEST);
-	break;
-
-case 'move_string':
-	$base = Input::Get('base');
-	$directory = Input::Get('dir');
-	if ($directory == '/globals') {
-		$directory = '/';
-		$base = 'globals';
-	}
-	$pos1 = Input::Get('pos1', 'int');
-	$pos2 = Input::Get('pos2', 'int');
-    Localizer::MoveString($base, $directory, $pos1, $pos2);
-    require_once("translate.php");    
-    translationForm($_REQUEST);
-	break;
-
-case 'add_missing_translation_strings':
-	$base = Input::Get('base');
-	$directory = Input::Get('dir');
-	if ($directory == '/globals') {
-		$directory = '/';
-		$base = 'globals';
-	}
-	//echo "Base: $base, Directory: $directory<br>";
-	$missingStrings = Localizer::FindMissingStrings($directory);
-    if (count($missingStrings) > 0) {
-        Localizer::AddStringAtPosition($base, $directory, 0, $missingStrings);
-    }
-    require_once("translate.php");    
-    translationForm($_REQUEST);
-	break;
+	case 'save_translation':
+	    $targetLanguageId = Input::Get('localizer_target_language');
+	    $data = Input::Get('data', 'array');
+	    Localizer::ModifyStrings($base, $directory, $targetLanguageId, $data);
+	    // Localizer strings are changed -> reload files
+	    Localizer::LoadLanguageFiles('/', 'globals');
+	    Localizer::LoadLanguageFiles('/localizer', 'locals'); 
+	    require_once("translate.php");    
+	    translationForm($_REQUEST);
+		break;
+		
+	case 'remove_string':
+	    $deleteMe = Input::Get('string', 'string');
+	    Localizer::RemoveString($base, $directory, $deleteMe);
+	    require_once("translate.php");    
+	    translationForm($_REQUEST);
+		break;
 	
-case 'delete_unused_translation_strings':
-	$base = Input::Get('base');
-	$directory = Input::Get('dir');
-	if ($directory == '/globals') {
-		$directory = '/';
-		$base = 'globals';
-	}
-	//echo "Base: $base, Directory: $directory<br>";
-	$unusedStrings = Localizer::FindUnusedStrings($directory);
-    if (count($unusedStrings) > 0) {
-       	Localizer::RemoveString($base, $directory, $unusedStrings);
-    }
-    require_once("translate.php");    
-    translationForm($_REQUEST);
-	break;
+	case 'move_string':
+		$pos1 = Input::Get('pos1', 'int');
+		$pos2 = Input::Get('pos2', 'int');
+	    Localizer::MoveString($base, $directory, $pos1, $pos2);
+	    require_once("translate.php");    
+	    translationForm($_REQUEST);
+		break;
+	
+	case 'add_missing_translation_strings':
+		$missingStrings = Localizer::FindMissingStrings($directory);
+	    if (count($missingStrings) > 0) {
+	        Localizer::AddStringAtPosition($base, $directory, 0, $missingStrings);
+	    }
+	    require_once("translate.php");    
+	    translationForm($_REQUEST);
+		break;
+		
+	case 'delete_unused_translation_strings':
+		$unusedStrings = Localizer::FindUnusedStrings($directory);
+	    if (count($unusedStrings) > 0) {
+	       	Localizer::RemoveString($base, $directory, $unusedStrings);
+	    }
+	    require_once("translate.php");    
+	    translationForm($_REQUEST);
+		break;
+	
+	//case 'add_string':
+	//	$pos = Input::Get('pos');
+	//	if ($pos == 'begin') {
+	//		$pos = 0;
+	//	}
+	//	elseif ($pos == 'end') {
+	//		$pos = null;
+	//	}
+	//
+	//    $msg = Localizer::CompareKeys($directory, $_REQUEST['newKey']);
+	//    if (count($msg) > 0) {
+	//        foreach ($msg as $val => $err) {
+	//            while ($key = array_search($val, $_REQUEST['newKey'])) {
+	//                unset($_REQUEST['newKey'][$key]);
+	//            }
+	//        }
+	//    }
+	//	// skip if all was unset above
+	//    if (count($_REQUEST['newKey'])) {  
+	//        Localizer::AddStringAtPosition($base, $directory, $pos, $_REQUEST['newKey']);
+	//    }
+	//
+	//    require_once("translate.php");
+	//    translationForm($_REQUEST);
+	//	break;
 
 } // switch
 
