f13bbf717ce49ac658e6bdfe1ed2ae19f83f4554
paul <paul@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-06-08 19:09:36 +0000

    *** empty log message ***

---
+++ implementation/management/localizer/locals.en.php
@@ -0,0 +1,33 @@
+<?php
+regGS("success", "success");
+regGS("fail", "fail");
+regGS("edit '$1'", "edit '$1'");
+regGS("edit \"$1\"", "edit \"$1\"");
+regGS("cannot read $1", "cannot read $1");
+regGS("at position:", "at position:");
+regGS("User Subscriptions Sections", "User Subscriptions Sections");
+regGS("User Subscriptions", "User Subscriptions");
+regGS("Translate to:", "Translate to:");
+regGS("Translate from:", "Translate from:");
+regGS("The following strings are not used:", "The following strings are not used:");
+regGS("The following strings are missing from the translation files:", "The following strings are missing from the translation files:");
+regGS("Screen:", "Screen:");
+regGS("Save", "Save");
+regGS("No source strings found.", "No source strings found.");
+regGS("No matches found.", "No matches found.");
+regGS("Localizer", "Localizer");
+regGS("Image Archive", "Image Archive");
+regGS("Hide translated strings?", "Hide translated strings?");
+regGS("Globals", "Globals");
+regGS("Converting...", "Converting...");
+regGS("At the end", "At the end");
+regGS("At the beginning", "At the beginning");
+regGS("Article Type Fields", "Article Type Fields");
+regGS("Article Topics", "Article Topics");
+regGS("Article Images", "Article Images");
+regGS("Are you sure you want to delete this entry?", "Are you sure you want to delete this entry?");
+regGS("All strings have been translated.", "All strings have been translated.");
+regGS("After \"$1\"", "After \"$1\"");
+regGS("Add new string:", "Add new string:");
+regGS("Add", "Add");
+?>
