9a3e3ec5037aa5494b690e39a5c1230ec30f2ec3
paul <paul@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-06-10 10:25:18 +0000

    Fixed documentation so that it looks nice in PHPDocumentor, fixed function braces to the spec.

--- campsite/implementation/management/lib_campsite.php
+++ campsite/implementation/management/lib_campsite.php
@@ -5,22 +5,25 @@ $DOCUMENT_ROOT = $_SERVER['DOCUMENT_ROOT'];
  * Query the database with the given string and assign the result 
  * to a global variable.  Use fetchRow() to get the results.
  *
- * @param string p_queryString
+ * @param string $p_queryString
  *		The database query.
  *
- * @param string p_globalVar
+ * @param string $p_globalVar
  *		The name of the global variable to assign the result to.
+ *
+ * @param boolean $p_setvars
  *
  * @return void
  */
-function query($p_queryString, $p_globalVar='', $setvars=true) {
+function query($p_queryString, $p_globalVar='', $p_setvars = true) 
+{
 	$queryResult = mysql_query($p_queryString);
 	$queryParts = explode(" ", $p_queryString);
 	//$queryType=strtoupper(substr(trim($p_queryString),0,6));
 	$queryType = strtoupper($queryParts[0]);
 	$GLOBALS['NUM_ROWS']=0;
 	$GLOBALS['AFFECTED_ROWS']=0;
-	if ($setvars && $queryResult) {
+	if ($p_setvars && $queryResult) {
 		if (($queryType=='SELECT') || ($queryType == 'SHOW')) {
 			$GLOBALS['NUM_ROWS'] = mysql_num_rows($queryResult);
 		}
@@ -36,23 +39,30 @@ function query($p_queryString, $p_globalVar='', $setvars=true) {
 	}
 } // fn query
 
-function encURL($s) {
+
+
+function encURL($s) 
+{
     return rawurlencode($s);
 }
 
-function decURL($s) {
+function decURL($s) 
+{
     return rawurldecode($s);
 }
 
-function pencURL($s) {
+function pencURL($s) 
+{
     print rawurlencode($s);
 }
 
-function pdecURL($s) {
+function pdecURL($s) 
+{
     print rawurldecode($s);
 }
 
-function encHTML($s) {
+function encHTML($s) 
+{
     $res_str = str_replace("&", "&amp;", $s);
     $res_str = str_replace("<", "&lt;", $res_str);
     $res_str = str_replace(">", "&gt;", $res_str);
@@ -63,7 +73,8 @@ function encHTML($s) {
 }
 
 
-function pencHTML($s) {
+function pencHTML($s) 
+{
     print encHTML($s);
 }
 
@@ -71,17 +82,18 @@ function pencHTML($s) {
 /**
  * Compute the difference between two string times.
  *
- * @param string p_time1
+ * @param string $p_time1
  *      Any string that can be converted with strtotime();
  *
- * @param string p_time2
+ * @param string $p_time2
  *      (optional) Any string that can be converted with strtotime();
  *      If not specified, the current time is used.
  *
  * @return array
  *      An array of (days, hours, minutes, seconds)
  */
-function camp_time_diff_str($p_time1, $p_time2 = null) {
+function camp_time_diff_str($p_time1, $p_time2 = null) 
+{
     // Convert the string times into absolute seconds
     $p_time1 = strtotime($p_time1);
     if (is_null($p_time2)) {
@@ -107,20 +119,21 @@ function camp_time_diff_str($p_time1, $p_time2 = null) {
  * Return a value from the array, or if the value does not exist,
  * return the given default value.
  *
- * @param array p_array
- * @param mixed p_index
- * @param mixed p_defaultValue
+ * @param array $p_array
+ * @param mixed $p_index
+ * @param mixed $p_defaultValue
  *
  * @return mixed
  */
-function array_get_value($p_array, $p_index, $p_defaultValue = null) {
+function camp_array_get_value($p_array, $p_index, $p_defaultValue = null) 
+{
 	if (isset($p_array[$p_index])) {
 		return $p_array[$p_index];
 	}
 	else {
 		return $p_defaultValue;
 	}
-} // fn array_get_value
+} // fn camp_array_get_value
 
 
 /**
@@ -129,14 +142,55 @@ function array_get_value($p_array, $p_index, $p_defaultValue = null) {
  * This is for confirmation dialogs so that database strings do not
  * mess things up.
  *
- * @param string p_string
+ * @param string $p_string
  * @return string
  */
-function javascriptspecialchars($p_string) {
+function camp_javascriptspecialchars($p_string) 
+{
 	$encodedString = htmlspecialchars($p_string);
 	$slashedString = addslashes($encodedString);
 	return $slashedString;
-} // fn javascriptspecialchars
+} // fn camp_javascriptspecialchars
+
+
+/**
+ * Format the values of an array into a string.
+ *
+ * @param array $p_array
+ *      The array to format.
+ * @param string $p_keyValueSeparator
+ *      The string to put between the key and the value.
+ * @param string $p_elementSeparator
+        The string to put between elements of the array.
+ * @param string $p_keyPrefixString
+ *      The string to put before each key. Default is the empty string.
+ * @param string $p_keyPostfixString
+ *      The string to put after each key.  Default is the empty string.
+ * @param string $p_valuePrefixString
+ *      The string to put before each element value. Default is the empty string.
+ * @param string $p_valuePostfixString
+ *      The string to put after each element value. Default is the empty string.
+ * @return string
+ */
+function camp_implode_keys_and_values($p_array,
+                                      $p_keyValueSeparator, 
+                                      $p_elementSeparator, 
+                                      $p_keyPrefixString = '', 
+                                      $p_keyPostfixString = '', 
+                                      $p_valuePrefixString = '', 
+                                      $p_valuePostfixString = '') 
+{
+    $returnString = '';
+    if (is_array($p_array)) {
+        $elements = array();
+        foreach ($p_array as $key => $value) {
+            $elements[] = $p_keyPrefixString . $key . $p_keyPostfixString . 
+                $p_keyValueSeparator . $p_valuePrefixString . $value . $p_valuePostfixString;
+        }
+        $returnString = implode($p_elementSeparator, $elements);
+    }
+    return $returnString;
+} // fn camp_implode_keys_and_values
 
 
 /**
@@ -146,7 +200,8 @@ function javascriptspecialchars($p_string) {
  *
  * @return void
  */
-function todef($p_varName, $p_defaultValue='') {
+function todef($p_varName, $p_defaultValue='') 
+{
 	if (!isset($_REQUEST[$p_varName])) {
 		$GLOBALS[$p_varName] = $p_defaultValue;
 	}
@@ -160,7 +215,8 @@ function todef($p_varName, $p_defaultValue='') {
 } // fn todef
 
 
-function encSQL($s) {
+function encSQL($s) 
+{
 	if (!get_magic_quotes_gpc()) {
 		return mysql_escape_string($s);
 	}
@@ -174,7 +230,8 @@ function encSQL($s) {
  *
  * @return void
  */
-function todefnum($p_varName, $p_defaultValue=0) {
+function todefnum($p_varName, $p_defaultValue=0) 
+{
 	if (!isset($_REQUEST[$p_varName])) {
 		$GLOBALS[$p_varName] = (int)$p_defaultValue;
 	}
@@ -188,17 +245,19 @@ function todefnum($p_varName, $p_defaultValue=0) {
  * Fetch a row from the mysql result and store it in a global variable.
  * Use getVar() to retrieve values.
  *
- * @param object p_mysqlResult
+ * @param object $p_mysqlResult
  * 		The result of a mysql query.
  *
  * @return void
  */
-function fetchRow($p_mysqlResult) {
+function fetchRow($p_mysqlResult) 
+{
     $GLOBALS['fetch_'.$p_mysqlResult]=mysql_fetch_array($p_mysqlResult,MYSQL_ASSOC);
 } // fn fetchRow
 
 
-function fetchRowNum($q) {
+function fetchRowNum($q) 
+{
     $GLOBALS['fetch_num_'.$q]=mysql_fetch_array($q,MYSQL_NUM);
 }
 
@@ -208,23 +267,27 @@ function fetchRowNum($q) {
  *
  * @return string
  */
-function getVar($p_queryResult, $p_columnName) {
+function getVar($p_queryResult, $p_columnName) 
+{
     $arr=$GLOBALS['fetch_'.$p_queryResult];
     return $arr[$p_columnName];
 } // fn getVar
 
 
-function getNumVar($q,$n=0) {
+function getNumVar($q, $n=0) 
+{
     $arr=$GLOBALS['fetch_num_'.$q];
     return $arr[$n];
 }
 
-function pgetNumVar($q,$n=0) {
+function pgetNumVar($q,$n=0) 
+{
     $arr=$GLOBALS['fetch_num_'.$q];
     print $arr[$n];
 }
 
-function getHVar($q,$s) {
+function getHVar($q,$s) 
+{
     return encHTML(getVar($q,$s));
 }
 
@@ -233,16 +296,19 @@ function getHVar($q,$s) {
  * Get the variable and run HTMLSPECIALCHARS on it.
  * @return string
  */
-function pgetHVar($q,$s) {
-    print getHVar($q,$s);
+function pgetHVar($q,$s) 
+{
+    print getHVar($q, $s);
 } // fn pgetHVar
 
 
-function getUVar($q,$s) {
+function getUVar($q, $s) 
+{
     return encURL(getVar($q,$s));
 }
 
-function getSVar($q,$s) {
+function getSVar($q, $s) 
+{
     return addslashes(getVar($q,$s));
 }
 
@@ -250,15 +316,18 @@ function getSVar($q,$s) {
  * URL encode the fetched value.
  * @return string
  */
-function pgetUVar($q,$s) {
+function pgetUVar($q,$s) 
+{
     print rawurlencode(getVar($q,$s));
 } // fn pgetUVar
 
-function pgetVar($q,$s) {
+function pgetVar($q,$s) 
+{
     print getVar($q,$s);
 }
 
-function todefradio($s) {
+function todefradio($s) 
+{
 	if (!isset($GLOBALS[$s])) {
 		$GLOBALS[$s]='';
 	}
@@ -270,7 +339,8 @@ function todefradio($s) {
 	}
 }
 
-function checkedIfY($qh,$field) {
+function checkedIfY($qh,$field) 
+{
 	if (getVar($qh,$field) == 'Y') {
 		print " CHECKED";
 	}
@@ -280,12 +350,13 @@ function checkedIfY($qh,$field) {
 /**
  * Create an HTML OPTION element.
  *
- * @param string p_value
- * @param string p_selectedValue
- * @param string p_printValue
+ * @param string $p_value
+ * @param string $p_selectedValue
+ * @param string $p_printValue
  * @return void
  */
-function pcomboVar($p_value, $p_selectedValue, $p_printValue) {
+function pcomboVar($p_value, $p_selectedValue, $p_printValue) 
+{
 	print '<OPTION VALUE="'.encHTML($p_value).'"';
 	if (!strcmp($p_value, $p_selectedValue)) {
 		print ' SELECTED';
@@ -294,75 +365,19 @@ function pcomboVar($p_value, $p_selectedValue, $p_printValue) {
 } // fn pcombovar
 
 
-function encS($s) {
+function encS($s) 
+{
     return addslashes($s);
 }
 
-function decS($s) {
+function decS($s) 
+{
     return stripslashes($s);
 }
 
-/**
- * Translate the given string and print it.  This function accepts a variable
- * number of parameters and works something like printf().
- *
- * @param string p_translateString
- *		The string to translate.
- *
- * @return void
- */
-//function putGS($p_translateString) {
-//	$args = func_get_args();
-//	//array_unshift($args, $p_translateString);
-//	echo call_user_func_array("getGS", $args);
-//} // fn putGS
 
-
-/**
- * Translate the given string and return it.  This function accepts a variable
- * number of parameters and works something like printf().
- *
- * @param string p_translateString
- *		The string to translate.
- *
- * @return string
- */
-//function getGS($p_translateString) {
-//	global $g_translationStrings, $TOL_Language;
-//	$numFunctionArgs = func_num_args();
-//	if (!isset($g_translationStrings[$p_translateString]) || ($g_translationStrings[$p_translateString]=='')) {
-//		$translatedString = "$p_translateString (not translated)";
-//	}
-//	else {
-//		$translatedString = $g_translationStrings[$p_translateString];
-//	}
-//	if ($numFunctionArgs > 1) {
-//		for ($i = 1; $i < $numFunctionArgs; $i++){
-//			$name = '$'.$i;
-//			$val = func_get_arg($i);
-//			$translatedString = str_replace($name, $val, $translatedString);
-//		}
-//	}
-//	return $translatedString;
-//} // fn getGS
-//
-//function regGS($key,$value) {
-//	global $g_translationStrings;
-//	if (isset($g_translationStrings[$key])) {
-//		if ($key!='') {
-//			print "The global string is already set in $_SERVER[PHP_SELF]: $key<BR>";
-//		}
-//	}
-//	else{
-//		if (substr($value,strlen($value)-3)==(":".$_REQUEST["TOL_Language"])){
-//			$value=substr($value,0,strlen($value)-3);
-//		}
-//		$g_translationStrings[$key]=$value;
-//	}
-//} // fn regGS
-
-
-function dSystem($s) {
+function dSystem($s) 
+{
 //    print ("<BR>Executing <BR>$s<BR>");
     system($s);
 }
@@ -370,15 +385,17 @@ function dSystem($s) {
 
 /** 
  * An alias for "print()".
- * @param string p_string
+ * @param string $p_string
  * @return void
  */
-function p($p_string) {
+function p($p_string) 
+{
     print $p_string;
 } // fn p
 
 
-function ifYthenCHECKED($q,$f) {
+function ifYthenCHECKED($q,$f) 
+{
 	if (getVar($q,$f)=='Y') {
 		echo ' CHECKED';
 	}
@@ -387,35 +404,41 @@ function ifYthenCHECKED($q,$f) {
 
 /**
  * Load the global and local language files.
- * @param string p_path
- * @param string p_name
+ * @param string $p_path
+ * @param string $p_name
  * @return void
  */
-function selectLanguageFile($p_path, $p_name) {
+function selectLanguageFile($p_path, $p_name) 
+{
     require_once('localizer/Localizer.php');
     Localizer::LoadLanguageFilesAbs($p_path, $p_name);
 } // fn selectLanguageFile
 
 
-function decSlashes($s) {
+function decSlashes($s) 
+{
     return str_replace("%2F", "/", $s);
 }
 
-function encParam($s) {
+function encParam($s) 
+{
     return str_replace("\"", "%22", str_replace("&", "%26", str_replace(";", "%3B", str_replace("%", "%25", $s))));
 }
 
-function printSelected($var_value, $current_value) {
+function printSelected($var_value, $current_value) 
+{
 	if ($var_value == $current_value) {
    		echo " selected";
 	}
 }
 
-function pencParam($s) {
+function pencParam($s) 
+{
     print encParam($s);
 }
 
-function add_subs_section($publication_id, $section_nr) {
+function add_subs_section($publication_id, $section_nr) 
+{
 	// retrieve the default trial and paid time of the subscriptions
 	$dd_query = "select TimeUnit, TrialTime, PaidTime from Publications where Id = "
 	   . $publication_id;
@@ -515,7 +538,8 @@ function add_subs_section($publication_id, $section_nr) {
 	return $subs_count;
 } // fn add_subs_section
 
-function del_subs_section($publication_id, $section_nr) {
+function del_subs_section($publication_id, $section_nr) 
+{
 	$subs_query = "select Id from Subscriptions where IdPublication = " . $publication_id;
 	query($subs_query, 'subs');
 	if ($GLOBALS['NUM_ROWS'] < 0) {
@@ -535,7 +559,8 @@ function del_subs_section($publication_id, $section_nr) {
 	return $index;
 }
 
-function limitchars($text, $lim, $break, $tail) {
+function limitchars($text, $lim, $break, $tail) 
+{
 	//  $text = split("$break", $text);
 	// If you want this function case insensitive
 	// replace above line with these two lines
@@ -566,7 +591,8 @@ function limitchars($text, $lim, $break, $tail) {
 } // fn limitchars
 
 
-function valid_field_name($name) {
+function valid_field_name($name) 
+{
 	if (strlen($name) == 0) {
 		return false;
 	}
@@ -580,7 +606,8 @@ function valid_field_name($name) {
 	return true;
 }
 
-function valid_short_name($name) {
+function valid_short_name($name) 
+{
 	if (strlen($name) == 0) {
 		return false;
 	}
@@ -800,7 +827,8 @@ function template_is_used($template_name)
 }
 
 
-if (file_exists($_SERVER['DOCUMENT_ROOT']."/$ADMIN_DIR/modules/admin/priv_functions.php"))
+if (file_exists($_SERVER['DOCUMENT_ROOT']."/$ADMIN_DIR/modules/admin/priv_functions.php")) {
 	include ($_SERVER['DOCUMENT_ROOT']."/$ADMIN_DIR/modules/admin/priv_functions.php");
+}
 
 ?>
