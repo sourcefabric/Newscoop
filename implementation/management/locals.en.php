a8b4a0918fb8fd62067a45935e1d465d1d7da0d3
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-06-23 17:28:47 +0000

    added new messages

--- implementation/management/locals.en.php
+++ implementation/management/locals.en.php
@@ -1,17 +1,11 @@
-<?php 
-
-// -- login.m4
-regGS("Please enter your user name and password","Please enter your user name and password:en");
-
-// -- ad.m4
-regGS("You do not have the right to access this page.","You do not have the right to access this page.:en");
-
-// -- do_login.m4
-regGS("Login failed","Login failed:en");
-regGS("Pease make sure that you typed the correct user name and password.","Pease make sure that you typed the correct user name and password.:en");
-regGS("If your problem persists please contact the site administrator $1","If your problem persists please contact the site administrator $1:en");
-
-// -- home.m4
-regGS("Localizer","Localizer:en");
-
+<?php
+regGS("Signed in: $1", "Signed in: $1");
+regGS("Help", "Help");
+regGS("About", "About");
+regGS("Please enter your user name and password", "Please enter your user name and password");
+regGS("You do not have the right to access this page.", "You do not have the right to access this page.");
+regGS("Login failed", "Login failed");
+regGS("Pease make sure that you typed the correct user name and password.", "Pease make sure that you typed the correct user name and password.");
+regGS("If your problem persists please contact the site administrator $1", "If your problem persists please contact the site administrator $1");
+regGS("Localizer", "Localizer");
 ?>
