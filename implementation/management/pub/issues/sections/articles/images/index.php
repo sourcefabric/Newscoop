a6b81c0774eef7d5c4c7a8f0399c44e880be9471
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-07-01 11:47:29 +0000

    fixed bug #1219:  Change "Back to article details" to "Back to edit article"

--- implementation/management/pub/issues/sections/articles/images/index.php
+++ implementation/management/pub/issues/sections/articles/images/index.php
@@ -40,7 +40,7 @@ CampsiteInterface::ContentTop(getGS('Article Image List'), $topArray);
 		<TABLE BORDER="0" CELLSPACING="0" CELLPADDING="1">
 		<TR>
 			<TD><A HREF="../edit.php?Pub=<?php  p($Pub); ?>&Issue=<?php  p($Issue); ?>&Section=<?php  p($Section); ?>&Article=<?php  p($Article); ?>&Language=<?php  p($Language); ?>&sLanguage=<?php  p($sLanguage); ?>" ><IMG SRC="/<?php echo $ADMIN; ?>/img/icon/back.png" BORDER="0"></A></TD>
-			<TD><A HREF="../edit.php?Pub=<?php  p($Pub); ?>&Issue=<?php  p($Issue); ?>&Section=<?php  p($Section); ?>&Article=<?php  p($Article); ?>&Language=<?php  p($Language); ?>&sLanguage=<?php  p($sLanguage); ?>" ><B><?php  putGS('Back to article details'); ?></B></A></TD>
+			<TD><A HREF="../edit.php?Pub=<?php  p($Pub); ?>&Issue=<?php  p($Issue); ?>&Section=<?php  p($Section); ?>&Article=<?php  p($Article); ?>&Language=<?php  p($Language); ?>&sLanguage=<?php  p($sLanguage); ?>" ><B><?php putGS('Back to Edit Article'); ?></B></A></TD>
 		</TR>
 		</TABLE>
 	</td>
