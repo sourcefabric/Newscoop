990c86e8704eddbaf515f2de76650c63136e6e17
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-07-01 15:39:46 +0000

    allow null values for gender and country

--- implementation/database/upgrade/2.2.x/tables.sql
+++ implementation/database/upgrade/2.2.x/tables.sql
@@ -3,5 +3,7 @@ BEGIN;
 ALTER TABLE `UserPerm` ADD COLUMN `ManageReaders` ENUM('N', 'Y') NOT NULL DEFAULT 'N';
 ALTER TABLE `UserTypes` ADD COLUMN `ManageReaders` ENUM('N', 'Y') NOT NULL DEFAULT 'N';
 ALTER TABLE `Topics` CHANGE COLUMN `Name` `Name` varchar(255) NOT NULL default '';
+ALTER TABLE `campsite`.`Users` MODIFY COLUMN `CountryCode` VARCHAR(21);
+ALTER TABLE `campsite`.`Users` MODIFY COLUMN `Gender` ENUM('M','F')  DEFAULT 'M';
 
 COMMIT;
