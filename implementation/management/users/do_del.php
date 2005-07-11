abbaff6dc49f7e6565558afb3669a8160a42f063
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-07-11 15:05:49 +0000

    - implemented feature #1124: Give the user a sign that the user data has been updated - replaced old 'query()' function call with $Campsite['db']->Execute()

--- implementation/management/users/do_del.php
+++ implementation/management/users/do_del.php
@@ -20,23 +20,23 @@ if ($editUser->getUserName() == '') {
 	exit;
 }
 $typeParam = 'uType=' . urlencode($uType);
+$uName = $editUser->getUserName();
 
-query("DELETE FROM Users WHERE Id=$userId");
-if ($AFFECTED_ROWS > 0) {
-	query("DELETE FROM UserPerm WHERE IdUser=$userId");
-	query("SELECT Id FROM Subscriptions WHERE IdUser=$userId", 's');
+if ($Campsite['db']->Execute("DELETE FROM Users WHERE Id=$userId")) {
+	$Campsite['db']->Execute("DELETE FROM UserPerm WHERE IdUser=$userId");
+	$Campsite['db']->Execute("SELECT Id FROM Subscriptions WHERE IdUser=$userId", 's');
 	$nr=$NUM_ROWS;
 	for($loop=0;$loop<$nr;$loop++) {
 		fetchRowNum($s);
 		query ("DELETE FROM SubsSections WHERE IdSubscription=".encS(getNumVar($s,0)) );
 	}
 
-	query ("DELETE FROM Subscriptions WHERE IdUser=$userId");
-	query ("DELETE FROM SubsByIP WHERE IdUser=$userId");
-	$logtext = getGS('The user account $1 has been deleted.', $editUser->getUserName());
-	Log::Message($logtext, $editUser->getUserName(), 52);
+	$Campsite['db']->Execute("DELETE FROM Subscriptions WHERE IdUser=$userId");
+	$Campsite['db']->Execute("DELETE FROM SubsByIP WHERE IdUser=$userId");
+	Log::Message(getGS('The user account $1 has been deleted.', $uName), $User->getUserName(), 52);
 }
 
-header("Location: /$ADMIN/users/?$typeParam");
+$resMsg = getGS('User account $1 was deleted successfully.', $uName);
+header("Location: /$ADMIN/users/?$typeParam&res=OK&resMsg=" . urlencode($resMsg));
 
 ?>
