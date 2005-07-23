cfc65811dc2472da7c10e8362b94f6bc725bdb05
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-07-23 20:43:11 +0000

    added missing message

--- implementation/management/pub/issues/sections/locals.en.php
+++ implementation/management/pub/issues/sections/locals.en.php
@@ -59,5 +59,6 @@ regGS("The section $1 could not be duplicated", "The section $1 could not be dup
 regGS("Section $1 has been duplicated to $2. $3 of $4", "Section $1 has been duplicated to $2. $3 of $4:en");
 
 regGS("You do not have the right to modify sections.", "You do not have the right to modify sections.:en");
+regGS("Delete all articles from the section.", "Delete all articles from the section.:en");
 
 ?>
