79b1d3e55485d397ab78481e3431d295dcc770f9
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-07-23 20:28:38 +0000

    return directly to issue list instead of displaying the confirmation message

--- campsite/implementation/management/pub/issues/do_edit.php
+++ campsite/implementation/management/pub/issues/do_edit.php
@@ -37,99 +37,39 @@ if (!Input::IsValid()) {
 $publicationObj =& new Publication($Pub);
 $issueObj =& new Issue($Pub, $Language, $Issue);
 
-$created = 0;
-$correct = true;
+$backLink = "/$ADMIN/pub/issues/edit.php?Pub=$Pub&Issue=$Issue&Language=$Language";
 if ($cLang == 0) {
-	$correct = false;
+	CampsiteInterface::DisplayError(getGS('You must select a language.'), $backLink);
+	exit;
 }
 if ($cName == '') {
-	$correct = false;
+	CampsiteInterface::DisplayError(getGS('You must complete the $1 field.', "'".getGS('Name')."'"),
+		$backLink);
+	exit;
 }
 if ($cShortName == '') {
-	$correct = false;
+	CampsiteInterface::DisplayError(getGS('You must complete the $1 field.', "'".getGS('URL Name')."'"),
+		$backLink);
+	exit;
 }
 if (!valid_short_name($cShortName)) {
-	$correct = false;
+	CampsiteInterface::DisplayError(getGS('The $1 field may only contain letters, digits and underscore (_) character.', "'" . getGS('URL Name') . "'"), $backLink);
+	exit;
 }
-if ($correct) {
-	$issueObj->setProperty('Name', $cName, false);
-	$issueObj->setProperty('IdLanguage', $cLang, false);
-	if ($issueObj->getPublished() == 'Y') {
-		$issueObj->setProperty('PublicationDate', $cPublicationDate, false);
-	}
-	$issueObj->setProperty('IssueTplId', $cIssueTplId, false);
-	$issueObj->setProperty('SectionTplId', $cSectionTplId, false);
-	$issueObj->setProperty('ArticleTplId', $cArticleTplId, false);
-	$issueObj->setProperty('ShortName', $cShortName, false);
-	$created = $issueObj->commit();
-	if ($created) {
-		$logtext = getGS('Issue $1 updated in publication $2', $cName, $publicationObj->getName());
-		Log::Message($logtext, $User->getUserName(), 11);
-	}
+$issueObj->setProperty('Name', $cName, false);
+$issueObj->setProperty('IdLanguage', $cLang, false);
+if ($issueObj->getPublished() == 'Y') {
+	$issueObj->setProperty('PublicationDate', $cPublicationDate, false);
+}
+$issueObj->setProperty('IssueTplId', $cIssueTplId, false);
+$issueObj->setProperty('SectionTplId', $cSectionTplId, false);
+$issueObj->setProperty('ArticleTplId', $cArticleTplId, false);
+$issueObj->setProperty('ShortName', $cShortName, false);
+if ($issueObj->commit()) {
+	$logtext = getGS('Issue $1 updated in publication $2', $cName, $publicationObj->getName());
+	Log::Message($logtext, $User->getUserName(), 11);
 }
 
-CampsiteInterface::ContentTop(getGS('Updating issue'), array('Pub' => $publicationObj, 'Issue' => $issueObj));
+header("Location: /$ADMIN/pub/issues/?Pub=" . $publicationObj->getPublicationId());
 
 ?>
-<P>
-<CENTER>
-<TABLE BORDER="0" CELLSPACING="0" CELLPADDING="8" class="message_box" ALIGN="CENTER">
-<TR>
-	<TD COLSPAN="2">
-		<B> <?php  putGS("Changing issue's details"); ?> </B>
-		<HR NOSHADE SIZE="1" COLOR="BLACK">
-	</TD>
-</TR>
-<TR>
-	<TD COLSPAN="2">
-		<BLOCKQUOTE>
-		<?php 
-		if ($cLang == 0) {	?>
-			<LI><?php  putGS('You must select a language.'); ?></LI>
-			<?php
-		}
-		if ($cName == '') {
-			echo "<LI>" . getGS('You must complete the $1 field.','<B>'.getGS('Name').'</B>') . "</LI>\n";
-		}
-		if ($cShortName == '') {
-			echo "<LI>" . getGS('You must complete the $1 field.','<B>'.getGS('URL Name').'</B>') . "</LI>\n";
-		}
-		if (!valid_short_name($cShortName)) {
-			echo "<LI>" . getGS('The $1 field may only contain letters, digits and underscore (_) character.', '</B>' . getGS('URL Name') . '</B>') . "</LI>\n";
-		}
-		if ($created) {
-?>			<LI><?php  putGS('The issue $1 has been successfuly changed.', '<B>'.encHTML(decS($cName)).'</B>'); ?></LI>
-			<?php
-		} else {
-			if ($correct != 0) { ?>
-				<LI><?php  putGS('The issue could not be changed.'); ?></LI>
-				<?php
-			}
-		}
-		?>	
-		</BLOCKQUOTE>
-	</TD>
-</TR>
-
-<?php  if ($correct && $created) { ?>	
-<TR>
-	<TD COLSPAN="2">
-	<DIV ALIGN="CENTER">
-	<INPUT TYPE="button" class="button" NAME="Done" VALUE="<?php  putGS('Done'); ?>" ONCLICK="location.href='/admin/pub/issues/?Pub=<?php  pencURL($Pub); ?>'">
-	</DIV>
-	</TD>
-</TR>
-<?php  } else { ?>	
-<TR>
-	<TD COLSPAN="2">
-	<DIV ALIGN="CENTER">
-	<INPUT TYPE="button" class="button" NAME="OK" VALUE="<?php  putGS('OK'); ?>" ONCLICK="location.href='/admin/pub/issues/edit.php?Pub=<?php  pencURL($Pub); ?>&Issue=<?php  pencURL($Issue); ?>&Language=<?php  pencURL($Language); ?>'">
-	</DIV>
-	</TD>
-</TR>
-<?php  } ?>
-</TABLE>
-</CENTER>
-<P>
-
-<?php CampsiteInterface::CopyrightNotice(); ?>
