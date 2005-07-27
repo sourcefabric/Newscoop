adcc81758608a3f12e41c692f8268687114a0d00
paul <paul@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-07-27 07:35:15 +0000

    Allow class to be used from command line scripts.

--- implementation/management/classes/Image.php
+++ implementation/management/classes/Image.php
@@ -6,11 +6,17 @@
 /**
  * Includes
  */
-require_once($_SERVER['DOCUMENT_ROOT'].'/db_connect.php');
-require_once($_SERVER['DOCUMENT_ROOT'].'/classes/DatabaseObject.php');
-require_once($_SERVER['DOCUMENT_ROOT'].'/classes/DbObjectArray.php');
-require_once($_SERVER['DOCUMENT_ROOT'].'/classes/Article.php');
-require_once($_SERVER['DOCUMENT_ROOT'].'/classes/ArticleImage.php');
+// We indirectly reference the DOCUMENT_ROOT so we can enable 
+// scripts to use this file from the command line, $_SERVER['DOCUMENT_ROOT'] 
+// is not defined in these cases.
+if (!isset($g_documentRoot)) {
+    $g_documentRoot = $_SERVER['DOCUMENT_ROOT'];
+}
+require_once($g_documentRoot.'/db_connect.php');
+require_once($g_documentRoot.'/classes/DatabaseObject.php');
+require_once($g_documentRoot.'/classes/DbObjectArray.php');
+require_once($g_documentRoot.'/classes/Article.php');
+require_once($g_documentRoot.'/classes/ArticleImage.php');
 require_once('HTTP/Client.php');
 
 /**
--- implementation/management/classes/Template.php
+++ implementation/management/classes/Template.php
@@ -6,8 +6,8 @@
 /**
  * Includes
  */
-require_once($_SERVER['DOCUMENT_ROOT'].'/classes/DatabaseObject.php');
-require_once($_SERVER['DOCUMENT_ROOT'].'/classes/DbObjectArray.php');
+require_once($g_documentRoot.'/classes/DatabaseObject.php');
+require_once($g_documentRoot.'/classes/DbObjectArray.php');
 
 /**
  * @package Campsite
