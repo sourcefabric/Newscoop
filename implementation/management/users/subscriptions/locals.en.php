a341bc6724010aaccb2396ec29ccef50c18e59cd
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-07-27 13:27:26 +0000

    cleaning

--- implementation/management/users/subscriptions/locals.en.php
+++ implementation/management/users/subscriptions/locals.en.php
@@ -1,47 +1,27 @@
 <?php 
-
-// -- index.m4
 regGS("Publication<BR><SMALL>(click to see sections)</SMALL>","Publication<BR><SMALL>(click to see sections)</SMALL>:en");
 regGS("Delete subscriptions to $1","Delete subscriptions to $1:en");
 regGS("Paid subscription", "Paid subscription:en");
 regGS("Trial subscription", "Trial subscription:en");
-
-// -- del.m4
-regGS("Are you sure you want to delete the subscription to the publication $1?","Are you sure you want to delete the subscription to the publication $1?:en");
-
-// -- do_del.m4
 regGS("The subscription has been deleted.","The subscription has been deleted.:en");
 regGS("The subscription could not be deleted.","The subscription could not be deleted.:en");
-
-// -- add.m4
 regGS("Add sections now", "Add sections now:en");
 regGS("Add sections later", "Add sections later:en");
 regGS("Subscription Type", "Subscription Type:en");
 regGS("Paid (payment will be confirmed later)", "Paid (payment will be confirmed later):en");
 regGS("Paid (confirm payment now)", "Paid (confirm payment now):en");
-
-// -- do_add.m4
 regGS("The subscription has been added successfully.","The subscription has been added successfully.:en");
 regGS("The subscription could not be added.","The subscription could not be added.:en");
 regGS("Please check if there isn't another subscription to the same publication.","Please check if there isn't another subscription to the same publication.:en");
-
-// -- status.m4
 regGS("Change subscription status","Change subscription status:en");
 regGS("You do not have the right to change subscriptions status.","You do not have the right to change subscriptions status.:en");
 regGS("Are you sure you want to deactivate the subscription?","Are you sure you want to deactivate the subscription?:en");
 regGS("Are you sure you want to activate the subscription?","Are you sure you want to activate the subscription?:en");
-
-// -- do_status.m4
 regGS("Changing subscription status","Changing subscription status:en");
 regGS("The subscription has been activated.","The subscription has been activated.:en");
 regGS("The subscription has been deactivated.","The subscription has been deactivated.:en");
 regGS("Subscription status could not be changed.","Subscription status could not be changed.:en");
-
-// -- topay.m4
 regGS("Update payment","Update payment:en");
-
-// -- do_topay.m4
 regGS("The subscription payment was updated.","The subscription payment was updated.:en");
 regGS("Subscription payment could not be changed.","Subscription payment could not be changed.:en");
-
 ?>
