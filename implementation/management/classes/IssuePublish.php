dcc42076b184e1d3cb483bec1bffb37d6bca49e1
paul <paul@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-07-27 07:33:25 +0000

    Allow class to be used from command line scripts. Added Completed flag so that we know which actions have already been completed.  Renamed getPublishTime() to getActionTime().  Moved all the cron job script functionality into this class: added doAction(), GetPendingActions(), DoPendingActions().  Added GetFutureActions() for home page.  Fixed major bug #1273 where events could be lost.

--- implementation/management/classes/IssuePublish.php
+++ implementation/management/classes/IssuePublish.php
@@ -6,18 +6,26 @@
 /**
  * Includes
  */
-require_once($_SERVER['DOCUMENT_ROOT'].'/db_connect.php');
-require_once($_SERVER['DOCUMENT_ROOT'].'/classes/DatabaseObject.php');
-require_once($_SERVER['DOCUMENT_ROOT'].'/classes/DbObjectArray.php');
+// We indirectly reference the DOCUMENT_ROOT so we can enable 
+// scripts to use this file from the command line, $_SERVER['DOCUMENT_ROOT'] 
+// is not defined in these cases.
+if (!isset($g_documentRoot)) {
+    $g_documentRoot = $_SERVER['DOCUMENT_ROOT'];
+}
+require_once($g_documentRoot.'/db_connect.php');
+require_once($g_documentRoot.'/classes/DatabaseObject.php');
+require_once($g_documentRoot.'/classes/DbObjectArray.php');
+require_once($g_documentRoot.'/classes/Article.php');
+require_once($g_documentRoot.'/classes/Issue.php');
 
 /**
  * @package Campsite
  */
 class IssuePublish extends DatabaseObject {
-	var $m_keyColumnNames = array('IdPublication', 'NrIssue', 'IdLanguage', 'PublishTime');
+	var $m_keyColumnNames = array('IdPublication', 'NrIssue', 'IdLanguage', 'ActionTime');
 	var $m_dbTableName = 'IssuePublish';
 	var $m_columnNames = array('IdPublication', 'NrIssue', 'IdLanguage', 
-							   'PublishTime', 'Action', 'PublishArticles');
+							   'ActionTime', 'Action', 'PublishArticles', 'Completed');
 	
 	/**
 	 * This table delays an issue's publish time to a later date.
@@ -25,21 +33,48 @@ class IssuePublish extends DatabaseObject {
 	 * @param int $p_articleId
 	 * @param int $p_issueId
 	 * @param int $p_languageId
-	 * @param string $p_publishTime
+	 * @param string $p_actionTime
 	 *
 	 */
 	function IssuePublish($p_publicationId = null, $p_issueId = null, 
-	                      $p_languageId = null, $p_publishTime = null) 
+	                      $p_languageId = null, $p_actionTime = null) 
 	{
 		parent::DatabaseObject($this->m_columnNames);
 		$this->m_data['IdPublication'] = $p_publicationId;
 		$this->m_data['NrIssue'] = $p_issueId;
 		$this->m_data['IdLanguage'] = $p_languageId;
-		$this->m_data['PublishTime'] = $p_publishTime;
+		$this->m_data['ActionTime'] = $p_actionTime;
 		if ($this->keyValuesExist()) {
 			$this->fetch();
 		}
 	} // constructor
+
+	
+	/**
+	 * @return int
+	 */
+	function getPublicationId()
+	{
+	    return $this->m_data['IdPublication'];
+	} // fn getPublicationId
+	
+	
+	/**
+	 * @return int
+	 */
+	function getIssueId()
+	{
+	    return $this->m_data['NrIssue'];
+	} // fn getIssueId
+	
+	
+	/**
+	 * @return int
+	 */
+	function getLanguageId() 
+	{
+	    return $this->m_data['IdLanguage'];
+	} // fn getLanguageId
 	
 	
 	/**
@@ -54,8 +89,9 @@ class IssuePublish extends DatabaseObject {
 
 	
 	/**
-	 * Set the published state to switch to when the "publish time" arrives.
-	 * This can be 'P' for Publish, or 'U' for Unpublish.	 
+	 * Set the published state to switch to when the "publish time" arrives:
+	 * 'P' for Publish, or 'U' for Unpublish.	 
+	 * @param string $p_value
 	 * @return void
 	 */
 	function setPublishAction($p_value) 
@@ -79,8 +115,9 @@ class IssuePublish extends DatabaseObject {
 	
 	
 	/**
-	 * Set the front page state to switch to when the "publish time" arrives.
-	 * This can be 'Y' for Yes, or 'N' for No.
+	 * Set the front page state to switch to when the "publish time" arrives:
+	 * 'Y' for Yes, or 'N' for No.
+	 * @param string $p_value
 	 * @return mixed
 	 */
 	function setPublishArticlesAction($p_value) 
@@ -96,12 +133,48 @@ class IssuePublish extends DatabaseObject {
 	 * Get the time the event is scheduled to happen.
 	 * @return string
 	 */
-	function getPublishTime() 
+	function getActionTime() 
 	{
-		return $this->m_data['PublishTime'];
-	} // fn getPublishTime
+		return $this->m_data['ActionTime'];
+	} // fn getActionTime
 	
 	
+	/**
+	 * Mark that this action has been completed.
+	 * @return void
+	 */
+	function setCompleted() 
+	{
+	    $this->setProperty('Completed', 'Y');
+	} // fn setCompleted
+	
+	
+	/**
+	 * Execute the action.
+	 * @return void
+	 */
+	function doAction()
+	{
+		$publicationId = $this->m_data['IdPublication'];
+		$issueId = $this->m_data['NrIssue'];
+		$languageId = $this->m_data['IdLanguage'];
+		$publishAction = $this->m_data['Action'];
+		$publishArticlesAction = $this->m_data['PublishArticles'];
+
+		$articleState = ($publishAction == 'P') ? 'Y' : 'S';
+		if ($publishArticlesAction == 'Y') {
+		    $articles =& Article::GetArticles(null, $issueId, null, $languageId);
+            foreach ($articles as $article) {
+                $article->setPublished($articleState);
+            }
+		}
+		$issueState = ($publishAction == 'P') ? 'Y' : 'N';
+		$issue =& new Issue($publicationId, $languageId, $issueId);
+		$issue->setPublished($issueState);
+		$this->setCompleted();
+	} // fn doAction
+	
+
 	/**
 	 * Get all the events that will change the issue's state.
 	 * Returns an array of IssuePublish objects.
@@ -118,10 +191,72 @@ class IssuePublish extends DatabaseObject {
 					." WHERE IdPublication = $p_publicationId "
 					." AND NrIssue = $p_issueId "
 					." AND IdLanguage = $p_languageId "
-					." ORDER BY PublishTime ASC";
+					." ORDER BY ActionTime ASC";
 		$result =& DbObjectArray::Create('IssuePublish', $queryStr);
 		return $result;
 	} // fn GetIssueEvents
+	
+	
+	/**
+	 * Get all the actions that currently need to be performed.
+	 * @return array
+	 */
+	function GetPendingActions() 
+	{
+	    $datetime = strftime("%Y-%m-%d %H:%M:00");
+    	$queryStr = "SELECT * FROM IssuePublish "
+    	           . " WHERE ActionTime <= '$datetime'"
+                   . " AND Completed != 'Y'"
+                   . " ORDER BY ActionTime ASC";
+        $result =& DbObjectArray::Create('IssuePublish', $queryStr);
+        return $result;	
+	} // fn GetPendingActions
+	
+	
+	/**
+	 * Execute all pending actions.
+	 * @return void
+	 */
+	function DoPendingActions()
+	{
+        $actions =& IssuePublish::GetPendingActions();
+    	foreach ($actions as $issuePublishObj) {
+    	    $issuePublishObj->doAction();
+    	}	    
+	} // fn DoPendingActions
+
+	
+	/**
+	 * For now, this is mostly a hack to get the home page working.
+	 * The raw array is returned.
+	 *
+	 * @param int $p_limit
+	 * @return array
+	 */
+	function GetFutureActions($p_limit) 
+	{
+	    global $Campsite;
+	    $datetime = strftime("%Y-%m-%d %H:%M:00");
+	    $dummyIssue =& new Issue();
+	    $columnNames = $dummyIssue->getColumnNames(true);
+        $queryStr = "SELECT ActionTime, Action, PublishArticles, "
+                    . implode(",", $columnNames). " FROM Issues, IssuePublish "
+                    . " WHERE ActionTime >= '" . $datetime . "'"
+                    . " AND Completed != 'Y'"
+                    . " AND Issues.IdPublication=IssuePublish.IdPublication"
+                    . " AND Issues.Number=IssuePublish.NrIssue"
+                    . " AND Issues.IdLanguage=IssuePublish.IdLanguage "
+                    . " ORDER BY ActionTime DESC"
+                    . " LIMIT $p_limit";
+        //echo $queryStr."<br>";
+		$rows = $Campsite['db']->GetAll($queryStr);
+		$addKeys = array();
+		foreach ($rows as $row) {
+		    $row["ObjectType"] = "issue";		    
+		    $addKeys[$row['ActionTime']] = $row;
+		}
+        return $addKeys;
+	} // fn GetFutureActions
 	
 } // class IssuePublish
 
