09d202c8aeecb8b06823b09e86f0e67131dacf6d
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-07-27 12:02:38 +0000

    the parameter is now the WWW directory instead of the configuration directory

--- campsite/implementation/management/bin/autopublish.php
+++ campsite/implementation/management/bin/autopublish.php
@@ -1,8 +1,7 @@
 <?php
 
-exec('./campsite_config --www_dir', $www_dir);
-$www_dir = array_pop($www_dir) . '/campsite/html';
-echo $www_dir;
+$www_dir = $GLOBALS['argv'][1];
+$www_dir = $www_dir . '/html';
 $g_documentRoot = $www_dir;
 require_once($www_dir.'/db_connect.php');
 require_once($www_dir.'/classes/ArticlePublish.php');
