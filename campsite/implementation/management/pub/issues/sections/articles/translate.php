ee430fb1573b18f6dd6c8f4a5e8ce680a562d43f
paul <paul@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-07-28 08:11:41 +0000

    Fixed bug #1299 - User without Add Article permission can still create translations of articles.

--- campsite/implementation/management/pub/issues/sections/articles/translate.php
+++ campsite/implementation/management/pub/issues/sections/articles/translate.php
@@ -44,7 +44,7 @@ if (!$articleObj->exists()) {
 	exit;
 }
 
-if (!$articleObj->userCanModify($User)) {
+if (!$User->hasPermission("AddArticle")) {
 	$errorStr = getGS('You do not have the right to change this article.  You may only edit your own articles and once submitted an article can only changed by authorized users.');
 	CampsiteInterface::DisplayError($errorStr, $BackLink);
 	exit;	
