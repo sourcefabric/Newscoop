1421df1d006481694c454214a5e7098e17ee719f
paul <paul@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-07-28 08:27:56 +0000

    Fixed bug #1304 - if user does not have Manage Templates permission, but has the Delete templates permission, they cannot get to the Templates screen.

--- implementation/management/templates/index.php
+++ implementation/management/templates/index.php
@@ -8,7 +8,7 @@ if (!$access) {
 	exit;
 }
 
-if (!$User->hasPermission('ManageTempl')) {
+if (!$User->hasPermission('ManageTempl') && !$User->hasPermission('DeleteTempl')) {
 	header("Location: /$ADMIN/");
 	exit;
 }
@@ -49,15 +49,19 @@ if ($path != "") {
 		<TD><TABLE BORDER="0" CELLSPACING="0" CELLPADDING="1"><TR><TD><A HREF="?Path=<?php pencURL($new_path); ?>" ><IMG SRC="/<?php echo $ADMIN; ?>/img/tol.gif" BORDER="0"></A></TD><TD><A HREF="?Path=<?php pencURL($new_path); ?>" ><B><?php  putGS("Go up"); ?></B></A></TD></TR></TABLE></TD>
 <?php
 }
+if ($User->hasPermission("ManageTempl")) {
 ?>
+
 		<TD><TABLE BORDER="0" CELLSPACING="0" CELLPADDING="1"><TR><TD><A HREF="/<?php echo $ADMIN; ?>/templates/new_dir.php?Path=<?php  pencURL($path); ?>" ><IMG SRC="/<?php echo $ADMIN; ?>/img/icon/add.png" BORDER="0"></A></TD><TD><A HREF="/<?php echo $ADMIN; ?>/templates/new_dir.php?Path=<?php  pencURL($path); ?>" ><B><?php  putGS("Create new folder"); ?></B></A></TD></TR></TABLE></TD>
 		<TD><TABLE BORDER="0" CELLSPACING="0" CELLPADDING="1"><TR><TD><A HREF="/<?php echo $ADMIN; ?>/templates/upload_templ.php?Path=<?php  pencURL($path); ?>" ><IMG SRC="/<?php echo $ADMIN; ?>/img/tol.gif" BORDER="0"></A></TD><TD><A HREF="/<?php echo $ADMIN; ?>/templates/upload_templ.php?Path=<?php  pencURL($path); ?>" ><B><?php  putGS("Upload template"); ?></B></A></TD></TR></TABLE></TD>
 		<TD><TABLE BORDER="0" CELLSPACING="0" CELLPADDING="1"><TR><TD><A HREF="/<?php echo $ADMIN; ?>/templates/new_template.php?Path=<?php  pencURL($path); ?>" ><IMG SRC="/<?php echo $ADMIN; ?>/img/icon/add.png" BORDER="0"></A></TD><TD><A HREF="/<?php echo $ADMIN; ?>/templates/new_template.php?Path=<?php  pencURL($path); ?>" ><B><?php  putGS("Create new template"); ?></B></A></TD></TR></TABLE></TD>
+<?PHP
+}
+?>
 	</TR>
 </TABLE>
 <?php
 $listbasedir = $path;
-$dta = $User->hasPermission('DeleteTempl');
 require_once($Campsite['HTML_DIR'] . "/$ADMIN_DIR/templates/list_dir.php");
 ?>
 <P>
