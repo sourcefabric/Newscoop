7a001382c6adba9e4643416672f55932b3f9c87f
paul <paul@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-08-04 12:22:13 +0000

    Possible fix for John Pye's login problems he discovered

--- campsite/implementation/management/logout.php
+++ campsite/implementation/management/logout.php
@@ -4,8 +4,8 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/classes/Article.php');
 list($access, $User) = check_basic_access($_REQUEST);
 
 // Delete the cookies
-setcookie("TOL_UserId", "", time() - 3600);
-setcookie("TOL_UserKey", "", time() - 3600);
+setcookie("TOL_UserId", "", time() - 86400);
+setcookie("TOL_UserKey", "", time() - 86400);
 
 // Unlock all articles that are locked by this user
 if ($access) {
