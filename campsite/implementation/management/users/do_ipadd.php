20cde6ff11eb693d98f318f42b0761418d8476d1
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-08-19 12:01:20 +0000

    fixed ticket:585 IP access problem

--- campsite/implementation/management/users/do_ipadd.php
+++ campsite/implementation/management/users/do_ipadd.php
@@ -3,61 +3,67 @@
 require_once($_SERVER['DOCUMENT_ROOT']. "/$ADMIN_DIR/users/users_common.php");
 require_once($_SERVER['DOCUMENT_ROOT']. "/classes/Log.php");
 
-list($access, $User) = check_basic_access($_REQUEST);
+list($g_hasAccess, $g_user) = check_basic_access($_REQUEST);
 
 read_user_common_parameters(); // $uType, $userOffs, $lpp, search parameters
 $uType = 'Subscribers';
-compute_user_rights($User, $canManage, $canDelete);
-if (!$canManage) {
+compute_user_rights($g_user, $g_canManage, $g_canDelete);
+if (!$g_canManage) {
 	CampsiteInterface::DisplayError(getGS('You do not have the right to change user account information.'));
 	exit;
 }
 
 // read input
-$userId = Input::Get('User', 'int', 0);
-$editUser = new User($userId);
-if ($editUser->getUserName() == '') {
+$g_userId = Input::Get('User', 'int', 0);
+$g_editUser = new User($g_userId);
+if ($g_editUser->getUserName() == '') {
 	CampsiteInterface::DisplayError(getGS('No such user account.'));
 	exit;
 }
-$backLink = "/$ADMIN/users/edit.php?uType=Subscribers&User=$userId";
-$cStartIP1 = Input::Get('cStartIP1', 'int', 0);
-$cStartIP2 = Input::Get('cStartIP2', 'int', 0);
-$cStartIP3 = Input::Get('cStartIP3', 'int', 0);
-$cStartIP4 = Input::Get('cStartIP4', 'int', 0);
-$cAddresses = Input::Get('cAddresses', 'int', 0);
+$g_backLink = "/$ADMIN/users/edit.php?uType=Subscribers&User=$g_userId";
+$g_cStartIP1 = Input::Get('cStartIP1', 'int', 0);
+$g_cStartIP2 = Input::Get('cStartIP2', 'int', 0);
+$g_cStartIP3 = Input::Get('cStartIP3', 'int', 0);
+$g_cStartIP4 = Input::Get('cStartIP4', 'int', 0);
+$g_cAddresses = Input::Get('cAddresses', 'int', 0);
 
 // check if input was correct
-if ($cStartIP1 == 0 || $cStartIP2 == 0 || $cStartIP3 == 0 || $cStartIP4 == 0) {
+if ($g_cStartIP1 == 0 || $g_cStartIP2 == 0 || $g_cStartIP3 == 0 || $g_cStartIP4 == 0) {
 	CampsiteInterface::DisplayError(getGS('You must complete the $1 field.', 'Start IP'),
-		$backLink);
+		$g_backLink);
 	exit;
 }
-if ($cAddresses == 0) {
-	CampsiteInterface::DisplayError(getGS('You must complete the $1 field.',
-			'Number of addresses'), $backLink);
+if ($g_cAddresses == 0) {
+	$g_errorMsg = getGS('You must complete the $1 field.', 'Number of addresses');
+	CampsiteInterface::DisplayError($g_errorMsg, $g_backLink);
 	exit;
 }
 
 // check if the IP address group exists already
-$StartIP = $cStartIP1*256*256*256+$cStartIP2*256*256+$cStartIP3*256+$cStartIP4;
-$ip = "$cStartIP1.$cStartIP2.$cStartIP3.$cStartIP4";
-query ("SELECT Addresses FROM SubsByIP WHERE IdUser=$userId and StartIP=$StartIP", 'ig');
-if ($NUM_ROWS) {
-	CampsiteInterface::DisplayError(getGS('An IP address group having the $1 start address already exists.', $ip), $backLink);
+$g_startIP = $g_cStartIP1*256*256*256+$g_cStartIP2*256*256+$g_cStartIP3*256+$g_cStartIP4;
+$g_endIP = $g_startIP + $g_cAddresses - 1;
+$g_startIPStr = "$g_cStartIP1.$g_cStartIP2.$g_cStartIP3.$g_cStartIP4";
+$g_sql = "SELECT Addresses FROM SubsByIP WHERE "
+	. "(StartIP <= $g_startIP AND (StartIP + Addresses - 1) >= $g_startIP) OR "
+	. "(StartIP <= $g_endIP AND (StartIP + Addresses - 1) >= $g_endIP)";
+$g_res = $Campsite['db']->Execute($g_sql);
+if ($g_res->RecordCount() > 0) {
+	$g_errorMsg = getGS('The IP address group $1:$2 conflicts with another existing group.',
+		$g_startIPStr, $g_cAddresses);
+	CampsiteInterface::DisplayError($g_errorMsg, $g_backLink);
 	exit;
 }
 
-if ($Campsite['db']->Execute("INSERT IGNORE INTO SubsByIP SET IdUser=$userId, StartIP='$StartIP', Addresses=$cAddresses")) {
-	$logtext = getGS('IP Group $1 added for user $2', encHTML("$ip:$cAddresses"),
-		encHTML($editUser->getUserName()));
-	Log::Message($logtext, $User->getUserName(), 57);
+if ($Campsite['db']->Execute("INSERT IGNORE INTO SubsByIP SET IdUser=$g_userId, StartIP='$g_startIP', Addresses=$g_cAddresses")) {
+	$logtext = getGS('IP Group $1 added for user $2', encHTML("$g_startIPStr:$g_cAddresses"),
+		encHTML($g_editUser->getUserName()));
+	Log::Message($logtext, $g_user->getUserName(), 57);
 } else {
-	CampsiteInterface::DisplayError(getGS('There was an error creating the IP address group.', "$ip:$cAddresses"), $backLink);
+	CampsiteInterface::DisplayError(getGS('There was an error creating the IP address group.', "$g_startIPStr:$g_cAddresses"), $g_backLink);
 	exit;
 }
 
-$resMsg = getGS("The IP Group $1 has been created.", "$ip:$cAddresses");
-header("Location: $backLink&res=OK&resMsg=" . urlencode($resMsg));
+$g_resMsg = getGS("The IP Group $1 has been created.", "$g_startIPStr:$g_cAddresses");
+header("Location: $g_backLink&res=OK&resMsg=" . urlencode($g_resMsg));
 
 ?>
