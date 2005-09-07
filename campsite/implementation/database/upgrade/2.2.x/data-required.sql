38002e6761f4c6160db4a81bdcf50d541ee656cd
paul <paul@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-09-07 10:29:48 +0000

    Fixed ticket:1445 - Upgrade from 2.2 to 2.3 fails on MySQL 3.X.

--- campsite/implementation/database/upgrade/2.2.x/data-required.sql
+++ campsite/implementation/database/upgrade/2.2.x/data-required.sql
@@ -3,8 +3,7 @@ BEGIN;
 UPDATE UserPerm SET ManageClasses = 'N', ManageDictionary = 'N', DeleteDictionary = 'N';
 UPDATE UserTypes SET ManageClasses = 'N', ManageDictionary = 'N', DeleteDictionary = 'N';
 
-UPDATE Articles, ArticlePublish SET Articles.PublishDate = ArticlePublish.ActionTime WHERE Articles.Number = ArticlePublish.NrArticle AND Articles.IdLanguage = ArticlePublish.IdLanguage;
-UPDATE Articles SET PublishDate = UploadDate WHERE PublishDate = 0;
+UPDATE Articles SET PublishDate = UploadDate;
 UPDATE ArticlePublish SET Completed='Y' WHERE ActionTime <= NOW();
 
 UPDATE IssuePublish SET Completed='Y' WHERE ActionTime <= NOW();
