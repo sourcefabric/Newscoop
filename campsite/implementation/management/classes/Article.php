b9c4f7484899bf3319946db18582d05a301896d7
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-09-07 17:44:14 +0000

    fixed ticket:1446 - Put the new articles at the top

--- campsite/implementation/management/classes/Article.php
+++ campsite/implementation/management/classes/Article.php
@@ -128,6 +128,8 @@ class Article extends DatabaseObject {
 	 */
 	function create($p_articleType, $p_name = null) 
 	{
+		global $Campsite;
+		
 		$this->m_data['Number'] = $this->__generateArticleId();
 		$this->m_data['ArticleOrder'] = $this->m_data['Number'];
 	
@@ -139,7 +141,23 @@ class Article extends DatabaseObject {
 		$values['ShortName'] = $this->m_data['Number'];
 		$values['Type'] = $p_articleType;
 		$values['Public'] = 'Y';
-		$values['ArticleOrder'] = $this->m_data['Number'];
+		
+		// compute article order number
+		$queryStr = 'SELECT MIN(ArticleOrder) AS min FROM Articles WHERE IdPublication = '
+		          . $this->m_data['IdPublication'] . ' AND NrIssue = ' . $this->m_data['NrIssue']
+		          . ' and NrSection = ' . $this->m_data['NrSection'];
+		$articleOrder = $Campsite['db']->GetOne($queryStr) - 1;
+		if ($articleOrder < 0)
+			$articleOrder = $this->m_data['Number'];
+		if ($articleOrder == 0) {
+			$queryStr = 'UPDATE Articles SET ArticleOrder = ArticleOrder + 1 WHERE IdPublication = '
+			          . $this->m_data['IdPublication'] . ' AND NrIssue = ' . $this->m_data['NrIssue']
+			          . ' AND NrSection = ' . $this->m_data['NrSection'];
+			$Campsite['db']->Execute($queryStr);
+			$articleOrder = 1;
+		}
+		$values['ArticleOrder'] = $articleOrder;
+		
 		$success = parent::create($values);
 		if (!$success) {
 			return;
