5d6ecb9967712b58c96a3e11c026c8fbbd95b9c7
paul <paul@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-11-30 17:45:56 +0000

    Fixed bug in url given to SimplePager. Moved the Attach icon to the left hand side so that is in a similar place as the checkboxes in the topics popup.  Fixed bug in default sort order 'Most Recently Added': Since it is actually sorted by ID, you need to reverse the order for this case.

--- campsite/implementation/management/priv/articles/images/search.php
+++ campsite/implementation/management/priv/articles/images/search.php
@@ -33,6 +33,9 @@ if ($f_order_direction == 'DESC') {
 	$OrderSign = "<img src=\"".$Campsite["ADMIN_IMAGE_BASE_URL"]."/ascending.png\" border=\"0\">";
 }
 
+if ($f_order_by == "id") {
+	$f_order_direction = $ReverseOrderDirection;
+}
 $TotalImages = Image::GetTotalImages();
 $imageSearch =& new ImageSearch($f_search_string, $f_order_by, $f_order_direction, $f_image_offset, $f_items_per_page);
 $imageSearch->run();
@@ -41,12 +44,6 @@ $NumImagesFound = $imageSearch->getNumImagesFound();
 
 $orderDirectionUrl = camp_html_article_url($articleObj, $f_language_selected, 'images/popup.php');
 
-$articleUrlData = "&f_publication_id=$f_publication_id"
-				 ."&f_issue_number=$f_issue_number"
-				 ."&f_section_number=$f_section_number"
-				 ."&f_language_id=$f_language_id"
-				 ."&f_language_selected=$f_language_selected"
-				 ."&f_article_number=$f_article_number";
 ?>
 
 <TABLE BORDER="0" CELLSPACING="0" CELLPADDING="3" class="table_input" style="margin-bottom: 10px; margin-top: 5px;" align="center">
@@ -60,7 +57,6 @@ $articleUrlData = "&f_publication_id=$f_publication_id"
 <input type="hidden" name="f_language_selected" value="<?php p($f_language_selected); ?>">
 <input type="hidden" name="f_article_number" value="<?php p($f_article_number); ?>">
 <tr>
-	<!--<td style="padding-left: 10px;"><?php putGS('Search')?>:</td>-->
 	<td><input type="submit" name="submit_button" value="Search" class="button"></td>
 	<td><input type="text" name="f_search_string" value="<?php echo $f_search_string; ?>" class="input_text" style="width: 150px;"></td>
 	<td>
@@ -77,12 +73,11 @@ $articleUrlData = "&f_publication_id=$f_publication_id"
 				camp_html_select_option('place', $f_order_by, getGS("Place"));
 				camp_html_select_option('date', $f_order_by, getGS("Date"));
 				camp_html_select_option('inuse', $f_order_by, getGS("In Use"));
-				//camp_html_select_option('id', $f_order_by, getGS("Id"));
 				?>
 				</select>
 			</td>
 			<td>
-				<a href="<?php p($orderDirectionUrl); ?>"><?php p($OrderSign); ?></a>
+				<a href="<?php p($orderDirectionUrl); ?>&f_order_direction=<?php p($ReverseOrderDirection); ?>"><?php p($OrderSign); ?></a>
 			</td>
 		</tr>
 		</table>
@@ -94,7 +89,7 @@ $articleUrlData = "&f_publication_id=$f_publication_id"
 
 <?php
 if (count($imageData) > 0) {
-    $pagerUrl = camp_html_article_url($articleObj, $f_language_selected, "images/popup.php")."?";
+    $pagerUrl = camp_html_article_url($articleObj, $f_language_selected, "images/popup.php")."&";
     $pager =& new SimplePager($NumImagesFound, $f_items_per_page, "f_image_offset", $pagerUrl);
 
 ?>
@@ -103,6 +98,9 @@ if (count($imageData) > 0) {
 </table>
 <TABLE BORDER="0" CELLSPACING="1" CELLPADDING="6" class="table_list">
 <TR class="table_list_header">
+    <?php if ($articleObj->userCanModify($User)) { ?>
+    <TD ALIGN="center" VALIGN="top" style="padding: 3px;"><B><?php p(getGS("Attach")); ?></B></TD>
+	<?php } ?>
     <TD ALIGN="LEFT" VALIGN="TOP">
       <?php  putGS("Thumbnail"); ?>
     </TD>
@@ -121,15 +119,34 @@ if (count($imageData) > 0) {
     <TD ALIGN="center" VALIGN="top" style="padding: 3px;" nowrap>
       <?php  putGS("In use"); ?>
     </TD>
-    <?php if ($articleObj->userCanModify($User)) { ?>
-    <TD ALIGN="center" VALIGN="top" style="padding: 3px;"><B><?php p(getGS("Attach")); ?></B></TD>
-	<?php } ?>
 </TR>  
 <?php
 $color = 0;
 foreach ($imageData as $image) {
     ?>
     <TR <?php  if ($color) { $color=0; ?>class="list_row_even"<?php  } else { $color=1; ?>class="list_row_odd"<?php  } ?>>
+        <?php
+        if ($articleObj->userCanModify($User)) { ?>
+    		<form method="POST" action="do_link.php">
+			<input type="hidden" name="f_publication_id" value="<?php p($f_publication_id); ?>">
+			<input type="hidden" name="f_issue_number" value="<?php p($f_issue_number); ?>">
+			<input type="hidden" name="f_section_number" value="<?php p($f_section_number); ?>">
+			<input type="hidden" name="f_language_id" value="<?php p($f_language_id); ?>">
+			<input type="hidden" name="f_language_selected" value="<?php p($f_language_selected); ?>">
+			<input type="hidden" name="f_article_number" value="<?php p($f_article_number); ?>">
+    		<input type="hidden" name="f_image_id" value="<?php echo $image['id']; ?>">
+        	<TD ALIGN="CENTER">
+				<input type="image" src="<?php echo $Campsite["ADMIN_IMAGE_BASE_URL"]; ?>/add.png"></td>
+          	</TD>
+       		</form>
+        	<?php
+     	}
+     	else {
+     		?>
+        	<TD ALIGN="CENTER">&nbsp;</TD>             		
+     		<?php
+     	}
+        ?>
         <TD ALIGN="center">
             <A HREF="<?php echo 
             camp_html_article_url($articleObj, $f_language_selected, "images/view.php", camp_html_article_url($articleObj, $f_language_selected, "images/popup.php"))
@@ -154,28 +171,6 @@ foreach ($imageData as $image) {
         <TD align="center">
             <?php echo htmlspecialchars($image['in_use']); ?>&nbsp;
         </TD>
-        <?php
-        if ($articleObj->userCanModify($User)) { ?>
-    		<form method="POST" action="do_link.php">
-			<input type="hidden" name="f_publication_id" value="<?php p($f_publication_id); ?>">
-			<input type="hidden" name="f_issue_number" value="<?php p($f_issue_number); ?>">
-			<input type="hidden" name="f_section_number" value="<?php p($f_section_number); ?>">
-			<input type="hidden" name="f_language_id" value="<?php p($f_language_id); ?>">
-			<input type="hidden" name="f_language_selected" value="<?php p($f_language_selected); ?>">
-			<input type="hidden" name="f_article_number" value="<?php p($f_article_number); ?>">
-    		<input type="hidden" name="f_image_id" value="<?php echo $image['id']; ?>">
-        	<TD ALIGN="CENTER">
-				<input type="image" src="<?php echo $Campsite["ADMIN_IMAGE_BASE_URL"]; ?>/add.png"></td>
-          	</TD>
-       		</form>
-        	<?php
-     	}
-     	else {
-     		?>
-        	<TD ALIGN="CENTER">&nbsp;</TD>             		
-     		<?php
-     	}
-        ?>
     </TR>
 <?php
 }
