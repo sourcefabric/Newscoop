031b2d47a71580f0ae30eee3eccf3d0013bdbd92
paul <paul@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-12-13 07:01:57 +0000

    Added ability to add a new publication in the publication edit screen.

--- campsite/implementation/management/priv/pub/edit.php
+++ campsite/implementation/management/priv/pub/edit.php
@@ -40,18 +40,25 @@ echo camp_html_content_top(getGS("Configure publication"), array("Pub" => $publi
 	<TD><A HREF="/<?php echo $ADMIN; ?>/issues/?Pub=<?php  p($Pub); ?>"><B><?php  putGS("Go To Issues"); ?></B></A></TD>
 </TR>
 </TABLE>
-
+<TABLE BORDER="0" CELLSPACING="0" CELLPADDING="1" class="action_buttons">
+<TR>
+<?php  if ($User->hasPermission("ManagePub")) { ?>    <P>
+	<TD>
+		<A HREF="/<?php echo $ADMIN; ?>/pub/add.php?Back=<?php p(urlencode($_SERVER['REQUEST_URI'])); ?>"><IMG SRC="<?php echo $Campsite["ADMIN_IMAGE_BASE_URL"]; ?>/add.png" BORDER="0"></A>
+	</TD>
+	<TD>
+		<A HREF="/<?php echo $ADMIN; ?>/pub/add.php?Back=<?php p(urlencode($_SERVER['REQUEST_URI'])); ?>"><B><?php  putGS("Add new publication"); ?></B></A>
+	</TD>
+<?php  } ?>
 <?php
 if ($User->hasPermission("DeletePub")) {
 ?>
-<P>
-<TABLE BORDER="0" CELLSPACING="0" CELLPADDING="1" class="action_buttons">
-<TR>
-	<TD><A HREF="do_del.php?Pub=<?php p($Pub); ?>" onclick="return confirm('<?php putGS('Are you sure you want to delete the publication $1?', htmlspecialchars($publicationObj->getName())); ?>');"><IMG SRC="<?php echo $Campsite["ADMIN_IMAGE_BASE_URL"]; ?>/delete.png" BORDER="0"></A></TD>
+	<TD style="padding-left: 10px;"><A HREF="do_del.php?Pub=<?php p($Pub); ?>" onclick="return confirm('<?php putGS('Are you sure you want to delete the publication $1?', htmlspecialchars($publicationObj->getName())); ?>');"><IMG SRC="<?php echo $Campsite["ADMIN_IMAGE_BASE_URL"]; ?>/delete.png" BORDER="0"></A></TD>
 	<TD><A HREF="do_del.php?Pub=<?php p($Pub); ?>" onclick="return confirm('<?php putGS('Are you sure you want to delete the publication $1?', htmlspecialchars($publicationObj->getName())); ?>');"><B><?php  putGS("Delete"); ?></B></A></TD>
+<?php } ?>
 </TR>
 </TABLE>
-<?php } ?>
+
 
 <P>
 <FORM NAME="dialog" METHOD="POST" ACTION="do_edit.php"  >
