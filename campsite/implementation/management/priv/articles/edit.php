6075ca173bf6122ebc7a0af7e2ec8539611c9d04
paul <paul@952d13e4-84fd-0310-bd7c-ee2827c68691>
2006-01-09 18:27:50 +0000

    Small tweak - show scheduled publishing icon if there are any future events, and not just if the article is in the SUBMITTED state.

--- campsite/implementation/management/priv/articles/edit.php
+++ campsite/implementation/management/priv/articles/edit.php
@@ -273,7 +273,7 @@ if ($f_edit_mode == "edit") { ?>
 									break;
 							}
 						} 
-						if ( ($articleObj->getPublished() == 'S') && (count($articleEvents) > 0)) {
+						if ( count($articleEvents) > 0 ) {
 							?>
 							<img src="<?php echo $Campsite["ADMIN_IMAGE_BASE_URL"]; ?>/automatic_publishing.png" alt="<?php  putGS("Scheduled Publishing"); ?>" title="<?php  putGS("Scheduled Publishing"); ?>" border="0" width="22" height="22" align="middle" style="padding-bottom: 1px;">
 							<?php
