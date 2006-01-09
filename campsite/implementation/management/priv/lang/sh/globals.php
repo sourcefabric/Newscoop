9c5de1ad718b8ea3a0ea7d5e7f1b986d5dffdbb2
paul <paul@952d13e4-84fd-0310-bd7c-ee2827c68691>
2006-01-09 00:15:47 +0000

    Tweaks to serbian translation

--- campsite/implementation/management/priv/lang/sh/globals.php
+++ campsite/implementation/management/priv/lang/sh/globals.php
@@ -364,5 +364,4 @@ regGS("Add new user account", "Dodaj novog korisnika");
 regGS("Schedule a new publish action", "Zakaži novu akciju objavljivanja");
 regGS("User may add/change user accounts and passwords", "Korisnik može da dodaje/menja korisničke naloge i lozinke");
 regGS("User may delete user accounts", "Korisnik može da briše korisničke naloge");
-regGS("Are you sure you want to delete the image \\'$1\\'?", "Da li ste sigurni da želite da obrišete sliku \\'$1\\'?");
 ?>
