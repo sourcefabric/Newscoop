18c9a3a83be55ee55d430f2184ea56688f93e5ab
paul <paul@952d13e4-84fd-0310-bd7c-ee2827c68691>
2006-01-09 22:15:21 +0000

    Austrian localization

---
+++ campsite/implementation/management/priv/lang/at/topics.php
@@ -0,0 +1,6 @@
+<?php
+regGS("Top", "Anfang");
+regGS("You do not have the right to add topics.", "Keine Berechtigung Themen hinzuzufügen");
+regGS("You do not have the right to delete topics.", "Keine Berechtigung Thema zu löschen");
+regGS("Are you sure you want to delete the topic $1?", "Thema $1 wirklich löschen?");
+?>
---
+++ campsite/implementation/management/priv/lang/at/user_subscription_sections.php
@@ -0,0 +1,19 @@
+<?php
+regGS("Subscribed sections", "Abonierte Rubriken");
+regGS("Add new section to subscription", "Neue Rubriken zu Abonoment hinzufügen");
+regGS("Start Date<BR><SMALL>(yyyy-mm-dd)</SMALL>", "(JJJJ-MM-TT)");
+regGS("Delete subscription to section $1?", "Abonoment für Rubrik $1 löschen");
+regGS("Change all sections", "Alle Rubriken verändern");
+regGS("WARNING: If you subscribe to all sections, the periods for previously added sections will be overriden!", "Warnung: Beim abonnieren aller Rubriken werden die Zeiträume für vormals hinzugefügte Rubriken überschrieben!");
+regGS("Adding sections", "Rubriken werden hinzugefügt");
+regGS("Adding sections to subscription", "Rubriken werden zu Abonoment hinzugefügtq");
+regGS("Please check if there isn't another subscription with the same section.", "Bitte überprüfen, ob ein anderes Abonmoment mit derselben Rubrik bereits existiert");
+regGS("Are you sure you want to delete the subscription to the section $1?", "Abonoment zur Rubrik $1 wirklich löschen?");
+regGS("Change subscription", "Abonoment ändern");
+regGS("You do not have the right to change subscriptions.", "Keine Berechtigung Abonoments zu ändern");
+regGS("Paid Days", "Bezahlte Tage");
+regGS("-- ALL SECTIONS --", "-- ALLE RUBRIKEN --");
+regGS("Updating subscription", "Abonoment wird aktualisiert");
+regGS("The subscription could not be updated.", "Abonoment konnte nicht aktualisiert werden");
+regGS("Changing subscription", "Abonoment wird geändert");
+?>
