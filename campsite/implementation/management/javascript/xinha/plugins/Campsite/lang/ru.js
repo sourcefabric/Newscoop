a6af553713d03b441f39ec46023fc9416830c632
paul <paul@952d13e4-84fd-0310-bd7c-ee2827c68691>
2006-01-09 05:37:33 +0000

    Russian translation for Xinha plugins.

---
+++ campsite/implementation/management/javascript/xinha/plugins/Campsite/lang/ru.js
@@ -0,0 +1,21 @@
+﻿// I18N constants
+
+// LANG: "ru", ENCODING: UTF-8
+// Author: Andrei Blagorazumov, a@fnr.ru
+
+// FOR TRANSLATORS:
+//
+//   1. PLEASE PUT YOUR CONTACT INFO IN THE ABOVE LINE
+//      (at least a valid email address)
+//
+//   2. PLEASE TRY TO USE UTF-8 FOR ENCODING;
+//      (if this is not possible, please include a comment
+//       that states what encoding is necessary.)
+
+// This file is for Campsite-specific strings.
+// They are kept in a separate file to ease the upgrade process
+// between different versions of the editor.
+{
+	"Subhead": "Подзаголовок",
+	"Insert Internal Link": "Вставить внешнюю ссылку",
+};
