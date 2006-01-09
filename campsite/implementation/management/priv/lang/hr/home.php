b4f1604cdce8c675d82cb24ce7b53ba55874f680
paul <paul@952d13e4-84fd-0310-bd7c-ee2827c68691>
2006-01-09 05:18:04 +0000

    hr translation

--- campsite/implementation/management/priv/lang/hr/home.php
+++ campsite/implementation/management/priv/lang/hr/home.php
@@ -1,7 +1,18 @@
 <?php
+regGS("You haven't written any articles yet.", "");
+regGS("There are no pending items to be published.", "");
+regGS("There are currently no submitted articles.", "");
+regGS("Recently Published Articles", "");
+regGS("Publish articles", "");
+regGS("Publish Date", "");
+regGS("No articles have been published yet.", "");
+regGS("Event(s)", "");
+regGS("Signed in: $1", "");
+regGS("Help", "");
+regGS("About", "");
 regGS("Please enter your user name and password", "Unesite vaše ime i lozinku");
 regGS("You do not have the right to access this page.", "Nemate pravo pristupiti ovoj stranici");
 regGS("Login failed", "Prijava nije uspjela");
 regGS("Please make sure that you typed the correct user name and password.", "Provjerite da li ste ispravno upisali korisničko ime i lozinku");
-regGS("If your problem persists please contact the site administrator $1", "Ukoliko vam se ovaj problem ponavlja, obratite se vašem administratoru");
+regGS("If your problem persists please contact the site administrator $1", "Ukoliko vam se ovaj problem ponavlja, obratite se vašem administratoru $1");
 ?>
