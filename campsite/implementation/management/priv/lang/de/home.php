71491e67486fa0cd4a70d4639c3c26b1becd7a54
paul <paul@952d13e4-84fd-0310-bd7c-ee2827c68691>
2006-01-09 05:28:09 +0000

    German translation

--- campsite/implementation/management/priv/lang/de/home.php
+++ campsite/implementation/management/priv/lang/de/home.php
@@ -1,4 +1,15 @@
 <?php
+regGS("You haven't written any articles yet.", "");
+regGS("There are no pending items to be published.", "");
+regGS("There are currently no submitted articles.", "");
+regGS("Recently Published Articles", "");
+regGS("Publish articles", "");
+regGS("Publish Date", "");
+regGS("No articles have been published yet.", "");
+regGS("Event(s)", "");
+regGS("Signed in: $1", "");
+regGS("Help", "");
+regGS("About", "");
 regGS("Please enter your user name and password", "Bitte Namen und Passwort eingeben");
 regGS("You do not have the right to access this page.", "Keine Berechtigung  die Seite zu verändern");
 regGS("Login failed", "Anmeldung fehlgeschlagen");
