7e47aae15ca6cce1aa78fe07d203e9789cf7e32e
paul <paul@952d13e4-84fd-0310-bd7c-ee2827c68691>
2006-02-01 22:26:23 +0000

    Create the KeywordsSeparator system preference

--- campsite/implementation/database/upgrade/2.4.x/tables.sql
+++ campsite/implementation/database/upgrade/2.4.x/tables.sql
@@ -2,5 +2,7 @@
 -- the queries for fetching images become unbearably slow.
 ALTER  TABLE  `ArticleImages`  ADD  INDEX  `IdImage` (  `IdImage`  ) ;
 
+INSERT INTO UserConfig(`fk_user_id`, `varname`, `value`) VALUES (0, 'KeywordSeparator', ',');
+
 -- Run the upgrade script
 system php ./upgrade_user_perms.php
