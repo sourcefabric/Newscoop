00c3a39e6fead0f02eda0e047d5536a7dadd6b7b
paul <paul@952d13e4-84fd-0310-bd7c-ee2827c68691>
2006-03-02 18:11:45 +0000

    Fixed bug #1765 - do not try to create a translated article that already exists.

--- campsite/implementation/management/priv/articles/do_translate.php
+++ campsite/implementation/management/priv/articles/do_translate.php
@@ -8,6 +8,12 @@ if (!$access) {
 	exit;
 }
 
+if (!$User->hasPermission("AddArticle")) {
+	$errorStr = getGS('You do not have the right to change this article.  You may only edit your own articles and once submitted an article can only be changed by authorized users.');
+	camp_html_display_error($errorStr);
+	exit;
+}
+
 // Optional input.
 $f_language_id = Input::Get('f_language_id', 'int', 0, true);
 $f_publication_id = Input::Get('f_publication_id', 'int', 0, true);
@@ -26,7 +32,7 @@ $BackLink = "/$ADMIN/articles/translate.php?f_language_id=$f_language_id"
 
 if (!Input::IsValid()) {
 	camp_html_display_error(getGS('Invalid input: $1', Input::GetErrorString()));
-	exit;	
+	exit;
 }
 
 $articleObj =& new Article($languageId, $articleNumber);
@@ -41,30 +47,31 @@ if (!$translationLanguageObj->exists()) {
 	exit;
 }
 
-if (!$User->hasPermission("AddArticle")) {
-	$errorStr = getGS('You do not have the right to change this article.  You may only edit your own articles and once submitted an article can only be changed by authorized users.');
-	camp_html_display_error($errorStr);
-	exit;	
+$translationArticle =& new Article($f_translation_language, $articleNumber);
+if ($translationArticle->exists()) {
+	$BackLink = camp_html_article_url($articleObj, $f_language_id, "edit.php");
+	camp_html_display_error(getGS("The article has already been translated into $1.", $translationLanguageObj->getNativeName()), $BackLink);
+	exit;
 }
 
 $f_publication_id = $articleObj->getPublicationId();
 
-// Only create the translated issue and section if the article has been 
+// Only create the translated issue and section if the article has been
 // categorized.
 if ($f_publication_id > 0) {
 	$publicationObj =& new Publication($f_publication_id);
 	if (!$publicationObj->exists()) {
 		camp_html_display_error(getGS('Publication does not exist.'), $BackLink);
-		exit;	
+		exit;
 	}
-	
+
 	$f_issue_number = $articleObj->getIssueNumber();
 	$issueObj =& new Issue($f_publication_id, $f_language_id, $f_issue_number);
 	if (!$issueObj->exists()) {
 		camp_html_display_error(getGS('No such issue.'), $BackLink);
-		exit;	
+		exit;
 	}
-	
+
 	$translationIssueObj =& new Issue($f_publication_id, $f_translation_language, $f_issue_number);
 	if (!$translationIssueObj->exists()) {
 		if (!$User->hasPermission("ManageIssue")) {
@@ -95,14 +102,14 @@ if ($f_publication_id > 0) {
 			exit;
 		}
 	}
-	
+
 	$f_section_number = $articleObj->getSectionNumber();
 	$sectionObj =& new Section($f_publication_id, $f_issue_number, $f_language_id, $f_section_number);
 	if (!$sectionObj->exists()) {
 		camp_html_display_error(getGS('No such section.'), $BackLink);
-		exit;		
+		exit;
 	}
-	
+
 	$translationSectionObj =& new Section($f_publication_id, $f_issue_number, $f_translation_language,
 										  $f_section_number);
 	if (!$translationSectionObj->exists()) {
@@ -135,6 +142,6 @@ if ($f_publication_id > 0) {
 
 $articleCopy =& $articleObj->createTranslation($f_translation_language, $User->getUserId(), $f_translation_title);
 
-header('Location: '.camp_html_article_url($articleCopy, $f_language_id, 'edit.php')); 
+header('Location: '.camp_html_article_url($articleCopy, $f_language_id, 'edit.php'));
 exit;
 ?>
