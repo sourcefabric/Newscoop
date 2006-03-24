75a46171fa927c35b26dfc0456b8944c59638457
paul <paul@952d13e4-84fd-0310-bd7c-ee2827c68691>
2006-03-24 05:48:35 +0000

    Fixed bug #1697 - Xinha mishandling images when more than more than 9 are inserted.

--- campsite/implementation/management/javascript/xinha/plugins/ImageManager/Classes/ImageManager.php
+++ campsite/implementation/management/javascript/xinha/plugins/ImageManager/Classes/ImageManager.php
@@ -15,7 +15,7 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/classes/Image.php');
  * @author $Author$
  * @version $Id$
  */
-class ImageManager 
+class ImageManager
 {
 	/**
 	 * Configuration array.
@@ -31,7 +31,7 @@ class ImageManager
 	 * Constructor. Create a new Image Manager instance.
 	 * @param array $config configuration array, see config.inc.php
 	 */
-	function ImageManager($config) 
+	function ImageManager($config)
 	{
 		$this->config = $config;
 	}
@@ -40,7 +40,7 @@ class ImageManager
 	 * Get the base directory.
 	 * @return string base dir, see config.inc.php
 	 */
-	function getBaseDir() 
+	function getBaseDir()
 	{
 		Return $this->config['base_dir'];
 	}
@@ -49,7 +49,7 @@ class ImageManager
 	 * Get the base URL.
 	 * @return string base url, see config.inc.php
 	 */
-	function getBaseURL() 
+	function getBaseURL()
 	{
 		Return $this->config['base_url'];
 	}
@@ -63,7 +63,7 @@ class ImageManager
 	 * Get the tmp file prefix.
      * @return string tmp file prefix.
 	 */
-	function getTmpPrefix() 
+	function getTmpPrefix()
 	{
 		Return $this->config['tmp_prefix'];
 	}
@@ -71,12 +71,12 @@ class ImageManager
 	/**
 	 * Get the sub directories in the base dir.
 	 * Each array element contain
-	 * the relative path (relative to the base dir) as key and the 
+	 * the relative path (relative to the base dir) as key and the
 	 * full path as value.
 	 * @return array of sub directries
 	 * <code>array('path name' => 'full directory path', ...)</code>
 	 */
-	function getDirs() 
+	function getDirs()
 	{
 		if(is_null($this->dirs))
 		{
@@ -95,7 +95,7 @@ class ImageManager
 	 * @return array of accessiable sub-directories
 	 * <code>array('path name' => 'full directory path', ...)</code>
 	 */
-	function _dirs($base, $path) 
+	function _dirs($base, $path)
 	{
 		$base = Files::fixPath($base);
 		$dirs = array();
@@ -104,14 +104,14 @@ class ImageManager
 			return $dirs;
 
 		$d = @dir($base);
-		
-		while (false !== ($entry = $d->read())) 
+
+		while (false !== ($entry = $d->read()))
 		{
 			//If it is a directory, and it doesn't start with
 			// a dot, and if is it not the thumbnail directory
-			if(is_dir($base.$entry) 
+			if(is_dir($base.$entry)
 				&& substr($entry,0,1) != '.'
-				&& $this->isThumbDir($entry) == false) 
+				&& $this->isThumbDir($entry) == false)
 			{
 				$relative = Files::fixPath($path.$entry);
 				$fullpath = Files::fixPath($base.$entry);
@@ -131,19 +131,19 @@ class ImageManager
 	 *
 	 * @return array of file and path information.
 	 * <code>
-	 *   array(0 => array('relative'=>'fullpath',...), 
+	 *   array(0 => array('relative'=>'fullpath',...),
 	 *		   1 => array('filename'=>fileinfo array(),...)
 	 * </code>
-	 * fileinfo array: 
+	 * fileinfo array:
 	 * <code>
-	 *   array('url'=>'full url', 
-	 *         'relative'=>'relative to base', 
-	 *         'fullpath'=>'full file path', 
+	 *   array('url'=>'full url',
+	 *         'relative'=>'relative to base',
+	 *         'fullpath'=>'full file path',
 	 *         'image'=>imageInfo array() false if not image,
 	 *         'stat' => filestat)
 	 * </code>
 	 */
-	function getFiles($path, $articleId) 
+	function getFiles($path, $articleId)
 	{
 		$files = array();
 		$dirs = array();
@@ -160,6 +160,7 @@ class ImageManager
 			$image = $articleImage->getImage();
 			$img = $this->getImageInfo($image->getImageUrl());
 			if (is_array($img) || !$this->config['validate_images']) {
+				$file['template_id'] = $articleImage->getTemplateId();
 				$file['url'] = $image->getImageUrl();
 				$file['image'] = $img;
 				$file['image_object'] = $image;
@@ -167,31 +168,31 @@ class ImageManager
 				$files[$articleImage->getTemplateId()] = $file;
 			}
 		}
-		
+
 		ksort($dirs);
 		ksort($files);
 		return array($dirs, $files);
-	}	
+	}
 
 	/**
 	 * Count the number of files and directories in a given folder
 	 * minus the thumbnail folders and thumbnails.
 	 */
-	function countFiles($path) 
+	function countFiles($path)
 	{
 		$total = 0;
 
-		if(is_dir($path)) 
+		if(is_dir($path))
 		{
 			$d = @dir($path);
 
-			while (false !== ($entry = $d->read())) 
+			while (false !== ($entry = $d->read()))
 			{
 				//echo $entry."<br>";
 				if(substr($entry,0,1) != '.'
 					&& $this->isThumbDir($entry) == false
 					&& $this->isTmpFile($entry) == false
-					&& $this->isThumb($entry) == false) 
+					&& $this->isThumb($entry) == false)
 				{
 					$total++;
 				}
@@ -204,10 +205,10 @@ class ImageManager
 	/**
 	 * Get image size information.
 	 * @param string $file the image file
-	 * @return array of getImageSize information, 
+	 * @return array of getImageSize information,
 	 *  false if the file is not an image.
 	 */
-	function getImageInfo($file) 
+	function getImageInfo($file)
 	{
 		Return @getImageSize($file);
 	}
@@ -217,7 +218,7 @@ class ImageManager
 	 * @param string $file filename to be checked
 	 * @return true if the file contains the thumbnail prefix, false otherwise.
 	 */
-	function isThumb($file) 
+	function isThumb($file)
 	{
 		$len = strlen($this->config['thumbnail_prefix']);
 		if(substr($file,0,$len)==$this->config['thumbnail_prefix'])
@@ -231,11 +232,11 @@ class ImageManager
 	 * @param string $entry directory name
 	 * @return true if it is a thumbnail directory, false otherwise
 	 */
-	function isThumbDir($entry) 
+	function isThumbDir($entry)
 	{
 		if($this->config['thumbnail_dir'] == false
 			|| strlen(trim($this->config['thumbnail_dir'])) == 0)
-			Return false;		
+			Return false;
 		else
 			Return ($entry == $this->config['thumbnail_dir']);
 	}
@@ -245,13 +246,13 @@ class ImageManager
 	 * @param string $file file name
 	 * @return boolean true if it is a tmp file, false otherwise
 	 */
-	function isTmpFile($file) 
+	function isTmpFile($file)
 	{
 		$len = strlen($this->config['tmp_prefix']);
 		if(substr($file,0,$len)==$this->config['tmp_prefix'])
 			Return true;
 		else
-			Return false;	 	
+			Return false;
 	}
 
 	/**
@@ -260,10 +261,10 @@ class ImageManager
 	 * @param string $fullpathfile the full path to the image file
 	 * @return string of the thumbnail file
 	 */
-	function getThumbName($fullpathfile) 
+	function getThumbName($fullpathfile)
 	{
 		$path_parts = pathinfo($fullpathfile);
-		
+
 		$filenameParts = split("[-.]", $path_parts['basename']);
 		$imageId = $filenameParts[2];
 		$extension = $filenameParts[3];
@@ -290,15 +291,15 @@ class ImageManager
 			}
 		}
 	}
-	
+
 	/**
 	 * Similar to getThumbName, but returns the URL, base on the
 	 * given base_url in config.inc.php
-	 * @param string $relative the relative image file name, 
+	 * @param string $relative the relative image file name,
 	 * relative to the base_dir path
 	 * @return string the url of the thumbnail
 	 */
-	function getThumbURL($relative) 
+	function getThumbURL($relative)
 	{
 		$path_parts = pathinfo($relative);
 		$filenameParts = split("[-.]", $path_parts['basename']);
@@ -336,12 +337,12 @@ class ImageManager
 	 * @param string $path the relative path to be checked
 	 * @return boolean true if the path exists, false otherwise
 	 */
-	function validRelativePath($path) 
+	function validRelativePath($path)
 	{
 		$dirs = $this->getDirs();
 		if($path == '/')
 			Return true;
-		//check the path given in the url against the 
+		//check the path given in the url against the
 		//list of paths in the system.
 		for($i = 0; $i < count($dirs); $i++)
 		{
@@ -349,28 +350,28 @@ class ImageManager
 			//we found the path
 			if($key == $path)
 				Return true;
-		
+
 			next($dirs);
-		}		
+		}
 		Return false;
 	}
 
 	/**
-	 * Process uploaded files, assumes the file is in 
+	 * Process uploaded files, assumes the file is in
 	 * $_FILES['upload'] and $_POST['dir'] is set.
 	 * The dir must be relative to the base_dir and exists.
 	 * If 'validate_images' is set to true, only file with
 	 * image dimensions will be accepted.
 	 * @return null
 	 */
-	function processUploads() 
+	function processUploads()
 	{
 		if($this->isValidBase() == false)
 			return;
 
 		$relative = null;
 
-		if(isset($_POST['dir'])) 
+		if(isset($_POST['dir']))
 			$relative = rawurldecode($_POST['dir']);
 		else
 			return;
@@ -383,7 +384,7 @@ class ImageManager
 	}
 
 	/**
-	 * Process upload files. The file must be an 
+	 * Process upload files. The file must be an
 	 * uploaded file. If 'validate_images' is set to
 	 * true, only images will be processed. Any duplicate
 	 * file will be renamed. See Files::copyFile for details
@@ -391,12 +392,12 @@ class ImageManager
 	 * @param string $relative the relative path where the file
 	 * should be copied to.
 	 * @param array $file the uploaded file from $_FILES
-	 * @return boolean true if the file was processed successfully, 
+	 * @return boolean true if the file was processed successfully,
 	 * false otherwise
 	 */
 	function _processFiles($relative, $file)
 	{
-		
+
 		if($file['error']!=0)
 		{
 			Return false;
@@ -412,7 +413,7 @@ class ImageManager
 			Files::delFile($file['tmp_name']);
 			Return false;
 		}
-		
+
 
 		if($this->config['validate_images'] == true)
 		{
@@ -442,12 +443,12 @@ class ImageManager
 
 	/**
 	 * Get the URL of the relative file.
-	 * basically appends the relative file to the 
+	 * basically appends the relative file to the
 	 * base_url given in config.inc.php
 	 * @param string $relative a file the relative to the base_dir
 	 * @return string the URL of the relative file.
 	 */
-	function getFileURL($relative) 
+	function getFileURL($relative)
 	{
 		Return Files::makeFile($this->getBaseURL(),$relative);
 	}
@@ -457,32 +458,32 @@ class ImageManager
 	 * @param string $relative the relative file.
 	 * @return string the full path, .ie. the base_dir + relative.
 	 */
-	function getFullPath($relative) 
+	function getFullPath($relative)
 	{
 		Return Files::makeFile($this->getBaseDir(),$relative);;
 	}
 
 	/**
 	 * Get the default thumbnail.
-	 * @return string default thumbnail, empty string if 
+	 * @return string default thumbnail, empty string if
 	 * the thumbnail doesn't exist.
 	 */
-	function getDefaultThumb() 
+	function getDefaultThumb()
 	{
 		if(is_file($this->config['default_thumbnail']))
 			Return $this->config['default_thumbnail'];
-		else 
+		else
 			Return '';
 	}
 
 
 	/**
-	 * Get the thumbnail url to be displayed. 
+	 * Get the thumbnail url to be displayed.
 	 * If the thumbnail exists, and it is up-to-date
-	 * the thumbnail url will be returns. If the 
+	 * the thumbnail url will be returns. If the
 	 * file is not an image, a default image will be returned.
-	 * If it is an image file, and no thumbnail exists or 
-	 * the thumbnail is out-of-date (i.e. the thumbnail 
+	 * If it is an image file, and no thumbnail exists or
+	 * the thumbnail is out-of-date (i.e. the thumbnail
 	 * modified time is less than the original file)
 	 * then a thumbs.php?img=filename.jpg is returned.
 	 * The thumbs.php url will generate a new thumbnail
@@ -493,7 +494,7 @@ class ImageManager
 	 * actually thumbnail or a script to generate the
 	 * thumbnail on the fly.
 	 */
-	function getThumbnail($relative) 
+	function getThumbnail($relative)
 	{
 		$fullpath = Files::makeFile($this->getBaseDir(),$relative);
 
@@ -502,7 +503,7 @@ class ImageManager
 			Return $this->getDefaultThumb();
 
 		$imgInfo = @getImageSize($fullpath);
-		
+
 		//not an image
 		if(!is_array($imgInfo))
 			Return $this->getDefaultThumb();
@@ -514,7 +515,7 @@ class ImageManager
 			Return $this->getFileURL($relative);
 
 		$thumbnail = $this->getThumbName($fullpath);
-		
+
 		//check for thumbnails, if exists and
 		// it is up-to-date, return the thumbnail url
 		if(is_file($thumbnail))
@@ -532,7 +533,7 @@ class ImageManager
 	 * Delete and specified files.
 	 * @return boolean true if delete, false otherwise
 	 */
-	function deleteFiles() 
+	function deleteFiles()
 	{
 		if(isset($_GET['delf']))
 			$this->_delFile(rawurldecode($_GET['delf']));
@@ -542,10 +543,10 @@ class ImageManager
 	 * Delete and specified directories.
 	 * @return boolean true if delete, false otherwise
 	 */
-	function deleteDirs() 
+	function deleteDirs()
 	{
 		 if(isset($_GET['deld']))
-			return $this->_delDir(rawurldecode($_GET['deld']));		
+			return $this->_delDir(rawurldecode($_GET['deld']));
 		 else
 			 Return false;
 	}
@@ -555,10 +556,10 @@ class ImageManager
 	 * @param string $relative the relative file.
 	 * @return boolean true if deleted, false otherwise.
 	 */
-	function _delFile($relative) 
+	function _delFile($relative)
 	{
 		$fullpath = Files::makeFile($this->getBaseDir(),$relative);
-		
+
 		//check that the file is an image
 		if($this->config['validate_images'] == true)
 		{
@@ -579,7 +580,7 @@ class ImageManager
 	 * @param string $relative the relative path to be deleted.
 	 * @return boolean true if deleted, false otherwise.
 	 */
-	function _delDir($relative) 
+	function _delDir($relative)
 	{
 		$fullpath = Files::makePath($this->getBaseDir(),$relative);
 		if($this->countFiles($fullpath) <= 0)
@@ -593,7 +594,7 @@ class ImageManager
 	 * If in safe_mode, nothing happens.
 	 * @return boolean true if created, false otherwise.
 	 */
-	function processNewDir() 
+	function processNewDir()
 	{
 		if($this->config['safe_mode'] == true)
 			Return false;
