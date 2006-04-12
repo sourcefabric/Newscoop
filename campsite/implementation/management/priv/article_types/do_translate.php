1d13064586e477e4dd43a3cf7b374b1c68607783
peterjh <peterjh@952d13e4-84fd-0310-bd7c-ee2827c68691>
2006-04-12 12:12:30 +0000

    Unfinished initial draft of do_translation code.

---
+++ campsite/implementation/management/priv/article_types/do_translate.php
@@ -0,0 +1,100 @@
+<?php
+require_once($_SERVER['DOCUMENT_ROOT'].'/classes/common.php');
+load_common_include_files("article_types");
+require_once($_SERVER['DOCUMENT_ROOT']."/$ADMIN_DIR/camp_html.php");
+require_once($_SERVER['DOCUMENT_ROOT'].'/classes/Log.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/classes/Input.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/classes/Article.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/classes/ArticleType.php');
+
+// Check permissions
+list($access, $User) = check_basic_access($_REQUEST);
+if (!$access) {
+	header("Location: /$ADMIN/logout.php");
+	exit;
+}
+
+if (!$User->hasPermission('ManageArticleTypes')) {
+	camp_html_display_error(getGS("You do not have the right to add article types."));
+	exit;
+}
+
+
+$f_type_id = Input::Get('f_type_id', 'int', 0, true);
+$f_type_language_id = Input::Get('f_type_language_id', 'int', 0);
+$f_type_translation_name = trim(Input::Get('f_type_translation_name'));
+$correct = true;
+$created = false;
+//$topicParent =& new Topic($f_topic_parent_id);
+//$Path = camp_topic_path($topicParent, $f_topic_language_id);
+
+$errorMsgs = array();
+if (empty($f_type_translation_name)) {
+	$correct = false; 
+	$errorMsgs[] = getGS('You must fill in the $1 field.','<B>'.getGS('Name').'</B>'); 
+}
+if ($f_type_language_id <= 0) {
+	$correct = false; 
+	$errorMsgs[] = getGS('You must choose a language for the topic.'); 
+}
+
+$correct = true;
+$created = false;
+$errorMsgs = array();
+
+if ($correct) {
+	// Translate existing type
+	$type =& new ArticleType($f_type_id);
+	$created = $type->setName($f_type_language_id, $f_type_translation_name);
+	
+
+	if ($created) {
+		$foo = $type->getTranslations();
+		print "<BR>translations:<BR><BR>";
+		print_r($foo);
+//		header("Location: /$ADMIN/topics/index.php");
+//		exit;
+	}
+	else {
+		$errorMsgs[] = getGS('The article type could not be added.');
+	}
+}
+
+$crumbs = array();
+$crumbs[] = array(getGS("Configure"), "");
+$crumbs[] = array(getGS("Article Types"), "/$ADMIN/article_types/");
+$crumbs[] = array(getGS("Adding new article type"), "");
+
+echo camp_html_breadcrumbs($crumbs);
+
+?>
+<P>
+<TABLE BORDER="0" CELLSPACING="0" CELLPADDING="8" class="message_box">
+<TR>
+	<TD COLSPAN="2">
+		<B> <?php  putGS("Adding new article type"); ?> </B>
+		<HR NOSHADE SIZE="1" COLOR="BLACK">
+	</TD>
+</TR>
+<TR>
+	<TD COLSPAN="2">
+		<BLOCKQUOTE>
+		<?php 
+		foreach ($errorMsgs as $errorMsg) { 
+			echo "<li>".$errorMsg."</li>";
+		}
+		?>
+		</BLOCKQUOTE>
+	</TD>
+</TR>
+<TR>
+	<TD COLSPAN="2">
+	<DIV ALIGN="CENTER">
+	<INPUT TYPE="button" class="button" NAME="OK" VALUE="<?php  putGS('OK'); ?>" ONCLICK="location.href='/<?php p($ADMIN); ?>/article_types/add.php'">
+	</DIV>
+	</TD>
+</TR>
+</TABLE>
+<P>
+
+<?php camp_html_copyright_notice(); ?>
