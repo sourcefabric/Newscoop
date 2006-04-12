0717aea06fa1e0685356bcd14fdd8af2e2583062
peterjh <peterjh@952d13e4-84fd-0310-bd7c-ee2827c68691>
2006-04-12 12:11:49 +0000

    The UI for renaming an article type.

---
+++ campsite/implementation/management/priv/article_types/rename.php
@@ -0,0 +1,56 @@
+<?php
+require_once($_SERVER['DOCUMENT_ROOT'].'/classes/common.php');
+load_common_include_files("article_types");
+require_once($_SERVER['DOCUMENT_ROOT']."/$ADMIN_DIR/camp_html.php");
+require_once($_SERVER['DOCUMENT_ROOT'].'/classes/ArticleType.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/classes/Input.php');
+
+// Check permissions
+list($access, $User) = check_basic_access($_REQUEST);
+if (!$access) {
+	header("Location: /$ADMIN/logout.php");
+	exit;
+}
+
+if (!$User->hasPermission('ManageArticleTypes')) {
+	camp_html_display_error(getGS("You do not have the right to rename article types."));
+	exit;
+}
+
+$articleTypes = ArticleType::GetArticleTypes();
+$f_name = trim(Input::get('f_name'));
+
+$crumbs = array();
+$crumbs[] = array(getGS("Configure"), "");
+$crumbs[] = array(getGS("Article Types"), "/$ADMIN/article_types/");
+$crumbs[] = array(getGS("Rename article type"), "");
+echo camp_html_breadcrumbs($crumbs);
+
+?>
+<P>
+<FORM NAME="dialog" METHOD="POST" ACTION="do_rename.php">
+<INPUT TYPE="hidden" VALUE="<?php p($f_name); ?>" NAME="cOldName">
+<TABLE BORDER="0" CELLSPACING="0" CELLPADDING="6" CLASS="table_input">
+<TR>
+	<TD COLSPAN="6">
+		<B><?php putGS("Rename article type"); ?></B>
+		<HR NOSHADE SIZE="1" COLOR="BLACK">
+</TR>
+<TR>
+	<TD ALIGN="RIGHT" ><?php  putGS("Name"); ?>:</TD>
+	<TD COLSPAN="5">
+	<INPUT TYPE="TEXT" VALUE="<?php p($f_name); ?>" class="input_text" NAME="f_name" SIZE="15" MAXLENGTH="15">
+	</TD>
+</TR>
+<TR>
+	<TD COLSPAN="2">
+	<DIV ALIGN="CENTER">
+	<INPUT TYPE="submit" class="button" NAME="Ok" VALUE="<?php  putGS('Save'); ?>">
+	</DIV>
+	</TD>
+</TR>
+</TABLE>
+</FORM>
+<P>
+
+<?php camp_html_copyright_notice(); ?>
