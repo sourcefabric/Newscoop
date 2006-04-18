4c202a97507792fdf3e2ea54574b340171e2398d
peterjh <peterjh@952d13e4-84fd-0310-bd7c-ee2827c68691>
2006-04-18 13:23:41 +0000

    New ArticleTypeMetadata field for article types / fields.

--- campsite/implementation/database/upgrade/2.5.x/tables.sql
+++ campsite/implementation/database/upgrade/2.5.x/tables.sql
@@ -1,11 +1,12 @@
 -- For new Article type features - ability
 -- to rename, translate, reorder, and hide them.
-CREATE TABLE data_type_fields (
-    table_name VARCHAR(255),
+CREATE TABLE ArticleTypeMetadata (
+    type_name VARCHAR(255) NOT NULL,
     field_name VARCHAR(255),
-    weight INT,
-    is_hidden INT,
-    fk_phrase_id INT UNSIGNED
+    field_weight INT,
+    is_hidden INT DEFAULT 1,
+    fk_phrase_id INT UNSIGNED,
+    field_type VARCHAR(255),
+    field_type_param VARCHAR(255)
 );
-
 
