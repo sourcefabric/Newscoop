7692be1cb1a0846f3d4c2000c1c451dc90957d5c
possum <possum@952d13e4-84fd-0310-bd7c-ee2827c68691>
2006-04-19 14:54:38 +0000

    Fixed broken link in "Tickets by Milestone" tab.

--- campsite/implementation/trac/trac_plugin_milestones/milestones/milestones.py
+++ campsite/implementation/trac/trac_plugin_milestones/milestones/milestones.py
@@ -13,15 +13,15 @@ class UserbaseModule(Component):
         return 'milestones'
                 
     def get_navigation_items(self, req):
-        yield 'mainnav', 'milestones', Markup('<a href="/trac/report/3">Tickets by Milestone</a>')
+#        yield 'mainnav', 'milestones', Markup('<a href=%s">Tickets by Milestone</a>' % req.href.report)
 
+        yield 'mainnav', 'milestones', Markup('<a href="%s/3">Tickets by Milestone</a>', self.env.href.report())
+
+
+#Markup('<a href="%s">Hello</a>', self.env.href.report())
     # IRequestHandler methods
     def match_request(self, req):
         return req.path_info == '/milestones'
     
     def process_request(self, req):
-        req.send_response(200)
-        req.send_header('Content-Type', 'text/plain')
-        req.end_headers()
-        req.write('Milestones')
-        
+        req.redirect (self.env.href.report())
