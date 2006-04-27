7aa1660aea069d2e4905d5df5debd5b628ce48cb
paul <paul@952d13e4-84fd-0310-bd7c-ee2827c68691>
2006-04-27 13:55:12 +0000

    Changes to time fields to support down-to-the-second resolution.  Added article comments tables and columns.

--- campsite/implementation/database/upgrade/2.5.x/tables.sql
+++ campsite/implementation/database/upgrade/2.5.x/tables.sql
@@ -13,3 +13,33 @@ CREATE TABLE ArticleTypeMetadata (
 -- Change article creation time so we know when
 -- it was created, down to the second.
 ALTER TABLE `Articles` CHANGE `UploadDate` `UploadDate` DATETIME NOT NULL DEFAULT '0000-00-00';
+
+-- Change Issue publish time so we know hour, minute, second.
+ALTER TABLE `Issues` CHANGE `PublicationDate` `PublicationDate` DATETIME NOT NULL DEFAULT '0000-00-00';
+
+-- Add a "last-modified" field to article table
+ALTER TABLE `Articles` ADD `time_updated` TIMESTAMP NOT NULL ;
+
+
+--
+-- Article Comments
+--
+
+-- The table to map articles to comment threads and vice versa.
+CREATE TABLE `ArticleComments` (
+`fk_article_number` INT UNSIGNED NOT NULL ,
+`fk_language_id` INT UNSIGNED NOT NULL ,
+`fk_comment_thread_id` INT UNSIGNED NOT NULL ,
+PRIMARY KEY ( `fk_article_number` , `fk_language_id` ) ,
+INDEX ( `fk_comment_thread_id` )
+) ENGINE = MYISAM ;
+
+
+-- The forum ID for article comments.
+ALTER TABLE `Publications` ADD `fk_forum_id` INT NULL ;
+
+
+-- As always, there are more user permissions!
+-- Run the user permission upgrade script
+-- system php ./upgrade_user_perms.php
+
