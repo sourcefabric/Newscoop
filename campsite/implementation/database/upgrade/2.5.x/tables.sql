f4b4672764915182af52c5e2d29518f55a8ceb1d
peterjh <peterjh@952d13e4-84fd-0310-bd7c-ee2827c68691>
2006-04-27 14:12:10 +0000

    Changed is_hidden to default 0 rather than 1.  However, this is just for clarity--the code already ensures that it is default to 0.

--- campsite/implementation/database/upgrade/2.5.x/tables.sql
+++ campsite/implementation/database/upgrade/2.5.x/tables.sql
@@ -4,7 +4,7 @@ CREATE TABLE ArticleTypeMetadata (
     type_name VARCHAR(255) NOT NULL,
     field_name VARCHAR(255),
     field_weight INT,
-    is_hidden INT DEFAULT 1,
+    is_hidden INT DEFAULT 0,
     fk_phrase_id INT UNSIGNED,
     field_type VARCHAR(255),
     field_type_param VARCHAR(255)
