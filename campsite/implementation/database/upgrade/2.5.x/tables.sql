e1c6484d92ce3c586422751f3d0a2407223c998c
peterjh <peterjh@952d13e4-84fd-0310-bd7c-ee2827c68691>
2006-04-29 17:24:18 +0000

    Uncommented the CREATE for ArticleTypeMetadata.  (Accidently committed a commented out version.)

--- campsite/implementation/database/upgrade/2.5.x/tables.sql
+++ campsite/implementation/database/upgrade/2.5.x/tables.sql
@@ -1,15 +1,15 @@
 -- For new Article type features - ability
 -- to rename, translate, reorder, and hide them.
---CREATE TABLE ArticleTypeMetadata (
---    type_name VARCHAR(255) NOT NULL,
---    field_name VARCHAR(255) NOT NULL DEFAULT 'NULL',
---    field_weight INT,
---    is_hidden INT DEFAULT 0,
---    fk_phrase_id INT UNSIGNED,
---    field_type VARCHAR(255),
---    field_type_param VARCHAR(255),
---    PRIMARY KEY (`type_name`,`field_name`)
---);
+CREATE TABLE ArticleTypeMetadata (
+    type_name VARCHAR(255) NOT NULL,
+    field_name VARCHAR(255) NOT NULL DEFAULT 'NULL',
+    field_weight INT,
+    is_hidden INT DEFAULT 0,
+    fk_phrase_id INT UNSIGNED,
+    field_type VARCHAR(255),
+    field_type_param VARCHAR(255),
+    PRIMARY KEY (`type_name`,`field_name`)
+);
 
 -- Change article creation time so we know when
 -- it was created, down to the second.
