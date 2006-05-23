b22bfa1e8f475609253e804656a05a8c2432166b
paul <paul@952d13e4-84fd-0310-bd7c-ee2827c68691>
2006-05-23 16:29:30 +0000

    Added global variables  and  - somehow these missing started causing problems.

--- campsite/implementation/management/include/phorum_load.php
+++ campsite/implementation/management/include/phorum_load.php
@@ -1,4 +1,6 @@
 <?php
+global $Campsite;
+global $PHORUM;
 define( "PHORUM", "5.1-dev" );
 
 require_once($_SERVER['DOCUMENT_ROOT']."/include/phorum/include/constants.php");
@@ -15,7 +17,7 @@ $PHORUM['DBCONFIG']=array(
 
     // 'down_page'     => 'http://www.example.com/phorum/down.html',
 
-    // 1=enabled, 0=disabled 
+    // 1=enabled, 0=disabled
     // (always disable this option for MySQL versions prior to 4.0.18!)
     'mysql_use_ft'  =>  '1'
 );
