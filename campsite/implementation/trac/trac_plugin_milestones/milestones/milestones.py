f074fc0cf0d1fbaf18c1caae973e6bdebf350ba8
possum <possum@952d13e4-84fd-0310-bd7c-ee2827c68691>
2006-05-26 18:00:15 +0000

    Cleaned up code somewhat, in an attempt to fix the problem of the Milestons tab disappearing intermittently.

--- campsite/implementation/trac/trac_plugin_milestones/milestones/milestones.py
+++ campsite/implementation/trac/trac_plugin_milestones/milestones/milestones.py
@@ -13,15 +13,9 @@ class UserbaseModule(Component):
         return 'milestones'
                 
     def get_navigation_items(self, req):
-#        yield 'mainnav', 'milestones', Markup('<a href=%s">Tickets by Milestone</a>' % req.href.report)
-
         yield 'mainnav', 'milestones', Markup('<a href="%s/3">Tickets by Milestone</a>', self.env.href.report())
 
-
-#Markup('<a href="%s">Hello</a>', self.env.href.report())
     # IRequestHandler methods
     def match_request(self, req):
-        return req.path_info == '/milestones'
-    
-    def process_request(self, req):
-        req.redirect (self.env.href.report())
+        return
+
