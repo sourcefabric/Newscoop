5b1fe2f41fff07a29956e3fddff36909834e2724
possum <possum@952d13e4-84fd-0310-bd7c-ee2827c68691>
2006-05-29 16:59:05 +0000

    I think the problem with the *Tickets by Milestone* problem appearing only intermittently was relating to the code being dependent on the Report tab being already loaded.  This was not always the case, at which case the *Tickets by Milestone* tab would return an error, and not load.
    Assuming the above analysis is correct, this new submission should
    solve the problem.

--- campsite/implementation/trac/trac_plugin_milestones/milestones/milestones.py
+++ campsite/implementation/trac/trac_plugin_milestones/milestones/milestones.py
@@ -9,17 +9,22 @@ class UserbaseModule(Component):
     implements(INavigationContributor, IRequestHandler)
 
     # INavigationContributor methods
-#     def get_active_navigation_item(self, req):
-#         return 'milestones'
+    def get_active_navigation_item(self, req):
+        return 'milestones'
                 
     def get_navigation_items(self, req):
         if not req.perm.has_permission('REPORT_VIEW'):
             return
         yield ('mainnav', 'milestones',
-               Markup('<a href="%s/3">Tickets by Milestone</a>',
-                      self.env.href.report()))
+               Markup('<a href="%s">Tickets by Milestone</a>',
+                      # self.env.href.report()))
+                      self.env.href.milestones()))
 
     # IRequestHandler methods
     def match_request(self, req):
-        return
+        return req.path_info == '/milestones'
 
+    def process_request (self, req):
+        req.redirect (self.env.href.report() + "/3")
+
+        
