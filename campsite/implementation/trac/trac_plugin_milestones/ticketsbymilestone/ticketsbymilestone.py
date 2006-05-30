32e72ef668aaab63f5fe216ad7f943bd24912097
possum <possum@952d13e4-84fd-0310-bd7c-ee2827c68691>
2006-05-30 15:19:53 +0000

    Correction to a time-delay that was just submitted.

--- campsite/implementation/trac/trac_plugin_milestones/ticketsbymilestone/ticketsbymilestone.py
+++ campsite/implementation/trac/trac_plugin_milestones/ticketsbymilestone/ticketsbymilestone.py
@@ -16,6 +16,7 @@ class UserbaseModule(Component):
     def get_navigation_items(self, req):
         if not req.perm.has_permission('REPORT_VIEW'):
             return
+        time.sleep(.5)
         yield ('mainnav', 'ticketsbymilestone',
                Markup('<a href="%s">Tickets by Milestone</a>',
                       self.env.href.ticketsbymilestone()))
@@ -25,7 +26,6 @@ class UserbaseModule(Component):
         return req.path_info == '/ticketsbymilestone'
 
     def process_request (self, req):
-        time.sleep(.5)
         req.redirect (self.env.href.report() + "/3")
 
         
