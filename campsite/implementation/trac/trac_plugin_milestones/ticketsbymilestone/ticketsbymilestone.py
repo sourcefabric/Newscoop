99ff93d2e53a4bc34e9c3ea1f98b1ffc440cbba2
possum <possum@952d13e4-84fd-0310-bd7c-ee2827c68691>
2006-05-31 15:24:51 +0000

    Trying a change of mudule name in ticketsbymilestone.py in a further attempt to fix the trouble of the module intermittently not loading.

--- campsite/implementation/trac/trac_plugin_milestones/ticketsbymilestone/ticketsbymilestone.py
+++ campsite/implementation/trac/trac_plugin_milestones/ticketsbymilestone/ticketsbymilestone.py
@@ -6,7 +6,7 @@ from trac.web.main import IRequestHandler
 from trac.util import escape, Markup
 import time
 
-class UserbaseModule(Component):
+class TicketsByMilestone (Component):
     implements(INavigationContributor, IRequestHandler)
 
     # INavigationContributor methods
@@ -25,7 +25,6 @@ class UserbaseModule(Component):
         return req.path_info == '/ticketsbymilestone'
 
     def process_request (self, req):
-        time.sleep(.5)
         req.redirect (self.env.href.report() + "/3")
 
         
