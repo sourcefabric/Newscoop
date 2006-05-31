e6c60dd0d59444936625776600dbc8262f2b70fb
possum <possum@952d13e4-84fd-0310-bd7c-ee2827c68691>
2006-05-31 17:50:44 +0000

    Fix of typo.

--- campsite/implementation/management/priv/bugreporter/senderrorform.php
+++ campsite/implementation/management/priv/bugreporter/senderrorform.php
@@ -14,7 +14,7 @@ import_request_variables('p', "f_");
 
 // --- If this information is a POST from errormessage.php, send it to
 //     the server ---
-if ($_SERVER['REQUEST_METHOD'] == "POST" && isset($f_isPostFromBugreporter)) {
+if (isset($f_isPostFromBugreporter) && $_SERVER['REQUEST_METHOD'] == "POST") {
 
     $sendWasAttempted = true;
 
