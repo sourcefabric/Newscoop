03304fdbb92b9a3dcdce071219234b0fdc7d134d
paul <paul@952d13e4-84fd-0310-bd7c-ee2827c68691>
2006-06-19 04:39:58 +0000

    Changed instructions in user feedback screen

--- campsite/implementation/management/priv/feedback/index.php
+++ campsite/implementation/management/priv/feedback/index.php
@@ -1,5 +1,6 @@
 <?php
 require_once($_SERVER['DOCUMENT_ROOT'].'/classes/Language.php');
+camp_load_translation_strings("bug_reporting");
 camp_load_translation_strings("feedback");
 
 global $Campsite;
@@ -8,66 +9,56 @@ global $ADMIN;
 ?>
 <br>
 <br>
-<table border="0">
-<tr>
-    <td valign="top">
-        <form name="dialog" method="POST" action="<?php echo "/$ADMIN/feedback/do_feedback.php"; ?>">
-        <input type="hidden" value="1" NAME="f_isFromInterface">
+<form name="dialog" method="POST" action="<?php echo "/$ADMIN/feedback/do_feedback.php"; ?>">
+<input type="hidden" value="1" NAME="f_isFromInterface">
 
-        <table border="0" cellspacing="0" align="left" class="table_input"  width="600px">
+<table border="0" cellspacing="0" cellpadding="6" align="left" class="table_input"  width="500px">
+<tr>
+    <td align="left">
+        <table border="0" cellspacing="0" cellpadding="3" align="left">
         <tr>
-            <td align="left">
-                <table border="0" cellspacing="0" cellpadding="3" align="left">
-                <tr>
-                    <td colspan="2"><b><font color="red"><?php putGS("Feedback") ?></font></b>
-                        <hr noshade size="1" color="black">
-                    </td>
-                </tr>
-                <tr>
-                    <td colspan="2">&nbsp;
-                    </td>
-                </tr>
-                <tr valign="top">
-                    <td colspan="2"><?php
-                        putGS("What would you like to leave as feedback?")
-                    ?></td>
-                </tr>
-                <tr><td align="left" nowrap><?php putGS("Summary"); ?></td>
-                    <td><input type="text" class="input_text" name="f_description" value="" size="32" maxalength="255"></td>
-                </tr>
-                <tr>
-                    <td align="left" nowrap>&nbsp;</td>
-                    <td>
-                        <textarea cols="50" rows="4" class="input_text" name="f_body"></textarea>
-                    </td>
-                </tr>
-                <tr>
-                    <td align="left" nowrap><?php
-                        putGS("Email (optional)");
-                    ?>
-                    </td>
-                    <td><input type="text" class="input_text" name="f_email" value="" size="32" maxlength="255">
-                    </td>
-                </tr>
-                </table>
+            <td colspan="2"><b><font color="red"><?php putGS("Feedback") ?></font></b>
+                <hr noshade size="1" color="black">
             </td>
         </tr>
-        </td>
-    <tr>
+        <tr valign="top">
+            <td colspan="2" style="padding-bottom: 15px;"><?php
+                putGS("Please feel free to tell us about a problem you are having, make a suggestion, or offer any other feedback.")
+            ?></td>
+        </tr>
+        <tr>
+        	<td align="left" nowrap><?php putGS("Subject:"); ?></td>
+            <td><input type="text" class="input_text" name="f_description" value="" size="32" maxlength="255"></td>
+        </tr>
+        <tr>
+            <td align="left" valign="top" nowrap><?php putGS("Comment:"); ?></td>
             <td>
-                <table border="0" cellspacing="0" cellpadding="6" align="center"
-                width="100%">
-                <tr>
-                    <td colspan="2">
-                        <div align="center"><input type="submit"
-                        class="button" value="<?php putGS("Send"); ?>"></div>
-                    </td>
-                </tr>
-                </table>
+                <textarea cols="50" rows="6" class="input_text" name="f_body"></textarea>
+            </td>
+        </tr>
+        <tr>
+            <td align="left" nowrap><?php
+                putGS("Email (optional):");
+            ?>
+            </td>
+            <td><input type="text" class="input_text" name="f_email" value="" size="32" maxlength="255">
             </td>
         </tr>
         </table>
-        </form>
+    </td>
+</tr>
+<tr>
+    <td>
+        <table border="0" cellspacing="0" cellpadding="6" align="center"
+        width="100%">
+        <tr>
+            <td colspan="2" align="">
+                <div align="center"><input type="submit"
+                class="button" value="<?php putGS("Send"); ?>"></div>
+            </td>
+        </tr>
+        </table>
     </td>
 </tr>
 </table>
+</form>
