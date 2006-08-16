f4153891bc003819980873423a00e2c423f693bd
paul <paul@952d13e4-84fd-0310-bd7c-ee2827c68691>
2006-08-16 13:41:56 +0000

    All the templates converted to 2.6

---
+++ campsite/implementation/templates/campsite2.6-sample-template-04/campsite/apache_conf.php
@@ -0,0 +1,8 @@
+<?php
+
+$Campsite['APACHE_USER'] = 'www-data';
+$Campsite['APACHE_GROUP'] = 'www-data';
+
+$CampsiteVars['apache'] = array('APACHE_USER', 'APACHE_GROUP');
+
+?>
---
+++ campsite/implementation/templates/campsite2.6-sample-template-04/campsite/smtp_conf.php
@@ -0,0 +1,8 @@
+<?php
+
+$Campsite['SMTP_SERVER_ADDRESS'] = '127.0.0.1';
+$Campsite['SMTP_SERVER_PORT'] = '25';
+
+$CampsiteVars['smtp'] = array('SMTP_SERVER_ADDRESS', 'SMTP_SERVER_PORT');
+
+?>
