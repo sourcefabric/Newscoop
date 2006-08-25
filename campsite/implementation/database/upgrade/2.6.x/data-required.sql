8c4e9c9dfc0286a05d07906cecb8ad9ed54e993a
holman <holman@952d13e4-84fd-0310-bd7c-ee2827c68691>
2006-08-25 17:11:41 +0000

    ticket:2224 upgrade Events table by inserting Rename and Move templates events.

--- campsite/implementation/database/upgrade/2.6.x/data-required.sql
+++ campsite/implementation/database/upgrade/2.6.x/data-required.sql
@@ -1,2 +1,3 @@
 -- Upgrade the system configuration
 system php ./upgrade_user_config.php
+system php ./upgrade_events.php
---
+++ campsite/implementation/database/upgrade/2.6.x/upgrade_events.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once("database_conf.php");
+require_once("install_conf.php");
+if (!is_array($Campsite)) {
+	echo "Invalid configuration file(s)";
+	exit(1);
+}
+
+$db_name = $Campsite['DATABASE_NAME'];
+$db_user = $Campsite['DATABASE_USER'];
+$db_passwd = $Campsite['DATABASE_PASSWORD'];
+$db_host = $Campsite['DATABASE_SERVER_ADDRESS'];
+$max_upload_filesize = ini_get('upload_max_filesize');
+
+if (!mysql_connect($db_host, $db_user, $db_passwd)) {
+	die("Unable to connect to the database.\n");
+}
+
+if (!mysql_select_db($db_name)) {
+	die("Unable to use the database " . $db_name . ".\n");
+}
+
+// 
+// populate the Events table
+//
+$sql = "INSERT INTO Events (Id, Name, Notify, IdLanguage) VALUES ('116','Rename Template','N','1'),('117','Move Template','N','1')";
+if (!($res = mysql_query($sql))) {
+	die("Unable to write to the database.\n");
+}
+
+?>
