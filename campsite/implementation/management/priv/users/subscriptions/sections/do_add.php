228868c7cad6c15af8520133c29c8ad186e99a90
paul <paul@952d13e4-84fd-0310-bd7c-ee2827c68691>
2006-10-10 17:48:44 +0000

    Attempt to fix #2335 - adding a subscription section gives an error.  Put in an extra check for input parameters.

--- campsite/implementation/management/priv/users/subscriptions/sections/do_add.php
+++ campsite/implementation/management/priv/users/subscriptions/sections/do_add.php
@@ -31,13 +31,17 @@ $errorMsgs = array();
 
 $new_sections = array();
 if ($f_language_set == 'all') {
-	foreach ($f_section_number as $section_number) {
-		$new_sections[$section_number][] = 0;
+	if (is_array($f_section_number)) {
+		foreach ($f_section_number as $section_number) {
+			$new_sections[$section_number][] = 0;
+		}
 	}
 } else {
-	foreach ($f_section_id as $section_id) {
-		$id = explode('_', $section_id);
-		$new_sections[$id[0]][] = $id[1];
+	if (is_array($f_section_id)) {
+		foreach ($f_section_id as $section_id) {
+			$id = explode('_', $section_id);
+			$new_sections[$id[0]][] = $id[1];
+		}
 	}
 }
 
@@ -46,10 +50,10 @@ $columns = array('StartDate' => $f_subscription_start_date,
 				 'PaidDays' => $f_subscription_days);
 foreach ($new_sections as $section_number=>$section_languages) {
 	foreach ($section_languages as $section_language) {
-		echo "<p>try $section_number:$section_language</p>\n";
+		//echo "<p>try $section_number:$section_language</p>\n";
 		$subscriptionSection =& new SubscriptionSection($f_subscription_id, $section_number, $section_language);
 		if (!$subscriptionSection->exists()) {
-			echo "<p>create $section_number:$section_language</p>\n";
+			//echo "<p>create $section_number:$section_language</p>\n";
 			$success &= $subscriptionSection->create($columns);
 		}
 	}
