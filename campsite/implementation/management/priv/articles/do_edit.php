b5e89cb06d286b11724c8b554cf62a4fced17b9d
paul <paul@952d13e4-84fd-0310-bd7c-ee2827c68691>
2006-10-10 15:46:24 +0000

    Fix for bug #2334 - if an internal link has a question mark at the end, but no query string, it produces an error.  It is only possible to get this error if you edit your HTML directly.  In this error condition, we remove the link entirely.

--- campsite/implementation/management/priv/articles/do_edit.php
+++ campsite/implementation/management/priv/articles/do_edit.php
@@ -13,6 +13,11 @@ $g_spanCounter = -1;
 global $g_internalLinkCounter;
 $g_internalLinkCounter = 0;
 
+// This is used in TransformInternalLinks() to remember the opening link
+// tag.
+global $g_internalLinkStartTag;
+$g_internalLinkStartTag = 0;
+
 /**
  * This function is a callback for preg_replace_callback();
  * It will replace <span class="campsite_subhead">...</span>
@@ -51,42 +56,63 @@ function TransformSubheads($match) {
  */
 function TransformInternalLinks($p_match) {
 	global $g_internalLinkCounter;
+	global $g_internalLinkStartTag;
+
+	// This matches '<a href="campsite_internal_link?IdPublication=1&..." ...>'
+	$internalLinkStartRegex = "/<\s*a\s*(((href\s*=\s*[\"']campsite_internal_link[?][\w&=;]*[\"'])|(target\s*=\s*['\"][_\w]*['\"]))[\s]*)*[\s\w\"']*>/i";
+
 	// This matches '</a>'
-	if (preg_match("/<\s*\/a\s*>/i", $p_match[0])) {
+	$internalLinkEndRegex = "/<\s*\/a\s*>/i";
+
+	if (preg_match($internalLinkEndRegex, $p_match[0])) {
 		// Check if we are closing an internal link
 		if ($g_internalLinkCounter > 0) {
-			// Replace the HTML tag with a template tag
-			$retval = "<!** EndLink>";
 			$g_internalLinkCounter = 0;
-			return $retval;
-		}
-		else {
+			// Make sure the starting link was not blank (a blank
+			// indicates it was a link to no where)
+			if ($g_internalLinkStartTag != "") {
+				// Replace the HTML tag with a template tag
+				$retval = "<!** EndLink>";
+				$g_internalLinkStartTag = "";
+				return $retval;
+			} else {
+				// The starting link was blank, so we return blank for the
+				// ending link.
+				return "";
+			}
+		} else {
 			// Leave the HTML tag as is (for external links).
 			return '</a>';
 		}
-	}
-	// This matches '<a href="campsite_internal_link?IdPublication=1&..." ...>'
-	elseif (preg_match("/<\s*a\s*(((href\s*=\s*[\"']campsite_internal_link[?][\w&=;]*[\"'])|(target\s*=\s*['\"][_\w]*['\"]))[\s]*)*[\s\w\"']*>/i", $p_match[0])) {
-
+	} elseif (preg_match($internalLinkStartRegex, $p_match[0])) {
 		// Get the URL
 		preg_match("/href\s*=\s*[\"'](campsite_internal_link[?][\w&=;]*)[\"']/i", $p_match[0], $url);
 		$url = isset($url[1]) ? $url[1] : '';
 		$parsedUrl = parse_url($url);
 		$parsedUrl = str_replace("&amp;", "&", $parsedUrl);
 
-		// Get the target, if there is one
-		preg_match("/target\s*=\s*[\"']([_\w]*)[\"']/i", $p_match[0], $target);
-		$target = isset($target[1]) ? $target[1] : null;
+		$retval = "";
+		// It's possible that there isnt a query string - in which case
+		// its a link to no where, so we remove it ($retval is empty
+		// string).
+		if (isset($parsedUrl["query"])) {
+			// Get the target, if there is one
+			preg_match("/target\s*=\s*[\"']([_\w]*)[\"']/i", $p_match[0], $target);
+			$target = isset($target[1]) ? $target[1] : null;
 
-		// Replace the HTML tag with a template tag
-		$retval = "<!** Link Internal ".$parsedUrl["query"];
-		if (!is_null($target)) {
-			$retval .= " TARGET ".$target;
+			// Replace the HTML tag with a template tag
+			$retval = "<!** Link Internal ".$parsedUrl["query"];
+			if (!is_null($target)) {
+				$retval .= " TARGET ".$target;
+			}
+			$retval .= ">";
 		}
-		$retval .= ">";
 
 		// Mark that we are now inside an internal link.
 		$g_internalLinkCounter = 1;
+		// Remember the starting link tag
+		$g_internalLinkStartTag = $retval;
+
 		return $retval;
 	}
 } // fn TransformInternalLinks
