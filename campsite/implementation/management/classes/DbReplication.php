af8841ded7ab569b672f628de7eafa34c137ce81
holman <holman@952d13e4-84fd-0310-bd7c-ee2827c68691>
2006-10-18 05:59:30 +0000

    ticket:2330 - cleaning

--- campsite/implementation/management/classes/DbReplication.php
+++ campsite/implementation/management/classes/DbReplication.php
@@ -13,66 +13,75 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/classes/SystemPref.php');
  * @package Campsite
  */
 class DbReplication {
-    /**
-     * Remote (Online) db server connection details.
-     */
-    var $m_rDbName = $rDbName = $Campsite['DATABASE_NAME'];
-    var $m_rDbHost = SystemPref::Get('DBReplicationHost') . ':' . SystemPref::Get('DBReplicationPort');
-    var $m_rDbUser = SystemPref::Get('DBReplicationUser');
-    var $m_rDbPass = SystemPref::Get('DBReplicationPass');
+	/**
+	 * Remote (Online) db server connection details.
+	 */
+	var $m_rDbName = '';
+	var $m_rDbHost = '';
+	var $m_rDbUser = '';
+	var $m_rDbPass = '';
 
-    /**
-     * Constructor
-     *
-     * @return void
-     */
-    function DbReplication() {}
+	/**
+	 * Constructor
+	 *
+	 * @return void
+	 */
+	function DbReplication() {}
 
-    /**
-     * Try to connect the resource based on supplied parameter.
-     *
-     * @param  string  (optional) Host/Server alias [online | local]
-     * @return bool
-     */
-    function Connect($host = 'online')
-    {
-	global $Campsite;
-	global $g_ado_db;
+	/**
+	 * Try to connect the resource based on supplied parameter.
+	 *
+	 * @param  string  (optional) Host/Server alias [online | local]
+	 * @return bool
+	 */
+	function Connect($host = 'online')
+	{
+		global $Campsite;
+		global $g_ado_db;
 
-	if ($host == 'local') {
-	    if (isset($g_ado_db) && $g_ado_db->host == $Campsite['DATABASE_SERVER_ADDRESS']) {
-		return true;
-	    } else {
+		if ($host == 'local') {
+			if (isset($g_ado_db)
+				&& $g_ado_db->host == $Campsite['DATABASE_SERVER_ADDRESS']) {
+				return true;
+			} else {
+				$g_ado_db = ADONewConnection('mysql');
+				$g_ado_db->SetFetchMode(ADODB_FETCH_ASSOC);
+				if ($g_ado_db->Connect($Campsite['DATABASE_SERVER_ADDRESS'],
+							$Campsite['DATABASE_USER'],
+							$Campsite['DATABASE_PASSWORD'],
+							$Campsite['DATABASE_NAME'])) {
+					return true;
+				} else {
+					return false;
+				}
+			}
+		}
+
+		$g_ado_db_tmp = $g_ado_db;
+
+		if (isset($g_ado_db) && $g_ado_db->host == $rDbHost) {
+			return true;
+		}
+
+		$this->m_rDbName = $Campsite['DATABASE_NAME'];
+		$this->m_rDbHost = SystemPref::Get('DBReplicationHost')
+				 . ':'
+				 . SystemPref::Get('DBReplicationPort');
+		$this->m_rDbUser = SystemPref::Get('DBReplicationUser');
+		$this->m_rDbPass = SystemPref::Get('DBReplicationPass');
+
 		$g_ado_db = ADONewConnection('mysql');
 		$g_ado_db->SetFetchMode(ADODB_FETCH_ASSOC);
-		if ($g_ado_db->Connect($Campsite['DATABASE_SERVER_ADDRESS'],
-			               $Campsite['DATABASE_USER'],
-			               $Campsite['DATABASE_PASSWORD'],
-				       $Campsite['DATABASE_NAME'])) {
-		    return true;
+		if ($g_ado_db->Connect($this->m_rDbHost,
+					$this->m_rDbUser,
+					$this->m_rDbPass,
+					$this->m_rDbName) == false) {
+			$g_ado_db = $g_ado_db_tmp;
+			return false;
 		} else {
-		    return false;
+			return true;
 		}
-	    }
-	}
-
-        $g_ado_db_tmp = $g_ado_db;
-
-	if (isset($g_ado_db) && $g_ado_db->host == $rDbHost) {
-	    return true;
-	}
-        $g_ado_db = ADONewConnection('mysql');
-        $g_ado_db->SetFetchMode(ADODB_FETCH_ASSOC);
-        if ($g_ado_db->Connect($this->m_rDbHost,
-			       $this->m_rDbUser,
-			       $this->m_rDbPass,
-			       $this->m_rDbName) == false) {
-            $g_ado_db = $g_ado_db_tmp;
-            return false;
-        } else {
-            return true;
-        }
-    } // fn Connect
+	} // fn Connect
 
 } // class DbReplication
 
