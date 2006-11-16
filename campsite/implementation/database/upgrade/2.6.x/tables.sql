21db78d26fef53bc4950b86149f90f4f8b56bc3e
holman <holman@952d13e4-84fd-0310-bd7c-ee2827c68691>
2006-11-16 07:53:51 +0000

    ticket:2326 and ticket:2327 - Added an 'order' field to ArticleAudioclips table

--- campsite/implementation/database/upgrade/2.6.x/tables.sql
+++ campsite/implementation/database/upgrade/2.6.x/tables.sql
@@ -5,5 +5,6 @@ ALTER TABLE `Sections` ADD `Description` BLOB AFTER ShortName;
 CREATE TABLE ArticleAudioclips (
     fk_article_number INT(10) UNSIGNED NOT NULL DEFAULT 0,
     fk_audioclip_gunid VARCHAR(16) NOT NULL,
+	order SMALLINT UNSIGNED NOT NULL DEFAULT 0,
     PRIMARY KEY (fk_article_number, fk_audioclip_gunid)
 );
