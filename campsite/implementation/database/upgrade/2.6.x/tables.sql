4bee109ce3f0e5ff93434fb885d14368c2508c3d
holman <holman@952d13e4-84fd-0310-bd7c-ee2827c68691>
2006-11-16 22:26:09 +0000

    rename 'order' field to 'order_no'. order is reserved word and cannot be used.

--- campsite/implementation/database/upgrade/2.6.x/tables.sql
+++ campsite/implementation/database/upgrade/2.6.x/tables.sql
@@ -1,10 +1,10 @@
 -- ticket #2220 - A new field to input section description
-ALTER TABLE `Sections` ADD `Description` BLOB AFTER ShortName;
+ALTER TABLE `Sections` ADD `Description` BLOB AFTER `ShortName`;
 
 -- create the ArticleAudioclips table
-CREATE TABLE ArticleAudioclips (
-    fk_article_number INT(10) UNSIGNED NOT NULL DEFAULT 0,
-    fk_audioclip_gunid VARCHAR(16) NOT NULL,
-	order SMALLINT UNSIGNED NOT NULL DEFAULT 0,
-    PRIMARY KEY (fk_article_number, fk_audioclip_gunid)
+CREATE TABLE `ArticleAudioclips` (
+    `fk_article_number` INT(10) UNSIGNED NOT NULL DEFAULT 0,
+    `fk_audioclip_gunid` VARCHAR(16) NOT NULL,
+	`order_no` SMALLINT UNSIGNED NOT NULL DEFAULT 0,
+    PRIMARY KEY (`fk_article_number`, `fk_audioclip_gunid`)
 );
