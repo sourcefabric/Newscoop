b210176facec60d453c69e4cd3cdb9e7af2e7a25
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2006-11-23 13:07:54 +0000

    fixed ticket:2368 - The upgrade script fails to create phorum users

--- campsite/implementation/database/upgrade/2.6.2/data-required.sql
+++ campsite/implementation/database/upgrade/2.6.2/data-required.sql
@@ -1,2 +1,5 @@
 -- add new event: to be used to log system preferences changes 
 INSERT INTO Events (Id, Name, Notify, IdLanguage) VALUES (171, 'Change system preferences', 'N', 1);
+
+-- create the corresponding phorum users for each campsite subscriber
+system php ./upgrade_phorum_users.php
