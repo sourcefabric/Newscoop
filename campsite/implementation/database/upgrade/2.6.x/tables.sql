e9402965f4a3307ea8dc5fec1296be3cf639dbe4
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2006-11-29 00:13:15 +0000

    updates for ticket:2326 - Attach Campcaster audio files to Campsite articles

--- campsite/implementation/database/upgrade/2.6.x/tables.sql
+++ campsite/implementation/database/upgrade/2.6.x/tables.sql
@@ -3,21 +3,18 @@ ALTER TABLE `Sections` ADD `Description` BLOB AFTER `ShortName`;
 
 -- create the ArticleAudioclips table
 CREATE TABLE `ArticleAudioclips` (
-    `fk_article_number` INT(10) UNSIGNED NOT NULL DEFAULT 0,
-    `fk_audioclip_gunid` BIGINT(20) UNSIGNED NOT NULL DEFAULT 0,
-	`order_no` SMALLINT UNSIGNED NOT NULL DEFAULT 0,
-    PRIMARY KEY (`fk_article_number`, `fk_audioclip_gunid`)
+  `fk_article_number` int(10) unsigned NOT NULL DEFAULT 0,
+  `fk_audioclip_gunid` varchar(20) NOT NULL DEFAULT 0,
+  `order_no` smallint unsigned NOT NULL DEFAULT 0,
+  PRIMARY KEY (`fk_article_number`, `fk_audioclip_gunid`)
 );
 
 CREATE TABLE  `AudioclipMetadata` (
   `id` bigint(20) unsigned NOT NULL auto_increment,
-  `gunid` bigint(20) unsigned NOT NULL default '0',
-  `subject_ns` varchar(255) default '',
-  `subject` varchar(255) NOT NULL default '',
+  `gunid` varchar(20) NOT NULL default '0',
   `predicate_ns` varchar(255) default '',
   `predicate` varchar(255) NOT NULL default '',
-  `predicate_xml` char(1) NOT NULL default '',
-  `object_ns` varchar(255) default '',
   `object` text,
-  PRIMARY KEY  (`id`)
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `gunid_tag_id` (`gunid`, `predicate_ns`, `predicate`)
 );
