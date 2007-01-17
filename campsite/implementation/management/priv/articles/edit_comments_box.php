975506a11f25efbed02c98220b1bdedbdf8122b6
holman <holman@952d13e4-84fd-0310-bd7c-ee2827c68691>
2007-01-17 14:35:08 +0000

    updates for ticket:2330 - Offline editing/Synchronizing Articles with Another Server - Checks replication use to show the status in the comments box of the article edit screen

--- campsite/implementation/management/priv/articles/edit_comments_box.php
+++ campsite/implementation/management/priv/articles/edit_comments_box.php
@@ -1,31 +1,43 @@
-			<TABLE width="100%" style="border: 1px solid #EEEEEE;">
-			<TR>
-				<TD>
-					<TABLE width="100%" bgcolor="#EEEEEE" cellpadding="3" cellspacing="0">
-					<TR>
-						<TD align="left">
-						<STRONG><?php putGS("Comments"); ?></STRONG>
-						</TD>
-					</TR>
-					</TABLE>
-				</TD>
-			</TR>
-            <TR>
-				<TD align="left" width="100%" style="padding-left: 8px;">
-				<?php
-				if (is_array($comments)) {
-					putGS("Total:"); ?> <?php p(count($comments));
-				?>
-				    <BR />
-				    <?php if ($f_show_comments) { ?>
-				    <A href="<?php echo camp_html_article_url($articleObj, $f_language_selected, "edit.php", "", "&f_show_comments=0"); ?>"><?php putGS("Hide Comments"); ?></A>
-				    <?php } else { ?>
-				    <A href="<?php echo camp_html_article_url($articleObj, $f_language_selected, "edit.php", "", "&f_show_comments=1"); ?>"><?php putGS("Show Comments"); ?></A>
-				    <?php }
-				} else {
-					putGS("Comments Disabled");
-				}
-				?>
-                </TD>
-            </TR>
-            </TABLE>
+			<table width="100%" style="border: 1px solid #EEEEEE;">
+			<tr>
+				<td>
+					<table width="100%" bgcolor="#EEEEEE" cellpadding="3" cellspacing="0">
+					<tr>
+						<td align="left">
+                            <strong><?php putGS("Comments"); ?></strong>
+						</td>
+					</tr>
+					</table>
+				</td>
+			</tr>
+            <tr>
+				<td align="left" width="100%" style="padding-left: 8px;">
+                <?php
+                if (SystemPref::Get("UseDBReplication") == 'Y') {
+                    if ($connectedToOnlineServer) {
+                ?>
+                    <span class="success_message">
+                <?php
+                        putGS("Online Server: On");
+                    } elseif (isset($connectedToOnlineServer)
+                              &&$connectedToOnlineServer == false) {
+                ?>
+                    <span class="failure_message">
+                <?php
+                        putGS("Online Server: Off");
+                    }
+                ?>
+                    </span><br />
+                <?php
+                }
+                ?>
+                    <?php putGS("Total:"); ?> <?php p(count($comments)); ?>
+                    <br />
+                    <?php if ($f_show_comments) { ?>
+                    <a href="<?php echo camp_html_article_url($articleObj, $f_language_selected, "edit.php", "", "&f_show_comments=0"); ?>"><?php putGS("Hide Comments"); ?></a>
+                    <?php } else { ?>
+				    <a href="<?php echo camp_html_article_url($articleObj, $f_language_selected, "edit.php", "", "&f_show_comments=1"); ?>"><?php putGS("Show Comments"); ?></a>
+				    <?php } ?>
+                </td>
+            </tr>
+            </table>
