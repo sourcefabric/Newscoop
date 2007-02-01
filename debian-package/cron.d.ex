f41f98dc0db21457a55ae8e758e5e684bbb7f1c0
holman <holman@952d13e4-84fd-0310-bd7c-ee2827c68691>
2007-02-01 05:46:28 +0000

    Needed files to build the Campsite debian package

---
+++ debian-package/cron.d.ex
@@ -0,0 +1,4 @@
+#
+# Regular cron jobs for the campsite package
+#
+0 4	* * *	root	campsite_maintenance
---
+++ debian-package/menu.ex
@@ -0,0 +1,2 @@
+?package(campsite):needs="X11|text|vc|wm" section="Apps/see-menu-manual"\
+  title="campsite" command="/usr/bin/campsite"
