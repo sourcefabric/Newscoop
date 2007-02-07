95962c04e5a58118c1cde1bb230cbcdaa8bf43a3
holman <holman@952d13e4-84fd-0310-bd7c-ee2827c68691>
2007-02-07 18:57:07 +0000

    updates for ticket:2329 - Apt-get installation and Updates

--- packaging/debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the campsite package
-#
-0 4	* * *	root	campsite_maintenance
--- packaging/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(campsite):needs="X11|text|vc|wm" section="Apps/see-menu-manual"\
-  title="campsite" command="/usr/bin/campsite"
