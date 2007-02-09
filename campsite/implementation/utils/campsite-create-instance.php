5fe27b99013188c4ea6f06bae0fa02f1630b16c5
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2007-02-09 13:19:37 +0000

    fixed ticket:2425 - Campsite and phorum users may become unsynchronized

--- campsite/implementation/utils/campsite-create-instance.php
+++ campsite/implementation/utils/campsite-create-instance.php
@@ -269,7 +269,7 @@ function camp_upgrade_database($p_db_name, $p_defined_parameters)
 
 	$first = true;
 	$versions = array("2.0.x", "2.1.x", "2.2.x", "2.3.x", "2.4.x", "2.5.x",
-					  "2.6.0", "2.6.1", "2.6.2", "2.6.3");
+					  "2.6.0", "2.6.1", "2.6.2", "2.6.3", "2.6.4");
 	foreach ($versions as $index=>$db_version) {
 		if ($old_version > $db_version) {
 			continue;
@@ -387,6 +387,13 @@ function camp_detect_database_version($p_db_name, &$version)
 					return "Unable to query the database $p_db_name";
 				}
 				if (mysql_num_rows($res2) > 0) {
+					$version = "2.6.4";
+				}
+				if (!$res2 = mysql_query("SELECT * from phorum_users "
+										 . "WHERE fk_campsite_user_id IS NULL")) {
+					return "Unable to query the database $p_db_name";
+				}
+				if (mysql_num_rows($res2) == 0) {
 					$version = "2.6.x";
 				}
 			}
