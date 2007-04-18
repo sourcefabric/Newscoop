f89d18a318f2787d3999707a71a2b32953c9533e
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2007-04-18 15:26:12 +0000

    updates to ticket:425 - Replace C++ Parser with Smarty on the frontend

---
+++ campsite/implementation/site/template_engine/MetaSubscription.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * @package Campsite
+ */
+
+/**
+ * Includes
+ */
+// We indirectly reference the DOCUMENT_ROOT so we can enable
+// scripts to use this file from the command line, $_SERVER['DOCUMENT_ROOT']
+// is not defined in these cases.
+$g_documentRoot = $_SERVER['DOCUMENT_ROOT'];
+
+require_once($g_documentRoot.'/classes/Subscription.php');
+require_once($g_documentRoot.'/classes/Language.php');
+require_once($g_documentRoot.'/template_engine/MetaDbObject.php');
+require_once($g_documentRoot.'/template_engine/CampTemplate.php');
+
+/**
+ * @package Campsite
+ */
+final class MetaSubscription extends MetaDbObject {
+
+	private function InitProperties()
+	{
+		if (!is_null($this->m_properties)) {
+			return;
+		}
+		$this->m_properties['currency'] = 'Currency';
+	}
+
+
+    public function __construct($p_subscriptionId)
+    {
+        $subscriptionObj = new Subscription($p_subscriptionId);
+		if (!is_object($subscriptionObj) || !$subscriptionObj->exists()) {
+			return false;
+		}
+        $this->m_dbObject =& $subscriptionObj;
+
+		$this->InitProperties();
+		$this->m_customProperties['type'] = 'getType';
+		$this->m_customProperties['expiration_date'] = 'getExpirationDate';
+		$this->m_customProperties['active'] = 'isActive';
+        $this->m_customProperties['defined'] = 'defined';
+    } // fn __construct
+
+
+    public function getType()
+    {
+    	$type = $this->m_dbObject->getType();
+    	return $type == 'T' ? 'trial' : 'paid';
+    }
+
+
+    public function getExpirationDate()
+    {
+    	return '';
+    }
+
+
+    public function isActive()
+    {
+    	return $this->m_dbObject->isActive();
+    }
+
+} // class MetaTopic
+
+?>
