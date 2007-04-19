6e5b9398f7d3ff698dfa23b959899b79e145c9ef
holman <holman@952d13e4-84fd-0310-bd7c-ee2827c68691>
2007-04-19 04:35:10 +0000

    updates to ticket:425 - Replace C++ Parser with Smarty on the frontend

--- campsite/implementation/site/template_engine/CampContext.php
+++ campsite/implementation/site/template_engine/CampContext.php
@@ -11,38 +11,107 @@
 // is not defined in these cases.
 $g_documentRoot = $_SERVER['DOCUMENT_ROOT'];
 
+// Meta classes
+require_once($_SERVER['DOCUMENT_ROOT'].'/template_engine/MetaLanguage.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/template_engine/MetaPublication.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/template_engine/MetaIssue.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/template_engine/MetaSection.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/template_engine/MetaArticle.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/template_engine/MetaImage.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/template_engine/MetaAttachment.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/template_engine/MetaAudioclip.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/template_engine/MetaComment.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/template_engine/MetaTopic.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/template_engine/MetaUser.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/template_engine/MetaTemplate.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/template_engine/MetaSubscription.php');
+
+define('INVALID_OBJECT_STRING', 'invalid object');
+
 
 /**
  * @package Campsite
  */
 final class CampContext {
     //
-    private $m_articleId = null;
-    //
-    private $m_publicationId = null;
-    //
-    private $m_issueNr = null;
-    //
-    private $m_sectionNr = null;
+    private $m_objects = array('publication',
+                               'issue',
+                               'section',
+                               'article',
+                               'language',
+                               'image',
+                               'attachment',
+                               'audioclip',
+                               'comment',
+                               'topic',
+                               'user',
+                               'template',
+                               'subscription'
+                               );
 
 
-    public function __construct() {} // fn __construct
-
-
-    public function setArticleId($p_articleId)
+    /**
+     *
+     */
+    public function __construct()
     {
-        if (empty($p_articleId)) {
+
+    } // fn __construct
+
+
+    /**
+     *
+     */
+    public function __get($p_object)
+    {
+        try {
+            return $this->getObject($p_object);
+        } catch (InvalidObjectException $e) {
+            $this->trigger_invalid_object_error($p_object);
+            return null;
+        }
+    } // fn __get
+
+
+    /**
+     *
+     */
+    public function __set($p_object, $p_value)
+    {
+        if (!in_array($p_object, $this->m_objects)
+                || !is_object($p_value)
+                || !$p_value->defined()) {
             return false;
         }
 
-        $this->m_articleId = $p_articleId;
-    }
+        $this->$p_object = $p_value;
+    } // fn __set
 
 
-    function getArticleId()
+    /**
+     *
+     */
+    protected function getObject($p_object)
     {
-        return $this->m_articleId;
-    }
+        if (!is_array($this->m_objects)
+            || !in_array($p_object, $this->m_objects)) {
+            throw new InvalidObjectException($p_object);
+        }
+        if (!is_object($this->$p_object)) {
+            return null;
+        }
+
+        return $this->$p_object;
+    } // fn getObject
+
+
+    /**
+     *
+     */
+    final protected function trigger_invalid_object_error($p_object)
+    {
+		CampTemplate::singleton()->trigger_error(INVALID_OBJECT_STRING . " $p_object ");
+    } // fn trigger_invalid_object_error
 
 } // class CampContext
 
