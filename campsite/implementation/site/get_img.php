29664f026347beb5677c63996c21d701939d7145
holman <holman@952d13e4-84fd-0310-bd7c-ee2827c68691>
2007-11-26 03:20:33 +0000

    updates to ticket:425 - Replace C++ Parser with Smarty on the frontend - get_image rewritten

---
+++ campsite/implementation/site/get_img.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @package Campsite
+ *
+ * @author Holman Romero <holman.romero@gmail.com>
+ * @copyright 2007 MDLF, Inc.
+ * @license http://www.gnu.org/licenses/gpl.txt
+ * @version $Revision$
+ * @link http://www.campware.org
+ */
+
+/**
+ * Includes
+ *
+ * We indirectly reference the DOCUMENT_ROOT so we can enable
+ * scripts to use this file from the command line, $_SERVER['DOCUMENT_ROOT']
+ * is not defined in these cases.
+ */
+$g_documentRoot = $_SERVER['DOCUMENT_ROOT'];
+
+require_once($g_documentRoot.'/template_engine/classes/CampRequest.php');
+require_once($g_documentRoot.'/template_engine/classes/CampGetImage.php');
+
+// reads parameters from image link URI
+$articleNr = CampRequest::GetVar('NrArticle', null, 'get');
+$imageNr = CampRequest::GetVar('NrImage', null, 'get');
+
+$showImage = new CampGetImage($imageNr, $articleNr);
+
+?>
