a1c781d8849db1bc40a3111b8b2cf9da944446fa
holman <holman@952d13e4-84fd-0310-bd7c-ee2827c68691>
2008-02-27 22:52:43 +0000

    updates to ticket:425 - Replace C++ Parser with Smarty on the frontend - unset_article_comment smarty function

---
+++ campsite/implementation/site/smarty_camp_plugins/function.unset_article_comment.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Campsite customized Smarty plugin
+ * @package Campsite
+ */
+
+/**
+ * Campsite unset_article_comment function plugin
+ *
+ * Type:     function
+ * Name:     unset_article_comment
+ * Purpose:
+ *
+ * @param empty
+ *     $p_params
+ * @param object
+ *     $p_smarty The Smarty object
+ */
+function smarty_function_unset_article_comment($p_params, &$p_smarty)
+{
+    // gets the context variable
+    $campsite = $p_smarty->get_template_vars('campsite');
+    if (!is_object($campsite->comment)
+            || !$campsite->comment->defined) {
+        return;
+    }
+
+    $campsite->comment = new MetaComment();
+
+} // fn smarty_function_unset_article_comment
+
+?>
