67e66275c7d4bb1529f667ed4a1f8590a33c46da
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2008-03-21 21:30:39 +0000

    updates to ticket:425 - Replace C++ Parser with Smarty on the frontend - fixed error messages in Articles list, added new error "invalid operator"

--- campsite/implementation/site/admin-files/lang/es/preview.php
+++ campsite/implementation/site/admin-files/lang/es/preview.php
@@ -1,10 +1,11 @@
 <?php 
-regGS(SYNTAX_ERROR_UNKNOWN, "Error desconocido: <em>$1</em>");
-regGS(SYNTAX_ERROR_UNRECOGNIZED_TAG, "Tag <em>$1</em> no reconocida");
-regGS(SYNTAX_ERROR_UNKNOWN_REFERENCE, "Referencia desconocida <em>$1</em>");
-regGS(SYNTAX_ERROR_INVALID_PROPERTY, "Propiedad invalida <em>$1</em> del objeto <em>$2</em>");
-regGS(SYNTAX_ERROR_INVALID_PROPERTY_VALUE, "Valor invalido <em>$1</em> de la propiedad <em>$2</em> del objeto <em>$3</em>");
-regGS(SYNTAX_ERROR_INVALID_PARAMETER, "Parametro invalido <em>$1</em> en la sentencia <em>$2</em>");
-regGS(SYNTAX_ERROR_INVALID_PARAMETER_VALUE, "Valor invalido <em>$1</em> del parametro <em>$2</em> en la sentencia <em>$3</em>");
-regGS(SYNTAX_ERROR_MISSING_PARAMETER, "Parametro <em>$1</em> ausente en la sentencia <em>$2</em>");
+regGS(SYNTAX_ERROR_UNKNOWN, "Unknown error: <em>$1</em>");
+regGS(SYNTAX_ERROR_UNRECOGNIZED_TAG, "Unrecognized tag <em>$1</em>");
+regGS(SYNTAX_ERROR_UNKNOWN_REFERENCE, "Unknown reference <em>$1</em>");
+regGS(SYNTAX_ERROR_INVALID_PROPERTY, "Invalid property <em>$1</em> of object <em>$2</em>");
+regGS(SYNTAX_ERROR_INVALID_PROPERTY_VALUE, "Invalid value <em>$1</em> of property <em>$2</em> of object <em>$3</em>");
+regGS(SYNTAX_ERROR_INVALID_PARAMETER, "Invalid parameter <em>$1</em> in statement <em>$2</em>");
+regGS(SYNTAX_ERROR_INVALID_PARAMETER_VALUE, "Invalid value <em>$1</em> of parameter <em>$2</em> in statement <em>$3</em>");
+regGS(SYNTAX_ERROR_MISSING_PARAMETER, "Missing parameter <em>$1</em> in statement <em>$2</em>");
+regGS(SYNTAX_ERROR_INVALID_OPERATOR, "Invalid operator <em>$1</em> of parameter <em>$2</em> in statement <em>$3</em>");
 ?>
--- campsite/implementation/site/template_engine/classes/SyntaxError.php
+++ campsite/implementation/site/template_engine/classes/SyntaxError.php
@@ -15,8 +15,9 @@ define('SYNTAX_ERROR_UNKNOWN_REFERENCE', 'Unknown reference <em>$1</em>');
 define('SYNTAX_ERROR_INVALID_PROPERTY', 'Invalid property <em>$1</em> of object <em>$2</em>');
 define('SYNTAX_ERROR_INVALID_PROPERTY_VALUE', 'Invalid value <em>$1</em> of property <em>$2</em> of object <em>$3</em>');
 define('SYNTAX_ERROR_INVALID_PARAMETER', 'Invalid parameter <em>$1</em> in statement <em>$2</em>');
-define('SYNTAX_ERROR_INVALID_PARAMETER_VALUE', 'Invalid parameter value <em>$1</em> of parameter <em>$2</em> in statement <em>$3</em>');
+define('SYNTAX_ERROR_INVALID_PARAMETER_VALUE', 'Invalid value <em>$1</em> of parameter <em>$2</em> in statement <em>$3</em>');
 define('SYNTAX_ERROR_MISSING_PARAMETER', 'Missing parameter <em>$1</em> in statement <em>$2</em>');
+define('SYNTAX_ERROR_INVALID_OPERATOR', 'Invalid operator <em>$1</em> of parameter <em>$2</em> in statement <em>$3</em>');
 
 //getGS("Unknown error: <em>$1</em>");
 //getGS("Unrecognized tag <em>$1</em>");
