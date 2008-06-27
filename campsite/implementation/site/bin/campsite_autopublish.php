b7b8fb7a2627d31f2eaf9bf115aa70fe73658534
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2008-06-27 15:01:11 +0000

    fixes for autopublish feature

--- campsite/implementation/site/bin/campsite_autopublish.php
+++ campsite/implementation/site/bin/campsite_autopublish.php
@@ -1,8 +1,9 @@
+#!/usr/bin/php
 <?php
 
-$www_dir = $GLOBALS['argv'][1];
-$www_dir = $www_dir . '/html';
+$www_dir = dirname(dirname(__FILE__));
 $_SERVER['DOCUMENT_ROOT'] = $www_dir;
+require_once($www_dir.'/conf/configuration.php');
 require_once($www_dir.'/db_connect.php');
 require_once($www_dir.'/classes/ArticlePublish.php');
 require_once($www_dir.'/classes/IssuePublish.php');
