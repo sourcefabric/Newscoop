d439c466a6cbec04beb81dffed0b2123c45e9a5a
holman <holman@952d13e4-84fd-0310-bd7c-ee2827c68691>
2008-07-28 06:07:05 +0000

    update to ticket:2602 - Password strength meter and automatic strong password generator

--- campsite/implementation/site/admin-files/users/info.php
+++ campsite/implementation/site/admin-files/users/info.php
@@ -38,6 +38,7 @@ $my_user_type = $editUser->getUserType();
 
 ?>
 <script type="text/javascript" src="<?php echo $Campsite['WEBSITE_URL']; ?>/javascript/campsite.js"></script>
+<script type="text/javascript" src="<?php echo $Campsite['WEBSITE_URL']; ?>/javascript/pwd_strength.js"></script>
 
 <form name="user_add" method="POST" action="<?php echo $action; ?>" onsubmit="return <?php camp_html_fvalidate(); ?>;">
 <input type="hidden" name="uType" value="<?php echo $uType; ?>">
@@ -51,27 +52,31 @@ if (!$isNewUser) {
 <table border="0" cellspacing="0" align="left" class="table_input" width="600px">
 <tr>
 	<td align="left">
-		<table border="0" cellspacing="0" cellpadding="3" align="left">
+		<table border="0" cellspacing="0" cellpadding="3" align="left" width="100%">
 			<tr>
-				<td align="right" nowrap><?php putGS("Account name"); ?>:</td>
+				<td width="20%" align="right"><?php putGS("Account name"); ?>:</td>
 <?php
 if (!$isNewUser) {
 ?>
-				<td align="left" nowrap><b><?php p(htmlspecialchars($editUser->getUserName())); ?></b></td>
+				<td colspan="2" align="left" nowrap><b><?php p(htmlspecialchars($editUser->getUserName())); ?></b></td>
 <?php
 } else {
 ?>
-				<td><input type="text" class="input_text" name="UName" size="32" maxlength="32" value="<?php p(htmlspecialchars($UName)); ?>" alt="blank" emsg="<?php putGS("You must complete the $1 field.", "Account name"); ?>"></td>
+				<td colspan="2"><input type="text" class="input_text" name="UName" size="32" maxlength="32" value="<?php p(htmlspecialchars($UName)); ?>" alt="blank" emsg="<?php putGS("You must complete the $1 field.", "Account name"); ?>"></td>
 			</tr>
 			<tr>
 				<td align="right"><?php putGS("Password"); ?>:</td>
-				<td>
-				<input type="password" class="input_text" name="password" size="16" maxlength="32" alt="length|6" emsg="<?php putGS("The password must be at least 6 characters long and both passwords should match."); ?>">
-				</td>
+				<td width="20%">
+				<input type="password" class="input_text" id="password" name="password" onkeyup="runPassword(this.value, 'password');" size="16" maxlength="32" alt="length|6" emsg="<?php putGS("The password must be at least 6 characters long and both passwords should match."); ?>">
+                                </td>
+                                <td>
+                                  <div id="password_text" style="font-size: 10px;"></div>
+                                  <div id="password_bar" style="font-size: 1px; height: 2px; width: 0px; border: 1px solid white;"></div>
+                                </td>
 			</tr>
 			<tr>
-				<td align="right"><?php putGS("Confirm password"); ?>:</td>
-				<td>
+				<td align="right" nowrap><?php putGS("Confirm password"); ?>:</td>
+				<td colspan="2">
 				<input type="password" class="input_text" name="passwordConf" size="16" maxlength="32" alt="length|6" emsg="<?php putGS("The confirm password must be at least 6 characters long and both passwords should match."); ?>">
 				</td>
 <?php
@@ -80,18 +85,18 @@ if (!$isNewUser) {
 			</tr>
 			<tr>
 				<td align="right" nowrap><?php putGS("Full Name"); ?>:</td>
-				<td><input type="text" class="input_text" name="Name" VALUE="<?php p(htmlspecialchars($Name)); ?>" size="32" maxlength="128" alt="blank" emsg="<?php putGS("You must complete the $1 field.", "Full Name");?>">
+				<td colspan="2"><input type="text" class="input_text" name="Name" VALUE="<?php p(htmlspecialchars($Name)); ?>" size="32" maxlength="128" alt="blank" emsg="<?php putGS("You must complete the $1 field.", "Full Name");?>">
 				</td>
 			</tr>
 			<tr>
 				<td align="right" nowrap><?php putGS("E-Mail"); ?>:</td>
-				<td>
+				<td colspan="2">
 				<input type="text" class="input_text" name="EMail" value="<?php p(htmlspecialchars($EMail)); ?>" size="32">
 				</td>
 			</tr>
 			<tr>
 				<td align="right" nowrap><?php putGS("Phone"); ?>:</td>
-				<td>
+				<td colspan="2">
 				<input type="text" class="input_text" name="Phone" value="<?php p(htmlspecialchars($Phone)); ?>" size="20">
 				</td>
 			</tr>
@@ -101,7 +106,7 @@ if (!$isNewUser) {
 			?>
 			<tr>
 				<td align="right"><?php putGS("Type"); ?>:</td>
-				<td>
+				<td colspan="2">
 				<select name="Type" class="input_select" alt="select" emsg="<?php putGS("You must select a $1", "Type"); ?>">
 				<option value=""><?php putGS("Make a selection"); ?></option>
 				<?php
@@ -149,7 +154,7 @@ if (!$isNewUser) {
 		?>
 		<tr>
 			<td align="right" nowrap width="1%"><?php putGS("Old Password"); ?>:</td>
-			<td>
+			<td colspan="2">
 			<input type="password" class="input_text" name="oldPassword" size="16" maxlength="32">
 			</td>
 		</tr>
@@ -159,14 +164,18 @@ if (!$isNewUser) {
 
 		<tr>
 			<td align="right" nowrap width="1%"><?php putGS("Password"); ?>:</td>
-			<td>
-			<input type="password" class="input_text" name="password" size="16" maxlength="32">
+			<td width="20%">
+			<input type="password" class="input_text" id="password" name="password" onkeyup="runPassword(this.value, 'password');" size="16" maxlength="32">
 			</td>
+                        <td>
+                                <div id="password_text" style="font-size: 10px;"></div>
+                                <div id="password_bar" style="font-size: 1px; height: 2px; width: 0px; border: 1px solid white;"></div>
+                        </td>
 		</tr>
 
 		<tr>
 			<td align="right" nowrap width="1%"><?php putGS("Confirm password"); ?>:</td>
-			<td>
+			<td colspan="2">
 			<input type="password" class="input_text" name="passwordConf" size="16" maxlength="32">
 			</td>
 		</tr>
