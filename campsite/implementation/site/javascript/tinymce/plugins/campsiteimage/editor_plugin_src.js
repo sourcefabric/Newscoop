2e0e516af3b1690344f580a24bd815aa0534120a
holman <holman@952d13e4-84fd-0310-bd7c-ee2827c68691>
2009-01-12 05:29:32 +0000

    update to ticket:2312 - Swap out Xinha for TinyMCE - implementation of the images manager plugin

---
+++ campsite/implementation/site/javascript/tinymce/plugins/campsiteimage/editor_plugin_src.js
@@ -0,0 +1,55 @@
+/**
+ * $Id: editor_plugin_src.js 539 2008-01-14 19:08:58Z holman $
+ *
+ * @author Campware
+ * @copyright Copyright 2008-2009, Campware - MDLF, All rights reserved.
+ */
+
+(function() {
+    tinymce.create('tinymce.plugins.CampsiteImage', {
+        init : function(ed, url) {
+            this.editor = ed;
+	    editorId = ed.id;
+	    articleNo = editorId.substring(editorId.lastIndexOf('_')+1);
+
+	    // Register commands
+	    ed.addCommand('mceCampsiteImage', function() {
+		ed.windowManager.open({
+                    file : url + '/popup.php?article_id=' + articleNo,
+		        width : 580 + parseInt(ed.getLang('campsiteimage.delta_width', 0)),
+			height : 330 + parseInt(ed.getLang('campsiteimage.delta_height', 0)),
+			inline : 1
+		    }, {
+		    plugin_url : url
+		});
+	    });
+
+	    // Register buttons
+	    ed.addButton('campsiteimage', {
+	        title : 'campsiteimage.image_desc',
+		cmd : 'mceCampsiteImage',
+		image : url + '/img/example.gif'
+	    });
+
+	    ed.addShortcut('ctrl+g', 'campsiteimage.campsiteimage_desc', 'mceCampsiteImage');
+
+	    ed.onNodeChange.add(function(ed, cm, n, co) {
+                cm.setDisabled('link', co && n.nodeName != 'A');
+		cm.setActive('link', n.nodeName == 'A' && !n.name);
+	    });
+	},
+
+	getInfo : function() {
+            return {
+		longname : 'Campsite Image',
+		author : 'Campware',
+		authorurl : 'http://www.campware.org',
+		infourl : 'http://code.campware.org/projects/campsite',
+		version : '3.2'
+	    };
+	}
+    });
+
+    // Register plugin
+    tinymce.PluginManager.add('campsiteimage', tinymce.plugins.CampsiteImage);
+})();
---
+++ campsite/implementation/site/javascript/tinymce/plugins/campsiteimage/js/campsiteimage.js
@@ -0,0 +1,49 @@
+/* Functions for the campsiteimage plugin popup */
+
+tinyMCEPopup.requireLangPack();
+
+var CampsiteImageDialog = {
+
+    init : function(ed) {
+	tinyMCEPopup.resizeToInnerSize();
+    },
+
+    insert : function() {
+	var ed = tinyMCEPopup.editor, dom = ed.dom;
+	var topDoc = window.top.document;
+
+	tinyMCEPopup.execCommand('mceInsertContent', false, dom.createHTML('img', {
+		    src : topDoc.getElementById('f_url').value,
+		    align : topDoc.getElementById('f_align').value,
+		    id : topDoc.getElementById('f_image_template_id').value,
+		    alt : ed.getLang(topDoc.getElementById('f_alt').value),
+		    title : ed.getLang(topDoc.getElementById('f_caption').value),
+		    border : 0
+	}));
+
+	tinyMCEPopup.close();
+    },
+
+    select : function(p_image_template_id, p_filename, p_alt) {
+	var topDoc = window.top.document;
+
+	var obj = topDoc.getElementById('f_image_template_id');
+	obj.value = p_image_template_id;
+
+	var obj = topDoc.getElementById('f_url');
+	obj.value = p_filename;
+
+	var obj = topDoc.getElementById('f_alt');
+	obj.value = p_alt;
+
+	var obj = topDoc.getElementById('f_caption');
+	obj.value = p_alt;
+    },
+
+    close : function() {
+	tinyMCEPopup.close();
+    }
+};
+
+// While loading
+tinyMCEPopup.onInit.add(init);
