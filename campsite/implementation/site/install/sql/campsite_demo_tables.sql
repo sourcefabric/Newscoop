86fab6b1e86406f2936506cda410af8262d6b5e3
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2009-01-16 22:08:56 +0000

    demo database scripts updates for ticket:2538 - Advanced search

--- campsite/implementation/site/install/sql/campsite_demo_tables.sql
+++ campsite/implementation/site/install/sql/campsite_demo_tables.sql
@@ -74,4 +74,4 @@ SET character_set_client = @saved_cs_client;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2009-01-15 20:35:44
+-- Dump completed on 2009-01-16 22:05:13
