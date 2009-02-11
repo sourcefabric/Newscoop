97dd524bba1773a17087ab3b8bf9f9df02aa42b2
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2009-02-11 09:15:25 +0000

    updates to ticket:689 - Make TinyMCE status bar (where it prints current breadcrumb of html structure) toggable via permissions

--- campsite/implementation/site/install/sql/upgrade/3.1.x/data-required.sql
+++ campsite/implementation/site/install/sql/upgrade/3.1.x/data-required.sql
@@ -1,5 +1,7 @@
 -- add new log event for article editing
 INSERT INTO Events (Id, Name, Notify, IdLanguage) VALUES (37, 'Edit article content', 'N', 1);
 
+INSERT INTO `liveuser_rights` VALUES (93, 0, 'EditorStatusBar', 1);
+
 -- do not set the article author to the article owner 
 -- system php ./update_article_authors.php
