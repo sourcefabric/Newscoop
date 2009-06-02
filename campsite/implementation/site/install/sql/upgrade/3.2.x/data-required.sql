c260aef7a96664d5171bc994de6946f011a74729
holman <holman@952d13e4-84fd-0310-bd7c-ee2827c68691>
2009-06-02 14:17:35 +0000

    fix to ticket:2847 - Preference setting for images resizing inserted via TinyMCE

--- campsite/implementation/site/install/sql/upgrade/3.2.x/data-required.sql
+++ campsite/implementation/site/install/sql/upgrade/3.2.x/data-required.sql
@@ -1,2 +1,3 @@
--- add new log event for article editing
+-- add new log event for article editing and tinymce editor image shrinking ratio
 INSERT INTO SystemPreferences (id, varname, value) VALUES (26, 'CacheEngine', 'APC');
+INSERT INTO SystemPreferences (id, varname, value) VALUES (27, 'EditorImageRatio', '100');
