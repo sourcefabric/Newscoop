c3605cbce64fc48ce6cad46ef32f4d9d1d697041
sebastian <sebastian@952d13e4-84fd-0310-bd7c-ee2827c68691>
2009-06-22 17:05:13 +0000

    #2846 Improve images derivates chache

--- campsite/implementation/site/install/sql/upgrade/3.2.x/data-required.sql
+++ campsite/implementation/site/install/sql/upgrade/3.2.x/data-required.sql
@@ -3,6 +3,7 @@ INSERT INTO SystemPreferences (`varname`, `value`) VALUES ('CacheEngine', 'APC')
 INSERT INTO SystemPreferences (`varname`, `value`) VALUES ('EditorImageRatio', '100');
 -- add/enable default template filters
 INSERT INTO `SystemPreferences` (`varname`, `value`) VALUES ('TemplateFilter', '.*, CVS');
+INSERT INTO `SystemPreferences` (`varname`, `value`) VALUES ('ImagecacheLifetime', '86400');
 -- add new log events for article files
 INSERT INTO `Events` (`Id`, `Name`, `Notify`, `IdLanguage`) VALUES (38, 'Add file to article', 'N', 1);
 INSERT INTO `Events` (`Id`, `Name`, `Notify`, `IdLanguage`) VALUES (39, 'Delete file from article', 'N', 1);
