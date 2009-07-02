14396215b49fa0c706dc8beef56a66d9db9da88c
sebastian <sebastian@952d13e4-84fd-0310-bd7c-ee2827c68691>
2009-07-02 14:21:52 +0000

    #2923 Editarea template editor mess up lines on syntax highlighting

--- campsite/implementation/site/admin-files/templates/edit_template.php
+++ campsite/implementation/site/admin-files/templates/edit_template.php
@@ -96,9 +96,9 @@ if (in_array($extension, $imageExtensions)) {
       <script language="javascript" type="text/javascript">
       editAreaLoader.init({
       	id : "cField"		          // textarea id
-      	,syntax: "html"			      // syntax to be used for highgliting
+      	,syntax: "html"			  // syntax to be used for highgliting
       	,start_highlight: true		  // to display with highlight mode on start-up
-      	,toolbar: "search, go_to_line, |, undo, redo, |, select_font, |, syntax_selection, |, change_smooth_selection, highlight, reset_highlight, |, help"
+      	,toolbar: "search, go_to_line, |, undo, redo, |, select_font, |, syntax_selection, highlight, reset_highlight, |, change_smooth_selection, fullscreen, |, help"
         ,syntax_selection_allow: "css,html,js,php,xml"
         ,replace_tab_by_spaces: 2
       });
