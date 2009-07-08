bed982f70da0dddd65538a9528b63d7aa5535f86
holman <holman@952d13e4-84fd-0310-bd7c-ee2827c68691>
2009-07-08 02:44:18 +0000

    fix to ticket:2876 - XML import does not support empty fields

--- campsite/implementation/site/admin-files/articles/la_import.php
+++ campsite/implementation/site/admin-files/articles/la_import.php
@@ -174,17 +174,21 @@ if ($isValidXMLFile) {
 	}
 
 	// Updates the article author
-	$authorObj = new Author((string) $article->author);
-	if (!$authorObj->exists()) {
-	    $authorData = Author::ReadName((string) $article->author);
-	    $authorObj->create($authorData);
+	if (isset($article->author) && !empty($article->author)) {
+	    $authorObj = new Author((string) $article->author);
+	    if (!$authorObj->exists()) {
+	        $authorData = Author::ReadName((string) $article->author);
+		$authorObj->create($authorData);
+	    }
+	    $articleObj->setAuthorId($authorObj->getId());
 	}
-	$articleObj->setAuthorId($authorObj->getId());
+	$articleFields['author'] = true;
 
 	// Updates the article
 	$articleObj->setCreatorId($g_user->getUserId());
-	$articleObj->setKeywords((string) $article->keywords);
-	$articleFields['author'] = true;
+	if (isset($article->keywords) && !empty($article->keywords)) {
+	    $articleObj->setKeywords((string) $article->keywords);
+	}
 	$articleFields['keywords'] = true;
 
 	foreach($xmlArticle as $articleFieldName => $articleFieldValue) {
