c367a1443054a0a6b1ebd91c2a1237ad0d7a7f87
holman <holman@952d13e4-84fd-0310-bd7c-ee2827c68691>
2009-08-17 04:30:48 +0000

    implmentation of ticket:2947 - campsite-backup should use --skip-extended-insert

--- campsite/implementation/site/bin/cli_script_lib.php
+++ campsite/implementation/site/bin/cli_script_lib.php
@@ -274,7 +274,7 @@ function camp_backup_database($p_dbName, $p_destFile, &$p_output,
 
 	$user = $Campsite['DATABASE_USER'];
 	$password = $Campsite['DATABASE_PASSWORD'];
-	$cmd = "mysqldump --add-drop-table -c -e -Q --user=$user --host="
+	$cmd = "mysqldump --add-drop-table -c -Q --skip-extended-insert --user=$user --host="
 		. $Campsite['DATABASE_SERVER_ADDRESS']
 		. " --port=" . $Campsite['DATABASE_SERVER_PORT'];
 	if ($password != "") {
