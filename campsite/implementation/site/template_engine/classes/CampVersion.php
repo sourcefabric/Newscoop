e24cf42945277ab0bbc7d705922ff36cda047352
holman <holman@952d13e4-84fd-0310-bd7c-ee2827c68691>
2009-08-18 15:34:29 +0000

    Updated info files for 3.3.2 release

--- campsite/implementation/site/template_engine/classes/CampVersion.php
+++ campsite/implementation/site/template_engine/classes/CampVersion.php
@@ -32,7 +32,7 @@ final class CampVersion
     /**
      * @var string
      */
-    private $m_devStatus = 'rc2';
+    private $m_devStatus = '';
 
     /**
      * @var string
@@ -42,7 +42,7 @@ final class CampVersion
     /**
      * @var string
      */
-    private $m_releaseDate = '2009-08-01';
+    private $m_releaseDate = '2009-08-18';
 
     /**
      * @var string
