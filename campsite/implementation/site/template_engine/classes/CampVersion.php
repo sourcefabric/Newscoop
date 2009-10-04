aee0238670751fa2347d76e518deee0ad397044c
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2009-10-04 15:32:24 +0000

    updated version and changelog for 3.3.3-rc1

--- campsite/implementation/site/template_engine/classes/CampVersion.php
+++ campsite/implementation/site/template_engine/classes/CampVersion.php
@@ -32,7 +32,7 @@ final class CampVersion
     /**
      * @var string
      */
-    private $m_devStatus = '';
+    private $m_devStatus = 'rc1';
 
     /**
      * @var string
@@ -42,7 +42,7 @@ final class CampVersion
     /**
      * @var string
      */
-    private $m_releaseDate = '2009-10-01';
+    private $m_releaseDate = '2009-10-04';
 
     /**
      * @var string
