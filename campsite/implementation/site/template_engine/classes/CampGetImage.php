cd70d4ffc5a338beef3d05b84e89758d8977d0bc
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2009-12-07 09:47:49 +0000

    fixed ticket:3058 - Notice message breaks image display

--- campsite/implementation/site/template_engine/classes/CampGetImage.php
+++ campsite/implementation/site/template_engine/classes/CampGetImage.php
@@ -139,6 +139,7 @@ class CampGetImage
     public function getTargetPath()
     {
         $fetched = !$this->m_isLocal ? $this->m_fetch_dir : null;
+        $derivates = null;
         if ($this->m_ratio < 100) {
             $derivates = $this->m_derivates_dir.$this->m_ratio.'/';    
         } 
