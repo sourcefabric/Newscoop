02d036076680edfaebb6a98bcf4d64b33e2b1241
holman <holman@952d13e4-84fd-0310-bd7c-ee2827c68691>
2009-12-28 14:12:55 +0000

    update to ticket:3054 - Enable Firefox spell checkers to check article fields by default

--- campsite/implementation/site/install/sql/upgrade/3.2.x/data-required.sql
+++ campsite/implementation/site/install/sql/upgrade/3.2.x/data-required.sql
@@ -4,6 +4,11 @@ INSERT INTO SystemPreferences (`varname`, `value`) VALUES ('EditorImageRatio', '
 -- add/enable default template filters
 INSERT INTO `SystemPreferences` (`varname`, `value`) VALUES ('TemplateFilter', '.*, CVS');
 INSERT INTO `SystemPreferences` (`varname`, `value`) VALUES ('ImagecacheLifetime', '86400');
+-- add right to enable editor spellchecker --
+DELETE FROM `liveuser_rights_right_id_seq`;
+INSERT INTO `liveuser_rights_right_id_seq` SELECT MAX(right_id) AS id FROM `liveuser_rights`;
+INSERT INTO `liveuser_rights` VALUES ((SELECT id + 1 FROM `liveuser_rights_right_id_seq`), 0, 'EditorSpellcheckerEnabled', 1);
+UPDATE `liveuser_rights_right_id_seq` set id = id + 1;
 -- add new log events for article files
 INSERT INTO `Events` (`Id`, `Name`, `Notify`, `IdLanguage`) VALUES (38, 'Add file to article', 'N', 1);
 INSERT INTO `Events` (`Id`, `Name`, `Notify`, `IdLanguage`) VALUES (39, 'Delete file from article', 'N', 1);
