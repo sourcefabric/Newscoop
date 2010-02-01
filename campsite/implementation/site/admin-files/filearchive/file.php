487c60cbd86f4389c9c74339cd458562c0729084
holman <holman@952d13e4-84fd-0310-bd7c-ee2827c68691>
2010-02-01 15:27:58 +0000

    updates to ticket:2899 - Unify and improve handling of all media (images, videos, audio, attachments) - file edit screen shows up right after file is uploaded

--- campsite/implementation/site/admin-files/filearchive/file.php
+++ campsite/implementation/site/admin-files/filearchive/file.php
@@ -49,7 +49,7 @@ if (PEAR::isError($resp)) {
 <div id="camp-message"></div>
 <p>
 <div id="form_upload">
-  <form name="file_upload" id="file_upload" method="post" action="#" enctype="multipart/form-data">
+  <form name="file_upload" id="file_upload" method="post" action="yui-assets/upload.php" enctype="multipart/form-data">
   <fieldset id="pushbuttonsfrommarkup" class="yui-skin-sam">
   <table border="0" cellspacing="0" cellpadding="6" class="table_input">
   <tr>
@@ -74,65 +74,13 @@ if (PEAR::isError($resp)) {
   <tr>
     <td colspan="2" align="center">
       <input type="button" name="uploadButton" id="uploadButton" value="<?php putGS('Upload'); ?>" class="button" />
+      <input type="submit" name="sButton" id="sButton" value="<?php putGS('Submit'); ?>" class="button" />
     </td>
   </tr>
   </table>
   </fieldset>
   </form>
 </div>
-
-<div id="form_mdata">
-  <p>
-  <table border="0" cellspacing="0" cellpadding="6" class="table_input">
-  <tr>
-    <td colspan="2">
-      <b><?php putGS('File info'); ?></b>
-      <hr noshade size="1" color="black">
-    </td>
-  </tr>
-  <tr>
-    <td colspan="2">
-      <table cellspacing="0" cellpadding="1">
-      <tr>
-        <td align="right"><strong><?php putGS('Title'); ?>:&nbsp;</strong> </td>
-        <td align="left"><div id="file-title"></div></td>
-      </tr>
-      <tr>
-        <td align="right"><strong><?php putGS('Format'); ?>:&nbsp;</strong> </td>
-        <td align="left"><div id="file-type"></div></td>
-      </tr>
-      <tr>
-        <td align="right"><strong><?php putGS('Size'); ?>:&nbsp;</strong> </td>
-        <td align="left"><div id="file-size"></div></td>
-      </tr>
-      </table>
-    </td>
-  </tr>
-  <form name="file_mdata" method="POST" action="#" enctype="multipart/form-data" onsubmit="return checkAddForm(this);">
-  <fieldset id="pushbuttonsfrommarkup" class="yui-skin-sam">
-  <tr>
-    <td colspan="2">
-      <b><?php putGS('File metadata'); ?></b>
-      <hr noshade size="1" color="black">
-    </td>
-  </tr>
-  <tr>
-    <td>
-      <table id="mdata_fields">
-
-      </table>
-    </td>
-  </tr>
-  <tr>
-    <td colspan="2" align="center">
-      <input type="button" name="editButton" id="editButton" value="<?php putGS('Save'); ?>" class="button" />
-    </td>
-  </tr>
-  </form>
-  </table>
-</div>
-
-
 
 <script>
 document.forms.file_upload.f_file_name.focus();
@@ -143,88 +91,64 @@ document.forms.file_upload.f_file_name.focus();
 YAHOO.namespace("camp.container");
 
 var mesg = document.getElementById('camp-message');
-var fdesc = document.getElementById('file-title');
-var ftype = document.getElementById('file-type');
-var fsize = document.getElementById('file-size');
-
-YAHOO.util.Event.onDOMReady(function () {
-    YAHOO.camp.container.form_upload = new YAHOO.widget.Module("form_upload");
-    YAHOO.camp.container.form_upload.render();
-    YAHOO.camp.container.form_mdata = new YAHOO.widget.Module("form_mdata", { visible: false });
-    YAHOO.camp.container.form_mdata.render();
-});
-
 var sUrl = '/admin/filearchive/yui-assets/upload.php';
 
 function init() {
-    var onUploadButtonClick = function(e){
+    var onUploadButtonClick = function(e) {
         YAHOO.util.Connect.setForm('file_upload', true);
 
-	var uploadHandler = {
-	    upload: function(o) {
-	        var json = o.responseText.substring(o.responseText.indexOf('{'), o.responseText.lastIndexOf('}') + 1);
-		//alert(json);
-		var data = eval('(' + json + ')');
-		//alert(data);
-		mesg.style.display = 'inline';
-		if (data.Results.success == false) {
-		    mesg.style.color = 'red';
-		    if (data.Results.camp_error != undefined) {
-		        mesg.innerHTML = '<?php putGS("Error"); ?>' + ': '
-			    + data.Results.camp_error;
-		    }
-		    YAHOO.camp.container.wait.hide();
-		    return false;
-		}
+        var uploadHandler = {
+            upload: function(o) {
+                var json = o.responseText.substring(o.responseText.indexOf('{'), o.responseText.lastIndexOf('}') + 1);
+                var data = eval('(' + json + ')');
+                mesg.style.display = 'inline';
+                if (data.Results.success == false) {
+                    mesg.style.color = 'red';
+                    if (data.Results.camp_error != undefined) {
+                        mesg.innerHTML = '<?php putGS("Error"); ?>' + ': '
+                            + data.Results.camp_error;
+                    }
+                    YAHOO.camp.container.wait.hide();
+                    return false;
+                }
 
-		// now we are pretty sure upload went ok
-		mesg.style.color = 'green';
-		mesg.innerHTML = '<?php putGS("File"); ?>' + ' '
-		    + data.Results.file_name + ' '
-		    + '<?php putGS("was uploaded successfully"); ?>';
-		fdesc.innerHTML = data.Results.file_desc;
-		ftype.innerHTML = data.Results.file_type;
-		fsize.innerHTML = formatBytes(data.Results.file_size);
+                // now we are pretty sure upload went ok
+                mesg.style.color = 'green';
+                mesg.innerHTML = '<?php putGS("File"); ?>' + ' '
+                    + data.Results.file_name + ' '
+                    + '<?php putGS("was uploaded successfully"); ?>';
+                YAHOO.camp.container.wait.hide();
+                window.location= '/admin/filearchive/edit.php?gunid=' + data.Results.file_gunid;
+            }
+        };
 
-		for (x in data.Results.file_mdata) {
-		    if (typeof data.Results.file_mdata[x] == 'object') {
-		        continue;
-		    }
-		    addInputTextField('mdata_fields', x, data.Results.file_mdata[x]);
-		}
-
-		YAHOO.camp.container.wait.hide();
-		YAHOO.camp.container.form_upload.hide();
-		YAHOO.camp.container.form_mdata.show();
-	    }
-	};
-
-	// Initialize the temporary Panel to display while waiting
-	// for file upload
-	YAHOO.camp.container.wait = 
+        // Initialize the temporary Panel to display while waiting
+        // for file upload
+        YAHOO.camp.container.wait =
             new YAHOO.widget.Panel("wait",
-                                   { width:"240px",
-				     fixedcenter:true,
-				     close:false,
-				     draggable:false,
-				     zindex: 4,
-				     modal:true,
-				     visible:false
-				   }
-				  );
+                    {
+                        width:"240px",
+                        fixedcenter:true,
+                        close:false,
+                        draggable:false,
+                        zindex: 4,
+                        modal:true,
+                        visible:false
+                    }
+            );
 
-	YAHOO.camp.container.wait.setHeader("<?php putGS('Uploading, please wait...'); ?>");
-	YAHOO.camp.container.wait.setBody("<img src=\"/css/rel_interstitial_loading.gif\"/>");
-	YAHOO.camp.container.wait.render(document.body);
+        YAHOO.camp.container.wait.setHeader("<?php putGS('Uploading, please wait...'); ?>");
+        YAHOO.camp.container.wait.setBody("<img src=\"/css/rel_interstitial_loading.gif\"/>");
+        YAHOO.camp.container.wait.render(document.body);
 
-	var yFileTitle = document.getElementById('f_file_title').value;
-	var postData = "file_title=" + encodeURIComponent(yFileTitle);
+        var yFileTitle = document.getElementById('f_file_title').value;
+        var postData = "file_title=" + encodeURIComponent(yFileTitle);
 
-	// Show the saving panel
-	YAHOO.camp.container.wait.show();
+        // Show the saving panel
+        YAHOO.camp.container.wait.show();
 
-	var request = YAHOO.util.Connect.asyncRequest('POST', sUrl, uploadHandler, postData);
-	setTimeout(function() { YAHOO.util.Connect.abort(request, uploadHandler) }, 30000);
+        var request = YAHOO.util.Connect.asyncRequest('POST', sUrl, uploadHandler, postData);
+        setTimeout(function() { YAHOO.util.Connect.abort(request, uploadHandler) }, 30000);
     };
 
     var onEditButtonClick = function(e){
