e63b97f0e230905adc04b06a28c398617d0d18f7
holman <holman@952d13e4-84fd-0310-bd7c-ee2827c68691>
2010-02-15 15:54:30 +0000

    fix to ticket:3076 - Alert message is not localized in editor attachments plugin

---
+++ campsite/implementation/site/javascript/tinymce/plugins/campsiteattachment/langs/es.js
@@ -0,0 +1,4 @@
+tinyMCE.addI18n('es.campsiteattachment',{
+title:"Insertar enlace a archivo adjunto del art&iacute;culo",
+select_to_link:"Debe seleccionar un texto antes de crear un enlace al archivo"
+});
--- campsite/implementation/site/javascript/tinymce/plugins/campsiteattachment/langs/sr_dlg.js
+++
@@ -1,3 +0,0 @@
-tinyMCE.addI18n('sr.campsiteattachment_dlg',{
-title:"Insert link to article file"
-});
