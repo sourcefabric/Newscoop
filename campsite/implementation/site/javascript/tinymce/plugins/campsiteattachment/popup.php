098e6e3253bcd1e210465fe83ec3804c03b516b7
holman <holman@952d13e4-84fd-0310-bd7c-ee2827c68691>
2010-02-15 16:49:48 +0000

    minor fix to ticket:3076 - Alert message is not localized in editor attachments plugin - Title for the select window

--- campsite/implementation/site/javascript/tinymce/plugins/campsiteattachment/popup.php
+++ campsite/implementation/site/javascript/tinymce/plugins/campsiteattachment/popup.php
@@ -6,7 +6,7 @@ require_once('config.inc.php');
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
 <head>
-  <title>{#campsiteattachment_dlg.title}</title>
+  <title>{#campsiteattachment.title}</title>
 
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   <link href="css/campsiteattachment.css" rel="stylesheet" type="text/css" />
