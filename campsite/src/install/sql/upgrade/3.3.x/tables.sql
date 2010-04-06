7d150ed33c2c61282693f77dd7fcd27baba1bb56
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2010-04-06 18:40:52 +0000

    implemented ticket:3098 - Make Campsite compatible with the newer, stricter MySQL version

--- campsite/src/install/sql/upgrade/3.3.x/tables.sql
+++ campsite/src/install/sql/upgrade/3.3.x/tables.sql
@@ -1,3 +1,51 @@
+-- Set default NULL for more strict MySQL versions
+ALTER TABLE `Authors` CHANGE `first_name` `first_name` varchar(100) NOT NULL DEFAULT '';
+ALTER TABLE `Authors` CHANGE `last_name` `last_name` varchar(100) NOT NULL DEFAULT '';
+
+ALTER TABLE `ObjectTypes` CHANGE `name` `name` varchar(255) NOT NULL DEFAULT '';
+
+ALTER TABLE `Plugins` CHANGE `Version` `Version` varchar(255) NOT NULL DEFAULT '';
+ALTER TABLE `Plugins` CHANGE `Enabled` `Enabled` tinyint(3) unsigned NOT NULL DEFAULT '0';
+
+ALTER TABLE `RequestObjects` CHANGE `object_type_id` `object_type_id` int(11) NOT NULL DEFAULT 0;
+ALTER TABLE `RequestObjects` CHANGE `request_count` `request_count` int(11) NOT NULL DEFAULT 0;
+
+ALTER TABLE `Requests` CHANGE `last_stats_update` `last_stats_update` datetime NOT NULL DEFAULT '0000-00-00 00:00:00';
+
+ALTER TABLE `Sessions` CHANGE `start_time` `start_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00';
+
+ALTER TABLE `Translations` CHANGE `translation_text` `translation_text` text NULL DEFAULT NULL;
+
+ALTER TABLE `URLTypes` CHANGE `Description` `Description` mediumblob NULL DEFAULT NULL;
+
+ALTER TABLE `liveuser_users` CHANGE `Interests` `Interests` MEDIUMBLOB NULL DEFAULT NULL;
+ALTER TABLE `liveuser_users` CHANGE `Improvements` `Improvements` MEDIUMBLOB NULL DEFAULT NULL;
+ALTER TABLE `liveuser_users` CHANGE `Text1` `Text1` MEDIUMBLOB NULL DEFAULT NULL;
+ALTER TABLE `liveuser_users` CHANGE `Text2` `Text2` MEDIUMBLOB NULL DEFAULT NULL;
+ALTER TABLE `liveuser_users` CHANGE `Text3` `Text3` MEDIUMBLOB NULL DEFAULT NULL;
+ALTER TABLE `liveuser_users` CHANGE `time_created` `time_created` TIMESTAMP NULL DEFAULT NULL;
+
+ALTER TABLE `phorum_files` CHANGE `file_data` `file_data` mediumtext NULL DEFAULT NULL;
+
+ALTER TABLE `phorum_forums` CHANGE `description` `description` text NULL DEFAULT NULL;
+ALTER TABLE `phorum_forums` CHANGE `template_settings` `template_settings` text NULL DEFAULT NULL;
+
+ALTER TABLE `phorum_messages` CHANGE `body` `body` text NOT NULL DEFAULT '';
+ALTER TABLE `phorum_messages` CHANGE `meta` `meta` mediumtext NOT NULL DEFAULT '';
+
+ALTER TABLE `phorum_pm_messages` CHANGE `message` `message` text NOT NULL DEFAULT '';
+
+ALTER TABLE `phorum_search` CHANGE `search_text` `search_text` mediumtext NOT NULL DEFAULT '';
+
+ALTER TABLE `phorum_settings` CHANGE `data` `data` text NULL DEFAULT NULL;
+
+ALTER TABLE `phorum_user_custom_fields` CHANGE `data` `data` text NULL DEFAULT NULL;
+
+ALTER TABLE `phorum_users` CHANGE `user_data` `user_data` TEXT NULL DEFAULT NULL;
+ALTER TABLE `phorum_users` CHANGE `signature` `signature` TEXT NULL DEFAULT NULL;
+ALTER TABLE `phorum_users` CHANGE `moderator_data` `moderator_data` TEXT NULL DEFAULT NULL;
+
+
 -- new columns for month and weekday short names
 ALTER TABLE `Languages` ADD COLUMN `ShortMonth1` VARCHAR(20);
 ALTER TABLE `Languages` ADD COLUMN `ShortMonth2` VARCHAR(20);
