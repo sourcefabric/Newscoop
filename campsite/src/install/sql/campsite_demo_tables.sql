aa390faf9f0a293c3002a509cdb2cc77053a0479
Mugur Rus <mugur@orion.(none)>
2010-07-07 14:02:32 +0300

    updates to issue CS-2145 - Update the default demo package (templates and database)
    Create the plugins tables in the tables script.

--- campsite/src/install/sql/campsite_demo_tables.sql
+++ campsite/src/install/sql/campsite_demo_tables.sql
@@ -31,6 +31,299 @@ CREATE TABLE `Xnews_article` (
   PRIMARY KEY (`NrArticle`,`IdLanguage`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `plugin_blog_blog`
+--
+
+DROP TABLE IF EXISTS `plugin_blog_blog`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `plugin_blog_blog` (
+  `blog_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `fk_language_id` int(10) unsigned NOT NULL,
+  `fk_user_id` int(10) unsigned NOT NULL DEFAULT '0',
+  `title` varchar(255) NOT NULL,
+  `date` datetime NOT NULL,
+  `info` text NOT NULL,
+  `admin_remark` text NOT NULL,
+  `request_text` text NOT NULL,
+  `status` enum('online','offline','moderated') NOT NULL DEFAULT 'online',
+  `admin_status` enum('online','offline','moderated','readonly','pending') NOT NULL DEFAULT 'pending',
+  `entries_online` int(10) unsigned NOT NULL DEFAULT '0',
+  `entries_offline` int(10) unsigned NOT NULL DEFAULT '0',
+  `comments_online` int(10) unsigned NOT NULL,
+  `comments_offline` int(10) unsigned NOT NULL,
+  `feature` varchar(255) NOT NULL,
+  `last_modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`blog_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `plugin_blog_comment`
+--
+
+DROP TABLE IF EXISTS `plugin_blog_comment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `plugin_blog_comment` (
+  `comment_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `fk_entry_id` int(10) unsigned NOT NULL DEFAULT '0',
+  `fk_blog_id` int(10) unsigned NOT NULL DEFAULT '0',
+  `fk_language_id` int(10) unsigned NOT NULL,
+  `fk_user_id` int(10) unsigned NOT NULL DEFAULT '0',
+  `user_name` varchar(255) NOT NULL,
+  `user_email` varchar(255) NOT NULL,
+  `date` datetime NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `content` text NOT NULL,
+  `fk_mood_id` varchar(255) NOT NULL,
+  `status` enum('online','offline','pending') NOT NULL DEFAULT 'pending',
+  `admin_status` enum('online','offline','pending') NOT NULL DEFAULT 'pending',
+  `feature` varchar(255) NOT NULL,
+  `last_modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`comment_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `plugin_blog_entry`
+--
+
+DROP TABLE IF EXISTS `plugin_blog_entry`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `plugin_blog_entry` (
+  `entry_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `fk_blog_id` int(10) unsigned NOT NULL DEFAULT '0',
+  `fk_language_id` int(10) unsigned NOT NULL,
+  `fk_user_id` int(10) unsigned NOT NULL DEFAULT '0',
+  `date` datetime NOT NULL,
+  `released` datetime NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `content` text NOT NULL,
+  `fk_mood_id` int(10) unsigned NOT NULL,
+  `status` enum('online','offline') NOT NULL DEFAULT 'online',
+  `admin_status` enum('online','offline','pending') NOT NULL DEFAULT 'pending',
+  `comments_online` int(10) unsigned NOT NULL DEFAULT '0',
+  `comments_offline` int(10) unsigned NOT NULL DEFAULT '0',
+  `feature` varchar(255) NOT NULL,
+  `last_modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`entry_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `plugin_blog_entry_topic`
+--
+
+DROP TABLE IF EXISTS `plugin_blog_entry_topic`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `plugin_blog_entry_topic` (
+  `fk_entry_id` int(10) unsigned NOT NULL,
+  `fk_topic_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`fk_entry_id`,`fk_topic_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `plugin_blog_topic`
+--
+
+DROP TABLE IF EXISTS `plugin_blog_topic`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `plugin_blog_topic` (
+  `fk_blog_id` int(10) unsigned NOT NULL,
+  `fk_topic_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`fk_blog_id`,`fk_topic_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `plugin_interview_interviews`
+--
+
+DROP TABLE IF EXISTS `plugin_interview_interviews`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `plugin_interview_interviews` (
+  `interview_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `fk_language_id` int(10) unsigned NOT NULL,
+  `fk_moderator_user_id` int(10) unsigned NOT NULL,
+  `fk_guest_user_id` int(10) unsigned NOT NULL,
+  `title` varchar(256) NOT NULL,
+  `fk_image_id` int(10) unsigned DEFAULT NULL,
+  `description_short` varchar(256) NOT NULL,
+  `description` text NOT NULL,
+  `interview_begin` datetime NOT NULL,
+  `interview_end` datetime NOT NULL,
+  `questions_begin` datetime NOT NULL,
+  `questions_end` datetime NOT NULL,
+  `questions_limit` int(10) unsigned NOT NULL,
+  `status` enum('draft','pending','published','rejected') NOT NULL,
+  `invitation_sender` varchar(256) NOT NULL,
+  `invitation_subject` varchar(256) NOT NULL,
+  `invitation_template_guest` text NOT NULL,
+  `invitation_template_questioneer` text NOT NULL,
+  `guest_invitation_sent` datetime DEFAULT NULL,
+  `questioneer_invitation_sent` datetime DEFAULT NULL,
+  `invitation_password` varchar(10) NOT NULL,
+  `position` int(10) unsigned NOT NULL,
+  `last_modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`interview_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `plugin_interview_items`
+--
+
+DROP TABLE IF EXISTS `plugin_interview_items`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `plugin_interview_items` (
+  `item_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `fk_interview_id` int(10) unsigned NOT NULL,
+  `fk_questioneer_user_id` int(11) DEFAULT NULL,
+  `question` text NOT NULL,
+  `question_date` datetime NOT NULL,
+  `status` enum('draft','pending','published','rejected') NOT NULL DEFAULT 'draft',
+  `answer` text NOT NULL,
+  `answer_date` datetime NOT NULL,
+  `position` int(10) unsigned NOT NULL,
+  `last_modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`item_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=34 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `plugin_poll`
+--
+
+DROP TABLE IF EXISTS `plugin_poll`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `plugin_poll` (
+  `poll_nr` int(10) unsigned NOT NULL,
+  `fk_language_id` int(10) unsigned NOT NULL DEFAULT '0',
+  `parent_poll_nr` int(11) NOT NULL,
+  `is_extended` tinyint(4) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `question` varchar(255) NOT NULL,
+  `date_begin` date NOT NULL DEFAULT '0000-00-00',
+  `date_end` date NOT NULL DEFAULT '0000-00-00',
+  `nr_of_answers` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `votes_per_user` tinyint(3) unsigned NOT NULL DEFAULT '1',
+  `nr_of_votes` int(10) unsigned NOT NULL,
+  `nr_of_votes_overall` int(10) unsigned NOT NULL,
+  `percentage_of_votes_overall` float unsigned NOT NULL,
+  `last_modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`poll_nr`,`fk_language_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `plugin_poll_answer`
+--
+
+DROP TABLE IF EXISTS `plugin_poll_answer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `plugin_poll_answer` (
+  `fk_poll_nr` int(10) unsigned NOT NULL DEFAULT '0',
+  `fk_language_id` int(10) unsigned NOT NULL DEFAULT '0',
+  `nr_answer` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `answer` varchar(255) NOT NULL,
+  `nr_of_votes` int(10) unsigned NOT NULL DEFAULT '0',
+  `percentage` float unsigned NOT NULL,
+  `percentage_overall` float unsigned NOT NULL,
+  `value` int(11) NOT NULL,
+  `average_value` float NOT NULL,
+  `on_hitlist` tinyint(4) NOT NULL,
+  `last_modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  UNIQUE KEY `NrPoll` (`fk_poll_nr`,`fk_language_id`,`nr_answer`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `plugin_poll_article`
+--
+
+DROP TABLE IF EXISTS `plugin_poll_article`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `plugin_poll_article` (
+  `fk_poll_nr` int(10) unsigned NOT NULL DEFAULT '0',
+  `fk_article_nr` int(10) unsigned NOT NULL DEFAULT '0',
+  `fk_article_language_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`fk_poll_nr`,`fk_article_nr`,`fk_article_language_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `plugin_poll_issue`
+--
+
+DROP TABLE IF EXISTS `plugin_poll_issue`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `plugin_poll_issue` (
+  `fk_poll_nr` int(10) unsigned NOT NULL DEFAULT '0',
+  `fk_issue_nr` int(10) unsigned NOT NULL DEFAULT '0',
+  `fk_issue_language_id` int(10) unsigned NOT NULL,
+  `fk_publication_id` int(10) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`fk_poll_nr`,`fk_issue_nr`,`fk_issue_language_id`,`fk_publication_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `plugin_poll_publication`
+--
+
+DROP TABLE IF EXISTS `plugin_poll_publication`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `plugin_poll_publication` (
+  `fk_poll_nr` int(10) unsigned NOT NULL DEFAULT '0',
+  `fk_publication_id` int(10) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`fk_poll_nr`,`fk_publication_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `plugin_poll_section`
+--
+
+DROP TABLE IF EXISTS `plugin_poll_section`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `plugin_poll_section` (
+  `fk_poll_nr` int(10) unsigned NOT NULL DEFAULT '0',
+  `fk_section_nr` int(10) unsigned NOT NULL DEFAULT '0',
+  `fk_section_language_id` int(10) unsigned NOT NULL,
+  `fk_issue_nr` int(10) unsigned NOT NULL DEFAULT '0',
+  `fk_publication_id` int(10) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`fk_poll_nr`,`fk_section_nr`,`fk_section_language_id`,`fk_issue_nr`,`fk_publication_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `plugin_pollanswer_attachment`
+--
+
+DROP TABLE IF EXISTS `plugin_pollanswer_attachment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `plugin_pollanswer_attachment` (
+  `fk_poll_nr` int(11) NOT NULL,
+  `fk_pollanswer_nr` int(11) NOT NULL,
+  `fk_attachment_id` int(11) NOT NULL,
+  PRIMARY KEY (`fk_poll_nr`,`fk_pollanswer_nr`,`fk_attachment_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -41,4 +334,4 @@ CREATE TABLE `Xnews_article` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2010-07-06 21:58:04
+-- Dump completed on 2010-07-07 13:08:53
