0e80e9e05e0784e667d0a749a38da98e0bdf0048
omniton <planetaz@gmail.com>
2010-08-27 11:05:26 +0300

    added missed lang files for tiny plugins to resolve issue CS-2250git

---
+++ campsite/src/javascript/tinymce/plugins/codehighlighting/langs/ru.js
@@ -0,0 +1,12 @@
+// UK lang variables
+tinyMCE.addI18n('en.codehighlighting',{
+codehighlighting_desc : "Подсветка кода",
+codehighlighting_title : "Подсветка кода",
+codehighlighting_langaugepicker : "Выберите язык",
+codehighlighting_pagecode : "Вставьте код здесь",
+codehighlighting_button_desc: "Вставить код",
+codehighlighting_nogutter : "Убрать канал",
+codehighlighting_collapse : "Свернуть",
+codehighlighting_nocontrols : "Убрать управление",
+codehighlighting_showcolumns : "Показать колонки"
+});
---
+++ campsite/src/javascript/tinymce/plugins/iframe/langs/ru.js
@@ -0,0 +1,7 @@
+tinyMCE.addI18n('en.iframe',{
+desc:"Втавить карту/фрейм",
+error:"URL неверный.",
+url:"URL",
+height:"Высота",
+width:"Ширина"
+});
