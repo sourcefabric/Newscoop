326555958128cb0e9f7b905345fc4814df79ae05
Petr Jasek <petr.jasek@sourcefabric.org>
2010-10-04 12:51:02 +0200

    fix to issue CS-1789

--- campsite/src/classes/User.php
+++ campsite/src/classes/User.php
@@ -794,6 +794,43 @@ class User extends DatabaseObject {
         }
     } // fn syncPhorumUser
 
+    /**
+     * Sync all campsite and phorum users.
+     *
+     * @return void
+     */
+    public static function SyncPhorumUsers()
+    {
+        require_once($GLOBALS['g_campsiteDir'].'/include/phorum_load.php');
+        require_once($GLOBALS['g_campsiteDir'].'/classes/Phorum_user.php');
+
+        $g_ado_db = $GLOBALS['g_ado_db'];
+        $queryStr = 'SELECT u.Id, pu.user_id, u.UName, u.Password, u.EMail
+                        FROM liveuser_users AS u LEFT JOIN phorum_users AS pu
+                            ON u.UName = pu.username
+                        WHERE fk_campsite_user_id IS NULL
+                            OR fk_campsite_user_id != u.Id';
+        $nullUsers = $g_ado_db->GetAll($queryStr);
+        if (!is_array($nullUsers) || empty($nullUsers)) { // all synced
+            return;
+        }
+
+        foreach ($nullUsers as $nullUser) {
+            if (empty($nullUser['user_id'])) {
+                $phorumUser = new Phorum_user();
+                $phorumUser->create($nullUser['UName'], $nullUser['Password'],
+                                    $nullUser['EMail'], $nullUser['Id'], true);
+            } else {
+                $sql = 'UPDATE phorum_users SET fk_campsite_user_id = NULL
+                        WHERE fk_campsite_user_id = ' . $nullUser['Id'];
+                $g_ado_db->Execute($sql);
+                $sql = 'UPDATE phorum_users SET fk_campsite_user_id = ' . $nullUser['Id'] . '
+                        WHERE user_id = ' . $nullUser['user_id'];
+                $g_ado_db->Execute($sql);
+            }
+        }
+    } // fn SyncPhorumUsers
+
 } // class User
 
 ?>
