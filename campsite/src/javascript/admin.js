c1885a99d19e5d8ca3359d344b1513ec3773f7cb
petr.jasek <petr.jasek@sourcefabric.org>
2010-10-21 11:32:24 +0200

    update to cs-2277 - refactor both searches into one function

--- campsite/src/javascript/admin.js
+++ campsite/src/javascript/admin.js
@@ -134,31 +134,70 @@ function callServer(p_callback, p_args, p_handle)
 var autocomplete = false;
 var terms = [];
 $(document).ready(function() {
-    // topics autocomplete
-    $('.autocomplete.topics').autocomplete({
-        source: function(request, response) {
-            if (terms.length == 0) { // populate terms
-                $('ul.tree.sortable strong').each(function() {
-                    terms.push($(this).text());
-                });
-            }
-            if (terms.length == 0) { // still needs to populate
-                $('ul.tree label').each(function() {
-                    terms.push($(this).text());
-                });
-            }
 
-            var match = [];
-            var re = new RegExp(request.term, "i");
-            for (i = 0; i < terms.length; i++) {
-                if (terms[i].search(re) >= 0) {
-                    match.push(terms[i]);
+    // topics search autocomplete
+    $('input[name=search].topics').each(function() {
+        var input = $(this);
+        input.autocomplete({
+            source: function(request, response) {
+                if (terms.length == 0) { // populate terms
+                    $('ul.tree.sortable strong').each(function() {
+                        terms.push($(this).text());
+                    });
                 }
-            }
-            response(match);
-        },
-        close: function(event, ui) {
-            $('input[name=search]').change();
-        },
+                if (terms.length == 0) { // still needs to populate
+                    $('ul.tree label').each(function() {
+                        terms.push($(this).text());
+                    });
+                }
+
+                var match = [];
+                var re = new RegExp(request.term, "i");
+                for (i = 0; i < terms.length; i++) {
+                    if (terms[i].search(re) >= 0) {
+                        match.push(terms[i]);
+                    }
+                }
+                response(match);
+            },
+            close: function(event, ui) {
+                input.change(); // trigger search
+            },
+        });
+    }).change(function() {
+        // reset
+        $('ul.tree *').removeClass('match');
+        $('ul.tree li, ul.tree ul').show();
+        $('ul.tree.sortable').sortable('option', 'disabled', true);
+        if ($(this).val() == '') {
+            $('ul.tree.sortable').sortable('option', 'disabled', false);
+            return;
+        }
+
+        // search targets
+        var elem = 'label';
+        var elemParent = '.item';
+        if ($('ul.tree').hasClass('sortable')) {
+            elem = 'strong';
+            elemParent = '.item';
+        }
+
+        // search
+        var re = new RegExp($(this).val(), "i");
+        $('ul.tree > li').each(function() {
+            var li = $(this);
+            $(elem, li).each(function() {
+                if ($(this).text().search(re) >= 0) {
+                    li.addClass('match');
+                    $(this).addClass('match');
+                    $(this).closest(elemParent).addClass('match');
+                }
+            });
+        });
+
+        // hide non matching
+        $('ul.tree > li').not('.match').hide();
+        $('ul.tree li.match > ul').show();
     });
+
 });
