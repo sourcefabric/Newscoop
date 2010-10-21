f4a7d4448ddc86c8bb4cfae5d37f79371d9b2947
petr.jasek <petr.jasek@sourcefabric.org>
2010-10-21 11:37:36 +0200

    update to cs-2277 - fix topics popup search

--- campsite/src/javascript/admin.js
+++ campsite/src/javascript/admin.js
@@ -42,7 +42,7 @@ $(document).ready(function() {
 
         // search targets
         var elem = 'label';
-        var elemParent = '.item';
+        var elemParent = 'li';
         if ($('ul.tree').hasClass('sortable')) {
             elem = 'strong';
             elemParent = '.item';
