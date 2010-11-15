3c6086cbf4f15aa7bc48ca9bfd2c130c0d1b186f
petr.jasek <petr.jasek@sourcefabric.org>
2010-11-15 15:42:33 +0100

    update to CS-2218 - fix saving order, use new layout

--- campsite/src/admin-files/libs/ArticleList/do_order.php
+++ campsite/src/admin-files/libs/ArticleList/do_order.php
@@ -8,31 +8,8 @@
  * @link http://www.sourcefabric.org
  */
 
-header('Content-type: application/json');
-
 require_once($GLOBALS['g_campsiteDir']. "/$ADMIN_DIR/articles/article_common.php");
 
-if (!SecurityToken::isValid()) {
-    echo json_encode(array(
-        'success' => false,
-        'message' => getGS('Invalid security token!'),
-    ));
-    exit;
-}
-
-// get input
-$f_language = Input::Get('language', 'int', null, true);
-$f_order = Input::Get('order', 'array', array(), true);
-if (!Input::IsValid()) {
-    echo json_encode(array(
-        'success' => false,
-        'message' => getGS('Invalid input.'),
-    ));
-    exit;
-}
-
-$success = FALSE;
-$message = 'non';
 $article = new Article();
 foreach ($f_order as $order => $item) {
     list($prefix, $articleId) = explode('_', $item);
@@ -40,10 +17,4 @@ foreach ($f_order as $order => $item) {
     $article->setProperty('ArticleOrder', $order + 1);
 }
 
-$success = TRUE;
-$message = getGS('Articles order updated.');
-echo json_encode(array(
-    'success' => $success,
-    'message' => is_array($message) ? implode("\n", $message) : $message,
-));
-exit;
+return TRUE;
--- campsite/src/admin-files/menu.php
+++ campsite/src/admin-files/menu.php
@@ -325,7 +325,7 @@ $siteTitle = (!empty($Campsite['site']['title'])) ? htmlspecialchars($Campsite['
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
   <meta http-equiv="Expires" content="now" />
   <title><?php p($siteTitle); ?></title>
-  <link rel="stylesheet" type="text/css" href="<?php echo $Campsite['WEBSITE_URL']; ?>/css/adm/jquery-ui-1.8.5.custom.css" />
+  <link rel="stylesheet" type="text/css" href="<?php echo $Campsite['WEBSITE_URL']; ?>/css/adm/jquery-ui-1.8.6.custom.css" />
   <link rel="stylesheet" type="text/css" href="<?php echo $Campsite['WEBSITE_URL']; ?>/javascript/JSCookMenu/ThemeOffice/theme.css" />
   <link rel="stylesheet" type="text/css" href="<?php echo $Campsite['WEBSITE_URL']; ?>/css/admin_stylesheet.css" />
   <link rel="stylesheet" type="text/css" href="<?php echo $Campsite['WEBSITE_URL']; ?>/css/adm/ColVis.css" />
