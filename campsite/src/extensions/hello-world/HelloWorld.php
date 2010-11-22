96c4f731abe881baee1c45dd7b6ee98181c46583
petr.jasek <petr.jasek@sourcefabric.org>
2010-11-22 11:33:26 +0100

    update to CS-2218 - widgets metadata update

--- campsite/src/extensions/hello-world/HelloWorld.php
+++ campsite/src/extensions/hello-world/HelloWorld.php
@@ -1,7 +1,16 @@
 <?php
 /**
+ * @package Campsite
+ *
+ * @author Petr Jasek <petr.jasek@sourcefabric.org>
+ * @copyright 2010 Sourcefabric o.p.s.
+ * @license http://www.gnu.org/licenses/gpl.txt
+ * @link http://www.sourcefabric.org
+ */
+
+/**
  * @title Hello World
- * @author Petr Jašek
+ * @author Sourcefabric o.p.s.
  * @description Widget sample.
  * @homepage http://www.sourcefabric.org
  * @version 1.0
@@ -11,9 +20,5 @@ class HelloWorld extends Widget
     public function render()
     {
         echo '<p>', $this->_('Hello world!'), '</p>';
-
-        if ($this->isFullscreen()) {
-            echo '<p>Hi!</p>';
-        }
     }
 }
