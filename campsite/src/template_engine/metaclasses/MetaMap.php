5ae916f061b24612d5acd199c38a8391697d79ce
holman <holman.romero@sourcefabric.org>
2010-12-16 14:57:59 +0100

    implemented issue CS-2353

--- campsite/src/template_engine/metaclasses/MetaMap.php
+++ campsite/src/template_engine/metaclasses/MetaMap.php
@@ -5,7 +5,7 @@
 
 require_once dirname(__FILE__) . '/MetaDbObject.php';
 require_once dirname(__FILE__) . '/MetaMapLocation.php';
-require_once $GLOBALS['g_campsiteDir'] . '/classes/IGeoMap.php';
+require_once($GLOBALS['g_campsiteDir'] . '/classes/GeoMap.php');
 
 /**
  * @package Campsite
@@ -16,6 +16,7 @@ final class MetaMap extends MetaDbObject
      * @var array
      */
     private static $m_baseProperties = array(
+        'number' => 'id',
         'name' => 'MapName',
         'provider' => 'MapProvider',
 	);
@@ -61,5 +62,78 @@ final class MetaMap extends MetaDbObject
     public function isEnabled()
     {
         return $this->m_dbObject->isEnabled();
+    }
+
+    /**
+     * @param int $p_articleNumber
+     * @param int $p_languageId
+     * @return string
+     */
+    public static function GetMapTagBody($p_articleNumber, $p_languageId)
+    {
+        return Geo_Map::GetMapTagBody((int) $p_articleNumber, (int) $p_languageId);
+    }
+
+    /**
+     * @param int $p_articleNumber
+     * @param int $p_languageId
+     * @return string
+     */
+    public static function GetMapTagHeader($p_articleNumber, $p_languageId,
+                                           $p_mapWidth = 0, $p_mapHeight = 0)
+    {
+        return Geo_Map::GetMapTagHeader((int) $p_articleNumber, (int) $p_languageId,
+                                        (int) $p_mapWidth, (int) $p_mapHeight);
+    }
+
+    /**
+     * @param int $p_articleNumber
+     * @param int $p_languageId
+     * @return string
+     */
+    public static function GetMapTagCenter($p_articleNumber, $p_languageId)
+    {
+        return Geo_Map::GetMapTagCenter((int) $p_articleNumber, (int) $p_languageId);
+    }
+
+    /**
+     * @param int $p_article
+     * @param int $p_language
+     * @return string
+     */
+    public static function GetMapTagList($p_articleNumber, $p_languageId)
+    {
+        $geo = Geo_Map::GetMapTagList((int) $p_articleNumber, (int) $p_languageId);
+        $map = $geo['map'];
+        $pois = $geo['pois'];
+
+        $html = '
+            <div class="geomap_info">
+              <dl class="geomap_map_name">
+                <dt class="geomap_map_name_label">' .
+                  'Map' . ':
+                </dt>
+                <dd class="geomap_map_name_value">' .
+                  $map['name'] . '
+                </dd>
+              </dl>
+            </div>
+            <div id="side_info" class="geo_side_info">';
+        $poiIdx = 0;
+        foreach ($pois as $poi) {
+            $html .= '<div id="poi_seq_' . $poiIdx . '">
+                <a class="geomap_poi_name" href="#" onClick="'
+                . $poi['open'] . ' return false;">' . $poi['title'] . '</a>
+                <div class="geomap_poi_perex">' . $poi['perex'] . '</div>
+                <div class="geomap_poi_center">
+                    <a href="#" onClick="' . $poi['center'] . ' return false;">'
+                        . 'Center' . '
+                    </a>
+                </div>
+                <div class="geomap_poi_spacer">&nbsp;</div>
+            </div>';
+            $poiIdx += 1;
+        }
+        return $html;
     }
 }
