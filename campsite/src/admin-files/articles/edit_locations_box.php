1265ffd22da34be2e4e004deb900d9d83ea67af7
holman <holman.romero@sourcefabric.org>
2010-12-17 11:08:28 +0100

    update to issue CS-1498 - minor style changes to the box in the article edit screen

--- campsite/src/admin-files/articles/edit_locations_box.php
+++ campsite/src/admin-files/articles/edit_locations_box.php
@@ -1,6 +1,4 @@
 <?php
-//camp_load_translation_strings("geolocation");
-
 $locations = array();
 $map = $articleObj->getMap();
 if (is_object($map) && $map->exists()) {
@@ -14,54 +12,54 @@ $detachUrl = "/$ADMIN/articles/locations/do_unlink.php?f_publication_id=$f_publi
     <table cellpadding="3" cellspacing="0" style="width: 100%; background-color: #eee;">
     <tr>
       <td style="text-align: left">
-        <strong><?php putGS("Locations"); ?></strong>
+        <strong><?php putGS('Locations'); ?></strong>
       </td>
       <td style="text-align: right">
         <table cellpadding="2" cellspacing="0">
         <tr>
           <?php if (($f_edit_mode == "edit") && $g_user->hasPermission('ChangeArticle')) {  ?>
           <td><img src="<?php p($Campsite["ADMIN_IMAGE_BASE_URL"]);?>/add.png" /></td>
-          <td><a href="javascript: void(0);" onclick="window.open('<?php echo camp_html_article_url($articleObj, $f_language_id, "locations/popup.php"); ?>', 'autopublish_window', 'scrollbars=yes, resizable=yes, menubar=no, toolbar=no, width=1050, height=600, top=200, left=200');"><?php putGS("Edit"); ?></a></td>
+          <td><a href="javascript: void(0);" onclick="window.open('<?php echo camp_html_article_url($articleObj, $f_language_id, "locations/popup.php"); ?>', 'autopublish_window', 'scrollbars=yes, resizable=yes, menubar=no, toolbar=no, width=1050, height=600, top=200, left=200');"><?php putGS('Edit'); ?></a></td>
           <?php } ?>
         </tr>
         </table>
       </td>
     </tr>
-    <?php if ($map->exists()) { ?>
+    </table>
+  </td>
+</tr>
+<?php if ($map->exists()) { ?>
+<tr>
+  <td align="center" colspan="2">
+    <table>
     <tr>
-      <td align="center" colspan="2">
-        <table>
-        <tr>
-          <td>
-            <a href="#" onclick="window.open('<?php echo camp_html_article_url($articleObj, $f_language_id, "locations/preview.php"); ?>', 'map_preview_window', 'scrollbars=yes, resizable=yes, menubar=no, toolbar=no, width=1050, height=600, top=200, left=200');"><img src="<?php echo $Campsite['ADMIN_STYLE_URL']; ?>/images/world_map.png" /></a>
-          </td>
-          <?php if (($f_edit_mode == "edit") && $g_user->hasPermission('ChangeArticle')) {  ?>
-          <td align="left">
-            <a href="<?php p($detachUrl); ?>" onclick="return confirm('<?php putGS("Are you sure you want to remove the map from the article?"); ?>'); return false;"><img src="<?php p($Campsite["ADMIN_IMAGE_BASE_URL"]);?>/unlink.png" border="0"></a>
-          </td>
-          <?php } ?>
-        </tr>
-        </table>
+      <td>
+        <a href="#" onclick="window.open('<?php echo camp_html_article_url($articleObj, $f_language_id, "locations/preview.php"); ?>', 'map_preview_window', 'scrollbars=yes, resizable=yes, menubar=no, toolbar=no, width=1050, height=600, top=200, left=200');"><img src="<?php echo $Campsite['ADMIN_STYLE_URL']; ?>/images/world_map.png" /></a>
       </td>
+      <?php if (($f_edit_mode == "edit") && $g_user->hasPermission('ChangeArticle')) { ?>
+      <td align="left">
+        <a href="<?php p($detachUrl); ?>" onclick="return confirm('<?php putGS("Are you sure you want to remove the map from the article?"); ?>'); return false;"><img src="<?php p($Campsite["ADMIN_IMAGE_BASE_URL"]);?>/unlink.png" border="0"></a>
+      </td>
+      <?php } ?>
     </tr>
-    <?php } ?>
     </table>
   </td>
 </tr>
 <?php
+}
 if (!empty($locations)) { ?>
 <tr>
   <td>
     <ol class="points">
       <?php
       $language_usage = $f_language_selected;
-      if (!$language_usage) {$language_usage = $f_language_id;}
+      if (!$language_usage) { $language_usage = $f_language_id; }
       foreach ($locations as $location) {
           $content = $location->getContent($language_usage);
-          if ($location->isEnabled($f_language_id)) {
-              echo '<li class="map_list_location_enabled">' . $content->getName() . '</li>';
+          if ($location->isEnabled($language_usage)) {
+              echo '<li class="geomap_list_location_enabled">' . $content->getName() . '</li>';
           } else {
-              echo '<li class="map_list_location_disabled">' . $content->getName() . '</li>';
+              echo '<li class="geomap_list_location_disabled">' . $content->getName() . '</li>';
           }
       }
       ?>
