cdeb0bb4b6123bf04fead7bc942ed653ca0c20c8
Martin Saturka <martin.saturka@sourcefabric.org>
2011-01-04 10:20:28 +0100

    CS-2404, adjusting 'edit' press behavior on closed popup

--- campsite/src/admin-files/articles/edit_locations_box.php
+++ campsite/src/admin-files/articles/edit_locations_box.php
@@ -28,10 +28,32 @@ $map_article_spec = "" . $f_article_number . "_" . $map_language_used;
                 geomap_popup_editing = null;
                 geomap_art_spec_popup = "";
                 geomap_art_spec_main = "" + '<?php echo $map_article_spec; ?>';
+                geomap_popup_show = function ()
+                {
+                    var geomap_force_new = true;
+                    try { 
+                        if ((!geomap_popup_editing) || geomap_popup_editing.closed) {geomap_art_spec_popup = "";}
+                    }
+                    catch(e) {geomap_art_spec_popup = "";}
+                    try {
+                        if (geomap_art_spec_main == geomap_art_spec_popup)
+                        {
+                            geomap_popup_editing.focus();
+                            geomap_force_new = false;
+                        }
+                    } catch (e) { geomap_force_new = true; }
+                    if (geomap_force_new) 
+                    {
+                        geomap_popup_editing = window.open('<?php echo camp_html_article_url($articleObj, $f_language_id, "locations/popup.php"); ?>', 'geomap_edit_window', 'scrollbars=yes, resizable=no, menubar=no, toolbar=no, width=1050, height=600, top=200, left=200');
+                        try {
+                            geomap_popup_editing.focus();
+                        } catch (e) {} 
+                    };
+                }
           </script>
           <?php if (($f_edit_mode == "edit") && $g_user->hasPermission('ChangeArticle')) {  ?>
           <td><img src="<?php p($Campsite["ADMIN_IMAGE_BASE_URL"]);?>/add.png" /></td>
-          <td><a href="javascript: void(0);" onclick="try { var geomap_force_new = true; if (geomap_art_spec_main == geomap_art_spec_popup) {geomap_popup_editing.focus(); geomap_force_new = false; } } catch (e) { geomap_force_new = true; } if (geomap_force_new) { geomap_popup_editing = window.open('<?php echo camp_html_article_url($articleObj, $f_language_id, "locations/popup.php"); ?>', 'geomap_edit_window', 'scrollbars=yes, resizable=no, menubar=no, toolbar=no, width=1050, height=600, top=200, left=200'); try {geomap_popup_editing.focus();} catch (e) {} } return false;"><?php putGS('Edit'); ?></a></td>
+          <td><a href="javascript: void(0);" onclick="geomap_popup_show(); return false;"><?php putGS('Edit'); ?></a></td>
           <?php } ?>
         </tr>
         </table>
