d081205181f42965d8d73d095ebd6dc021961930
petr.jasek <petr.jasek@sourcefabric.org>
2011-01-31 14:56:28 +0100

    remove duplicated string for empty list

--- newscoop/admin-files/libs/ArticleList/table.php
+++ newscoop/admin-files/libs/ArticleList/table.php
@@ -91,7 +91,7 @@ tables['<?php echo $this->id; ?>'] = table.dataTable({
         'sEmpty': '<?php putGS('No entries to show'); ?>',
         'sInfoFiltered': '<?php putGS(' - filtering from _MAX_ records'); ?>',
         'sLengthMenu': '<?php putGS('Display _MENU_ records'); ?>',
-        'sInfoEmpty': '<?php putGS('No entries to show'); ?>'
+        'sInfoEmpty': '',
     },
     'aoColumnDefs': [
         { // inputs for id
