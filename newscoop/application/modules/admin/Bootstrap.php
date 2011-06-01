e1d3380e60814d810ddb9ee6bb7b5683a284d0b3
petr.jasek <petr.jasek@sourcefabric.org>
2011-06-01 09:54:22 +0200

    CS-2359: Templates refactoring
    Fix bootstrap for tests and application.

--- newscoop/application/modules/admin/Bootstrap.php
+++ newscoop/application/modules/admin/Bootstrap.php
@@ -227,10 +227,9 @@ class Admin_Bootstrap extends Zend_Application_Module_Bootstrap
         
         Zend_Controller_Front::getInstance()->registerPlugin( new \Newscoop\Controller\Plugin\Js( $this->getOptions() ) );
 
-        return; // @todo fix so it does not break tests
-        $view->addHelperPath( APPLICATION_PATH . "/modules/admin/views/helpers", "Admin_View_Helper" );
-        $jsPlaceholder = $view->getHelper( 'JQueryReady' );
-        $view->getHelper( 'JQueryUtils' )->setPlaceholder( $jsPlaceholder );
+        $view->addHelperPath(APPLICATION_PATH . '/modules/admin/views/helpers', 'Admin_View_Helper');
+        $jsPlaceholder = $view->getHelper('JQueryReady');
+        $view->getHelper('JQueryUtils')->setPlaceholder($jsPlaceholder);
         
     }
 
