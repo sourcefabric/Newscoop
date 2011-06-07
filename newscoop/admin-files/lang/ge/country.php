a5cfcd02ebf1a68349bfc25c455efce8269c4bb3
root <root@www.sourcefabric.org>
2011-06-07 16:34:01 +0000

    updated language resources

--- newscoop/admin-files/lang/ge/country.php
+++
@@ -1,4 +0,0 @@
-<?php 
-regGS("The country \"$1\" could not be created.", "");
-regGS("Country with code \"$1\" exists already.", "");
-?>
--- newscoop/admin-files/lang/ge/localizer.php
+++
@@ -1,8 +0,0 @@
-<?php 
-regGS("Dashboard", "");
-regGS("Authors", "");
-regGS("Libraries", "");
-regGS("Media Archive", "");
-regGS("Extensions", "");
-regGS("Geo-location", "");
-?>
