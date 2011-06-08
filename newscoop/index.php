b8a45cb2008d9363d5fa958c32ed057ca88eca22
Martin Saturka <martin.saturka@sourcefabric.org>
2011-06-08 20:53:44 +0200

    CS-3158: Statistics class must be re-factored * processing of the statistics requests was put out of zend

--- newscoop/index.php
+++ newscoop/index.php
@@ -1,4 +1,12 @@
 <?php
 
+// collect statistics
+require_once(dirname(__FILE__) . '/classes/Statistics.php');
+$stats_only = false;
+Statistics::ProcessStats($stats_only);
+if ($stats_only) {
+    exit(0);
+}
+
 // run zend
 require_once dirname(__FILE__) . '/public/index.php';
