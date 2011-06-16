e8cf65a80e37d5ba2c5fb0b183c4f27bd8360f9f
Holman Romero <holman.romero@sourcefabric.org>
2011-06-16 18:35:45 +0200

    CS-2595: About/Help screen
    First draft

---
+++ newscoop/application/modules/admin/controllers/ApplicationController.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @package Newscoop
+ * @copyright 2011 Sourcefabric o.p.s.
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt
+ */
+
+/**
+ * @Acl(ignore="1")
+ */
+class Admin_ApplicationController extends Zend_Controller_Action
+{
+    public function indexAction()
+    {
+        $this->_forward('help');
+    }
+
+    public function helpAction()
+    {
+        $this->view;
+    }
+}
+
