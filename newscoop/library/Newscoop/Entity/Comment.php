e48e0f75469a46e73b736a351d5509c09b5a8484
Mihai Nistor <mihai.nistor@sourcefabric.org>
2011-06-21 13:42:17 +0300

    CS-3222: Commets HTML tags don't work
    some formating rules
    fixed $content changed with $contentAfter

--- newscoop/library/Newscoop/Entity/Comment.php
+++ newscoop/library/Newscoop/Entity/Comment.php
@@ -574,9 +574,9 @@ class Comment
     {
         $parts = explode('<', $str);
         // if no < was found then return the original string
-        if(count($parts) === 1)
+        if (count($parts) === 1)
             return $str;
-        /** @type array vector where the tag list are keeped*/
+        /** @type array vector where the tag list are keeped */
         $tag = array();
         $attrib = array();
         $contentAfter = array(0 => $parts[0]);
@@ -604,7 +604,7 @@ class Comment
         $closed = $tag;
         $return = '';
         $allowedNameTags = array_keys($this->allowedTags);
-        for ($i = 0, $counti = count($content); $i < $counti; $i++) {
+        for ($i = 0, $counti = count($contentAfter); $i < $counti; $i++) {
             $isClosed = isset($tag[$i]) ? (substr($tag[$i], 0, 1) == '/') : false;
 
             if (isset($tag[$i]) && (in_array($tag[$i], $allowedNameTags))) {
@@ -615,10 +615,10 @@ class Comment
                     $composeTag = '<' . $tag[$i] . ' ';
                     if (isset($attrib[$i])) {
                         for ($j = 0, $countj = count($attrib[$i]); $j < $countj; $j++) {
-                            if($attrib[$i][$j][0]=='href')
+                            if ($attrib[$i][$j][0] == 'href') {
                                 $attrib[$i][$j][1] = preg_replace('/(javascript[:]?)/i','', $attrib[$i][$j][1]);
-                            if (in_array($attrib[$i][$j][0],
-                                            $this->allowedTags[$tag[$i]])) {
+                            }
+                            if (in_array($attrib[$i][$j][0], $this->allowedTags[$tag[$i]])) {
                                 $composeTag.=$attrib[$i][$j][0] . '="' . $attrib[$i][$j][1] . '" ';
                             }
                         }
@@ -630,9 +630,11 @@ class Comment
                     $cite = false;
                     if (isset($attrib[$i])) {
                         for ($j = 0, $countj = count($attrib[$i]); $j < $countj; $j++) {
-                            if (in_array($attrib[$i][$j][0], $this->allowedTags[$tag[$i]])) {
-                                if($attrib[$i][$j][0]=='href')
-                                    $attrib[$i][$j][1] = preg_replace('/(javascript[:]?)/i','', $attrib[$i][$j][1]);
+                            if (in_array($attrib[$i][$j][0],
+                                            $this->allowedTags[$tag[$i]])) {
+                                if ($attrib[$i][$j][0] == 'href') {
+                                    $attrib[$i][$j][1] = preg_replace('/(javascript[:]?)/i', '', $attrib[$i][$j][1]);
+                                }
                                 if ($attrib[$i][$j][0] == 'title') {
                                     $title = $attrib[$i][$j][0];
                                 } elseif ($attrib[$i][$j][0] == 'cite') {
@@ -656,9 +658,7 @@ class Comment
                         $return.=substr($composeTag, 0, -1) . '>' . $contentAfter[$i] . '</' . $tag[$i] . '>';
                     }
                 }
-            }
-            elseif (isset($tag[$i]) && $isClosed && in_array(substr($tag[$i], 1),
-                            $allowedNameTags)) {
+            } elseif (isset($tag[$i]) && $isClosed && in_array(substr($tag[$i], 1), $allowedNameTags)) {
                 unset($closed[$i]);
                 $return.='<' . $tag[$i] . '>' . $contentAfter[$i];
             } else {
