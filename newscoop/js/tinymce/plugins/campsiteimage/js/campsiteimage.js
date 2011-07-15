016799867381dd1949bfef457444ee89ec7214cb
Ozan Ozbek <ozan.ozbek@sourcefabric.org>
2011-07-15 11:55:56 +0200

    CS-3426: 'Resizing Ratio' doesn't resize image in tinyMCE editor, even image is correctly resize in preview (front-end)
    Many parts are rewritten.

--- newscoop/js/tinymce/plugins/campsiteimage/js/campsiteimage.js
+++ newscoop/js/tinymce/plugins/campsiteimage/js/campsiteimage.js
@@ -18,25 +18,59 @@ var CampsiteImageDialog = {
             alert('Double quotes are not allowed for Alt and Caption fields.\nUse single quotes or double single quotes instead.');
             return false;
         }
-
+        
         var shrinkRatio = topDoc.getElementById('f_ratio').value;
-        if (shrinkRatio < 1 || shrinkRatio > 99)
-            shrinkRatio = '';
-        else
-            shrinkRatio = '_' + shrinkRatio;
+        
+        if (shrinkRatio < 1 || shrinkRatio > 99) {
+			shrinkRatio = '';
+		}
+        else {
+			shrinkRatio = '_' + shrinkRatio;
+		}
 
         if (command == 'insert') var mce_command = 'mceInsertContent';
         if (command == 'edit') var mce_command = 'mceReplaceContent';
         
-        tinyMCEPopup.execCommand(mce_command, false, dom.createHTML('img', {
+        var resize_width = topDoc.getElementById('f_resize_width').value;
+        var resize_height = topDoc.getElementById('f_resize_height').value;
+        
+        if (topDoc.getElementById('f_ratio').value != '') {
+			resize_width = resize_width * topDoc.getElementById('f_ratio').value * 0.01;
+			resize_height = resize_height * topDoc.getElementById('f_ratio').value * 0.01;
+		}
+        
+        var element = dom.createHTML('img', {
             src : topDoc.getElementById('f_url').value,
             align : topDoc.getElementById('f_align').value,
             id : topDoc.getElementById('f_image_template_id').value + shrinkRatio,
             title : topDoc.getElementById('f_caption').value,
             alt : topDoc.getElementById('f_alt').value,
-            width : topDoc.getElementById('f_resize_width').value,
-            height : topDoc.getElementById('f_resize_height').value
-        }));
+            width : resize_width,
+            height : resize_height
+        });
+        
+        /*
+        var element_id = topDoc.getElementById('f_image_template_id').value;
+        var element_align = ' align=""';
+        if (topDoc.getElementById('f_align').value) element_align = ' align="' + topDoc.getElementById('f_align').value + '"';
+        var element_alt = '';
+        if (topDoc.getElementById('f_alt').value) element_alt = ' alt="' + topDoc.getElementById('f_alt').value + '"';
+        var element_caption = '';
+        if (topDoc.getElementById('f_caption').value) element_caption = ' sub="' + topDoc.getElementById('f_caption').value + '"';
+        var element_width = '';
+        if (topDoc.getElementById('f_resize_width').value) element_width = ' width="' + topDoc.getElementById('f_resize_width').value + '"';
+        var element_height = '';
+        if (topDoc.getElementById('f_resize_height').value) element_height = ' height="' + topDoc.getElementById('f_resize_height').value + '"';
+        var element_ratio = '';
+        if (topDoc.getElementById('f_ratio').value) element_ratio = ' ratio="' + topDoc.getElementById('f_ratio').value + '"';
+        
+        var element = '<!** Image ' + element_id + element_align + element_alt + element_caption + element_width + element_height + element_ratio + '>';
+        //console.log(element);
+        */
+        
+        //var element = '<img id="' + element_id + '" >';
+        
+        tinyMCEPopup.execCommand(mce_command, false, element);
 
         return(tinyMCEPopup.close());
 	},
@@ -67,15 +101,15 @@ var CampsiteImageDialog = {
         var obj = topDoc.getElementById('f_align');
         obj.value = p_align;
 
-        if (p_ratio != undefined) {
+        if (p_ratio != undefined && p_ratio != '') {
             var obj = topDoc.getElementById('f_ratio');
             obj.value = p_ratio;
         }
-        if (p_width != undefined) {
+        if (p_width != undefined && p_width != '') {
             var obj = topDoc.getElementById('f_resize_width');
             obj.value = p_width;
         }
-        if (p_height != undefined) {
+        if (p_height != undefined && p_height != '') {
             var obj = topDoc.getElementById('f_resize_height');
             obj.value = p_height;
         }
