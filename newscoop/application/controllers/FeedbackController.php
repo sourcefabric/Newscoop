ef7dd5ebd01a40d13915831e469b223f60680145
Ozan Ozbek <ozan.ozbek@sourcefabric.org>
2011-08-10 18:21:48 +0200

    WOBS-40: Omnibox
    Added feedback controller, but nothing in it yet. Fixed minor issues in comment controller and omnibox frontend js code.

--- newscoop/application/controllers/FeedbackController.php
+++ newscoop/application/controllers/FeedbackController.php
@@ -6,14 +6,14 @@
  */
 
 /**
- * Comments controller
+ * Feedback controller
  */
  
-use Newscoop\Entity\Comment;
+//use Newscoop\Entity\Feedback;
 
 require_once($GLOBALS['g_campsiteDir'].'/include/captcha/php-captcha.inc.php');
  
-class CommentController extends Zend_Controller_Action
+class FeedbackController extends Zend_Controller_Action
 {
     public function init()
     {
@@ -25,42 +25,39 @@ class CommentController extends Zend_Controller_Action
 		global $_SERVER;
 		
 		$this->_helper->layout->disableLayout();
-		$parameters = $this->getRequest()->getParams();
+		$this->view->params = $this->getRequest()->getParams();
 		
 		$errors = array();
 		
 		$auth = Zend_Auth::getInstance();
 		
-		$article = new Article($parameters['f_language'], $parameters['f_article_number']);
-		$publication = new Publication($article->getPublicationId());
+		$publication = new Publication($this->view->params['f_publication_id']);
 		
 		if (!$auth->getIdentity()) {
 			$errors[] = getGS('You are not logged in.');
 		}
-		if (!array_key_exists('f_comment_subject', $parameters) || empty($parameters['f_comment_subject'])) {
-			$errors[] = getGS('The comment subject was not filled in.');
-		}
-		if (!array_key_exists('f_comment_content', $parameters) || empty($parameters['f_comment_content'])) {
-			$errors[] = getGS('The comment content was not filled in.');
+		if (!array_key_exists('f_feedback_content', $this->view->params) || empty($this->view->params['f_feedback_content'])) {
+			$errors[] = getGS('The feedback content was not filled in.');
 		}
 		
 		if ($publication->isCaptchaEnabled()) {
-			if (!PhpCaptcha::Validate($parameters['f_captcha'], true)) {
+			if (!PhpCaptcha::Validate($this->view->params['f_captcha'], true)) {
 				$errors[] = getGS('The code you entered is not the same with the one shown in the image.');
 			}
 		}
 		
 		if (empty($errors)) {
+			/*
 			$commentRepository = $this->getHelper('entity')->getRepository('Newscoop\Entity\Comment');
 			$comment = new Comment();
 			
 			$values = array(
 				'user' => $auth->getIdentity(),
-				'name' => $parameters['f_comment_nickname'],
-				'subject' => $parameters['f_comment_subject'],
-				'message' => $parameters['f_comment_content'],
-				'language' => $parameters['f_language'],
-				'thread' => $parameters['f_article_number'],
+				'name' => $this->view->params['f_comment_nickname'],
+				'subject' => $this->view->params['f_comment_subject'],
+				'message' => $this->view->params['f_comment_content'],
+				'language' => $this->view->params['f_language'],
+				'thread' => $this->view->params['f_article_number'],
 				'ip' => $_SERVER['REMOTE_ADDR'],
 				'status' => 'approved',
 				'time_created' => new DateTime()
@@ -68,8 +65,9 @@ class CommentController extends Zend_Controller_Action
 			
 			$commentRepository->save($comment, $values);
 			$commentRepository->flush();
+			*/
 			
-			$this->view->response = 'OK';
+			$this->view->response = getGS('Your message is sent.');
 		}
 		else {
 			$errors = implode('<br>', $errors);
