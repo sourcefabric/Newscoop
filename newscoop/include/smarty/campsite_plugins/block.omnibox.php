3c8d8b194bc79f15da3ca3b757b0a7a3ba166895
Ozan Ozbek <ozan.ozbek@sourcefabric.org>
2011-08-17 10:55:13 +0200

    WOBS-40: Omnibox
    Several fixes, cleanup.

--- newscoop/include/smarty/campsite_plugins/block.omnibox.php
+++ newscoop/include/smarty/campsite_plugins/block.omnibox.php
@@ -28,14 +28,14 @@ function smarty_block_omnibox($p_params, $p_content, &$p_smarty, &$p_repeat)
 	
 	$gimme = $p_smarty->get_template_vars('gimme');
 	
+	$parameters = $p_params;
+	$parameters['gimme'] = $gimme;
+	
 	$p_content = $controller->view->action(
 		'index',
 		'omnibox',
 		'default',
-		array(
-			'gimme' => $gimme,
-			
-		)
+		$parameters
 	);
     
     return($p_content);
