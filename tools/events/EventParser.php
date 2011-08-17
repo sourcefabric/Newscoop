c78ec8f4da01bfb472eb0bed8df48d2aea2f073e
Martin Saturka <martin.saturka@sourcefabric.org>
2011-08-17 20:25:36 +0200

    WOBS-21: Research on how we could import, save and edit the eventdata delivered in xml to Newscoop * updates on event/kino/movie parsers

--- tools/events/EventParser.php
+++ tools/events/EventParser.php
@@ -4,29 +4,29 @@
  */
 
 /**
- * EventData Importer class for managing parsing of WXR files.
+ * EventData Importer class for managing parsing of EventData files.
  */
-class EventParser_Parser {
+class EventData_Parser {
 
     /**
-     * Parses EventData data (by ED_Parser_SimplXML)
+     * Parses EventData data (by EventData_Parser_SimpleXML)
      *
-     * @param string $p_file file name of the wxr file
+     * @param string $p_file file name of the event file
      * @return array
      */
     function parse($p_provider, $p_file, $p_categories) {
 
-        $parser = new ED_Parser_SimpleXML;
+        $parser = new EventData_Parser_SimpleXML;
         $result = $parser->parse($p_provider, $p_file, $p_categories);
 
         return $result;
     } // fn parse
-} // class Event_Parser
+} // class EventData_Parser
 
 /**
- * WXR Parser that makes use of the SimpleXML PHP extension.
+ * EventData Parser that makes use of the SimpleXML PHP extension.
  */
-class ED_Parser_SimpleXML {
+class EventData_Parser_SimpleXML {
 
     /**
      * Parses EventData data (by SimplXML)
@@ -35,21 +35,12 @@ class ED_Parser_SimpleXML {
      * @return array
      */
     function parse($p_provider, $p_file, $p_categories) {
-        //$authors = $posts = $categories = $categories_by_slug = $categories_slugs_by_name = $tags = $terms = array();
 
 		$events = array();
-		//$all_cats = array();
 
         libxml_clear_errors();
         $internal_errors = libxml_use_internal_errors(true);
         $xml = simplexml_load_file($p_file);
-
-//echo "\n<pre>\n";
-//var_dump($xml->date[0]->entry);
-//var_dump($xml->date[0]->entry[0]);
-//var_dump($xml->date[0]->entry[1]);
-//var_dump($xml->date[0]);
-//exit(0);
 
         // halt if loading produces an error
         if (!$xml) {
@@ -66,7 +57,6 @@ class ED_Parser_SimpleXML {
 
 			// day of event set (event record)
 			$recdate = $event_day->recdate;
-			echo "\n" . $recdate . "\n";
 
 			// array of events info
 			$entry_set = $event_day->entry;
@@ -120,10 +110,6 @@ class ED_Parser_SimpleXML {
 					}
 				}
 
-				//if (!array_key_exists($x_catnam, $all_cats)) {
-				//	$all_cats[$x_catnam] = true;
-				//}
-
 				// event subcategory
 				$x_catsub = '' . $event->catsub;
 				// * minor type fields
@@ -165,8 +151,6 @@ class ED_Parser_SimpleXML {
 					$x_trnnam = null;
 				}
 				$event_info['event_name'] = $x_trnnam;
-
-				//echo $x_trnnam . "\n";
 
 				// Prices, mostly empty, frequently inconsistent
 				// number, better to ignore
@@ -236,35 +220,39 @@ class ED_Parser_SimpleXML {
 				}
 				$event_info['event_time'] = $x_eveda2;
 
-				$event_info['event_open'] = array();
+				$event_open = array();
 
 				// * plaint text days/hours span info
 				// long-term days
 				$x_trntxx = '' . $event->trntxx;
 				if (!empty($x_trntxx)) {
-					$event_texts[] = $x_trntxx;
+					$event_open[] = $x_trntxx;
 				}
 
 				// long-term hours
 				$x_lochou = '' . $event->lochou;
 				if (!empty($x_lochou)) {
-					$event_texts[] = $x_lochou;
+					$event_open[] = $x_lochou;
 				}
 
 				// hour span
 				$x_evehou = '' . $event->evehou;
 				if (!empty($x_evehou)) {
-					$event_texts[] = $x_evehou;
+					$event_open[] = $x_evehou;
 				}
 
 				// hour span
 				$x_evemtx = '' . $event->evemtx;
+				if (!empty($x_evemtx)) {
+					$event_open[] = $x_evemtx;
+				}
 
+				$event_info['event_open'] = $event_open;
 
+				// Descriptions
 
 				$event_texts = array();
 
-				// Descriptions
 				// * usually the main texts
 				// short description
 				$x_trntxs = '' . $event->trntxs;
@@ -410,197 +398,8 @@ class ED_Parser_SimpleXML {
 
 		}
 
-		//echo "\ncats:\n";
-		//var_dump($all_cats);
-
-//exit(0);
-
-
-
-
-/*
-
-        $wxr_version = $xml->xpath('/rss/channel/wp:wxr_version');
-        if (!$wxr_version) {
-            return array("correct" => false, "errormsg" => "missing wxr version");
-        }
-
-        $wxr_version = (string) trim($wxr_version[0]);
-        // confirm that we are dealing with the correct file format
-        if (!preg_match('/^\d+\.\d+$/', $wxr_version)) {
-            return array("correct" => false, "errormsg" => "missing/invalid wxr version");
-        }
-        $base_url = $xml->xpath('/rss/channel/wp:base_site_url');
-        $base_url = (string) trim($base_url[0]);
-
-        $namespaces = $xml->getDocNamespaces();
-        if (!isset($namespaces['wp'])) {
-            $namespaces['wp'] = 'http://wordpress.org/export/1.1/';
-        }
-        if (!isset( $namespaces['excerpt'])) {
-            $namespaces['excerpt'] = 'http://wordpress.org/export/1.1/excerpt/';
-        }
-        // grab authors
-        foreach ($xml->xpath('/rss/channel/wp:author') as $author_arr) {
-            $a = $author_arr->children($namespaces['wp']);
-            $login = (string) $a->author_login;
-            $authors[$login] = array(
-                'author_id' => (int) $a->author_id,
-                'author_login' => $login,
-                'author_email' => (string) $a->author_email,
-                'author_display_name' => (string) $a->author_display_name,
-                'author_first_name' => (string) $a->author_first_name,
-                'author_last_name' => (string) $a->author_last_name
-            );
-        }
-
-        // grab cats, tags and terms
-        foreach ($xml->xpath('/rss/channel/wp:category') as $term_arr) {
-            $t = $term_arr->children($namespaces['wp']);
-
-            $one_cat_slug = str_replace(array("\"", ":", "/"), array("-", "-", "-"), (string) $t->category_nicename);
-            $one_cat_name = (string) $t->cat_name;
-            //$one_cat_slug = (string) $t->category_nicename;
-            $one_cat_data = array(
-                'term_id' => (int) $t->term_id,
-                'category_nicename' => $one_cat_slug,
-                'category_parent' => (string) $t->category_parent,
-                'cat_name' => $one_cat_name,
-                'category_description' => (string) $t->category_description
-            );
-            $categories[] = $one_cat_data;
-            $categories_by_slug[$one_cat_slug] = $one_cat_data;
-            $categories_slugs_by_name[$one_cat_name] = $one_cat_slug;
-        }
-
-        foreach ($xml->xpath('/rss/channel/wp:tag') as $term_arr) {
-            $t = $term_arr->children($namespaces['wp']);
-            $tags[] = array(
-                'term_id' => (int) $t->term_id,
-                'tag_slug' => (string) $t->tag_slug,
-                'tag_name' => (string) $t->tag_name,
-                'tag_description' => (string) $t->tag_description
-            );
-        }
-
-        foreach ($xml->xpath('/rss/channel/wp:term') as $term_arr) {
-            $t = $term_arr->children( $namespaces['wp'] );
-            $terms[] = array(
-                'term_id' => (int) $t->term_id,
-                'term_taxonomy' => (string) $t->term_taxonomy,
-                'slug' => (string) $t->term_slug,
-                'term_parent' => (string) $t->term_parent,
-                'term_name' => (string) $t->term_name,
-                'term_description' => (string) $t->term_description
-            );
-        }
-
-        // grab posts
-        foreach ($xml->channel->item as $item) {
-            $post = array(
-                'post_title' => (string) $item->title,
-                'guid' => (string) $item->guid,
-            );
-
-            $dc = $item->children('http://purl.org/dc/elements/1.1/');
-            $post['post_author'] = (string) $dc->creator;
-
-            $content = $item->children('http://purl.org/rss/1.0/modules/content/');
-            $excerpt = $item->children($namespaces['excerpt']);
-            $post['post_content'] = (string) $content->encoded;
-            $post['post_excerpt'] = (string) $excerpt->encoded;
-
-            $wp = $item->children($namespaces['wp']);
-            $post['post_id'] = (int) $wp->post_id;
-            $post['post_date'] = (string) $wp->post_date;
-            $post['post_date_gmt'] = (string) $wp->post_date_gmt;
-            $post['comment_status'] = (string) $wp->comment_status;
-            $post['ping_status'] = (string) $wp->ping_status;
-            $post['post_name'] = (string) $wp->post_name;
-            $post['status'] = (string) $wp->status;
-            $post['post_parent'] = (int) $wp->post_parent;
-            $post['menu_order'] = (int) $wp->menu_order;
-            $post['post_type'] = (string) $wp->post_type;
-            $post['post_password'] = (string) $wp->post_password;
-            $post['is_sticky'] = (int) $wp->is_sticky;
-
-            if (isset($item->link)) {
-                $post['link'] = (string) $item->link;
-            }
-
-            if (isset($wp->attachment_url)) {
-                $post['attachment_url'] = (string) $wp->attachment_url;
-            }
-            foreach ($item->category as $c) {
-                $att = $c->attributes();
-                if (isset($att['nicename'])) {
-                    $post['terms'][] = array(
-                        'name' => (string) $c,
-                        'slug' => str_replace(array("\"", ":", "/"), array("-", "-", "-"), (string) $att['nicename']),
-                        //'slug' => (string) $att['nicename'],
-                        'domain' => (string) $att['domain']
-                    );
-                }
-            }
-
-            foreach ( $wp->postmeta as $meta ) {
-                $post['postmeta'][] = array(
-                    'key' => (string) $meta->meta_key,
-                    'value' => (string) $meta->meta_value,
-                );
-            }
-
-            foreach ( $wp->comment as $comment ) {
-                $post['comments'][] = array(
-                    'comment_id' => (int) $comment->comment_id,
-                    'comment_author' => (string) $comment->comment_author,
-                    'comment_author_email' => (string) $comment->comment_author_email,
-                    'comment_author_IP' => (string) $comment->comment_author_IP,
-                    'comment_author_url' => (string) $comment->comment_author_url,
-                    'comment_date' => (string) $comment->comment_date,
-                    'comment_date_gmt' => (string) $comment->comment_date_gmt,
-                    'comment_content' => (string) $comment->comment_content,
-                    'comment_approved' => (string) $comment->comment_approved,
-                    'comment_type' => (string) $comment->comment_type,
-                    'comment_parent' => (string) $comment->comment_parent,
-                    'comment_user_id' => (int) $comment->comment_user_id,
-                );
-            }
-
-            $posts[] = $post;
-        }
-
-        $wxr_title = "";
-        $wxr_title_arr = $xml->xpath('/rss/channel/title');
-        if ($wxr_title_arr) {
-            $wxr_title = $wxr_title_arr[0];
-        }
-
-        $wxr_link = "";
-        $wxr_link_arr = $xml->xpath('/rss/channel/link');
-        if ($wxr_link_arr) {
-            $wxr_link = $wxr_link_arr[0];
-        }
-
-        return array(
-            'correct' => true,
-            'errormsg' => "",
-            'authors' => $authors,
-            'posts' => $posts,
-            'categories' => $categories,
-            'categories_by_slug' => $categories_by_slug,
-            'categories_slugs_by_name' => $categories_slugs_by_name,
-            'tags' => $tags,
-            'terms' => $terms,
-            'base_url' => $base_url,
-            'version' => $wxr_version,
-            'title' => $wxr_title,
-            'link' => $wxr_link,
-        );
-*/
-
     } // fn parse
-} // class WXR_Parser_SimpleXML
+} // class EventData_Parser_SimpleXML
 
 
 $known_categories = array(
@@ -630,7 +429,7 @@ $known_categories = array(
 
 $provider_id = 1;
 $fname = 'eventexport.xml';
-$ed_parser = new EventParser_Parser();
+$ed_parser = new EventData_Parser();
 $ed_parser->parse($provider_id, $fname, $known_categories);
 
 
