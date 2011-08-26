ea68cb5bb9219718d19d23800b3fd3c38dc0c085
Holman Romero <holman.romero@sourcefabric.org>
2011-08-26 17:55:20 +0200

    CS-3533: Message management in admin interface
    Fixed sizing of cells in the datatable by adding missing css classes.
    Added filter buttons.
    Added number of items displayed at the top of the datatable to correspond to the layout for comments.
    Number of items per page control moved up besides search box.
    Fixed date displaying wrong month number.

--- newscoop/application/modules/admin/controllers/FeedbackController.php
+++ newscoop/application/modules/admin/controllers/FeedbackController.php
@@ -1,8 +1,7 @@
 <?php
-
 /**
  * @package Newscoop
- * @subpackage Subscriptions
+ * @subpackage Feedback 
  * @copyright 2011 Sourcefabric o.p.s.
  * @license http://www.gnu.org/licenses/gpl-3.0.txt
  *
@@ -31,7 +30,6 @@ class Admin_FeedbackController extends Zend_Controller_Action
         $this->feedbackRepository = $this->_helper->entity->getRepository('Newscoop\Entity\Feedback');
 
         $this->form = new Admin_Form_Comment;
-
         $this->editForm = new Admin_Form_Comment_EditForm;
 
         return $this;
@@ -52,9 +50,11 @@ class Admin_FeedbackController extends Zend_Controller_Action
         /* @var $table Action_Helper_Datatable */
         $table->setDataSource($this->feedbackRepository);
         $table->setOption('oLanguage',array('sSearch'=>''));
-        $table->setCols(array('index' => $view->toggleCheckbox(), 'user' => getGS('Author'),
-                             'message' => getGS('Date') . ' / ' . getGS('Message'), 'url' => getGS('URL')),
-                              array('index' => false));
+        $table->setCols(array(
+            'index' => $view->toggleCheckbox(), 'user' => getGS('Author'),
+            'message' => getGS('Date') . ' / ' . getGS('Message'), 'url' => getGS('URL')),
+            array('index' => false)
+        );
         $index = 1;
         $table->setHandle(function($feedback) use ($view, &$index)
             {
@@ -63,35 +63,36 @@ class Admin_FeedbackController extends Zend_Controller_Action
                 $url = $feedback->getUrl();
                 $message = $feedback->getMessage();
                 $result = array(
-					'index' => $index++,
+                    'index' => $index++,
                     'user' => array(
-						'username' => $user->getUsername(),
-						'name' => $user->getFirstName(),
-						'email' => $user->getEmail(),
-						'avatar' => (string)$view->getAvatar($user->getEmail(), array('img_size' => 50, 'default_img' => 'wavatar'))
-					),
+                        'username' => $user->getUsername(),
+                        'name' => $user->getFirstName(),
+                        'email' => $user->getEmail(),
+                        'avatar' => (string)$view->getAvatar($user->getEmail(), array('img_size' => 50, 'default_img' => 'wavatar'))
+                    ),
                     'message' => array(
-						'id' => $feedback->getId(),
-						'created' => array(
-							'date' => $feedback->getTimeCreated()->format('Y.i.d'),
-                            'time' => $feedback->getTimeCreated()->format('H:i:s')),
+                        'id' => $feedback->getId(),
+                        'created' => array(
+                            'date' => $feedback->getTimeCreated()->format('Y.i.d'),
+                            'time' => $feedback->getTimeCreated()->format('H:i:s')
+                        ),
                         'message' => $feedback->getMessage(),
                         'subject' => $feedback->getSubject()
                     ),
                     'url' => $url
-				);
-				return($result);
+                );
+                return($result);
             });
 
         $table->setOption('fnDrawCallback', 'datatableCallback.draw')
                 ->setOption('fnRowCallback', 'datatableCallback.row')
                 ->setOption('fnServerData', 'datatableCallback.addServerData')
                 ->setOption('fnInitComplete', 'datatableCallback.init')
-                ->setOption('sDom','<"top">f<"#actionExtender">lrt<"bottom"ip>')
+                ->setOption('sDom','<"top">lf<"#actionExtender">rit<"bottom"ip>')
                 ->setStripClasses()
                 ->toggleAutomaticWidth(false)
                 ->setDataProp(array('index' => null, 'user' => null, 'message' => null, 'url' => null))
-                ->setClasses(array('index' => 'commentId', 'commenter' => 'commentUser', 'comment' => 'commentTimeCreated', 'thread' => 'commentThread'));
+                ->setClasses(array('index' => 'commentId', 'user' => 'commentUser', 'message' => 'commentTimeCreated', 'url' => 'commentThread'));
         $table->dispatch();
         $this->editForm->setSimpleDecorate()->setAction($this->_helper->url('update'));
         $this->view->editForm = $this->editForm;
