6ecf350f6d2f339d3884d8d3abbec3ee70004020
petr.jasek <petr.jasek@sourcefabric.org>
2011-09-20 18:35:43 +0200

    Fix deprecated smarty calls.

--- newscoop/include/smarty/campsite_plugins/function.unset_map.php
+++ newscoop/include/smarty/campsite_plugins/function.unset_map.php
@@ -19,7 +19,7 @@
 function smarty_function_unset_map($p_params, &$p_smarty)
 {
     // gets the context variable
-    $campsite = $p_smarty->get_template_vars('gimme');
+    $campsite = $p_smarty->getTemplateVars('gimme');
 
     $campsite->map_dynamic_constraints = null;
     $campsite->map_dynamic_areas = null;
