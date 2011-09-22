f2c8988e5b7dc83c271b85c68b07319e3b86c0fd
Martin Saturka <martin.saturka@sourcefabric.org>
2011-09-22 19:55:11 +0200

    WOBS-96: Importing XML-files for movies and cinemas * ad adjusting; a forgotten file

---
+++ plugins/newsimport/include/default_spool.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Where common cached info shall be stored.
+ * @var string
+ */
+$newsimport_default_cache = 'files/newsimport/caches/';
+
+/**
+ * Where common run locks shall be stored.
+ * @var string
+ */
+$newsimport_default_locks = 'files/newsimport/spool/';
+
