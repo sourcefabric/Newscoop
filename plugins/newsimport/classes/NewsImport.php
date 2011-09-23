6b293968d6382ff8b1b402e0857c33cb948ee3ac
Martin Saturka <martin.saturka@sourcefabric.org>
2011-09-23 18:57:52 +0200

    WOBS-48: Importing XML files for general events * fixed checking for situations where _SERVER is not filled

--- plugins/newsimport/classes/NewsImport.php
+++ plugins/newsimport/classes/NewsImport.php
@@ -42,6 +42,10 @@ class NewsImport
         //looking whether the request is of form used for xml import, i.e.
         //http(s)://newscoop_domain/(newscoop_dir/)_xmlimport(/...)(?...)
 
+        if (!isset($_SERVER['REQUEST_URI'])) {
+            return false;
+        }
+
         $path_request_parts = explode('?', $_SERVER['REQUEST_URI']);
         $path_request = strtolower($path_request_parts[0]);
         if (('' == $path_request) || ('/' != $path_request[strlen($path_request)-1])) {
