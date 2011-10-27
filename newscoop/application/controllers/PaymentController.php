5d847818d366c722db6a0905e0d5f268bdcab225
vlad <vlad@ubuntu.(none)>
2011-10-27 18:02:26 +0300

    WOBS-42 : Pay What You Like widget
    Sha signature plus misc

--- newscoop/application/controllers/PaymentController.php
+++ newscoop/application/controllers/PaymentController.php
@@ -17,25 +17,37 @@ class PaymentController extends Zend_Controller_Action
 
     public function postfinanceAction()
     {
-        //$params = $this->_request->getParams();
-
-        /*
-        $params = $this->_reqeust->getParams();
-
-        $client = new Zend_Http_Client();
-        $client->setUri('http://example.org');
-        $client->setMethod(Zend_Http_Client::POST);
-        $client->setConfig( array
-        (
-    		'maxredirects' => 0,
-    		'timeout'      => 30
-        ));
-        $client->setParameterPost(array
-        (
-    		'abc'  => '123',
-    		'xyz'  => 123,
-    		'smth' => array('a', 'b', 3)
-        ));
-        */
+    	$this->_helper->layout->disableLayout(true);
+        $params = $this->_request->getParams();
+        
+        if( !array_key_exists('accepturl', $params) || !array_key_exists('amount', $params) 
+	        || !array_key_exists('currency', $params) || !array_key_exists('language', $params)
+	        || !array_key_exists('orderID', $params) || !array_key_exists('PSPID', $params)
+        ) {
+			die('Forbidden');        	
+        }
+        
+        
+        $shaPass = 'nzzonline123456#';
+        
+        $accepturl = $params['accepturl'];
+        $amount = $params['amount'];
+        $currency = $params['currency'];
+        $language = $params['language'];
+        $orderId = $params['orderID'];
+        $PSPID = $params['PSPID'];
+        
+        $shaString = "ACCEPTURL=".$accepturl.$shaPass."AMOUNT=".$amount.$shaPass."CURRENCY=".$currency.$shaPass."LANGUAGE=".$language.$shaPass."ORDERID=".$orderId.$shaPass."PSPID=".$PSPID.$shaPass;
+        
+    	$SHASign = sha1($shaString);
+    	
+    	$this->view->amount = $amount;
+    	$this->view->accepturl = $accepturl;
+    	$this->view->realAmount = round($amount/100, 2);
+    	$this->view->language = $language;
+    	$this->view->orderID = $orderId;
+    	$this->view->PSPID = $PSPID;
+    	$this->view->SHASign = $SHASign;
+    	
     }
 }
