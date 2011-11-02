0a428d145435ac2cfe728353ce8e7927f6333772
Naomi Aro <naomiaro@gmail.com>
2011-11-02 16:47:00 +0100

    WOBS-233 Remove no-cache instructions from headers (html and image)

--- newscoop/application/plugins/ContentType.php
+++ newscoop/application/plugins/ContentType.php
@@ -8,6 +8,13 @@ class Application_Plugin_ContentType extends Zend_Controller_Plugin_Abstract
 
         // When no Content-Type has been set, set the default text/html; charset=utf-8
         $response->setHeader('Content-Type', 'text/html; charset=utf-8');
+
+        switch ($request->getModuleName()) {
+            case 'default':
+                $response->setHeader('Cache-Control', 'public, max-age=3600', true);
+                $response->setHeader('Pragma', 'cache', true);
+                break;
+        }
     }
 }
 
