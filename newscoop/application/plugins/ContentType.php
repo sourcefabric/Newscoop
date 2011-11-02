1ce19e8cc3e8d0c67dd50ad484b5a05948e4a068
Naomi Aro <naomiaro@gmail.com>
2011-11-02 16:50:53 +0100

    WOBS-233 Remove no-cache instructions from headers (html and image)

--- newscoop/application/plugins/ContentType.php
+++ newscoop/application/plugins/ContentType.php
@@ -4,6 +4,7 @@ class Application_Plugin_ContentType extends Zend_Controller_Plugin_Abstract
 {
     public function dispatchLoopShutdown()
     {
+        $request = $this->getRequest();
         $response = $this->getResponse();
 
         // When no Content-Type has been set, set the default text/html; charset=utf-8
