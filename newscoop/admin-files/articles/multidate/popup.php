548138191c8034e7c73ca8e1be2c0fc7bffe0f5e
Vlad <vlad@vlad-T500.(none)>
2011-12-01 13:26:19 +0200

    CS-3893 : Multiple dates UI - AJAX integration with backend
    Very small change made for a test

--- newscoop/admin-files/articles/multidate/popup.php
+++ newscoop/admin-files/articles/multidate/popup.php
@@ -21,6 +21,7 @@ $f_publication_id = Input::Get('f_publication_id', 'int', 0);
 $f_issue_number = Input::Get('f_issue_number', 'int', 0);
 $f_section_number = Input::Get('f_section_number', 'int', 0);
 $f_language_id = Input::Get('f_language_id', 'int', 1);
+
 if (isset($_SESSION['f_language_selected'])) {
 	$f_old_language_selected = (int)$_SESSION['f_language_selected'];
 } else {
