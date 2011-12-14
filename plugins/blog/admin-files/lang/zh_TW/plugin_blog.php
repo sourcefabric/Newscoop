88fe0f5be2f85f6360ba3bbb822676c97280fad8
Holman Romero <holman.romero@sourcefabric.org>
2011-12-14 11:26:19 +0100

    Removing NewsDesk widget from the dashboard widgets and blog and interview plugins

--- plugins/blog/admin-files/lang/zh_TW/plugin_blog.php
+++
@@ -1,5 +0,0 @@
-<?php 
-regGS("List comments", "");
-regGS("User may moderate Blogs", "");
-regGS("User may manage Blogs", "");
-?>
--- plugins/interview/install/sql/update.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `plugin_interview_items` ADD `question_date` DATETIME NOT NULL AFTER `question` ;
-ALTER TABLE `plugin_interview_items` ADD `answer_date` DATETIME NOT NULL AFTER `answer` ;
