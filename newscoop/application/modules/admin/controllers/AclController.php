53f1dbd36708dde9395a189c644ad50302dc4bb3
Holman Romero <holman.romero@sourcefabric.org>
2011-12-14 09:57:56 +0100

    Disabling Newswires Ingest for 4.0 beta release

--- newscoop/application/modules/admin/controllers/AclController.php
+++ newscoop/application/modules/admin/controllers/AclController.php
@@ -46,7 +46,7 @@ class Admin_AclController extends Zend_Controller_Action
         $this->resources = array(
             'authoring' => array(
                 'article' => getGS('Articles'),
-                'ingest' => getGS('Newswires Ingest'),
+                // 'ingest' => getGS('Newswires Ingest'),
                 'image' => getGS('Images'),
                 'comment' => getGS('Comments'),
                 'feedback' => getGS('Feedback Messages'),
