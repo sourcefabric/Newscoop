d8cbcc755cc5a047c3f48ab40451cae09dce0c40
Martin Saturka <martin.saturka@sourcefabric.org>
2012-09-18 10:50:09 +0200

    CS-4456: Remove TW-specific article listing * done

--- newscoop/template_engine/classes/ArticlesList.php
+++ newscoop/template_engine/classes/ArticlesList.php
@@ -61,7 +61,6 @@ class ArticlesList extends ListObject
                                           'bysectionorder',
                                           'bycomments',
                                           'bylastcomment',
-                                          'bypriority',
                                           'bykeywords',
                                     );
 
