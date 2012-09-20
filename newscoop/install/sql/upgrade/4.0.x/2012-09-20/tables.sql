592916ea830ab0ddc77986933ece1c3d6a846f58
petr.jasek <petr.jasek@sourcefabric.org>
2012-09-20 15:47:23 +0200

    CS-4419: in case huge amount of images it can be an perfomance issue since all images placed in same folder w/o sorting to subfolder
    Remove unnecessary db modification.

--- newscoop/install/sql/upgrade/4.0.x/2012-09-20/tables.sql
+++ newscoop/install/sql/upgrade/4.0.x/2012-09-20/tables.sql
@@ -1,4 +1,2 @@
-ALTER TABLE  `Images` CHANGE  `ThumbnailFileName`  `ThumbnailFileName` VARCHAR( 80 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT  '',
-CHANGE  `ImageFileName`  `ImageFileName` VARCHAR( 80 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT  '';
 ALTER TABLE  `Images` ADD  `is_updated_storage` TINYINT( 1 ) NOT NULL DEFAULT  '0',
-ADD INDEX (  `is_updated_storage` );
+ADD INDEX (  `is_updated_storage`, `ImageFileName` );
