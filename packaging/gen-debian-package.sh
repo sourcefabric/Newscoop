555dd4154e2bb1a20d4b0c9bf7eee84db20ba700
Daniel James <daniel@64studio.com>
2012-12-12 11:29:38 +0000

    Bring packaging files up to date in master branch

--- packaging/gen-debian-package.sh
+++ packaging/gen-debian-package.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# usage from git-repo: 
+# usage from git-repo:
 #  ./gen-debian-package.sh -rfakeroot -sa
 
 #
@@ -10,8 +10,10 @@ DEBRELEASE=$(head -n1 debian/changelog | cut -d ' ' -f 2 | sed 's/[()]*//g')
 DEBVERSION=$(echo $DEBRELEASE | sed 's/-.*$//g;s/~test[0-9]*//g')
 UPSTREAMVERSION=$(echo $DEBVERSION | sed 's/~/-/g')
 UPSTREAMDIST=$(echo $UPSTREAMVERSION | sed 's/^\([0-9]*\.[0-9]*\).*$/\1/')
+SFOCUSTOM=""
 DEBPATH=`pwd`/debian # TODO check dirname $0
 MIRRORPATH=/tmp
+BUILDDEST=/tmp/newscoop-${DEBVERSION}/
 
 #if test "${UPSTREAMDIST}" == "3.5"; then
 #	CUSTOMURL=http://www.sourcefabric.org/attachment2/000000024.gz
@@ -23,7 +25,7 @@ if test ! -d ${DEBPATH}; then
 fi
 
 echo "Debian Release:   ${DEBRELEASE}"
-echo "Upstream Version: ${UPSTREAMVERSION}"
+echo "Upstream Version: ${UPSTREAMVERSION}${SFOCUSTOM}"
 echo "Major:            ${UPSTREAMDIST}"
 echo "debuild opts:     $@"
 echo "build folder:     /tmp/newscoop-$DEBVERSION"
@@ -42,9 +44,9 @@ echo " +++ downloading upstream release.."
 if [ -f ${MIRRORPATH}/newscoop-$UPSTREAMVERSION.tar.gz ]; then
 	echo "using local file ${MIRRORPATH}/newscoop-$UPSTREAMVERSION.tar.gz"
   tar xzf ${MIRRORPATH}/newscoop-$UPSTREAMVERSION.tar.gz
-elif [ -f ${MIRRORPATH}/newscoop-$UPSTREAMDIST.tar.gz ]; then
-	echo "using local file ${MIRRORPATH}/newscoop-$UPSTREAMDIST.tar.gz"
-  tar xzf ${MIRRORPATH}/newscoop-$UPSTREAMDIST.tar.gz
+elif [ -f ${MIRRORPATH}/newscoop-$UPSTREAMDIST$SFOCUSTOM.tar.gz ]; then
+	echo "using local file ${MIRRORPATH}/newscoop-$UPSTREAMDIST$SFOCUSTOM.tar.gz"
+  tar xzf ${MIRRORPATH}/newscoop-$UPSTREAMDIST$SFOCUSTOM.tar.gz
 elif [ -n "$CUSTOMURL" ]; then
 	echo "download ${CUSTOMURL}"
 	curl -L ${CUSTOMURL} \
@@ -54,14 +56,48 @@ else
 	echo "download from sourceforge."
   #curl -L http://downloads.sourceforge.net/project/newscoop/$UPSTREAMDIST/newscoop-$UPSTREAMVERSION.tar.gz | tar xzf -
   curl -L http://downloads.sourceforge.net/project/newscoop/$UPSTREAMVERSION/newscoop-$UPSTREAMVERSION.tar.gz \
-		| tee ${MIRRORPATH}/newscoop-$UPSTREAMVERSION.tar.gz \
+		| tee ${MIRRORPATH}/newscoop-$UPSTREAMDIST$SFOCUSTOM.tar.gz \
 		| tar xzf - || exit
 fi
 
-# done in README.debian
+mv ${MIRRORPATH}/newscoop-$UPSTREAMVERSION/newscoop-$UPSTREAMVERSION/ ${MIRRORPATH}/newscoop-$UPSTREAMVERSION/newscoop/
+
+# done in README.Debian
 rm newscoop/INSTALL.txt
+
+# Sourcefabric licenses covered by debian/copyright
+rm newscoop/COPYING.txt
+rm newscoop/LICENSE_3RD_PARTY.txt
+
+# third party licences covered by debian/copyright
+rm newscoop/include/html2pdf/_tcpdf_5.0.002/LICENSE.TXT
+rm newscoop/js/domTT/LICENSE
+rm newscoop/js/flowplayer/LICENSE.txt
+rm newscoop/js/geocoding/openlayers/license.txt
+rm newscoop/js/plupload/license.txt
+rm newscoop/js/tinymce/license.txt
+rm newscoop/library/Nette/license.txt
+rm newscoop/library/Symfony/Component/Console/LICENSE
+rm newscoop/library/Symfony/Component/Yaml/LICENSE
+rm newscoop/library/fabpot-dependency-injection-07ff9ba/LICENSE
+rm newscoop/library/fabpot-event-dispatcher-782a5ef/LICENSE
+rm newscoop/include/html2pdf/_tcpdf_5.0.002/fonts/dejavu-fonts-ttf-2.30/LICENSE
+rm newscoop/include/html2pdf/_tcpdf_5.0.002/fonts/freefont-20090104/COPYING
+rm newscoop/js/tapmodo-Jcrop-5e58bc9/MIT-LICENSE.txt
+rm newscoop/js/tapmodo-Jcrop-5e58bc9/build/LICENSE
+
+# remove documentation under Creative Commons licenses
+rm -r newscoop/library/fabpot-dependency-injection-07ff9ba/doc/
+rm -r newscoop/library/fabpot-event-dispatcher-782a5ef/doc/
+
+# remove fonts installed as a package dependency
+rm -r newscoop/include/captcha/fonts/
+
+# fix the font path for captcha
+sed -i "5s:('fonts/VeraBd.ttf', 'fonts/VeraIt.ttf', 'fonts/Vera.ttf'):('/usr/share/fonts/truetype/ttf-dejavu/DejaVuSans-Bold.ttf', '/usr/share/fonts/truetype/ttf-dejavu/DejaVuSansMono.ttf', '/usr/share/fonts/truetype/ttf-dejavu/DejaVuSerif.ttf'):g" newscoop/include/captcha/image.php
+
 # documentation for /usr/share/doc/newscoop
-for file in ChangeLog CREDITS COPYING README UPGRADE; do
+for file in ChangeLog CREDITS README UPGRADE; do
   mv -vi newscoop/${file}.txt ./${file}
 done
 cp -vi newscoop/htaccess newscoop/.htaccess
@@ -89,19 +125,36 @@ if test "${UPSTREAMVERSION}" == "3.5.1"; then
 	rm newscoop/javascript/tinymce/plugins/codehighlighting/img/Thumbs.db
 fi
 
-### notify phone home metrics about debian
-touch newscoop/debian
+### fixes for 4.0.2 ###
+if test "${UPSTREAMVERSION}" == "4.0.2"; then
+
+	rm newscoop/install/sample_templates/rockstar/templates/set_rockstar/assets/.DS_Store
+	rm newscoop/install/sample_templates/rockstar/templates/set_rockstar/assets/css/.DS_Store
+	rm newscoop/install/sample_templates/rockstar/templates/set_rockstar/assets/fonts/.DS_Store
+	rm newscoop/install/sample_templates/rockstar/templates/set_rockstar/assets/js/libs/.DS_Store
+	rm newscoop/install/sample_templates/rockstar/templates/set_rockstar/pictures/.DS_Store
+	rm newscoop/themes/unassigned/set_rockstar/assets/.DS_Store
+	rm newscoop/themes/unassigned/set_rockstar/assets/css/.DS_Store
+	rm newscoop/themes/unassigned/set_rockstar/assets/fonts/.DS_Store
+	rm newscoop/themes/unassigned/set_rockstar/assets/js/libs/.DS_Store
+	rm newscoop/themes/unassigned/set_rockstar/pictures/.DS_Store
+
+fi
 
 ############################
 
+cd ../
+tar czf newscoop_${UPSTREAMVERSION}.orig.tar.gz  newscoop-${DEBVERSION}/newscoop/
+cd ${BUILDDEST} || exit
+
 cp -avi $DEBPATH ./ || exit
-debuild $@ || exit
+debuild -k174C1854 $@ || exit
 
 ls -l /tmp/newscoop*deb
 ls -l /tmp/newscoop*changes
 
 lintian -i --pedantic /tmp/newscoop_${DEBRELEASE}_*.changes | tee /tmp/newscoop-${DEBRELEASE}.issues
 
-echo -n "UPLOAD? [enter|CTRL-C]" ; read
+#echo -n "UPLOAD? [enter|CTRL-C]" ; read
 
-dput rg42 /tmp/newscoop_${DEBRELEASE}_*.changes 
+#dput sfo /tmp/newscoop_${DEBRELEASE}_*.changes
