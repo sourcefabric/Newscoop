8a132f4f5b23c3ef4d1abc8c90ced51e929e6e59
petr.jasek <petr.jasek@sourcefabric.org>
2013-01-25 18:01:37 +0100

    Remove old tests from repo.

--- newscoop/tests/old/bugreporter/notice.php
+++
@@ -1,5 +0,0 @@
-<?php
-    echo "<p>html directory: " . $Campsite[HTML_DIR] . "</p>";
-    echo "<p>line following a PHP Notice error.</p>";
-
-?>
--- newscoop/tests/old/set_path.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$GLOBALS['g_campsiteDir'] = dirname(dirname(__FILE__));
-
-set_include_path(get_include_path() . PATH_SEPARATOR . $GLOBALS['g_campsiteDir']);
-
-?>
