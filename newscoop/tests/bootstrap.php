0af945811c4c25f5b59870837f7d762e3d9b9c1e
petr.jasek <petr.jasek@sourcefabric.org>
2013-01-31 18:04:29 +0100

    Fix unit tests bootstrap.

--- newscoop/tests/bootstrap.php
+++ newscoop/tests/bootstrap.php
@@ -3,15 +3,13 @@
 error_reporting(E_ALL & ~E_DEPRECATED & ~E_STRICT);
 
 define('APPLICATION_ENV', 'testing');
-define('IN_PHPUNIT', true);
-define('INSTALL', false); // disable redirect
 define('DIR_SEP', DIRECTORY_SEPARATOR);
 
-require_once __DIR__ . '/../../application.php';
+require_once __DIR__ . '/../application.php';
 
 $application->bootstrap('container');
 $application->bootstrap('session');
 
-require_once __DIR__ . '/../TestCase.php';
-require_once __DIR__ . '/../RepositoryTestCase.php';
-require_once __DIR__ . '/../AdoDbDoctrineAdapter.php';
+require_once __DIR__ . '/TestCase.php';
+require_once __DIR__ . '/RepositoryTestCase.php';
+require_once __DIR__ . '/AdoDbDoctrineAdapter.php';
