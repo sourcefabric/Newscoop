2f834d76e8852b6a906ef605898038a2ee8ca008
Yorick Terweijden <yorick.terweijden@sourcefabric.org>
2013-05-07 13:53:24 +0200

    Moves the translation strings for the Feedback page from the Comments to the Feedback translation file

--- newscoop/admin-files/lang/es/feedback.php
+++ newscoop/admin-files/lang/es/feedback.php
@@ -1,2 +1,28 @@
 <?php 
+regGS("Approve", "Aprobar");
+regGS("Approved.", "Aprobado");
+regGS("at", "en");
+regGS("Attachment", "Adjunto");
+regGS("Ban/Unban user", "Bloquear/desbloquear usuario");
+regGS("Change selected messages status", "Cambiar los mensajes de estatus seleccionados");
+regGS("Deleted", "Eliminado");
+regGS("Full article", "Artículo completo");
+regGS("Message", "Mensaje");
+regGS("Message deleted.", "Mensaje eliminado.");
+regGS("Messages deleted.", "Mensajes eliminados.");
+regGS("Messages status change to $1.", "El estatus del mensaje cambió a $1.");
+regGS("Message status change to $1.", "Estatus del mensaje cambió a $1.");
+regGS("Messages updated.", "Mensajes actualizados");
+regGS("Message updated.", "Mensaje actualizado.");
+regGS("No atachment", "Sin adjunto");
+regGS("Processed", "Procesado");
+regGS("Reply", "Responder");
+regGS("Reply to message", "Responder a mensaje");
+regGS("Star", "Ponderación");
+regGS("Starred", "Ponderado");
+regGS("Unable to reach Newscoop. Please check your internet connection.", "No se pudo conectar a Newscoop. Por favor, verifique su conexión a Internet.");
+regGS("With document attachment", "Con documento adjunto");
+regGS("With image attachment", "Con imagen adjunta");
+regGS("With no attachment", "Sin adjunto");
+regGS("Feedback Messages", "Mensajes de retroalimentación");
 ?>
