26c8cdeba6e5a098aeae2ab3e4339a15612088b0
Paweł Mikołajczuk <pawel.mikolajczuk@sourcefabric.org>
2013-05-28 17:13:46 +0200

    improve upgrade from old versions

--- newscoop/install/sql/upgrade/4.2.x/2013.05.28/move_files.php
+++ newscoop/install/sql/upgrade/4.2.x/2013.05.28/move_files.php
@@ -1,11 +1,65 @@
 <?php
 
 //move all files from /files to /public/files
-
 $newscoop_dir = __DIR__ . '/../../../../..';
-rmove($newscoop_dir . '/files', $newscoop_dir . '/public/files');
-rmdir($newscoop_dir . '/files');
 
+$files = array(
+	'/application/configs/application.ini-dist',
+	'/library/Newscoop/Services/EventDispatcherService.php',
+	'/application/modules/admin/controllers/TestController.php',
+	'/library/Newscoop/Entity/User/Subscriber.php',
+	'/library/Newscoop/Entity/Entity.php',
+	'/.disable_upgrade',
+	'/README.txt',
+	'/.travis.yml',
+	'/UPGRADE.txt'
+);
+
+$dirs = array(
+	'/library/Newscoop/Entity/Proxy',
+	'/library/fabpot-dependency-injection-07ff9ba',
+	'/library/fabpot-event-dispatcher-782a5ef',
+	'/library/smarty3',
+	'/docs',
+	'/files',
+	'/videos',
+);
+
+$notWritable = array();
+foreach(array_merge($files, $dirs) as $object) {
+	if (file_exists($newscoop_dir . $object)) {
+		if (!is_writable($newscoop_dir . $object)) {
+			$notWritable[] = $newscoop_dir . $object;
+		}
+	}
+}
+
+if (count($notWritable)) {
+	foreach($notWritable as $object) {
+		echo 'File/dir "'.$object.'" must be writable'."\n";
+	}
+	exit;
+}
+
+
+rmove($newscoop_dir . '/files', $newscoop_dir . '/public/files');
+rmove($newscoop_dir . '/videos', $newscoop_dir . '/public/videos');
+
+foreach($files as $file) {
+	if (file_exists($newscoop_dir . $file)) {
+		if(unlink(realpath($newscoop_dir . $file)) !== true) {
+			echo 'Please remove file manulay rm "'.realpath($newscoop_dir . $file).'"'."\n";
+		};
+	}
+}
+
+foreach($dirs as $dir) {
+	if (file_exists($newscoop_dir . $dir)) {
+		if(rrmdir(realpath($newscoop_dir . $dir)) !== true) {
+			echo 'Please remove directory manulay rm -R "'.realpath($newscoop_dir . $dir).'"'."\n";
+		};
+	}
+}
 
 /**
  * Recursively move files from one directory to another
@@ -33,8 +87,23 @@ function rmove($src, $dest)
 			rename($f->getRealPath(), "$dest/" . $f->getFilename());
 		} else if(!$f->isDot() && $f->isDir()) {
 			rmove($f->getRealPath(), "$dest/$f");
-			unlink($f->getRealPath());
+			@unlink($f->getRealPath());
 		}
 	}
-	unlink($src);
+	@unlink($src);
+}
+
+// When the directory is not empty:
+function rrmdir($dir) {
+   	if (is_dir($dir)) {
+     	$objects = scandir($dir);
+    	foreach ($objects as $object) {
+       		if ($object != "." && $object != "..") {
+         		if (filetype($dir."/".$object) == "dir") rrmdir($dir."/".$object); else unlink($dir."/".$object);
+       		}
+     	}
+    	reset($objects);
+    	
+    	return rmdir($dir);
+   }
 }
