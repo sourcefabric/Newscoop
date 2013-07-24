3ceda44380dfc77a0f0fae8d23c35e19e6d8cc18
Daniel James <daniel@64studio.com>
2013-07-24 12:50:22 +0100

    Also convert the localization files for Newscoop plugins

--- scripts/localization/generate-po-files.sh
+++ scripts/localization/generate-po-files.sh
@@ -8,8 +8,14 @@ BUILDPATH=/tmp
 # Set the path to the Newscoop localization files
 GITPATH=newscoop/admin-files/lang/
 
+# Set the path to the Newscoop plugins
+PLUGINPATH=plugins/
+
 # Set the locales you want to convert
 LOCALES="ar be bn cs de de_AT el en en_GB es fr he hr hu it ka ko ku nl pl pt pt_BR ro ru sh sq sr sv uk zh zh_TW"
+
+# Set the plugins you want to convert
+PLUGINS="debate poll recaptcha soundcloud"
 
 echo "Cleaning up any previous builds..."
 
@@ -21,8 +27,14 @@ mkdir -p ${BUILDPATH}/po/
 
 echo "Copying the files to the temporary directory..."
 
-cd ${GITPATH}
-cp -r ${LOCALES} ${BUILDPATH}/po/
+for localization in ${LOCALES}; do
+cp -r ${GITPATH}${localization} ${BUILDPATH}/po/
+
+ for plugin in ${PLUGINS};
+ do cp -r ${PLUGINPATH}${plugin}/admin-files/lang/${localization}/plugin_${plugin}.php ${BUILDPATH}/po/${localization}/;
+ done
+
+done
 
 # Begin function that converts the files
 
