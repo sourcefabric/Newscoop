aa7ba7c2663d3320762ab7c5ef9365e14bc3d346
Yorick Terweijden <yorick.terweijden@sourcefabric.org>
2013-08-12 13:41:58 +0200

    Implements CS-4782

--- newscoop/include/smarty/campsite_plugins/function.build_solr_fq.php
+++ newscoop/include/smarty/campsite_plugins/function.build_solr_fq.php
@@ -70,7 +70,7 @@ function smarty_function_build_solr_fq($p_params = array(), &$p_smarty)
     if (array_key_exists('fqto', $cleanParam) && !empty($cleanParam['fqto'])) {
         $toDate = date_create_from_format($cleanParam['fqdateformat'], $cleanParam['fqto']);
         if ($toDate instanceof \DateTime) {
-            $solrToDate = date_format($toDate, 'Y-m-d').'T00:00:00Z/DAY';
+            $solrToDate = date_format($toDate, 'Y-m-d').'T23:59:59Z/DAY';
         }
     }
 
