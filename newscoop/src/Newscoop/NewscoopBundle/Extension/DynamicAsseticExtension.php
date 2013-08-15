e871af327853c71ed5abd627ce0ef8dfcd22dbe4
Paweł Mikołajczuk <pawel.mikolajczuk@sourcefabric.org>
2013-08-15 23:16:27 +0200

    override smartybundle plugins names - we need to use newscoop image smarty plugin

--- newscoop/src/Newscoop/NewscoopBundle/Extension/DynamicAsseticExtension.php
+++ newscoop/src/Newscoop/NewscoopBundle/Extension/DynamicAsseticExtension.php
@@ -8,12 +8,51 @@
 namespace Newscoop\NewscoopBundle\Extension;
 
 use Newscoop\NewscoopBundle\Extension\AsseticExtension;
-use NoiseLabs\Bundle\SmartyBundle\Extension\DynamicAsseticExtension as SmartyDynamicAsseticExtension;
+use Assetic\Factory\AssetFactory;
+use Assetic\Asset\AssetInterface;
+use Assetic\ValueSupplierInterface;
+use NoiseLabs\Bundle\SmartyBundle\Extension\RoutingExtension;
+use NoiseLabs\Bundle\SmartyBundle\Exception\RuntimeException;
 
 /**
  * The "dynamic" reincarnation of AsseticExtension.
  *
  * @author Vítor Brandão <vitor@noiselabs.com>
  */
-class DynamicAsseticExtension extends SmartyDynamicAsseticExtension
-{}
+class DynamicAsseticExtension extends AsseticExtension
+{
+    protected $routingExtension;
+
+    /**
+     * Constructor.
+     *
+     * @param RoutingExtension $routingExtension The routing extension
+     * @param AssetFactory     $factory          The asset factory
+     * @param boolean          $useController    Handle assets dynamically
+     *
+     * @see Symfony\Bundle\AsseticBundle\Templating\DynamicAsseticHelper
+     */
+    public function __construct(RoutingExtension $routingExtension, AssetFactory $factory, $useController = false, $enabledBundles = array(), ValueSupplierInterface $valueSupplier = null)
+    {
+        $this->routingExtension = $routingExtension;
+
+        parent::__construct($factory, $useController, $valueSupplier);
+    }
+
+    /**
+     * Returns an URL for the supplied asset.
+     *
+     * @param AssetInterface $asset   An asset
+     * @param array          $options An array of options
+     *
+     * @return string An echo-ready URL
+     */
+    protected function getAssetUrl(AssetInterface $asset, array $options = array())
+    {
+        try {
+            return $this->routingExtension->getPath('_assetic_'.$options['name']);
+        } catch (\Exception $e) {
+            throw RuntimeException::createFromPrevious($e);
+        }
+    }
+}
--- newscoop/src/Newscoop/NewscoopBundle/Extension/StaticAsseticExtension.php
+++ newscoop/src/Newscoop/NewscoopBundle/Extension/StaticAsseticExtension.php
@@ -7,13 +7,47 @@
 
 namespace Newscoop\NewscoopBundle\Extension;
 
+use Assetic\Factory\AssetFactory;
+use Assetic\Asset\AssetInterface;
+use Assetic\ValueSupplierInterface;
+use NoiseLabs\Bundle\SmartyBundle\Extension\AssetsExtension;
 use Newscoop\NewscoopBundle\Extension\AsseticExtension;
-use NoiseLabs\Bundle\SmartyBundle\Extension\StaticAsseticExtension as SmartyStaticAsseticExtension;
 
 /**
  * The "static" reincarnation of AsseticExtension.
  *
  * @author Vítor Brandão <vitor@noiselabs.com>
  */
-class StaticAsseticExtension extends SmartyStaticAsseticExtension
-{}
+class StaticAsseticExtension extends AsseticExtension
+{
+    protected $assetsExtension;
+
+    /**
+     * Constructor.
+     *
+     * @param AssetsExtension $assetsExtension The assets extension
+     * @param AssetFactory    $factory         The asset factory
+     * @param boolean         $useController   Handle assets dynamically
+     *
+     * @see Symfony\Bundle\AsseticBundle\Templating\StaticAsseticHelper
+     */
+    public function __construct(AssetsExtension $assetsExtension, AssetFactory $factory, $useController = false, $enabledBundles = array(), ValueSupplierInterface $valueSupplier = null)
+    {
+        $this->assetsExtension = $assetsExtension;
+
+        parent::__construct($factory, $useController, $valueSupplier);
+    }
+
+    /**
+     * Returns an URL for the supplied asset.
+     *
+     * @param AssetInterface $asset   An asset
+     * @param array          $options An array of options
+     *
+     * @return string An echo-ready URL
+     */
+    protected function getAssetUrl(AssetInterface $asset, array $options = array())
+    {
+        return $this->assetsExtension->getAssetUrl($asset->getTargetPath(), isset($options['package']) ? $options['package'] : null);
+    }
+}
