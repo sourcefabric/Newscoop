67b64a5ff09e452431da3dc06f2efb87df3fcf08
Rafał Muszyński <rmuszynski1@gmail.com>
2013-09-11 16:49:31 +0200

    changed translations - issues

--- newscoop/admin-files/issues/add_prev.php
+++ newscoop/admin-files/issues/add_prev.php
@@ -1,15 +1,16 @@
 <?php
 require_once($GLOBALS['g_campsiteDir']."/$ADMIN_DIR/issues/issue_common.php");
 
+$translator = \Zend_Registry::get('container')->getService('translator');
 // Check permissions
 if (!$g_user->hasPermission('ManageIssue')) {
-	camp_html_display_error(getGS('You do not have the right to add issues.'));
+	camp_html_display_error($translator->trans('You do not have the right to add issues.', array(), 'issues'));
 	exit;
 }
 
 $Pub = Input::Get('Pub', 'int');
 if (!Input::IsValid()) {
-	camp_html_display_error(getGS('Invalid Input: $1', Input::GetErrorString()));
+	camp_html_display_error($translator->trans('Invalid Input: $1', array('$1' => Input::GetErrorString()), 'issues'));
 	exit;
 }
 $publicationObj = new Publication($Pub);
@@ -19,12 +20,12 @@ $lastCreatedIssue = Issue::GetLastCreatedIssue($Pub);
 
 include_once($GLOBALS['g_campsiteDir']."/$ADMIN_DIR/javascript_common.php");
 
-camp_html_content_top(getGS('Copy previous issue'), array('Pub' => $publicationObj), true, true, array(getGS("Issues") => "/$ADMIN/issues/?Pub=$Pub"));
+camp_html_content_top($translator->trans('Copy previous issue', array(), 'issues'), array('Pub' => $publicationObj), true, true, array($translator->trans("Issues") => "/$ADMIN/issues/?Pub=$Pub"));
 
 
 if (is_null($lastCreatedIssue)) { ?>
     <BLOCKQUOTE>
-	<LI><?php  putGS('No previous issue.'); ?></LI>
+	<LI><?php echo $translator->trans('No previous issue.', array(), 'issues'); ?></LI>
     </BLOCKQUOTE>
     <?php
 } else {
@@ -37,23 +38,23 @@ if (is_null($lastCreatedIssue)) { ?>
 <TABLE BORDER="0" CELLSPACING="0" CELLPADDING="0" CLASS="box_table">
 <TR>
 	<TD COLSPAN="2">
-		<B><?php  putGS("Copy previous issue"); ?></B>
+		<B><?php echo $translator->trans("Copy previous issue", array(), 'issues'); ?></B>
 		<HR NOSHADE SIZE="1" COLOR="BLACK">
 	</TD>
 </TR>
 <TR>
-	<TD COLSPAN="2"><?php  putGS('Copy structure from issue number $1','<B>'. $lastCreatedIssue->getIssueNumber().'</B>'); ?></TD>
+	<TD COLSPAN="2"><?php echo $translator->trans('Copy structure from issue number $1', array('$1' => '<B>'. $lastCreatedIssue->getIssueNumber().'</B>'), 'issues'); ?></TD>
 </TR>
 <TR>
-	<TD ALIGN="RIGHT" ><?php  putGS("Issue Number"); ?>:</TD>
+	<TD ALIGN="RIGHT" ><?php echo $translator->trans("Issue Number", array(), 'issues'); ?>:</TD>
 	<TD>
-	<INPUT TYPE="TEXT" class="input_text" NAME="f_issue_number" VALUE="<?php  print ($lastCreatedIssue->getIssueNumber() + 1); ?>" SIZE="5" MAXLENGTH="10" alt="number|0|1|1000000000" emsg="<?php putGS("You must input a number greater than 0 into the $1 field.", "'".getGS("Number")."'"); ?>">
+	<INPUT TYPE="TEXT" class="input_text" NAME="f_issue_number" VALUE="<?php  print ($lastCreatedIssue->getIssueNumber() + 1); ?>" SIZE="5" MAXLENGTH="10" alt="number|0|1|1000000000" emsg="<?php echo $translator->trans("You must input a number greater than 0 into the $1 field.", array('$1' => "'".$translator->trans("Number")."'")); ?>">
 	</TD>
 </TR>
 <TR>
 	<TD COLSPAN="2" align="center">
 		<INPUT TYPE="HIDDEN" NAME="f_publication_id" VALUE="<?php p($Pub); ?>">
-		<INPUT TYPE="submit" class="button" NAME="Save" VALUE="<?php  putGS('Save'); ?>">
+		<INPUT TYPE="submit" class="button" NAME="Save" VALUE="<?php  echo $translator->trans('Save'); ?>">
 	</TD>
 </TR>
 </TABLE>
