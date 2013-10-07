47f5a52794327fec9f899c1f0b91365432e35989
Rafał Muszyński <rmuszynski1@gmail.com>
2013-10-07 17:13:17 +0200

    language choose fix, added new translation files

--- newscoop/src/Newscoop/NewscoopBundle/EventListener/LocaleListener.php
+++ newscoop/src/Newscoop/NewscoopBundle/EventListener/LocaleListener.php
@@ -41,6 +41,9 @@ class LocaleListener
     public function onRequest(GetResponseEvent $event) {
         $request = $event->getRequest();
         $pos = strpos($request->server->get('REQUEST_URI'), '/admin');
+        $cookies = $request->cookies->all();
+        $request->setLocale($cookies["TOL_Language"]);
+        
         if ($pos === false) {
             $publicationMetadata = $request->attributes->get('_newscoop_publication_metadata');
             $language = $this->em->getRepository('Newscoop\Entity\Language')->findOneById($publicationMetadata['publication']['id_default_language']);
