829378863202ea7d79440faff2bd124bbc89ca3e
Mischa Gorinskat <m.gorinskat@gmail.com>
2013-11-07 12:08:35 +0100

    CS-4817: Set default value to N

--- newscoop/install/sql/upgrade/4.2.x/2013.10.24/data-required.sql
+++ newscoop/install/sql/upgrade/4.2.x/2013.10.24/data-required.sql
@@ -1,2 +1,2 @@
-INSERT INTO `SystemPreferences` (`varname`, `value`, `last_modified`) VALUES ('MediaRichTextCaptions', 'Y', NOW());
+INSERT INTO `SystemPreferences` (`varname`, `value`, `last_modified`) VALUES ('MediaRichTextCaptions', 'N', NOW());
 INSERT INTO `SystemPreferences` (`varname`, `value`, `last_modified`) VALUES ('MediaCaptionLength', '255', NOW());
