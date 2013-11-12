38842c6678842caa9db7d8a96f19a9414ba867c6
Paweł Mikołajczuk <mikolajczuk.private@gmail.com>
2013-11-12 17:03:10 +0100

    add support for custom session lifetime values

---
+++ newscoop/src/Newscoop/NewscoopBundle/Session/Storage.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @package Newscoop\NewscoopBundle
+ * @author Paweł Mikołajczuk <pawel.mikolajczuk@sourcefabric.org>
+ * @copyright 2013 Sourcefabric o.p.s.
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt
+ */
+
+namespace Newscoop\NewscoopBundle\Session;
+
+use Symfony\Component\HttpFoundation\Session\Storage\PhpBridgeSessionStorage;
+
+/**
+ * Newscoop Session Storage adapter.
+ * Newscoop still uses the Zend_Session and we now have a custom wrapper for it (PhpBridgeSessionStorage). 
+ * This class provides support for a custom session lifetime value.
+ */
+class Storage extends PhpBridgeSessionStorage
+{
+	/**
+	 * @param array  														   $options
+	 * @param AbstractProxy|NativeSessionHandler|\SessionHandlerInterface|null $handler
+     * @param MetadataBag                                                      $metaBag MetadataBag
+	 */
+    public function __construct(array $options = array(), $handler = null, MetadataBag $metaBag = null)
+    {
+    	$seconds = \SystemPref::Get('SiteSessionLifeTime');
+        
+		$options['cookie_lifetime'] = $seconds;
+		$options['gc_maxlifetime'] = $seconds;
+
+		$this->setMetadataBag($metaBag);
+		$this->setOptions($options);
+        $this->setSaveHandler($handler);
+    }
+}
