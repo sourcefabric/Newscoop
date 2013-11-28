2d1a89c7ac4e91ccb44bb4a62a0e4ff8bde4a070
Mischa Gorinskat <m.gorinskat@gmail.com>
2013-11-28 13:45:35 +0100

    WOBS-2931: Added repository for issues
    Within repository a method is available to get the latest published issue for a
    publication.

---
+++ newscoop/library/Newscoop/Entity/Repository/IssueRepository.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * @package Newscoop
+ * @copyright 2011 Sourcefabric o.p.s.
+ * @license http://www.gnu.org/licenses/gpl.txt
+ */
+
+namespace Newscoop\Entity\Repository;
+
+use Doctrine\ORM\EntityRepository,
+    Newscoop\Entity\Publication;
+
+/**
+ * Issie repository
+ */
+class IssueRepository extends EntityRepository
+{
+    /**
+     * Retrieve the latest issue for a publication
+     *
+     * @param  NewscoopEntityPublication $publication    Publication
+     * @param  string                    $workflowStatus Published (Y) or not (N)
+     *
+     * @return \Newscoop\Entity\Issue|null
+     */
+    public function getLatestByPublication(
+        \Newscoop\Entity\Publication $publication,
+        $workflowStatus = 'Y'
+    ) {
+        $issue  = $this->getEntityManager()
+            ->getRepository('\Newscoop\Entity\Issue')
+            ->findOneBy(array(
+                'publication' => $publication->getId(),
+                'workflowStatus' => 'Y'
+            ), array(
+                'id' => 'DESC'
+            ));
+
+        return $issue;
+    }
+}
