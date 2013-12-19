9ca5896ffbc948d1b97a5d0ffeeb78f5ef2f5a0f
Rafał Muszyński <rmuszynski1@gmail.com>
2013-12-19 17:20:27 +0100

    registered users list

---
+++ newscoop/src/Newscoop/NewscoopBundle/EventListener/ListObjectsListener.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @package Newscoop\NewscoopBundle
+ * @author Rafał Muszyński <rafal.muszynski@sourcefabric.org>
+ * @copyright 2013 Sourcefabric o.p.s.
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt
+ */
+
+namespace Newscoop\NewscoopBundle\EventListener;
+
+use Newscoop\EventDispatcher\Events\CollectObjectsDataEvent;
+
+class ListObjectsListener
+{
+    /**
+     * Register plugin list objects in Newscoop
+     * 
+     * @param  CollectObjectsDataEvent $event
+     */
+    public function registerObjects(CollectObjectsDataEvent $event)
+    {
+        $event->registerListObject('newscoop\templatelist\users', array(
+            'class' => 'Newscoop\TemplateList\Users',
+            'list' => 'users',
+            'url_id' => 'uid',
+        ));
+    }
+}
