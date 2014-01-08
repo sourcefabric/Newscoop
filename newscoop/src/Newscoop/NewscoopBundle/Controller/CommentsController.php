d3a52bf1dbdcb027fe84810bbb07c3a2ce59b8ff
Rafał Muszyński <rmuszynski1@gmail.com>
2014-01-08 21:46:39 +0100

    comments buttons

--- newscoop/src/Newscoop/NewscoopBundle/Controller/CommentsController.php
+++ newscoop/src/Newscoop/NewscoopBundle/Controller/CommentsController.php
@@ -2,7 +2,7 @@
 /**
  * @package   Newscoop\NewscoopBundle
  * @author    Rafał Muszyński <rafal.muszynski@sourcefabric.org>
- * @copyright 2013 Sourcefabric o.p.s.
+ * @copyright 2014 Sourcefabric o.p.s.
  * @license   http://www.gnu.org/licenses/gpl-3.0.txt
  */
 
@@ -12,6 +12,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Newscoop\NewscoopBundle\Form\Type\CommentButtonsType;
+use Newscoop\Entity\Comment;
 
 /**
  * Comments controller.
@@ -25,6 +28,7 @@ class CommentsController extends Controller
     public function indexAction(Request $request)
     {
         $em = $this->container->get('em');
+        $commentService = $this->container->get('newscoop_newscoop.comments_service');
         $queryBuilder = $em->getRepository('Newscoop\Entity\Comment')
             ->createQueryBuilder('c')
             ->orderBy('c.time_created', 'desc');
@@ -36,6 +40,10 @@ class CommentsController extends Controller
             'recommended' => false,
             'unrecommended' => false,
         );
+
+        $buttonsForm = $this->container->get('form.factory')->create(new CommentButtonsType(), array(
+            'buttons' => 'pending'
+        ), array());
 
         $filterForm = $this->createFormBuilder()
             ->add('new', 'checkbox', array(
@@ -79,7 +87,7 @@ class CommentsController extends Controller
         $commentsArray = array();
         foreach ($comments as $comment) {
             $commentsArray[] = array(
-                'banned' => $this->isBanned($em, $comment->getCommenter(), null),
+                'banned' => $commentService->isBanned($comment->getCommenter()),
                 'avatarHash' => md5($comment->getCommenter()->getEmail()),
                 'issueNumber' => $comment->getThread()->getSection()->getIssue()->getNumber(),
                 'comment' => $comment,
@@ -101,42 +109,84 @@ class CommentsController extends Controller
         return array(
             'pagination' => $pagination,
             'filterForm' => $filterForm->createView(),
-            'defaultValues' => $defaultValues
+            'defaultValues' => $defaultValues,
+            'buttonsForm' => $buttonsForm->createView(),
         );
     }
 
     /**
-     * Checks if a commenter is banned
-     *
-     * @param EntityManager            $em            Entity Manager
-     * @param Newcoop\Entity\Commenter $commenter     Commenter
-     * @param int                      $publicationId Publication id
-     *
-     * @return bool
+     * @Route("/admin/comments/set-status")
      */
-    public function isBanned($em, $commenter, $publicationId)
+    public function setStatusAction(Request $request) 
     {
-        $queryBuilder = $em->getRepository('Newscoop\Entity\Comment\Acceptance')
-            ->createQueryBuilder('a');
+        $translator = $this->container->get('translator');
+        $user = $this->container->get('user');
+        $em = $this->container->get('em');
+        $commentService = $this->container->get('newscoop_newscoop.comments_service');
+        $status = $request->request->get('status');
+        $comments = $request->request->get('comment');
 
-        $queryBuilder->where($queryBuilder->expr()->orX(
-            $queryBuilder->expr()->eq('a.search', ':name'),
-            $queryBuilder->expr()->eq('a.search', ':email'),
-            $queryBuilder->expr()->eq('a.search', ':ip')
-        ));
+        if ($request->isMethod('POST')) {
+            if (!is_array($comments)) {
+                $comments = array($comments);
+            }
 
-        $queryBuilder->setParameters(array(
-            'name' => $commenter->getName(),
-            'email' => $commenter->getEmail(),
-            'ip' => $commenter->getIp()
-        ));
+            if ($status == "deleted") {
+                $comments = array_unique(array_merge($comments, $commentService->getAllReplies($comments)));
+            }
 
-        $query = $queryBuilder->getQuery()->getResult();
+            try {
+                foreach ($comments as $id) {
+                    $comment = $em->getRepository('Newscoop\Entity\Comment')->find($id);
+                    if ($status == "deleted") {
+                        $msg = $translator->trans('comments.msg.error.deletefromarticle', array('$1' => $user->getCurrentUser->getName(),
+                                    '$2' => $comment->getThread()->getName(), '$3' => $comment->getLanguage()->getCode()), 'new_comments');
+                    } else {
+                        $msg = $translator->trans('comments.msg.commentinarticle', array('$1' => $user->getCurrentUser()->getName(),
+                                    '$2' => $comment->getThread()->getName(), '$3' => $comment->getLanguage()->getCode(), '$4' => $status), 'new_comments');
+                    }
+                }
 
-        if ($query) {
-            return true;
+                $em->getRepository('Newscoop\Entity\Comment')->setStatus($comments, $status);
+                $em->flush();
+            } catch (\Exception $e) {
+                return;
+            }
+
+            return new JsonResponse(array('message' => $msg));
         }
+    }
 
-        return false;
+    /**
+     * @Route("/admin/comments/reply/{id}")
+     */
+    public function replyAction(Request $request, $id)
+    {
+        $translator = $this->container->get('translator');
+        $em = $this->container->get('em');
+
+        $values = $request->request->all();
+        $comment = new Comment();
+
+        if ($request->isMethod('POST')) {
+            $values['parent'] = $id;
+            $values['user'] = $this->container->get('user')->getCurrentUser();
+            $values['time_created'] = new \DateTime();
+            $values['ip'] = $request->getClientIp();
+            $values['status'] = 'approved';
+
+            try {
+                $comment = $em->getRepository('Newscoop\Entity\Comment')->save($comment, $values);
+                $em->flush();
+            } catch (\Exception $e) {
+                return new JsonResponse(array(
+                    'status' => false
+                ));
+            }
+
+            return new JsonResponse(array(
+                'status' => true
+            ));
+        }
     }
 }
