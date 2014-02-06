b170dcbb09d6bfb689c99b5e42307efdee6e7202
Mischa Gorinskat <m.gorinskat@gmail.com>
2014-02-06 11:09:50 +0100

    CS-4921: Implements RepositoryInterface in article repository

--- newscoop/library/Newscoop/Entity/Repository/ArticleRepository.php
+++ newscoop/library/Newscoop/Entity/Repository/ArticleRepository.php
@@ -12,11 +12,12 @@ use Doctrine\ORM\EntityRepository;
 use Doctrine\ORM\QueryBuilder;
 use Newscoop\Datatable\Source as DatatableSource;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Newscoop\Search\RepositoryInterface;
 
 /**
  * Article repository
  */
-class ArticleRepository extends DatatableSource
+class ArticleRepository extends DatatableSource implements RepositoryInterface
 {
     public function getArticles($publication, $type = null, $language = null)
     {
@@ -40,7 +41,7 @@ class ArticleRepository extends DatatableSource
                 'workflowStatus' => 'Y',
                 'publication' => $publication
             ));
-            
+
         if ($type) {
             $countQueryBuilder->andWhere('a.type = :type')
                 ->setParameter('type', $type);
@@ -68,7 +69,7 @@ class ArticleRepository extends DatatableSource
 
         $query = $queryBuilder->getQuery();
         $query->setHint('knp_paginator.count', $articlesCount);
-        
+
         return $query;
     }
 
@@ -100,7 +101,7 @@ class ArticleRepository extends DatatableSource
         }
 
         $query = $queryBuilder->getQuery();
-        
+
         return $query;
     }
 
@@ -138,7 +139,7 @@ class ArticleRepository extends DatatableSource
                 'count' => $articlesCount
             );
         }
-        
+
         return $query;
     }
 
@@ -162,7 +163,7 @@ class ArticleRepository extends DatatableSource
 
         $query = $queryBuilder->getQuery();
         $query->setHint('knp_paginator.count', $articlesCount);
-        
+
         return $query;
     }
 
@@ -188,7 +189,7 @@ class ArticleRepository extends DatatableSource
 
         $query = $queryBuilder->getQuery();
         $query->setHint('knp_paginator.count', $articlesCount);
-        
+
         return $query;
     }
 
@@ -207,7 +208,7 @@ class ArticleRepository extends DatatableSource
             ));
 
         $query = $queryBuilder->getQuery();
- 
+
         return $query;
     }
 
@@ -230,27 +231,106 @@ class ArticleRepository extends DatatableSource
     }
 
     /**
-     * Reset articles index
+     * Get articles for indexing
+     *
+     * @param int $count Number of articles to index
+     * @param array $filter Filter to apply to articles
+     *
+     * @return array
+     */
+    public function getBatch($count = self::BATCH_COUNT, array $filter = null)
+    {
+        $qb = $this->createQueryBuilder('a');
+
+        if (is_null($filter)) {
+            $qb->where('a.indexed IS NULL')
+                ->orWhere('a.indexed < a.updated')
+                ->orderBy('a.number', 'DESC');
+        } else {
+            die('Not implemented yet!');
+        }
+
+        if (is_numeric($count)) {
+            $qb->setMaxResults($count);
+        }
+
+        $batch = $qb->getQuery()
+            ->getResult();
+
+        return $batch;
+    }
+
+    /**
+     * Set indexed now
+     *
+     * @param array $articles
+     * @return void
+     */
+    public function setIndexedNow(array $articles)
+    {
+        $qb = $this->getEntityManager()->createQueryBuilder();
+        $qb = $qb->update('Newscoop\Entity\Article', 'a')
+                ->set('a.indexed', 'CURRENT_TIMESTAMP()');
+
+        if (!is_null($articles) && count($articles) > 0) {
+
+            $articleNumbers = array();
+            foreach ($articles AS $article) {
+                $articleNumbers[] = $article->getNumber();
+            }
+
+            $qb = $qb->where($qb->expr()->in('a.number',  $articleNumbers));
+        }
+
+        return $qb->getQuery()->execute();
+    }
+
+    /**
+     * Set indexed null
      *
      * @return void
      */
-    public function resetIndex()
+    public function setIndexedNull(array $articles = null)
     {
-        $query = $this->getEntityManager()
-            ->createQuery('UPDATE Newscoop\Entity\Article a SET a.indexed = null, a.updated = a.updated');
-        $query->execute();
+        $qb = $this->getEntityManager()->createQueryBuilder();
+        $qb = $qb->update('Newscoop\Entity\Article', 'a')
+                ->set('a.indexed', 'NULL');
+
+        if (!is_null($articles) && count($articles) > 0) {
+
+            $articleNumbers = array();
+            foreach ($articles AS $article) {
+                $articleNumbers[] = $article->getNumber();
+            }
+
+            $qb = $qb->where($qb->expr()->in('a.number',  $articleNumbers));
+        }
+
+        return $qb->getQuery()->execute();
     }
 
-    public function setArticleIndexedNow($article)
-    {
-        $query = $this->getEntityManager()
-                ->createQuery("UPDATE Newscoop\Entity\Article a SET a.indexed = :date, a.updated = a.updated WHERE a.number = :number AND a.language = :language")
-                ->setParameters(array(
-                    'date' => new \DateTime(), 
-                    'number' => $article->getNumber(), 
-                    'language' => $article->getLanguageId(), 
-                ));
+    // /**
+    //  * Reset articles index
+    //  *
+    //  * @return void
+    //  */
+    // public function resetIndex()
+    // {
+    //     $query = $this->getEntityManager()
+    //         ->createQuery('UPDATE Newscoop\Entity\Article a SET a.indexed = null, a.updated = a.updated');
+    //     $query->execute();
+    // }
 
-        $query->execute();
-    }
+    // public function setArticleIndexedNow($article)
+    // {
+    //     $query = $this->getEntityManager()
+    //             ->createQuery("UPDATE Newscoop\Entity\Article a SET a.indexed = :date, a.updated = a.updated WHERE a.number = :number AND a.language = :language")
+    //             ->setParameters(array(
+    //                 'date' => new \DateTime(),
+    //                 'number' => $article->getNumber(),
+    //                 'language' => $article->getLanguageId(),
+    //             ));
+
+    //     $query->execute();
+    // }
 }
