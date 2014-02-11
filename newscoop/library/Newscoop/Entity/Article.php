dd5290d8710392da3eb1f5b152b5a53438be7402
Mischa Gorinskat <m.gorinskat@gmail.com>
2014-02-11 10:22:02 +0100

    CS-4921: Adds filter topic_strict

--- newscoop/library/Newscoop/Entity/Article.php
+++ newscoop/library/Newscoop/Entity/Article.php
@@ -616,17 +616,6 @@ class Article implements DocumentInterface
     }
 
     /**
-     * Set date
-     *
-     * @param DateTime $updated
-     * @return void
-     */
-    public function setDate(DateTime $date)
-    {
-        $this->date = $date;
-    }
-
-    /**
      * Set data
      *
      * @param array $data
