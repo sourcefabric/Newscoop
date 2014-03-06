9e929ded1ea33a2bf9cd701e02fa15e61a366087
Daniel James <daniel@64studio.com>
2014-03-06 16:03:06 +0000

    CS-4796 Add empty Azerbaijani files to enable localization

---
+++ newscoop/admin-files/lang/az/logs.php
@@ -0,0 +1,13 @@
+<?php 
+regGS("show", "");
+regGS("hide", "");
+regGS("Title", "");
+regGS("Resource Type:", "");
+regGS("Resource Type", "");
+regGS("Resource", "");
+regGS("Diff", "");
+regGS("Broken data", "");
+regGS("Action Type:", "");
+regGS("No logs.", "");
+regGS("User", "");
+?>
