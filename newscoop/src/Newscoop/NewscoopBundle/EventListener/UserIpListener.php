f85dab24ff192bb8f3c245309e461d8ad1cf2927
Rafał Muszyński <rmuszynski1@gmail.com>
2014-03-19 14:10:02 +0100

    user ip listener

---
+++ newscoop/src/Newscoop/NewscoopBundle/EventListener/UserIpListener.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * @package Newscoop\NewscoopBundle
+ * @author Rafał Muszyński <rafal.muszynski@sourcefabric.org>
+ * @copyright 2014 Sourcefabric o.p.s.
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt
+ */
+
+namespace Newscoop\NewscoopBundle\EventListener;
+
+use Newscoop\Services\UserService;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\HttpKernel;
+
+/**
+ * Run user ip resolver on request
+ */
+class UserIpListener
+{
+    /**
+     * User service
+     *
+     * @var UserService
+     */
+    protected $userService;
+
+    /**
+     * Contruct UserIpListener object
+     *
+     * @param UserService $userService User service
+     */
+    public function __construct(UserService $userService)
+    {
+        $this->userService = $userService;
+    }
+
+    public function onRequest(GetResponseEvent $event)
+    {
+        if (HttpKernel::MASTER_REQUEST != $event->getRequestType()) {
+            // don't do anything if it's not the master request
+            return;
+        }
+
+        $this->userService->userResolver($event->getRequest());
+    }
+}
