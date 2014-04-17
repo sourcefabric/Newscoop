8047c57f8441cbde7c0cf733c5f0ab07ad1b6fc9
Rafał Muszyński <rmuszynski1@gmail.com>
2014-04-17 16:24:09 +0200

    backend session listener

---
+++ newscoop/src/Newscoop/NewscoopBundle/EventListener/BackendSessionListener.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * @package Newscoop\NewscoopBundle
+ * @author Rafał Muszyński <rafal.muszynski@sourcefabric.org>
+ * @copyright 2014 Sourcefabric o.p.s.
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt
+ */
+
+namespace Newscoop\NewscoopBundle\EventListener;
+
+use Newscoop\Services\UserService;
+use Symfony\Component\Routing\Router;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\HttpKernel;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+/**
+ * Run backend session resolver on request
+ */
+class BackendSessionListener
+{
+    /**
+     * User service
+     *
+     * @var UserService
+     */
+    protected $userService;
+
+    /**
+     * Contruct UserIpListener object
+     *
+     * @param UserService $userService User service
+     * @param UserService $routerService Symfony router
+     */
+    public function __construct(UserService $userService, Router $router)
+    {
+        $this->userService = $userService;
+        $this->router = $router;
+    }
+
+    public function onRequest(GetResponseEvent $event)
+    {
+        if (HttpKernel::MASTER_REQUEST != $event->getRequestType()) {
+            // don't do anything if it's not the master request
+            return;
+        }
+
+        if ($this->userService->getCurrentUser() === null) {
+            return new RedirectResponse($this->router->generate('admin_logout'));
+        }
+    }
+}
