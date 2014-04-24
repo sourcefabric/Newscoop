0312f3c335a429ee17a398ceb86c9dba6a417754
Rafał Muszyński <rmuszynski1@gmail.com>
2014-04-24 10:24:17 +0200

    [CS-5312] - Reinstallation of Newscoop fails with bogus upgrade message

--- newscoop/install/sql/upgrade/4.2.x/2013.10.24/data-required.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO `SystemPreferences` (`varname`, `value`, `last_modified`) VALUES ('MediaRichTextCaptions', 'N', NOW());
-INSERT INTO `SystemPreferences` (`varname`, `value`, `last_modified`) VALUES ('MediaCaptionLength', '255', NOW());
