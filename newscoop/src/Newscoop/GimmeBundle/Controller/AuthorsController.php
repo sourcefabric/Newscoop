120a07492d72db45f8fc4cec29e5299cbb38699d
ahilles107 <mikolajczuk.private@gmail.com>
2014-04-28 14:34:31 +0200

    add missing resources proposal

--- newscoop/src/Newscoop/GimmeBundle/Controller/AuthorsController.php
+++ newscoop/src/Newscoop/GimmeBundle/Controller/AuthorsController.php
@@ -21,7 +21,9 @@ class AuthorsController extends FOSRestController
     /**
      * Get author
      *
-     * @deprecated route author/{id}.{_format} will be removed in 4.4
+     * This route will be removed in 4.4
+     *
+     * @deprecated
      *
      * @ApiDoc(
      *     statusCodes={
@@ -34,11 +36,41 @@ class AuthorsController extends FOSRestController
      * )
      *
      * @Route("/author/{id}.{_format}", defaults={"_format"="json"})
-     * @Route("/authors/{id}.{_format}", defaults={"_format"="json"})
      * @Method("GET")
      * @View()
      */
     public function getAuthorAction($id)
+    {
+        $em = $this->container->get('em');
+        $author = $em->getRepository('Newscoop\Entity\Author')
+            ->getAuthor($id)
+            ->getOneOrNullResult();
+
+        if (!$author) {
+            throw new NotFoundHttpException('Author was not found.');
+        }
+
+        return $author;
+    }
+
+    /**
+     * Get author
+     *
+     * @ApiDoc(
+     *     statusCodes={
+     *         200="Returned when successful",
+     *         404={
+     *           "Returned when the author is not found",
+     *         }
+     *     },
+     *     output="\Newscoop\Entity\Author"
+     * )
+     *
+     * @Route("/authors/{id}.{_format}", defaults={"_format"="json"})
+     * @Method("GET")
+     * @View()
+     */
+    public function getAuthorByIdAction($id)
     {
         $em = $this->container->get('em');
         $author = $em->getRepository('Newscoop\Entity\Author')
@@ -116,5 +148,65 @@ class AuthorsController extends FOSRestController
      * @View()
      */
     public function searchAuthorsAction()
+    {}
+
+    /**
+     * Get article authors
+     *
+     * @ApiDoc(
+     *     statusCodes={
+     *         200="Returned when successful",
+     *         404={
+     *           "Returned when the authors are not found",
+     *         }
+     *     }
+     * )
+     *
+     * @Route("/articles/{number}/{language}/authors.{_format}", defaults={"_format"="json"})
+     * @Route("/authors/article/{number}/{language}.{_format}", defaults={"_format"="json"})
+     * @Method("GET")
+     * @View()
+     */
+    public function getArticleAuthorsAction($number, $language, $id)
+    {}
+
+    /**
+     * Get single article author
+     *
+     * @ApiDoc(
+     *     statusCodes={
+     *         200="Returned when successful",
+     *         404={
+     *           "Returned when the article author is not found",
+     *         }
+     *     }
+     * )
+     *
+     * @Route("/articles/{number}/{language}/authors/{id}.{_format}", defaults={"_format"="json"})
+     * @Route("/authors/{id}/article/{number}/{language}.{_format}", defaults={"_format"="json"})
+     * @Method("GET")
+     * @View()
+     */
+    public function getArticleAuthorAction($number, $language, $id)
+    {}
+
+    /**
+     * Set article authors order
+     *
+     * @ApiDoc(
+     *     statusCodes={
+     *         200="Returned when successful",
+     *         404={
+     *           "Returned when the article authors are not found",
+     *         },
+     *         400="Returned when data are invalid"
+     *     }
+     * )
+     *
+     * @Route("/articles/{number}/{language}/authors/order.{_format}", defaults={"_format"="json"})
+     * @Method("POST|PATCH")
+     * @View()
+     */
+    public function setArticleAuthorsOrderAction($number, $language, $id)
     {}
 }
