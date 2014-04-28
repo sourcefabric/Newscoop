cfc97a342358c3a538c19293c66fbc273e387836
ahilles107 <mikolajczuk.private@gmail.com>
2014-04-28 14:05:32 +0200

    authors resources

--- newscoop/src/Newscoop/GimmeBundle/Controller/AuthorsController.php
+++ newscoop/src/Newscoop/GimmeBundle/Controller/AuthorsController.php
@@ -21,6 +21,8 @@ class AuthorsController extends FOSRestController
     /**
      * Get author
      *
+     * @deprecated route author/{id}.{_format} will be removed in 4.4
+     *
      * @ApiDoc(
      *     statusCodes={
      *         200="Returned when successful",
@@ -32,6 +34,7 @@ class AuthorsController extends FOSRestController
      * )
      *
      * @Route("/author/{id}.{_format}", defaults={"_format"="json"})
+     * @Route("/authors/{id}.{_format}", defaults={"_format"="json"})
      * @Method("GET")
      * @View()
      */
@@ -48,4 +51,70 @@ class AuthorsController extends FOSRestController
 
         return $author;
     }
+
+    /**
+     * Get authors
+     *
+     * Get list of Author resources
+     *
+     * @ApiDoc(
+     *     statusCodes={
+     *         200="Returned when successful",
+     *         404={
+     *           "Returned when the authors are not found",
+     *         }
+     *     }
+     * )
+     *
+     * @Route("/authors.{_format}", defaults={"_format"="json"})
+     * @Method("GET")
+     * @View()
+     */
+    public function getAuthorsAction()
+    {}
+
+    /**
+     * Get authors types
+     *
+     * Get list of authors types
+     *
+     * @ApiDoc(
+     *     statusCodes={
+     *         200="Returned when successful",
+     *         404={
+     *           "Returned when the authors types are not found",
+     *         }
+     *     }
+     * )
+     *
+     * @Route("/authors/types.{_format}", defaults={"_format"="json"})
+     * @Method("GET")
+     * @View()
+     */
+    public function getAuthorsTypesAction()
+    {}
+
+    /**
+     * Search for authors
+     *
+     * Get list of authors for search query
+     *
+     * @ApiDoc(
+     *     statusCodes={
+     *         200="Returned when successful",
+     *         404={
+     *           "Returned when the authors are not found",
+     *         }
+     *     },
+     *     filters={
+     *          {"name"="query", "dataType"="string", "description"="search query"}
+     *     },
+     * )
+     *
+     * @Route("/authors/search.{_format}", defaults={"_format"="json"})
+     * @Method("GET")
+     * @View()
+     */
+    public function searchAuthorsAction()
+    {}
 }
