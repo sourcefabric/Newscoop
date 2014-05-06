b4415eaaf9b0a15a0051ad50a85b25f7953dab93
Rafał Muszyński <rmuszynski1@gmail.com>
2014-05-06 11:59:32 +0200

    IM-1501 - user register for commenting have access to the site's backend

---
+++ newscoop/install/sql/upgrade/4.2.x/2014.05.06/tables.sql
@@ -0,0 +1,2 @@
+UPDATE `liveuser_users` SET `is_admin` = false;
+UPDATE liveuser_users l0_ INNER JOIN liveuser_groupusers l2_ ON l0_.Id = l2_.perm_user_id INNER JOIN liveuser_groups l1_ ON l1_.group_id = l2_.group_id AND (l1_.group_id IN (SELECT `group_id` FROM `liveuser_groups`)) SET l0_.is_admin = true;
