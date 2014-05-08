c5448e2a8aa9c3c91d495cd200cf0d5f09a043d0
Rafał Muszyński <rmuszynski1@gmail.com>
2014-05-08 12:22:59 +0200

    console command to fetch publications

---
+++ newscoop/library/Newscoop/Tools/Console/Command/FetchPublicationsCommand.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * @package   Newscoop
+ * @author    Rafał Muszyński <rafal.muszynski@sourcefabric.org>
+ * @copyright 2014 Sourcefabric o.p.s.
+ * @license   http://www.gnu.org/licenses/gpl-3.0.txt
+ */
+
+namespace Newscoop\Tools\Console\Command;
+
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Newscoop\Service\IThemeManagementService;
+
+/**
+ * Fetch available publications Command
+ */
+class FetchPublicationsCommand extends ContainerAwareCommand
+{
+    /**
+     */
+    protected function configure()
+    {
+        $this
+        ->setName('publications:fetch')
+        ->setDescription('Fetch available publications');
+    }
+
+    /**
+     */
+    protected function execute(Console\Input\InputInterface $input, Console\Output\OutputInterface $output)
+    {
+        $em = $this->getContainer()->getService('em');
+        $resourceId = new \Newscoop\Service\Resource\ResourceId(__CLASS__);
+        $themeService = $resourceId->getService(IThemeManagementService::NAME_1);
+
+        $publicationService = $this->getContainer()->getService('content.publication');
+        $publications =  $publicationService->findAll();
+        foreach ($themeService->getUnassignedThemes() as $theme) {
+            foreach ($publications as $publication) {
+                $themeService->assignTheme($theme, $publication);
+            }
+        }
+    }
+}
