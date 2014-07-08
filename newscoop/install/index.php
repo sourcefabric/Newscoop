fd5521c33e2c84175db72f81e4b8d157b51bfa59
Rafał Muszyński <rmuszynski1@gmail.com>
2014-07-08 14:38:32 +0200

    fix

--- newscoop/install/index.php
+++ newscoop/install/index.php
@@ -35,7 +35,7 @@ if (count($missingReq) > 0) {
         echo $value.' <br />';
     }
     echo "</pre>";
-    $phpFile = php_ini_loaded_file() ?: "File couldn't be found."
+    $phpFile = php_ini_loaded_file() ?: "File couldn't be found.";
     echo "Your php.ini config file path: <strong>" . $phpFile . "</strong><br/><br/>";
     echo "You can try to fix common problems by running our fixer.php script: <br/>";
     echo "<pre>sudo php ". realpath(__DIR__."/../scripts/fixer.php")."</pre>";
