e675aab68f57ca0df78274df1d1e2363dcf05ea8
Daniel James <daniel@64studio.com>
2014-07-21 18:03:39 +0100

    AZ-31 initial commit of Azerbaijani localization backport

--- newscoop/admin-files/lang/az/help.php
+++ newscoop/admin-files/lang/az/help.php
@@ -1,13 +1,13 @@
 <?php 
-regGS("or suggest improvements in our bug tracker at", "");
-regGS("and help us support quality, independent journalism.", "");
-regGS("Welcome to the Newscoop help page. Whether you're just getting started or you have a specific question, the best place to look is in one of the Newscoop manuals", "");
-regGS("Version", "");
-regGS("To upgrade Newscoop please visit our website", "");
-regGS("To support us in other ways, visit our Community page", "");
-regGS("Talk to us today", "");
-regGS("Sourcefabric also offers a range of support options, which can include installation, hosting and technical support.", "");
-regGS("Release date", "");
-regGS("Help", "");
-regGS("Alternatively, you can ask a question in our forums at", "");
+regGS("or suggest improvements in our bug tracker at", "və ya bizim xəta izləyicimizə inkişaf etdirə biləcəyimiz sahələr üçün təklif ver");
+regGS("and help us support quality, independent journalism.", "və bizə keyfiyyətli, azad jurnalistikanı dəstəkləməkdə yardımçı ol.");
+regGS("Welcome to the Newscoop help page. Whether you're just getting started or you have a specific question, the best place to look is in one of the Newscoop manuals", "Newscoop dəstək səhifəsinə xoş gəldin. Burada yenisənsə, ya da hansısa xüsusi sualın varsa, ən yaxşısı Newscoop kitabçalarına bax.");
+regGS("Version", "Versiya");
+regGS("To upgrade Newscoop please visit our website", "Newscoop-u təkmilləşdirmək üçün bizim vebsaytımıza daxil ol");
+regGS("To support us in other ways, visit our Community page", "Bizi digər istiqamətlərdə dəstəkləmək üçün İctimaiyyət səhifəmizə keç");
+regGS("Talk to us today", "Bizimlə əlaqə saxla");
+regGS("Sourcefabric also offers a range of support options, which can include installation, hosting and technical support.", "Sourcefabric həmçinin müxtəlif dəstəkləmə seçimləri təqdim edir ki, bunlar quraşdırma, hostinq və texniki dəstəkdən ibarətdir.");
+regGS("Release date", "Buraxılış tarixi");
+regGS("Help", "Yardım");
+regGS("Alternatively, you can ask a question in our forums at", "Alternatıv olaraq sən forumlarımıza sualını daxil edə bilərsən");
 ?>
--- newscoop/admin-files/lang/az/logs.php
+++ newscoop/admin-files/lang/az/logs.php
@@ -1,13 +1,13 @@
 <?php 
-regGS("show", "");
-regGS("hide", "");
-regGS("Title", "");
-regGS("Resource Type:", "");
-regGS("Resource Type", "");
-regGS("Resource", "");
-regGS("Diff", "");
-regGS("Broken data", "");
-regGS("Action Type:", "");
-regGS("No logs.", "");
-regGS("User", "");
+regGS("show", "göstər");
+regGS("hide", "gizlə");
+regGS("Title", "Başlıq");
+regGS("Resource Type:", "Mənbə Növü:");
+regGS("Resource Type", "Mənbə Növü");
+regGS("Resource", "Mənbə");
+regGS("Diff", "Fərq");
+regGS("Broken data", "Sındırılmış məlumat");
+regGS("Action Type:", "Addım Növü:");
+regGS("No logs.", "Giriş yoxdur.");
+regGS("User", "İstifadəçi");
 ?>
