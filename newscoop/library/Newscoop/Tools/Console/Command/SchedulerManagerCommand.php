f0c83c68f039eb6fdef820b2144184e41356b8dc
Rafał Muszyński <rmuszynski1@gmail.com>
2014-08-01 17:25:08 +0200

    fixed typos

--- newscoop/library/Newscoop/Tools/Console/Command/SchedulerManagerCommand.php
+++ newscoop/library/Newscoop/Tools/Console/Command/SchedulerManagerCommand.php
@@ -57,7 +57,6 @@ class SchedulerManagerCommand extends ContainerAwareCommand
 
         try {
             foreach ($jobs as $job) {
-                ladybug_dump($jobs);
                 unset($job['id']);
                 unset($job['createdAt']);
                 if ($job['sendMail']) {
