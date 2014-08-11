527af29bbc538cc7fa3cae7afc0723b7d5fc9ad0
Mischa Gorinskat <m.gorinskat@gmail.com>
2014-08-11 16:31:01 +0200

    CS-4849: Adds rich text captions preferences

---
+++ newscoop/install/Resources/sql/upgrade/4.3.x/2014.08.11/data-required.sql
@@ -0,0 +1,3 @@
+INSERT IGNORE INTO `SystemPreferences` (`varname`, `value`, `last_modified`) VALUES ('MediaRichTextCaptions', 'Y', NOW());
+
+INSERT IGNORE INTO `SystemPreferences` (`varname`, `value`, `last_modified`) VALUES ('MediaCaptionLength', '255', NOW());
