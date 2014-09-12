3e7e54aa27a10e333ba2367927a62bc252208ed6
Rafał Muszyński <rmuszynski1@gmail.com>
2014-09-12 14:38:12 +0200

    text/blog can not have default value in older versions of mysql

--- newscoop/install/Resources/sql/upgrade/4.3.x/2014.08.11/tables.sql
+++ newscoop/install/Resources/sql/upgrade/4.3.x/2014.08.11/tables.sql
@@ -1,4 +1,4 @@
-ALTER TABLE `ArticleImageCaptions` CHANGE `caption` `caption` text COLLATE 'utf8_general_ci' NOT NULL DEFAULT '';
+ALTER TABLE `ArticleImageCaptions` CHANGE `caption` `caption` text COLLATE 'utf8_general_ci' NOT NULL;
 ALTER TABLE `Images` DROP INDEX `Description`;
-ALTER TABLE `Images` CHANGE `Description` `Description` text COLLATE 'utf8_general_ci' NOT NULL DEFAULT '';
+ALTER TABLE `Images` CHANGE `Description` `Description` text COLLATE 'utf8_general_ci' NOT NULL;
 ALTER TABLE `Images` ADD INDEX `Description` (`Description`(100));
