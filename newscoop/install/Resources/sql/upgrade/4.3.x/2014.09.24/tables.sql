c7bbc2a9ba470bf1a960082d696d53541c832e6b
Rafał Muszyński <rmuszynski1@gmail.com>
2014-09-24 10:30:38 +0200

    upgrade 4.2.4 to 4.3 fix

---
+++ newscoop/install/Resources/sql/upgrade/4.3.x/2014.09.24/tables.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `Plugins` DROP PRIMARY KEY;
+ALTER TABLE `Plugins` ADD `Id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY FIRST, ADD `Description` TEXT NOT NULL;
+UPDATE `SystemPreferences` SET `value` = 'public/videos/' WHERE `varname` = 'FlashDirectory';
