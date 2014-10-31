7c3183f797f32c94dc112f79775eed60e0f9ce0f
Rafał Muszyński <rmuszynski1@gmail.com>
2014-10-31 16:49:31 +0100

    get theme path implementation and specs

--- newscoop/library/Newscoop/Services/ThemesService.php
+++ newscoop/library/Newscoop/Services/ThemesService.php
@@ -9,6 +9,7 @@
 namespace Newscoop\Services;
 
 use Newscoop\ThemesServiceInterface;
+use Newscoop\IssueServiceInterface;
 
 /**
  * Themes service
@@ -16,10 +17,34 @@ use Newscoop\ThemesServiceInterface;
 class ThemesService implements ThemesServiceInterface
 {
     /**
+     * Issue service
+     *
+     * @var IssueServiceInterface
+     */
+    protected $issueService;
+
+    /**
+     * Cache service
+     *
+     * @var CacheService
+     */
+    protected $cacheService;
+
+    /**
+     * Publication service
+     *
+     * @var PublicationService
+     */
+    protected $publicationService;
+
+    /**
      * Construct
      */
-    public function __construct()
+    public function __construct(IssueServiceInterface $issueService, CacheService $cacheService, PublicationService $publicationService)
     {
+        $this->issueService = $issueService;
+        $this->cacheService = $cacheService;
+        $this->publicationService = $publicationService;
     }
 
     /**
@@ -27,6 +52,25 @@ class ThemesService implements ThemesServiceInterface
      */
     public function getThemePath()
     {
+       $issue = $this->issueService->getIssue();
+       $language = $issue->getLanguageId();
+       $publication = $this->publicationService->getPublication();
+       $cacheKeyThemePath = $this->cacheService->getCacheKey(array('getThemePath', $language, $publication->getId(), $issue->getNumber()), 'issue');
 
+       if ($this->cacheService->contains($cacheKeyThemePath)) {
+            $themePath = $this->cacheService->fetch($cacheKeyThemePath);
+        } else {
+            $cacheKey = $this->cacheService->getCacheKey(array('issue', $publication->getId(), $language, $issue->getNumber()), 'issue');
+            if ($this->cacheService->contains($cacheKey)) {
+                $issue = $this->cacheService->fetch($cacheKey);
+            } else {
+                $this->cacheService->save($cacheKey, $issue);
+            }
+
+            //$resourceId = new ResourceId('template_engine/classes/CampSystem');
+            //$outputService = $resourceId->getService(IOutputService::NAME);
+        }
+
+       return '';
     }
 }
--- spec/Newscoop/Services/ThemesServiceSpec.php
+++ spec/Newscoop/Services/ThemesServiceSpec.php
@@ -3,12 +3,39 @@
 namespace spec\Newscoop\Services;
 
 use PhpSpec\ObjectBehavior;
-use Prophecy\Argument;
+use Newscoop\IssueServiceInterface;
+use Newscoop\Services\CacheService;
+use Newscoop\Services\PublicationService;
+use Newscoop\Entity\Issue;
+use Newscoop\Entity\Publication;
 
 class ThemesServiceSpec extends ObjectBehavior
 {
-    function it_is_initializable()
+    public function it_is_initializable()
     {
         $this->shouldHaveType('Newscoop\Services\ThemesService');
+        $this->shouldImplement('Newscoop\ThemesServiceInterface');
+    }
+
+    public function let(
+        IssueServiceInterface $issueService,
+        Issue $issue,
+        CacheService $cacheService,
+        PublicationService $publicationService,
+        Publication $publication)
+    {
+        $issueService->getIssue()->willReturn($issue);
+        $publicationService->getPublication()->willReturn($publication);
+        $this->beConstructedWith($issueService, $cacheService, $publicationService);
+    }
+
+    public function it_gets_theme_path(Issue $issue)
+    {
+        $issue->getId()->willReturn(1);
+        $issue->getNumber()->willReturn(10);
+        $issue->getName()->willReturn("May 2014");
+        $issue->getShortName()->willReturn("may2014");
+        $issue->getLanguageId()->willReturn(1);
+        $this->getThemePath()->shouldBeString();
     }
 }
