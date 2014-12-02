13fb835c35eecb9dffc11c1a910584c3db38be58
Rafał Muszyński <rmuszynski1@gmail.com>
2014-12-02 12:24:18 +0100

    added upgrade and install scripts for tables related to topics

--- newscoop/install/Resources/sql/campsite_demo_tables.sql
+++ newscoop/install/Resources/sql/campsite_demo_tables.sql
@@ -1380,37 +1380,43 @@ CREATE TABLE `TopicFields` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `TopicNames`
+-- Table structure for table `main_topics`
 --
 
-DROP TABLE IF EXISTS `TopicNames`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `TopicNames` (
-  `fk_topic_id` int(10) unsigned NOT NULL,
-  `fk_language_id` int(10) unsigned NOT NULL,
-  `name` varchar(255) NOT NULL DEFAULT '',
-  PRIMARY KEY (`fk_topic_id`,`fk_language_id`),
-  UNIQUE KEY `fk_language_id` (`fk_language_id`,`name`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `Topics`
---
-
-DROP TABLE IF EXISTS `Topics`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Topics` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `node_left` int(10) unsigned NOT NULL,
-  `node_right` int(10) unsigned NOT NULL,
+CREATE TABLE IF NOT EXISTS `main_topics` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `parent_id` int(11) DEFAULT NULL,
+  `title` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `lft` int(11) NOT NULL,
+  `rgt` int(11) NOT NULL,
+  `root` int(11) DEFAULT NULL,
+  `lvl` int(11) NOT NULL,
+  `description` longtext COLLATE utf8_unicode_ci,
+  `created` datetime NOT NULL,
+  `updated` datetime NOT NULL,
+  `topicOrder` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `node_left` (`node_left`),
-  KEY `node_right` (`node_right`)
-) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  KEY `IDX_EEAFE2F727ACA70` (`parent_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+ALTER TABLE `main_topics` ADD CONSTRAINT `FK_EEAFE2F727ACA70` FOREIGN KEY (`parent_id`) REFERENCES `main_topics` (`id`) ON DELETE SET NULL;
+
+--
+-- Table structure for table `topic_translations`
+--
+
+CREATE TABLE IF NOT EXISTS `topic_translations` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `object_id` int(11) DEFAULT NULL,
+  `locale` varchar(8) COLLATE utf8_unicode_ci NOT NULL,
+  `field` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  `content` longtext COLLATE utf8_unicode_ci,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `lookup_unique_idx` (`locale`,`object_id`,`field`),
+  KEY `IDX_98B25D13232D562B` (`object_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+ALTER TABLE `topic_translations` ADD CONSTRAINT `FK_98B25D13232D562B` FOREIGN KEY (`object_id`) REFERENCES `main_topics` (`id`) ON DELETE CASCADE;
 
 --
 -- Table structure for table `Translations`
