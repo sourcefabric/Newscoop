11866ec69bc628692209686a2c5f157f769879c2
Rafał Muszyński <rmuszynski1@gmail.com>
2014-12-04 12:42:05 +0100

    fix topic entity namespace

--- newscoop/library/Newscoop/Entity/Article.php
+++ newscoop/library/Newscoop/Entity/Article.php
@@ -15,7 +15,6 @@ use Doctrine\ORM\EntityNotFoundException;
 use ArticleData;
 use Newscoop\View\ArticleView;
 use Newscoop\Search\DocumentInterface;
-use Newscoop\NewscoopBundle\Entity\Topic;
 
 /**
  * Article entity
@@ -1011,7 +1010,7 @@ class Article implements DocumentInterface
      *
      * @return boolean
      */
-    public function addTopic(Topic $topic)
+    public function addTopic(Newscoop\NewscoopBundle\Entity\Topic $topic)
     {
 
         if (!$this->topics->contains($topic)) {
@@ -1031,7 +1030,7 @@ class Article implements DocumentInterface
      *
      * @return boolean
      */
-    public function removeTopic(Topic $topic)
+    public function removeTopic(Newscoop\NewscoopBundle\Entity\Topic $topic)
     {
         if ($this->topics->contains($topic)) {
             $this->topics->removeElement($topic);
