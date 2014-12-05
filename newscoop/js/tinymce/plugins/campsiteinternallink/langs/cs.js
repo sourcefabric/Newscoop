065e5cffeb05ebd9e854209f8874ea364065c4b6
Daniel James <daniel@64studio.com>
2014-12-05 13:02:06 +0000

    CS-5587 Fix format of legacy translation files

--- newscoop/js/tinymce/plugins/campsiteinternallink/langs/cs.js
+++ newscoop/js/tinymce/plugins/campsiteinternallink/langs/cs.js
@@ -1,4 +1,4 @@
 tinyMCE.addI18n('cs.campsiteinternallink',{
     editor_button: "Insert/edit internal link",
-    select_text:"You need to select some text before creating a link"
+    select_text: "You need to select some text before creating a link"
 });
