44264b7735ce110dbc15f53078f648d107b730c8
Pawel Mikolajczuk <mikolajczuk.private@gmail.com>
2015-02-26 14:27:13 +0100

    fix searching articles with non default language, fix setuping defoult oauth client

--- newscoop/library/Newscoop/Entity/Repository/ArticleRepository.php
+++ newscoop/library/Newscoop/Entity/Repository/ArticleRepository.php
@@ -158,25 +158,24 @@ class ArticleRepository extends DatatableSource implements RepositoryInterface
     {
         $em = $this->getEntityManager();
 
-        $languageId = $em->getRepository('Newscoop\Entity\Language')
-            ->findOneByCode($articleSearchCriteria->language);
-        if (!$languageId) {
-            throw new NotFoundHttpException('Results with language "'.$language.'" was not found.');
-        }
-
         $queryBuilder = $em->getRepository('Newscoop\Entity\Article')
             ->createQueryBuilder('a')
             ->select('a, FIELD(a.number, :ids) as HIDDEN field')
             ->andWhere('a.number IN (:ids)')
-            ->andWhere('a.language = :language')
             ->leftJoin('a.publication', 'p')
             ->leftJoin('a.issue', 'i')
             ->leftJoin('a.section', 's')
             ->orderBy('field')
             ->setParameters(array(
-                'ids' => $ids,
-                'language' => $languageId
+                'ids' => $ids
             ));
+
+        $languageId = $em->getRepository('Newscoop\Entity\Language')
+            ->findOneByCode($articleSearchCriteria->language);
+        if ($languageId) {
+            $queryBuilder->andWhere('a.language = :language')
+                ->setParameter('language', $languageId);
+        }
 
         if ($articleSearchCriteria->article_type) {
             $queryBuilder->andWhere('a.type = :article_type')
