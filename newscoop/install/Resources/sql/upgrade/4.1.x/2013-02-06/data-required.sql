ee7577851b32f1dc9a6fa79028c6d224b4e9cbb9
Pawel Mikolajczuk <mikolajczuk.private@gmail.com>
2015-03-20 09:42:21 +0100

    [CS-5729] add upgrade script for initializing playlists articles order, remove some not used files

--- newscoop/install/Resources/sql/upgrade/4.1.x/2013-02-06/data-required.sql
+++
@@ -1,5 +0,0 @@
-
--- allowing the geo-locations search
-INSERT INTO SystemPreferences (varname, value) VALUES ('GeoSearchLocalGeonames','1');
-INSERT INTO SystemPreferences (varname, value) VALUES ('GeoSearchMapquestNominatim','1');
-
