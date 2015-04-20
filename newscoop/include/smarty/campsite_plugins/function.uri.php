b8c32cf09639e841b6c342de48db2717c2c576ed
Pawel Mikolajczuk <mikolajczuk.private@gmail.com>
2015-04-20 11:01:51 +0200

    check if smarty_function_uri before initializing
    I already had error with redeclaring that function many times. Can’t
    find why so it’s just hotfix.

--- newscoop/include/smarty/campsite_plugins/function.uri.php
+++ newscoop/include/smarty/campsite_plugins/function.uri.php
@@ -16,43 +16,44 @@
  * @return string $uriString
  *      The requested URI
  */
-function smarty_function_uri($p_params, &$p_smarty)
+if (!function_exists('smarty_function_uri'))
 {
-    $context = $p_smarty->getTemplateVars('gimme');
+    function smarty_function_uri($p_params, &$p_smarty)
+    {
+        $context = $p_smarty->getTemplateVars('gimme');
 
-    if (!isset($p_params['options'])) {
-        $p_params['options'] = '';
-    }
+        if (!isset($p_params['options'])) {
+            $p_params['options'] = '';
+        }
 
-    $url = 'url';
-    if (isset($p_params['static_file']) && !empty($p_params['static_file'])) {
-    	$p_params['options'] = 'static_file ' . $p_params['static_file'];
-    } else {
-    	$params = preg_split("/[\s]+/", $p_params['options']);
-    	foreach ($params as $index=>$param) {
-    		if (strcasecmp('fromstart', $param) == 0) {
-    			$url = 'default_url';
-    			unset($params[$index]);
-    			$p_params['options'] = implode(', ', $params);
-    			break;
-    		}
-    	}
-    }
+        $url = 'url';
+        if (isset($p_params['static_file']) && !empty($p_params['static_file'])) {
+        	$p_params['options'] = 'static_file ' . $p_params['static_file'];
+        } else {
+        	$params = preg_split("/[\s]+/", $p_params['options']);
+        	foreach ($params as $index=>$param) {
+        		if (strcasecmp('fromstart', $param) == 0) {
+        			$url = 'default_url';
+        			unset($params[$index]);
+        			$p_params['options'] = implode(', ', $params);
+        			break;
+        		}
+        	}
+        }
 
-    if ($p_params['options'] === 'author') {
-        $view = $p_smarty->getTemplateVars('view');
-        return $view->url(array('author' => $context->author->name), 'author');
-    }
+        if ($p_params['options'] === 'author') {
+            $view = $p_smarty->getTemplateVars('view');
+            return $view->url(array('author' => $context->author->name), 'author');
+        }
 
-    if ($p_params['options'] === 'topic') {
-        $view = $p_smarty->getTemplateVars('view');
-        return $view->url(array('id' => $context->topic->identifier, 'language' => $context->language->code, 'topicName' => $context->topic->name), 'topic');
-    }
+        if ($p_params['options'] === 'topic') {
+            $view = $p_smarty->getTemplateVars('view');
+            return $view->url(array('id' => $context->topic->identifier, 'language' => $context->language->code, 'topicName' => $context->topic->name), 'topic');
+        }
 
-    // sets the URL parameter option
-    $context->$url->uri_parameter = $p_params['options'];
+        // sets the URL parameter option
+        $context->$url->uri_parameter = $p_params['options'];
 
-    return $context->$url->uri;
-} // fn smarty_function_uri
-
-?>
+        return $context->$url->uri;
+    } // fn smarty_function_uri
+}
