edfa7cd7a2a3111caa400120fd18fbe581da01b4
Mark Lewis <thnkloud9@gmail.com>
2015-06-16 17:10:33 +0200

    fix snippets sql for both upgrades and new installs

--- newscoop/install/Resources/sql/upgrade/4.4.x/2015.06.16/tables.sql
+++ newscoop/install/Resources/sql/upgrade/4.4.x/2015.06.16/tables.sql
@@ -1,20 +1,23 @@
-DELETE FROM `SnippetTemplateField` WHERE TemplateId = 1;
-INSERT INTO `SnippetTemplateField` (`Id`, `Name`, `Type`, `Scope`, `Required`, `TemplateId`)
-VALUES
-    (1, 'URL', 'url', 'frontend', 1, 1),
-    (2, 'Endpoint', 'text', 'frontend', 0, 1),
-    (3, 'maxwidth', 'integer', 'frontend', 0, 1),
-    (4, 'provider_url', 'url', 'backend', 0, 1),
-    (5, 'description', 'textarea', 'backend', 0, 1),
-    (6, 'title', 'text', 'backend', 0, 1),
-    (7, 'type', 'text', 'backend', 0, 1),
-    (8, 'thumbnail_width', 'integer', 'backend', 0, 1),
-    (9, 'height', 'integer', 'backend', 0, 1),
-    (10, 'width', 'integer', 'backend', 0, 1),
-    (11, 'html', 'textarea', 'backend', 0, 1),
-    (12, 'author_name', 'text', 'backend', 0, 1),
-    (13, 'version', 'text', 'backend', 0, 1),
-    (14, 'provider_name', 'text', 'backend', 0, 1),
-    (15, 'thumbnail_url', 'url', 'backend', 0, 1),
-    (16, 'thumbnail_height', 'integer', 'backend', 0, 1),
-    (17, 'author_url', 'url', 'backend', 0, 1);
+SET FOREIGN_KEY_CHECKS=0;
+
+DROP TABLE IF EXISTS `ArticleSnippets`;
+CREATE TABLE `ArticleSnippets` (`ArticleNr` int(11) NOT NULL, `SnippetId` int(11) NOT NULL, KEY `FK_5080CDEB00DA91C` (`SnippetId`), CONSTRAINT `FK_5080CDEB00DA91C` FOREIGN KEY (`SnippetId`) REFERENCES `Snippets` (`Id`)) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+DROP TABLE IF EXISTS `Snippets`;
+CREATE TABLE `Snippets` (Id INT AUTO_INCREMENT NOT NULL, Name VARCHAR(255) NOT NULL, Enabled TINYINT(1) NOT NULL, Created DATETIME NOT NULL, Modified DATETIME NOT NULL, TemplateId INT DEFAULT NULL, INDEX IDX_1457978AF846113F (TemplateId), PRIMARY KEY(Id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB;
+
+DROP TABLE IF EXISTS `SnippetTemplates`;
+CREATE TABLE `SnippetTemplates` (Id INT AUTO_INCREMENT NOT NULL, Name VARCHAR(255) NOT NULL, Controller VARCHAR(255) DEFAULT NULL, TemplateCode LONGTEXT NOT NULL, Favourite TINYINT(1) DEFAULT NULL, Enabled TINYINT(1) NOT NULL, IconInactive LONGTEXT DEFAULT NULL, IconActive LONGTEXT DEFAULT NULL, Created DATETIME NOT NULL, Modified DATETIME NOT NULL, PRIMARY KEY(Id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB;
+
+DROP TABLE IF EXISTS `SnippetTemplateField`;
+CREATE TABLE `SnippetTemplateField` (Id INT AUTO_INCREMENT NOT NULL, Name VARCHAR(255) NOT NULL, Type VARCHAR(255) NOT NULL, Scope VARCHAR(255) NOT NULL, Required TINYINT(1) NOT NULL, TemplateId INT NOT NULL, INDEX IDX_2060662F846113F (TemplateId), PRIMARY KEY(Id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB;
+
+DROP TABLE IF EXISTS `SnippetFields`;
+CREATE TABLE `SnippetFields` (Id INT AUTO_INCREMENT NOT NULL, Data LONGTEXT DEFAULT NULL, Name VARCHAR(255) NOT NULL, SnippetId INT NOT NULL, TemplateFieldId INT NOT NULL, INDEX IDX_1F835121B00DA91C (SnippetId), INDEX IDX_1F835121EBCA9337 (TemplateFieldId), PRIMARY KEY(Id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB;
+
+ALTER TABLE SnippetTemplateField ADD CONSTRAINT FK_2060662F846113F FOREIGN KEY (TemplateId) REFERENCES SnippetTemplates (Id);
+ALTER TABLE SnippetFields ADD CONSTRAINT FK_1F835121B00DA91C FOREIGN KEY (SnippetId) REFERENCES Snippets (Id);
+ALTER TABLE SnippetFields ADD CONSTRAINT FK_1F835121EBCA9337 FOREIGN KEY (TemplateFieldId) REFERENCES SnippetTemplateField (Id);
+ALTER TABLE Snippets ADD CONSTRAINT FK_1457978AF846113F FOREIGN KEY (TemplateId) REFERENCES SnippetTemplates (Id);
+
+SET FOREIGN_KEY_CHECKS=1;
