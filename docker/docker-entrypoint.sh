ddd3d7ca6282f9d9b78edcc8adf553b14f9616a0
Mischa Gorinskat <m.gorinskat@gmail.com>
2015-06-22 16:00:00 +0200

    Adds prod/dev env switch and runs cron server
    Running prod:
     - docker-compose build
     - docker-compose up
    
    Running dev:
     - docker-compose -f docker-compose-dev.yml build
     - docker-compose -f docker-compose-dev.yml up

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -3,29 +3,44 @@
 if [ "$1" = 'newscoop' ]; then
     # install composer and install PHP dependencies
     if [ ! -f /var/www/newscoop/composer.phar ]; then
-        cd /var/www/newscoop && curl -s https://getcomposer.org/installer | php
+        cd /var/www/newscoop && curl -sS https://getcomposer.org/installer | php
     fi
-    cd /var/www/newscoop && /usr/bin/php composer.phar install --no-dev
-    chown -R www-data:www-data /var/www/newscoop
+    if [ "$APPLICATION_ENVIRONMENT" = "dev" ]; then
+        # Disable production, enable dev
+        a2dissite newscoop
+        a2ensite newscoop-dev
+        cd /var/www/newscoop && /usr/bin/php composer.phar install
+    else
+        cd /var/www/newscoop && /usr/bin/php composer.phar install --no-dev
+    fi
 
     # check if we should install
     if [ -f /var/www/newscoop/conf/installation.php ]; then
-      /usr/share/newscoop/import-newscoop.sh
+        /usr/share/newscoop/import-newscoop.sh
     fi
 
     # catch signlas
     trap "echo 'caught signal'" HUP INT QUIT KILL TERM
 
+    start cron
+
     #/usr/sbin/apachectl -D FOREGROUND
     /usr/sbin/apachectl start
 
-    tail -F /var/www/newscoop/log/prod.log
+    if [ "$APPLICATION_ENVIRONMENT" = "dev" ]; then
+        tail -F /var/www/newscoop/log/dev.log
+    else
+        tail -F /var/www/newscoop/log/prod.log
+    fi
 
     echo "[hit enter key to exit] or run docker stop <container>'"
     read
 
     echo "stopping apache"
     /usr/sbin/apachectl stop
+
+    echo "stopping cron"
+    stop cron
 
     echo "exited $0"
 fi
