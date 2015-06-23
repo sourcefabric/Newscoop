4560f5a0adb6ffb88c9c20b96f772e9a66d7aa19
Rafał Muszyński <rmuszynski1@gmail.com>
2015-06-23 10:31:23 +0200

    CS-5802 - Featured Article List limits do not work as expected

--- newscoop/src/Newscoop/NewscoopBundle/Resources/public/js/playlists/controllers/playlists.js
+++ newscoop/src/Newscoop/NewscoopBundle/Resources/public/js/playlists/controllers/playlists.js
@@ -406,7 +406,7 @@ angular.module('playlistsApp').controller('PlaylistsController', [
 
     /**
      * It makes a proper API calls (create, update, link, unlink) based on
-     * performed actions. If the list's limit will be change, popup will be displayed.
+     * performed actions. If the list's limit will be changed, popup will be displayed.
      */
     $scope.savePlaylist = function () {
         var newLimit = $scope.playlist.selected.maxItems,
@@ -435,13 +435,14 @@ angular.module('playlistsApp').controller('PlaylistsController', [
     };
 
     var showLimitPopupAndSave = function (newLimit, oldLimit, modal, okText, cancelText) {
-        if (newLimit && newLimit != 0 && newLimit != oldLimit) {
+        if (newLimit && newLimit != 0 && newLimit < oldLimit ||
+            ((oldLimit === null || oldLimit === 0) && newLimit > oldLimit)
+        ) {
             var title = Translator.trans('Info', {}, 'articles');
             var text = Translator.trans('articles.playlists.alert', {}, 'articles');
             modal = modalFactory.confirmLight(title, text, okText, cancelText);
             modal.result.then(function () {
                 saveList();
-                $scope.playlist.selected.oldLimit = $scope.playlist.selected.maxItems;
             }, function () {
                 return false;
             });
@@ -546,6 +547,7 @@ angular.module('playlistsApp').controller('PlaylistsController', [
         });
 
         $scope.playlist.selected.id = Playlist.getListId();
+        $scope.playlist.selected.oldLimit = $scope.playlist.selected.maxItems;
 
         if (response && response[0] !== undefined && response[0].object.articlesModificationTime !== undefined) {
             $scope.playlist.selected.articlesModificationTime = response[0].object.articlesModificationTime;
