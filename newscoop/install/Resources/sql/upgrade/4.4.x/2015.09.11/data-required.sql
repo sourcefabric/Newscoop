d5fa0f5bbfa57d672e9c2f1ea89abbb705f451ee
Mischa Gorinskat <m.gorinskat@gmail.com>
2015-09-11 14:55:37 +0200

    Fixes languages codes missing for Azerbaijani, Hungarian and Danish

---
+++ newscoop/install/Resources/sql/upgrade/4.4.x/2015.09.11/data-required.sql
@@ -0,0 +1,3 @@
+UPDATE `Languages` SET `RFC3066bis` = "az-AZ" WHERE `Code` = "az";
+UPDATE `Languages` SET `RFC3066bis` = "da-DK" WHERE `Code` = "da";
+UPDATE `Languages` SET `RFC3066bis` = "hu-HU" WHERE `Code` = "hu";
